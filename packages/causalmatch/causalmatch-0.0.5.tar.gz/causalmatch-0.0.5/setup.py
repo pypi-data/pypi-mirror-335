887e59c3e3df99154a7f475e78bf95f00e3961fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:51 +0000

    Add package causalmatch/0.0.5/causalmatch-0.0.5.tar.gz

---
+++ packages/causalmatch/causalmatch-0.0.5.tar.gz/causalmatch-0.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+dependencies = [
+    'pandas>=1.3.4, <=1.3.5',
+    'numpy>=1.3.0, <=1.26.4'
+]
+
+setup(
+    name="causalmatch",
+    version='0.0.5',
+    author="Xiaoyu Zhou",
+    author_email="xiaoyuzhou@bytedance.com",
+    url='https://github.com/bytedance/CausalMatch',
+    description="Propensity score matching and coarsened exact matching",
+    packages=find_packages(),
+    install_requires=dependencies,
+    python_requires='>=3.6'
+)
