9591cb0318712c03c520ff380b900eab81bd2358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:05:04 +0000

    Add package lsdb/0.5.0/lsdb-0.5.0-py3-none-any.whl

---
+++ packages/lsdb/lsdb-0.5.0-py3-none-any.whl/lsdb/core/search/pixel_search.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import nested_pandas as npd
+from hats.pixel_math import HealpixPixel
+
+from lsdb.core.search.abstract_search import AbstractSearch
+
+if TYPE_CHECKING:
+    from lsdb.types import HCCatalogTypeVar
+
+
+class PixelSearch(AbstractSearch):
+    """Filter the catalog by HEALPix pixels.
+
+    Filters partitions in the catalog to those that are in a specified pixel set.
+    Does not filter points inside those partitions.
+    """
+
+    def __init__(self, pixels: list[tuple[int, int]]):
+        super().__init__(fine=False)
+        self.pixels = [HealpixPixel(o, p) for o, p in set(pixels)]
+
+    def filter_hc_catalog(self, hc_structure: HCCatalogTypeVar) -> HCCatalogTypeVar:
+        return hc_structure.filter_from_pixel_list(self.pixels)
+
+    def search_points(self, frame: npd.NestedFrame, _) -> npd.NestedFrame:
+        return frame
