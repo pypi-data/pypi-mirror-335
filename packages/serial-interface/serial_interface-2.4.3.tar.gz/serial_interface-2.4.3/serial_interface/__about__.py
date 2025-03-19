65766deebeb269312394b5855e4c6e385648900f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:05:04 +0000

    Add package serial-interface/2.4.3/serial_interface-2.4.3.tar.gz

---
+++ packages/serial-interface/serial_interface-2.4.3.tar.gz/serial_interface-2.4.3/serial_interface/__about__.py
@@ -0,0 +1,11 @@
+"""Extends pyserial to make serial device interfaces."""
+# This file is generated automatically from metadata
+# File edits may be overwritten!
+
+__version__ = '2.4.3'
+__description__ = 'Extends pyserial to make serial device interfaces.'
+__license__ = 'BSD-3-Clause'
+__url__ = 'https://github.com/janelia-python/serial_interface_python'
+__author__ = 'Peter Polidoro'
+__email__ = 'peter@polidoro.io'
+__copyright__ = '2025 Howard Hughes Medical Institute'
---
+++ packages/serial-interface/serial_interface-2.4.3.tar.gz/serial_interface-2.4.3/setup.py
@@ -0,0 +1,8 @@
+"""Extends pyserial to make serial device interfaces."""
+# This file is generated automatically from metadata
+# File edits may be overwritten!
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup()
