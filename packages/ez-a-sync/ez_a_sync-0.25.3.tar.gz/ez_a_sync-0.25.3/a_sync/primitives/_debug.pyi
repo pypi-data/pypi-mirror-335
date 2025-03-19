92163c7a7f400d7b41855049272544248d7a384e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:11 +0000

    Add package ez-a-sync/0.25.3/ez_a_sync-0.25.3.tar.gz

---
+++ packages/ez-a-sync/ez_a_sync-0.25.3.tar.gz/ez_a_sync-0.25.3/a_sync/primitives/_debug.pyi
@@ -0,0 +1,18 @@
+"""
+This module provides a mixin class used to facilitate the creation of debugging daemons in subclasses.
+
+The mixin provides a framework for managing a debug daemon task, which can be used to emit rich debug logs from subclass instances whenever debug logging is enabled. Subclasses must implement the specific logging behavior.
+"""
+
+import abc
+from a_sync.primitives._loggable import _LoggerMixin
+
+class _DebugDaemonMixin(_LoggerMixin):
+    """
+    A mixin class that provides a framework for debugging capabilities using a daemon task.
+
+    This mixin sets up the structure for managing a debug daemon task. Subclasses are responsible for implementing the specific behavior of the daemon, including any logging functionality.
+
+    See Also:
+        :class:`_LoggerMixin` for logging capabilities.
+    """
