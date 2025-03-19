eda252ea7a9df5c779e76cd6a7f73f4f27714612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:25 +0000

    Add package tensorzero/2025.3.2/tensorzero-2025.3.2.tar.gz

---
+++ packages/tensorzero/tensorzero-2025.3.2.tar.gz/tensorzero-2025.3.2/tensorzero-internal/tests/e2e/tests.rs
@@ -0,0 +1,19 @@
+#![cfg(test)]
+mod best_of_n;
+mod cache;
+mod clickhouse;
+mod common;
+mod datasets;
+mod dicl;
+mod fallback;
+mod feedback;
+mod health;
+mod inference;
+mod mixture_of_n;
+mod object_storage;
+mod openai_compatible;
+mod prometheus;
+mod providers;
+mod proxy;
+mod retries;
+mod streaming_errors;
