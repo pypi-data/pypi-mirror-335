fe38977d30ef2c5fc70e8b118c920753bcb3f6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:14 +0000

    Add package MaixPy/4.10.3/maixpy-4.10.3-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/MaixPy/maixpy-4.10.3-cp38-cp38-manylinux2014_x86_64.whl/maix/_maix/util.pyi
@@ -0,0 +1,13 @@
+"""
+maix.util module
+"""
+from __future__ import annotations
+__all__ = ['do_exit_function', 'register_atexit']
+def do_exit_function() -> None:
+    """
+    exec all of exit function
+    """
+def register_atexit() -> None:
+    """
+    Registering default processes that need to be executed on exit
+    """
