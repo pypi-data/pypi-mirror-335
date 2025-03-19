1ac6ae4cf62397b0d11c936fd0a6975fb6d83d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:06 +0000

    Add package transformers-model/0.1.8/transformers-model-0.1.8.tar.gz

---
+++ packages/transformers-model/transformers-model-0.1.8.tar.gz/transformers-model-0.1.8/transformers_model/dataset.py
@@ -0,0 +1,46 @@
+import torch
+import numpy as np
+from typing import Tuple, Mapping, List, Union
+from torch.utils.data import Dataset
+
+class BertDataset(Dataset):
+	""" 配合BertCollator使用 """
+
+	def __init__(self, tokenizies: Mapping[str, torch.Tensor], labels: np.ndarray):
+		super().__init__()
+		self.tokenizies = tokenizies
+		self.labels = labels
+
+	def __getitem__(self, index: int) -> Tuple[Mapping[str, torch.Tensor], torch.Tensor]:
+		return {k: v[index] for k, v in self.tokenizies.items()}, self.labels[index]
+
+	def __len__(self):
+		return len(self.labels)
+	
+
+class HuggingFaceDataset(Dataset):
+	""" 配合HuggingFace的Dataset使用 """
+
+	def __init__(self, dataset, feature_names: Union[str, List[str]], label_name: str):
+		"""
+		return_type: auto, tuple, dict
+		"""
+		super().__init__()
+		self.dataset = dataset
+		self.feature_names = feature_names
+		self.label_name = label_name
+
+	def __getitem__(self, index: int):
+		data = self.dataset[index]
+		if isinstance(self.feature_names, str):
+			features = data[self.feature_names]
+		elif len(self.feature_names) == 1:
+			features = data[self.feature_names[0]]
+		else:
+			features = [data[k] for k in self.feature_names]
+		
+		return features, data[self.label_name]
+		
+	def __len__(self):
+		return len(self.dataset)
+		
