b5948e4615e07a3883549b4eb4d669aa37d291d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:31 +0000

    Add package odoo14-addon-l10n-it-delivery-note-order-link/14.0.1.1.1/odoo14_addon_l10n_it_delivery_note_order_link-14.0.1.1.1-py3-none-any.whl

---
+++ packages/odoo14-addon-l10n-it-delivery-note-order-link/odoo14_addon_l10n_it_delivery_note_order_link-14.0.1.1.1-py3-none-any.whl/odoo/addons/l10n_it_delivery_note_order_link/models/sale_order.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2019, Openindustry.it Sas
+# @author: Andrea Piovesana <andrea.m.piovesana@gmail.com>
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    delivery_note_ids = fields.Many2many(
+        "stock.delivery.note", compute="_compute_delivery_notes"
+    )
+    delivery_note_count = fields.Integer(compute="_compute_delivery_notes")
+
+    def _compute_delivery_notes(self):
+        for order in self:
+            delivery_notes = order.picking_ids.mapped("delivery_note_id")
+
+            order.delivery_note_ids = delivery_notes
+            order.delivery_note_count = len(delivery_notes)
+
+    def goto_delivery_notes(self, **kwargs):
+        delivery_notes = self.mapped("delivery_note_ids")
+
+        action = self.env["ir.actions.act_window"]._for_xml_id(
+            "l10n_it_delivery_note.stock_delivery_note_action"
+        )
+        action.update(kwargs)
+        if len(delivery_notes) > 1:
+            action["domain"] = [("id", "in", delivery_notes.ids)]
+        elif len(delivery_notes) == 1:
+            action["views"] = [
+                (
+                    self.env.ref(
+                        "l10n_it_delivery_note.stock_delivery_note_form_view"
+                    ).id,
+                    "form",
+                )
+            ]
+            action["res_id"] = delivery_notes.id
+
+        return action
