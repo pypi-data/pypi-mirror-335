d478e6e3c2e124a60ea3b1a93f4905d0149b4be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:05:03 +0000

    Add package tianmoucv/0.3.4.0/tianmoucv-0.3.4.0-py3-none-any.whl

---
+++ packages/tianmoucv/tianmoucv-0.3.4.0-py3-none-any.whl/tianmoucv/__init__.py
@@ -0,0 +1,17 @@
+# package
+# __init__.py
+import random
+import cv2
+import math
+import torch
+import os
+import sys
+
+# __init__.py
+__all__ = ['random', 'cv2', 'math', 'torch','os','sys']
+__author__ = 'Y. Lin'
+__contributor__ = 'T. Wang, Y. Chen, Y. Li'
+__authorEmail__ = '532109881@qq.com'
+
+print('TianMouCV™ 0.3.4.0, via',__author__,' update install logic and nature reconstructor')
+
