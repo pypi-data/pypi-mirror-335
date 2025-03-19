01c3f2d5aae05e8a18ff09edadf36a0dd911d60e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:22 +0000

    Add package aibi/0.0.1/aibi-0.0.1.tar.gz

---
+++ packages/aibi/aibi-0.0.1.tar.gz/aibi-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aibi',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Livid',
+    author_email='livid.su@gmail.com',
+    description='',
+    url='https://github.com/ilivid/aibi'
+)
