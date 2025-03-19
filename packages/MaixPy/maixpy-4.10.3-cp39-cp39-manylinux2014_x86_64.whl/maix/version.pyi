3c3b33b9cc4b455ef43619d1ae38c938cfd91383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:10 +0000

    Add package MaixPy/4.10.3/maixpy-4.10.3-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/MaixPy/maixpy-4.10.3-cp39-cp39-manylinux2014_x86_64.whl/maix/version.pyi
@@ -0,0 +1,6 @@
+from __future__ import annotations
+__all__ = ['version_major', 'version_minor', 'version_patch']
+__version__: str = '4.10.3'
+version_major: int = 4
+version_minor: int = 10
+version_patch: int = 3
