d8c6e65db5f5257dfd49f0a49056f8a41b78f202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:36 +0000

    Add package lexer-tools/0.1.0/lexer-tools-0.1.0.tar.gz

---
+++ packages/lexer-tools/lexer-tools-0.1.0.tar.gz/lexer-tools-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lexer-tools",  # Package name
+    version="0.1.0",  # Version number
+    description="A lexer-tools is a python package that help student on NLP",
+    author="Sam Anderson",
+    author_email="samanderson69@gmail.com",
+    packages=find_packages(),  # Automatically find packages
+    include_package_data=True,  # Include non-Python files
+    package_data={
+        "lexer_tools": [
+            "data/exp1/*",
+            "data/exp2/*",
+            "data/exp3/*",
+            "data/exp4/*",
+            "data/exp5/*",
+        ],  # Include all files in the data folders
+    },
+    install_requires=[],  # Add dependencies if needed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Python version requirement
+)
