0b9dffbae37b01ee701bfb9401ad45dc7656703c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:31 +0000

    Add package elan/0.3.8/elan-0.3.8-py3-none-any.whl

---
+++ packages/elan/elan-0.3.8-py3-none-any.whl/elan/list_utils.py
@@ -0,0 +1,22 @@
+class list_utils:
+    def __init__(self):
+        pass
+
+    def reverse(self, input_list):
+        return input_list[::-1]   
+    
+    def sort(self, input_list):
+        return sorted(input_list)
+    
+    def reverse(self, input_list):
+        return input_list[::-1]   
+   
+    def unique(self, input_list):
+        return list(set(input_list))
+    
+    
+    
+if __name__ == "__main__":
+    list_utils()
+    
+
