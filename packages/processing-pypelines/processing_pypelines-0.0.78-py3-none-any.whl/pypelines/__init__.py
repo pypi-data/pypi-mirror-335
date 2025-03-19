7c54a25c0391a2b4814828e36c1792de684d46cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:05:21 +0000

    Add package processing-pypelines/0.0.78/processing_pypelines-0.0.78-py3-none-any.whl

---
+++ packages/processing-pypelines/processing_pypelines-0.0.78-py3-none-any.whl/pypelines/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.0.78"
+
+from . import loggs
+from .pipes import *
+from .pipelines import *
+from .steps import *
+from .disk import *
+from .sessions import *
+
+# NOTE:
+# pypelines is enabling the logging system by default when importing it
+# (it comprises colored logging, session prefix-logging, and logging to a file located in downloads folder)
+loggs.enable_logging()
