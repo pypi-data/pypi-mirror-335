ed2ac0a15ca72f129435a4f3c64eba269e11f189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:41 +0000

    Add package sakshi-ocr/0.2.0/sakshi_ocr-0.2.0.tar.gz

---
+++ packages/sakshi-ocr/sakshi_ocr-0.2.0.tar.gz/sakshi_ocr-0.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='sakshi_ocr',        # Package name as it will appear on PyPI
+    version='0.2.0',           # Version number
+    py_modules=['sakshi_ocr'], # List of module files
+    author='sameer banchhor',
+    author_email='slimshadow.work@gmail.com',
+    description='A UNKNOWN sakshi_ocr',
+    url='https://github.com/NONE/sakshi_ocr_package',  # URL to your project (GitHub, etc.)
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
