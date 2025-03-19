27916a35311324d63ecfef1ddf86348bc4b2b7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:05:04 +0000

    Add package serial-interface/2.4.3/serial_interface-2.4.3-py3-none-any.whl

---
+++ packages/serial-interface/serial_interface-2.4.3-py3-none-any.whl/serial_interface/__init__.py
@@ -0,0 +1,13 @@
+"""Extends pyserial to make serial device interfaces."""
+# This file is generated automatically from metadata
+# File edits may be overwritten!
+from serial_interface.__about__ import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __description__,
+    __url__,
+    __version__)
+
+from .serial_interface import SerialInterface
