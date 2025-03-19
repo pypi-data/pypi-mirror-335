1f88ebe14519757cc0993babf05de7a0fb30a154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:07:53 +0000

    Add package pydantic-mongo/2.4.0/pydantic_mongo-2.4.0.tar.gz

---
+++ packages/pydantic-mongo/pydantic_mongo-2.4.0.tar.gz/pydantic_mongo-2.4.0/test/test_fields.py
@@ -0,0 +1,31 @@
+import pytest
+from bson import ObjectId
+from pydantic import BaseModel, ValidationError
+
+from pydantic_mongo import ObjectIdField
+
+
+class User(BaseModel):
+    id: ObjectIdField
+
+
+class TestFields:
+    def test_object_id_validation(self):
+        with pytest.raises(ValidationError):
+            User.model_validate({"id": "lala"})
+        User.model_validate({"id": "611827f2878b88b49ebb69fc"})
+
+    def test_object_id_serialize(self):
+        lala = User(id=ObjectId("611827f2878b88b49ebb69fc"))
+        json_result = lala.model_dump_json()
+        assert '{"id":"611827f2878b88b49ebb69fc"}' == json_result
+
+    def test_modify_schema(self):
+        user = User(id=ObjectId("611827f2878b88b49ebb69fc"))
+        schema = user.model_json_schema()
+        assert {
+            "title": "User",
+            "type": "object",
+            "properties": {"id": {"title": "Id", "type": "string"}},
+            "required": ["id"],
+        } == schema
