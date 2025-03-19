7a0a90970ff6378688efbc1d312ad40f0f76adc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:20 +0000

    Add package gmft/0.4.1rc1/gmft-0.4.1rc1-py3-none-any.whl

---
+++ packages/gmft/gmft-0.4.1rc1-py3-none-any.whl/gmft/pdf_bindings/bindings_pdfium.py
@@ -0,0 +1,6 @@
+"""
+Legacy module to import PyPDFium2 bindings. Please use gmft.pdf_bindings.pdfium instead.
+"""
+
+# PyPDFium2 bindings
+from gmft.pdf_bindings.pdfium import PyPDFium2Page, PyPDFium2Document, PyPDFium2Utils
