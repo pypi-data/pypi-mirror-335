b62a2c7c4b0a227231f54399a4b3d1c703408779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:39 +0000

    Add package adaptive-trend-analysis/0.1/adaptive_trend_analysis-0.1.tar.gz

---
+++ packages/adaptive-trend-analysis/adaptive_trend_analysis-0.1.tar.gz/adaptive_trend_analysis-0.1/README.md
@@ -0,0 +1,5 @@
+This library provides adaptive trend analysis for time series data using smoothing techniques.
+
+```sh
+pip install .
+```
---
+++ packages/adaptive-trend-analysis/adaptive_trend_analysis-0.1.tar.gz/adaptive_trend_analysis-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adaptive_trend_analysis',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas'],
+    author='JhersonAguto',
+    author_email='dalisayfernando4@gmail.com',
+    description='A library for Gradient-Adaptive Smoothing (GAS) trend analysis.',
+    url='https://github.com/yourusername/adaptive_trend_analysis',
+    license='MIT'
+)
