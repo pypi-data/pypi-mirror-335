2097010e8fa6e3bddc5be74fafa33d0868b20dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:15 +0000

    Add package pytest-funcnodes/0.1.0/pytest_funcnodes-0.1.0.tar.gz

---
+++ packages/pytest-funcnodes/pytest_funcnodes-0.1.0.tar.gz/pytest_funcnodes-0.1.0/src/pytest_funcnodes/__init__.py
@@ -0,0 +1,4 @@
+from .nodetest_decorator import nodetest
+from .subtests import all_nodes_tested
+
+__all__ = ["nodetest", "all_nodes_tested"]
---
+++ packages/pytest-funcnodes/pytest_funcnodes-0.1.0.tar.gz/pytest_funcnodes-0.1.0/tests/test_testing_setup.py
@@ -0,0 +1,29 @@
+from pytest_funcnodes import nodetest
+from pathlib import Path
+import tempfile
+import logging
+
+
+@nodetest(None)
+async def test_config_dir():
+    import funcnodes_core
+
+    consfig_dir: Path = funcnodes_core.config.get_config_dir()
+    # make sure config dir is a temporary directory
+    assert consfig_dir.is_dir()
+    # test if the config dir is a subdirectory of the temp dir
+    assert consfig_dir.parent == Path(tempfile.gettempdir())
+
+
+@nodetest(None)
+async def test_logger():
+    import funcnodes_core
+
+    logger = funcnodes_core._logging.FUNCNODES_LOGGER
+
+    # make sure no logging.FileHandler is present
+    assert not any(
+        isinstance(handler, logging.FileHandler) for handler in logger.handlers
+    )
+
+    assert logger.level == logging.DEBUG
