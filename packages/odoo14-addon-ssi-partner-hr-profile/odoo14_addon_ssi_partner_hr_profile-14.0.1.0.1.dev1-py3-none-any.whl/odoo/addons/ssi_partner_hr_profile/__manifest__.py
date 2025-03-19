c6782989769224209abfc3e07e5b2522533beb38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:11 +0000

    Add package odoo14-addon-ssi-partner-hr-profile/14.0.1.0.1.dev1/odoo14_addon_ssi_partner_hr_profile-14.0.1.0.1.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-partner-hr-profile/odoo14_addon_ssi_partner_hr_profile-14.0.1.0.1.dev1-py3-none-any.whl/odoo/addons/ssi_partner_hr_profile/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=C8101
+{
+    "name": "Partner App & HR My Profile Extension",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "ssi_partner",
+        "hr",
+    ],
+    "data": [
+        "views/res_users_views.xml",
+    ],
+    "demo": [],
+    "auto_install": True,
+}
