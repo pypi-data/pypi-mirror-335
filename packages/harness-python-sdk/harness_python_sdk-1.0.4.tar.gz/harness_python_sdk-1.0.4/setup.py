023a3618749f364650c5fb08f3b80a9f3df2de6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:25 +0000

    Add package harness-python-sdk/1.0.4/harness_python_sdk-1.0.4.tar.gz

---
+++ packages/harness-python-sdk/harness_python_sdk-1.0.4.tar.gz/harness_python_sdk-1.0.4/setup.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "harness_python_sdk"
+VERSION = "v1.0.4"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Harness NextGen Software Delivery Platform API Reference",
+    author_email="contact@harness.io",
+    url="",
+    keywords=["Swagger", "Harness NextGen Software Delivery Platform API Reference"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
