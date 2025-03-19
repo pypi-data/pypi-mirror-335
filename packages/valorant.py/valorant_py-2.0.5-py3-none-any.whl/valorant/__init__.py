3fe76fc9995d4a13644dde43b118ea82b10a8f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:35 +0000

    Add package valorant.py/2.0.5/valorant_py-2.0.5-py3-none-any.whl

---
+++ packages/valorant.py/valorant_py-2.0.5-py3-none-any.whl/valorant/__init__.py
@@ -0,0 +1,55 @@
+"""
+Valorant API Wrapper.
+
+A basic wrapper for the Valorant API.
+:copyright: (c) 2023-present STACiA
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'valorant'
+__author__ = 'STACiA'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023-present STACiA'
+__version__ = '2.0.5'
+
+from . import models, utils
+from .client import Client
+from .enums import (
+    AbilitySlot,
+    DivisionTier,
+    GameFeature,
+    GameRule,
+    Language,
+    MissionTag,
+    MissionType,
+    RelationType,
+    RewardType,
+    SeasonType,
+    ShopCategory,
+    WeaponCategory,
+)
+from .errors import BadRequest, Forbidden, HTTPException, InternalServerError, RateLimited, ValorantError
+
+__all__ = (
+    'AbilitySlot',
+    'BadRequest',
+    'Client',
+    'DivisionTier',
+    'Forbidden',
+    'GameFeature',
+    'GameRule',
+    'HTTPException',
+    'InternalServerError',
+    'Language',
+    'MissionTag',
+    'MissionType',
+    'RateLimited',
+    'RelationType',
+    'RewardType',
+    'SeasonType',
+    'ShopCategory',
+    'ValorantError',
+    'WeaponCategory',
+    'models',
+    'utils',
+)
