79c6a416b60a1f581580842bf621992f3d72b388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:31 +0000

    Add package eic-aichat-chatting/1.0.0/eic-aichat-chatting-1.0.0.tar.gz

---
+++ packages/eic-aichat-chatting/eic-aichat-chatting-1.0.0.tar.gz/eic-aichat-chatting-1.0.0/aichatchatting/chatprocessor/data/RequestV1.py
@@ -0,0 +1,12 @@
+class RequestV1:
+    """
+    Request data object.
+    """
+
+    def __init__(self, value: str = None):
+        """
+        Initializes a new instance of RequestV1.
+
+        :param value: Request value.
+        """
+        self.value = value
