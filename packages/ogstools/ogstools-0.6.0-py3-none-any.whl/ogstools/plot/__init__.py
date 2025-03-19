a555d3f6d8f6caf09052a2f4479955b9474517ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:28 +0000

    Add package ogstools/0.6.0/ogstools-0.6.0-py3-none-any.whl

---
+++ packages/ogstools/ogstools-0.6.0-py3-none-any.whl/ogstools/plot/__init__.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2012-2024, OpenGeoSys Community (http://www.opengeosys.org)
+#            Distributed under a Modified BSD License.
+#            See accompanying file LICENSE.txt or
+#            http://www.opengeosys.org/project/license
+#
+
+# Author: Florian Zill (Helmholtz Centre for Environmental Research GmbH - UFZ)
+"""Plotting utilities for simple access."""
+
+from .shared import setup  # noqa: I001
+from . import utils
+from .animation import animate
+from .contourplots import contourf, subplot
+from .features import shape_on_top
+from .levels import compute_levels
+from .lineplots import line
+from .vectorplots import quiver, streamlines
+
+__all__ = [
+    "animate",
+    "compute_levels",
+    "contourf",
+    "line",
+    "quiver",
+    "setup",
+    "shape_on_top",
+    "streamlines",
+    "subplot",
+    "utils",
+]
---
+++ packages/ogstools/ogstools-0.6.0-py3-none-any.whl/ogstools/plot/shared.py
@@ -0,0 +1,4 @@
+from .plot_setup import PlotSetup
+from .plot_setup_defaults import setup_dict
+
+setup = PlotSetup.from_dict(setup_dict)
