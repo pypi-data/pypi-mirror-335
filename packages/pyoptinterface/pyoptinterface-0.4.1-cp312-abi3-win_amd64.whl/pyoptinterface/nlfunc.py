cfc366c8aaabfbe9927f8d95309f7ce284d1468e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:16 +0000

    Add package pyoptinterface/0.4.1/pyoptinterface-0.4.1-cp312-abi3-win_amd64.whl

---
+++ packages/pyoptinterface/pyoptinterface-0.4.1-cp312-abi3-win_amd64.whl/pyoptinterface/nlfunc.py
@@ -0,0 +1,17 @@
+from pyoptinterface._src.function_tracing import (
+    Vars,
+    Params,
+    sin,
+    cos,
+    tan,
+    asin,
+    acos,
+    atan,
+    abs,
+    sqrt,
+    exp,
+    log,
+    log10,
+    pow,
+    ifelse,
+)
