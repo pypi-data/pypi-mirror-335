192a9a4071981434f232059e52254789ac1646c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:07:31 +0000

    Add package torch-memory-saver/0.0.3/torch_memory_saver-0.0.3.tar.gz

---
+++ packages/torch-memory-saver/torch_memory_saver-0.0.3.tar.gz/torch_memory_saver-0.0.3/setup.py
@@ -0,0 +1,19 @@
+import logging
+
+import setuptools
+from setuptools import setup
+
+logger = logging.getLogger(__name__)
+
+setup(
+    name='torch_memory_saver',
+    version='0.0.3',
+    ext_modules=[setuptools.Extension(
+        'torch_memory_saver_cpp',
+        ['csrc/torch_memory_saver.cpp'],
+        extra_compile_args=['-I/usr/local/cuda/include'],
+        extra_link_args=['-lcuda'],
+    )],
+    python_requires=">=3.9",
+    packages=['torch_memory_saver'],
+)
