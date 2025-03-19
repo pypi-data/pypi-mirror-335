a9da0878d2757276a204437a6e62ed443a72493e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:23 +0000

    Add package wah/1.13.20/wah-1.13.20.tar.gz

---
+++ packages/wah/wah-1.13.20.tar.gz/wah-1.13.20/setup.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="wah",
+    version="1.13.20",
+    description="a library so simple you will learn Within An Hour",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Juyeop Kim",
+    author_email="juyeopkim@yonsei.ac.kr",
+    url="https://github.com/yupeeee/WAH",
+    license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    entry_points={
+        "console_scripts": [
+            "wah=wah.cli:main",
+        ],
+    },
+    install_requires=[
+        "setuptools>=61.0",
+        "accelerate",
+        "bitsandbytes",
+        "diffusers",
+        "ftfy",
+        "lightning",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "PyYAML",
+        "pillow",
+        "regex",
+        "requests",
+        "sentencepiece",
+        "tensorboard",
+        "timm",
+        "torch",
+        "torchmetrics",
+        "torchvision",
+        "tqdm",
+        "transformers",
+    ],
+)
