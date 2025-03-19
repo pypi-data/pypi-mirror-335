d6ba4fa39cd4e27ce03260bd23fc58da43bdcd2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:28 +0000

    Add package isobiscuit-engine/0.1.0/isobiscuit_engine-0.1.0.tar.gz

---
+++ packages/isobiscuit-engine/isobiscuit_engine-0.1.0.tar.gz/isobiscuit_engine-0.1.0/scripts/build-extension.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+
+import os
+import shutil
+
+from pathlib import Path
+
+from Cython.Build import cythonize
+from setuptools import Distribution
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext
+
+
+COMPILE_ARGS = ["-march=native", "-O3", "-msse", "-msse2", "-mfma", "-mfpmath=sse"]
+LINK_ARGS = []
+INCLUDE_DIRS = []
+LIBRARIES = ["m"]
+
+
+def build() -> None:
+    extensions = [
+        Extension(
+            "*",
+            ["isobiscuit_engine/*.pyx"],
+            extra_compile_args=COMPILE_ARGS,
+            extra_link_args=LINK_ARGS,
+            include_dirs=INCLUDE_DIRS,
+            libraries=LIBRARIES,
+        )
+    ]
+    ext_modules = cythonize(
+        extensions,
+        include_path=INCLUDE_DIRS,
+        compiler_directives={"binding": True, "language_level": 3},
+    )
+
+    distribution = Distribution({
+        "name": "engine",
+        "ext_modules": ext_modules
+    })
+
+    cmd = build_ext(distribution)
+    cmd.ensure_finalized()
+    cmd.run()
+    for output in cmd.get_outputs():
+        output = Path(output)
+        relative_extension = Path("") / output.relative_to(cmd.build_lib)
+
+        shutil.copyfile(output, relative_extension)
+        mode = os.stat(relative_extension).st_mode
+        mode |= (mode & 0o444) >> 2
+        os.chmod(relative_extension, mode)
+
+
+if __name__ == "__main__":
+    build()
