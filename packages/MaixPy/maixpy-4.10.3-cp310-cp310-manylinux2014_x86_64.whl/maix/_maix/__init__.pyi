94a56a3be91ce909b0594271e50541ae4f7304c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:11 +0000

    Add package MaixPy/4.10.3/maixpy-4.10.3-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/MaixPy/maixpy-4.10.3-cp310-cp310-manylinux2014_x86_64.whl/maix/_maix/__init__.pyi
@@ -0,0 +1,32 @@
+"""
+{'breif': 'MaixPy C/C++ API from MaixCDK'}
+"""
+from __future__ import annotations
+from . import app
+from . import audio
+from . import camera
+from . import comm
+from . import display
+from . import err
+from . import example
+from . import ext_dev
+from . import fs
+from . import http
+from . import i18n
+from . import image
+from . import network
+from . import nn
+from . import peripheral
+from . import protocol
+from . import rtmp
+from . import rtsp
+from . import sys
+from . import tensor
+from . import thread
+from . import time
+from . import touchscreen
+from . import tracker
+from . import util
+from . import uvc
+from . import video
+__all__ = ['app', 'audio', 'camera', 'comm', 'display', 'err', 'example', 'ext_dev', 'fs', 'http', 'i18n', 'image', 'network', 'nn', 'peripheral', 'protocol', 'rtmp', 'rtsp', 'sys', 'tensor', 'thread', 'time', 'touchscreen', 'tracker', 'util', 'uvc', 'video']
