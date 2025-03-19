18f8ec05b3643cee5bcca402e599643dde2a4f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:33 +0000

    Add package fast-task-api/1.0.8/fast_task_api-1.0.8-py3-none-any.whl

---
+++ packages/fast-task-api/fast_task_api-1.0.8-py3-none-any.whl/fast_task_api/CONSTS.py
@@ -0,0 +1,16 @@
+from enum import Enum
+class FTAPI_BACKENDS(Enum):
+    RUNPOD = "runpod"
+    FASTAPI = "fastapi"
+
+class FTAPI_DEPLOYMENTS(Enum):
+    LOCALHOST = "localhost"
+    HOSTED = "hosted"
+    SERVERLESS = "serverless"
+
+class SERVER_HEALTH(Enum):
+    INITIALIZING = "initializing"
+    BOOTING = "booting"
+    RUNNING = "running"
+    BUSY = "busy"
+    ERROR = "error"
