6d2ce78899d53355d1938c64735b09812c51c040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:34 +0000

    Add package ephyspy/0.0.8/ephyspy-0.0.8-py3-none-any.whl

---
+++ packages/ephyspy/ephyspy-0.0.8-py3-none-any.whl/ephyspy/features/utils.py
@@ -0,0 +1,398 @@
+#!/usr/bin/env python3
+# Copyright 2023 Jonas Beck
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+
+import inspect
+import sys
+import warnings
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Tuple, Union
+
+import numpy as np
+from numpy import ndarray
+from pandas import DataFrame
+
+from ephyspy.sweeps import EphysSweep, EphysSweepSet
+from ephyspy.utils import where_between
+
+if TYPE_CHECKING:
+    from ephyspy.features.base import SweepFeature, SweepSetFeature
+
+CUSTOM_SWEEP_FEATURES = []
+CUSTOM_SWEEPSET_FEATURES = []
+CUSTOM_SPIKE_FEATURES = []
+
+
+def register_custom_feature(Feature: Union[Callable, SweepSetFeature, SweepFeature]):
+    """Add a custom feature class that inherits from `SweepFeature`
+    or from `SweepSetFeature`. This makes the feature available to all the
+    the EphysPy functionalities such as recursive computation of all dependend
+    features that are called with `lookup_X_feature`, where X can be spike,
+    sweep or sweepset.
+
+    Args:
+        Feature: Feature class to be added to EphysPy ecosystem. Feature
+            must inherit from either `SweepFeature` or `SweesetFeature`.
+    """
+    # TODO: assert more rigorously that Feature can be computed
+    # i.e. by calling it on a dummy sweep and checking if it raises an error
+    # only if it behaves as expected add it to the list of available features
+    from ephyspy.features.base import SweepFeature, SweepSetFeature
+
+    if issubclass(Feature, SweepFeature):
+        CUSTOM_SWEEP_FEATURES.append(Feature)
+    elif issubclass(Feature, SweepSetFeature):
+        CUSTOM_SWEEPSET_FEATURES.append(Feature)
+    elif isinstance(Feature, Callable):  # last, since SweepFeatures also are callable
+        CUSTOM_SPIKE_FEATURES.append(Feature)
+
+
+def fetch_available_fts() -> List[str]:
+    """Fetch all available features.
+
+    Returns a list of all available feature functions and classes that are
+    either part of the EphysPy package or have been registered as custom
+    features with `register_custom_feature`.
+
+    Returns:
+        List[str]: List of all available features.
+
+    Warnings:
+        If a custom feature has the same name as a feature that is part of
+        EphysPy, a warning is raised."""
+    classes = inspect.getmembers(sys.modules["ephyspy"], inspect.isclass)
+    classes = [c[1] for c in classes if "ephyspy.features" in c[1].__module__]
+    feature_classes = [
+        c for c in classes if "Feature" not in c.__name__
+    ]  # gets rid of base classes and NullFeatures
+
+    for custom_fts, base_class in zip(
+        [CUSTOM_SPIKE_FEATURES, CUSTOM_SWEEP_FEATURES, CUSTOM_SWEEPSET_FEATURES],
+        ["SpikeFeature", "SweepFeature", "SweepSetFeature"],
+    ):
+        base_feature_classes = [
+            ft for ft in feature_classes if ft.__base__.__name__ == base_class
+        ]
+        duplicate_features = set(ft.__name__.lower() for ft in custom_fts).intersection(
+            set(ft.__name__.lower() for ft in base_feature_classes)
+        )
+        if len(duplicate_features) > 0:
+            warnings.warn(
+                f"DUPLICATE FEATURES: Unwanted behaviour with custom versions of"
+                + ", ".join(duplicate_features)
+                + "cannot be ruled out. Please consider renaming these features."
+            )
+
+    return feature_classes + CUSTOM_SWEEP_FEATURES + CUSTOM_SWEEPSET_FEATURES
+
+
+class FeatureError(ValueError):
+    """Error raised when a feature is unknown."""
+
+    pass
+
+
+def get_allensdk_spike_features():
+    return [
+        "threshold_index",
+        "threshold_t",
+        "threshold_v",
+        "threshold_i",
+        "peak_index",
+        "peak_t",
+        "peak_v",
+        "peak_i",
+        "trough_index",
+        "trough_t",
+        "trough_v",
+        "trough_i",
+        "upstroke_index",
+        "upstroke",
+        "upstroke_t",
+        "upstroke_v",
+        "downstroke_index",
+        "downstroke",
+        "downstroke_t",
+        "downstroke_v",
+        "isi_type",
+        "fast_trough_index",
+        "fast_trough_t",
+        "fast_trough_v",
+        "fast_trough_i",
+        "adp_index",
+        "adp_t",
+        "adp_v",
+        "adp_i",
+        "slow_trough_index",
+        "slow_trough_t",
+        "slow_trough_v",
+        "slow_trough_i",
+        "width",
+        "upstroke_downstroke_ratio",
+    ]
+
+
+class during_stimulus_only:
+    def __init__(self, sweep, T_stim=None):
+        self.sweep = sweep
+        self.start = sweep.start
+        self.end = sweep.end
+        self.T_stim = T_stim
+        if self.T_stim is None:
+            where_stim = where_stimulus(sweep)
+            self.T_stim = sweep.t[where_stim][[0, -1]]
+        self.all_spikes = None
+
+    def __enter__(self):
+        self.sweep.start = self.T_stim[0]
+        self.sweep.end = self.T_stim[1]
+        if hasattr(self.sweep, "_spikes_df"):
+            self.all_spikes = self.sweep._spikes_df.copy()
+            t_thresh = self.sweep._spikes_df["threshold_t"]
+            while_stim = np.logical_and(
+                self.T_stim[0] < t_thresh, t_thresh < self.T_stim[1]
+            )
+            self.sweep._spikes_df = self.sweep._spikes_df[while_stim]
+        return self.sweep
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.sweep.start = self.start
+        self.sweep.end = self.end
+        if self.all_spikes is not None:
+            self.sweep._spikes_df = self.all_spikes
+        else:
+            delattr(self.sweep, "_spikes_df")
+        return self.sweep
+
+
+def get_sweep_burst_metrics(
+    sweep: EphysSweep,
+) -> Tuple[ndarray, ndarray, ndarray]:
+    """Calculate burst metrics for a sweep.
+
+    Uses EphysExtractor's _process_bursts() method to calculate burst metrics.
+    Handles case where no bursts are found.
+
+    Args:
+        sweep (EphysSweep): Sweep to calculate burst metrics for.
+
+    Returns:
+        Tuple[ndarray, ndarray, ndarray]: returns burst index, burst start index,
+            burst end index.
+    """
+    burst_metrics = sweep._process_bursts()
+    if len(burst_metrics) == 0:
+        return float("nan"), slice(0), slice(0)  # slice(0) acts as empty index
+    idx_burst, idx_burst_start, idx_burst_end = burst_metrics.T
+    return idx_burst, idx_burst_start.astype(int), idx_burst_end.astype(int)
+
+
+def sag_idxs(
+    sweep, v_steady, start, end, return_diagnostics: bool = True
+) -> Tuple[ndarray, Dict]:
+    """determine idxs in a sweep that are part of the sag.
+
+    description: all idxs below steady state and during stimulus.
+
+    Args:
+        sweep (EphysSweep): Sweep to check for sag.
+        v_steady (float): Steady state voltage during hyperpolarization.
+        start (float): Start of stimulus.
+        end (float): End of stimulus.
+
+    Returns:
+        boolean array with length of sweep.t; where sag.
+    """
+    # TODO: refine how sag idxs are chosen!
+    # currently uses all idxs below steady state and during stimulus
+    # can lead to very fragmented idx arrays
+    # fix: if too many idxs are False between ones that are True
+    # set all True ones after to False
+    # also if steady state is never reached again, sag will be massive
+    # -> set all idxs to False ?
+    v_deflect = sweep.voltage_deflection("min")[0]
+    if v_steady - v_deflect > 4:  # The sag should have a minimum depth of 4 mV
+        where_stimulus = np.logical_and(
+            sweep.t > start, sweep.t < end
+        )  # same as where_between (saves on import)
+        sag_idxs = np.logical_and(where_stimulus, sweep.v < v_steady)
+    else:
+        sag_idxs = np.zeros_like(sweep.t, dtype=bool)
+
+    if return_diagnostics:
+        diagnostics = {
+            "sag_idxs": np.where(sag_idxs)[0],
+            "v_deflect": v_deflect,
+            "v_steady": v_steady,
+            "t_sag": sweep.t[sag_idxs],
+            "v_sag": sweep.v[sag_idxs],
+        }
+        return sag_idxs, diagnostics
+    return sag_idxs
+
+
+def sag_period(sweep: EphysSweep, where_sag: ndarray) -> float:
+    """Calculate duration of sag.
+
+    Args:
+        sweep (EphysSweep): Sweep to calculate sag duration for.
+        where_sag (ndarray): Boolean array with length of sweep.t; True where
+            the voltage is below the deflection steady state.
+
+    Returns:
+        float: Duration of sag."""
+    if np.any(where_sag):
+        t = sweep.t
+        t_sag = t[where_sag]
+        return np.diff(t[where_sag][[0, -1]]).item()  # T of entire sag interval
+        # return np.sum(np.diff(t_sag))  # duration of sag
+    else:
+        return 0
+
+
+def where_stimulus(data: Union[EphysSweep, EphysSweepSet]) -> Union[bool, ndarray]:
+    """Checks where the stimulus is unequal to current at t=0.
+
+    Checks where stimulus is unequal to current at t=0 for a single sweep or each
+    sweep in a sweepset.
+
+    Args:
+        data (EphysSweep or EphysSweepSet):
+            Sweep or sweepset to check.
+
+    Returns:
+        bool: True if stimulus is non-zero.
+    """
+    return data.i.T != data.i.T[0]
+
+
+def where_spike_during_stimulus(
+    ft: SweepFeature, recompute: bool = False, spike_ft: str = "peak"
+) -> ndarray[bool]:
+    """Checks where spikes occur during stimulus.
+
+    Args:
+        ft (SweepFeature): Spike feature to check.
+        recompute (bool, optional): If True, recompute spikes. Defaults to False.
+        spike_ft (str, optional): Which spike feature to consider when inferring,
+            whether a spike is inside of the stimulus window, i.e. "peak",
+            "threshold", "trough", etc.. Defaults to "peak".
+
+    Returns:
+        ndarray: Boolean array with length of sweep.t; True where spikes occur
+            during stimulus."""
+    t_thresh = ft.lookup_spike_feature(f"{spike_ft}_t", recompute=recompute)
+    stim_onset = ft.lookup_sweep_feature("stim_onset", recompute=recompute)
+    stim_end = ft.lookup_sweep_feature("stim_end", recompute=recompute)
+    during_stim = where_between(t_thresh, stim_onset, stim_end)
+    return during_stim
+
+
+def has_spikes(sweep: EphysSweep) -> bool:
+    """Check if sweep has spikes.
+
+    Args:
+        sweep (EphysSweep): Sweep to check.
+
+    Returns:
+        bool: True if sweep has spikes.
+    """
+    if hasattr(sweep, "_spikes_df"):
+        return not sweep._spikes_df.empty
+    else:
+        sweep.process_spikes()
+        return not sweep._spikes_df.empty
+
+
+def has_stimulus(data: Union[EphysSweep, EphysSweepSet]) -> Union[bool, ndarray]:
+    """Check if sweep has stimulus that is non-zero.
+
+    Args:
+        data (EphysSweep or EphysSweepSet):
+            Sweep or sweepset to check.
+
+    Returns:
+        bool: True if sweep has stimulus."""
+    return np.any(data.i.T * where_stimulus(data) != 0, axis=0)
+
+
+def is_hyperpol(data: Union[EphysSweep, EphysSweepSet]) -> Union[bool, ndarray]:
+    """Check if sweep is hyperpolarizing, i.e. if the stimulus < 0.
+
+    Args:
+        data (EphysSweep or EphysSweepSet):
+            Sweep or sweepset to check.
+
+    Returns:
+        bool: True if sweep is hyperpolarizing."""
+    return np.any(data.i.T * where_stimulus(data) < 0, axis=0)
+
+
+def is_depol(data: Union[EphysSweep, EphysSweepSet]) -> Union[bool, ndarray]:
+    """Check if sweep is depolarizing, i.e. if the stimulus > 0.
+
+    Args:
+        data (EphysSweep or EphysSweepSet):
+            Sweep or sweepset to check.
+
+    Returns:
+        bool: True if sweep is depolarizing."""
+    return np.any(data.i.T * where_stimulus(data) > 0, axis=0)
+
+
+def has_rebound(feature: Any, T_rebound: float = 0.3) -> bool:
+    """Check if sweep rebounds.
+
+    description: rebound if voltage exceeds baseline after stimulus offset.
+
+    Args:
+        feature (SweepFeature): Feature to check for rebound.
+        T_rebound (float, optional): Time window after stimulus offset in which
+            rebound can occur. Defaults to 0.3.
+
+    Returns:
+        bool: True if sweep rebounds."""
+    sweep = feature.data
+    end = feature.lookup_sweep_feature("stim_end")
+    if is_hyperpol(sweep) and end < feature.data.end:
+        v_baseline = feature.lookup_sweep_feature("v_baseline")
+        ts_rebound = np.logical_and(sweep.t > end, sweep.t < end + T_rebound)
+        return np.any(sweep.v[ts_rebound] > v_baseline)
+    return False
+
+
+def median_idx(d: Union[DataFrame, ndarray]) -> Union[int, slice]:
+    """Get index of median value in a DataFrame.
+
+    If median is unique return index, otherwise return all indices that are
+    closest to the median. If dataframe is empty or all nan return slice(0).
+
+    Args:
+        d (Union[DataFrame, ndarray]): DataFrame or ndarray to get median index
+            from.
+
+    Returns:
+        Union[int, slice]: Index of median value or slice(0) if d is empty or
+            all nan."""
+    d = d if isinstance(d, DataFrame) else DataFrame(d)
+    if len(d) > 0 and not np.all(d.isna()):
+        is_median = d == d.median()
+        if any(is_median):
+            return int(d.index[is_median].to_numpy())
+        ranks = d.rank(pct=True)
+        close_to_median = abs(ranks - 0.5)
+        return int(np.array([close_to_median.idxmin()]))
+    return slice(0)
---
+++ packages/ephyspy/ephyspy-0.0.8-py3-none-any.whl/ephyspy/utils.py
@@ -0,0 +1,319 @@
+#!/usr/bin/env python3
+# Copyright 2023 Jonas Beck
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+
+import inspect
+import re
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Tuple, Union
+
+import numpy as np
+from matplotlib.axes import Axes
+from numpy import ndarray
+
+if TYPE_CHECKING:
+    from ephyspy.sweeps import EphysSweep, EphysSweepSet
+
+
+where_between = lambda t, t0, tend: np.logical_and(t > t0, t < tend)
+
+
+def remove_mpl_artist_by_label(ax: Axes, legend_handle: str) -> Axes:
+    for artist in ax.get_children():
+        if artist.get_label() == legend_handle:
+            artist.remove()
+        ax.legend()
+    return ax
+
+
+def fwhm(
+    t: ndarray, v: ndarray, t_start: float, t_end: float
+) -> Tuple[float, float, float]:
+    """Get full width at half maximum of a ap.
+
+    Args:
+        t (ndarray): time array.
+        v (ndarray): voltage array.
+        t_start (float): start time of ap.
+        t_end (float): end time of ap.
+
+    Returns:
+        Tuple[float, float, float]: full width at half maximum,
+            time of half maximum upstroke, time of half maximum downstroke.
+    """
+    in_T = where_between(t, t_start, t_end)
+    v_peak = np.max(v[in_T])
+    v_start = v[in_T][0]
+    t_peak = t[in_T][np.argmax(v[in_T])]
+    upstroke = where_between(t, t_start, t_peak)
+    downstroke = where_between(t, t_peak, t_end)
+    fwhm = v_start + (v_peak - v_start) / 2
+    hm_up_idx = np.argmin(np.abs(v[upstroke] - fwhm))
+    hm_down_idx = np.argmin(np.abs(v[downstroke] - fwhm))
+    hm_up_t = t[upstroke][hm_up_idx]
+    hm_down_t = t[downstroke][hm_down_idx]
+    return fwhm, hm_up_t, hm_down_t
+
+
+def unpack(
+    dict: Dict, keys: Union[str, Tuple[str, ...]]
+) -> Union[Any, Tuple[Any, ...]]:
+    """Unpack dict to tuple of values."""
+    if isinstance(keys, str):
+        return dict[keys]
+    return tuple(dict[k] for k in keys)
+
+
+def relabel_line(ax: Axes, old_label: str, new_label: str):
+    """Rename line label in one given axes."""
+    for child in ax._children:
+        if old_label in child.get_label():
+            child.set_label(new_label)
+
+
+def is_baseclass(
+    base_name: str,
+    obj: object,
+) -> bool:
+    base_names = [base.__name__ for base in inspect.getmro(obj)]
+    return base_name in base_names
+
+
+def is_spike_feature(ft: Any) -> bool:
+    return is_baseclass("SpikeFeature", ft)
+
+
+def is_sweep_feature(ft: Any) -> bool:
+    return is_baseclass("SweepFeature", ft) and not is_baseclass("SweepSetFeature", ft)
+
+
+def is_sweepset_feature(ft: Any) -> bool:
+    return is_baseclass("SweepSetFeature", ft)
+
+
+def has_spike_feature(sweep: EphysSweep, ft: str) -> bool:
+    """Checks if sweep has a given spike feature.
+
+    First checks for `_spikes_df` attribute, which should get instantiated if
+    spikes have already been processed. If not present `process_spikes` will be
+    called. Then, if the feature is present in the `_spikes_df` and not all values
+    are NaN, returns True.
+
+    Args:
+        sweep (EphysSweep): Sweep to check for existance of spike feature.
+        ft (str): Spike feature to check for. Feature must be present in `_spikes_df`
+            for a healthy spiking sweep.
+
+    Returns:
+        bool: Whether sweep has the given spike feature."""
+    if not hasattr(sweep, "_spikes_df"):
+        sweep.process_spikes()
+    ap_fts = sweep._spikes_df
+    if ap_fts.size:
+        if ft in ap_fts.columns:
+            if not np.all(np.isnan(ap_fts[ft])):
+                return True
+    return False
+
+
+def stimulus_type(sweep_or_sweepset: Union[EphysSweep, EphysSweepSet]) -> str:
+    """Detects stimulus type of sweep.
+
+    Stimulus can be:
+    - long_square
+    - short_square
+    - ramp
+    - null
+    - unknown
+
+    Args:
+        sweep_or_sweepset (Union[EphysSweep, EphysSweepSet]): Sweep or sweepset to
+            detect stimulus type of.
+
+    Returns:
+        str: Stimulus type.
+    """
+    cls_name = sweep_or_sweepset.__class__.__name__
+
+    sweep = sweep_or_sweepset
+    if cls_name == "EphysSweepSet":  # test for sweepset without using isinstance
+        sweep = sweep_or_sweepset[0]
+        if np.all(sweep.i == 0) and len(sweep_or_sweepset.sweeps()) > 1:
+            sweep = sweep_or_sweepset[1]
+
+    stim_window = sweep.i != 0
+    stim = sweep.i[stim_window]
+    t = sweep.t[stim_window]
+
+    if not np.any(stim_window):
+        return "null"
+
+    onset, end = t[[0, -1]]
+    if end - onset < 0.005:
+        return "short_square"
+
+    slope = np.diff(stim[1:-2])
+    if np.all(slope == 0):
+        return "long_square"
+
+    rel_slope_change = (
+        np.abs(slope - slope[0]) / slope[0] if np.all(slope > 0) else np.nan
+    )
+    if np.all(rel_slope_change < 0.001) and not np.isnan(
+        rel_slope_change
+    ):  # same slope
+        return "ramp"
+    else:
+        return "unknown"
+
+
+def scatter_spike_ft(
+    ft, sweep: EphysSweep, ax: Axes = None, selected_idxs=None, **kwargs
+) -> Axes:
+    f"""Plot action potential {ft} feature for all or selected aps.
+
+    Inherits additional kwargs / functionality from `spikefeatureplot`.
+
+    Args:
+        sweep (EphysSweep): Sweep to plot the feature for.
+        ax (Axes, optional): Matplotlib axes. Defaults to None.
+        selected_idxs (slice, optional): Slice object to select aps. Defaults to None.
+        **kwargs: Additional kwargs are passed to the plotting function.
+
+    Returns:
+        Axes: Matplotlib axes."""
+    if has_spike_feature(sweep, ft + "_v"):
+        idxs = slice(None) if selected_idxs is None else selected_idxs
+        t = sweep.spike_feature(ft + "_t", include_clipped=True)[idxs]
+        v = sweep.spike_feature(ft + "_v", include_clipped=True)[idxs]
+        ax.scatter(t, v, s=10, label=ft, **kwargs)
+    return ax
+
+
+# TODO: FIX ISSUES WITH DOCSTRING PARSING: trailing ' and parsing despite line breaks
+def parse_func_doc_attrs(func: Callable) -> Dict:
+    """Parses docstrings for attributes.
+
+    Docstrings should have the following format:
+    <Some text>
+    attr: <attr text>.
+    attr: <attr text>.
+    ...
+    <Some more text>
+
+    IMPORTANT: EACH ATTRIBUTE MUST END WITH A "."
+
+    Args:
+        func (Callable): Function to parse docstring of.
+
+    Returns:
+        doc_attrs: all attributes found in document string.
+    """
+    func_doc = func.__doc__
+
+    pattern = r"([\w\s]+):"
+    matches = re.findall(pattern, func_doc)
+    attrs = [m.strip() for m in matches]
+    if "Args" in attrs:
+        attrs = attrs[: attrs.index("Args")]
+
+    doc_attrs = {}
+    for attr in attrs:
+        doc_attrs[attr] = ""
+        if func_doc is not None:  # if func has no docstring
+            regex = re.compile(f"{attr}: (.*)")
+            match = regex.search(repr(func_doc))
+            if match:
+                match = match.group(1)
+                match = " ".join(match.split())  # rm whitespaces > 1
+                match = match.split("\\n\\n")[0]  # slice at double line break
+                match = match.replace("\\n", "")
+                doc_attrs[attr] = match
+
+    for attr_r in attrs[::-1]:  # traverse attr descriptions in reverse
+        for attr_f in attrs:  # rm attr descriptions from other attr descriptions
+            doc_attrs[attr_f] = doc_attrs[attr_f].split(f"{attr_r}:")[0].strip()
+    return doc_attrs
+
+
+def parse_desc(func: Callable) -> str:
+    """Parses docstring for description.
+
+    If no description is found, returns empty string.
+    Special case of `parse_func_doc_attrs`.
+
+    Args:
+        func (Callable): Function to parse docstring of.
+
+    Returns:
+        str: Description of function."""
+    dct = parse_func_doc_attrs(func)
+    if "description" in dct:
+        if "/." in dct["description"]:
+            return ""
+        return dct["description"]
+    return ""
+
+
+def parse_deps(deps_string: str) -> List[str]:
+    """Parses docstring for feature dependencies.
+
+    If no dependencies are found, returns empty list.
+    Special case of `parse_func_doc_attrs`.
+
+    Args:
+        deps_string (str): String to parse for dependencies.
+
+    Returns:
+        List[str]: List of dependencies."""
+    if deps_string == "/":
+        return []
+    else:
+        return [d.strip() for d in deps_string.split(",")]
+
+
+def get_feature(name: str, data: Union[EphysSweep, EphysSweepSet], **kwargs):
+    """Get feature by name.
+
+    This is a convenience function to compute features without having to import
+    the feature classes or think about wether a feature is computed on a sweep or
+    sweepset.
+
+    Args:
+        name (str): Name of feature.
+        data (EphysSweep or EphysSweepSet): Data to compute feature on. This can be
+            either a single sweep or a sweepset.
+
+    Raises:
+        FeatureError: If feature is not available for data type.
+
+    Returns:
+        Feature: Feature object.
+    """
+    # imports are done here to avoid circular imports
+    from ephyspy.features.sweep_features import available_sweep_features
+    from ephyspy.features.sweepset_features import available_sweepset_features
+    from ephyspy.features.utils import FeatureError
+    from ephyspy.sweeps import EphysSweep, EphysSweepSet
+
+    if isinstance(data, EphysSweep):
+        return available_sweep_features()[name](data, **kwargs)
+    elif isinstance(data, EphysSweepSet):
+        return available_sweepset_features()[name](data, **kwargs)
+    else:
+        raise FeatureError(
+            f"Feature {name} is not available for data of type {type(data)}."
+        )
