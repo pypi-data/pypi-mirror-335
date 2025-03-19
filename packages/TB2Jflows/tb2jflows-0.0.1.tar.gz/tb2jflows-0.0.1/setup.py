5ea7ffa9e73eaba7890c1e79084227588fd00823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:35 +0000

    Add package TB2Jflows/0.0.1/tb2jflows-0.0.1.tar.gz

---
+++ packages/TB2Jflows/tb2jflows-0.0.1.tar.gz/tb2jflows-0.0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+__version__ = "0.0.1"
+
+long_description = """ TB2Jflows: Workflows for automatically calculation of exchange parameters using TB2J """
+
+setup(
+    name='TB2Jflows',
+    version=__version__,
+    description=
+    'TB2Jflows: Workflows for automatically calculation of exchange parameters using TB2J',
+    long_description=long_description,
+    author='Xu He',
+    author_email='mailhexu@gmail.com',
+    license='BSD-2-clause',
+    packages=find_packages(),
+    scripts=[
+    ],
+    install_requires=['TB2J', 'ase', 'sisl', 'pyDFTutils'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: BSD License',
+    ],
+    python_requires='>=3.6',
+)
