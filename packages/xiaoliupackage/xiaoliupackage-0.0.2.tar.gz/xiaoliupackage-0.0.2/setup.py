a494bd0b97d77f816e1db292d4728b28bc0c17a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:38 +0000

    Add package xiaoliupackage/0.0.2/xiaoliupackage-0.0.2.tar.gz

---
+++ packages/xiaoliupackage/xiaoliupackage-0.0.2.tar.gz/xiaoliupackage-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+VERSION = '0.0.2'
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+# Configuration
+setup(
+    # The name must match the filename'package'
+    name="xiaoliupackage",
+    version=VERSION,
+    author="Liu",
+    author_email="xaliu@edolby.com",  # Replace with your actual email
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # Example dependencies, modify according to actual situation
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
