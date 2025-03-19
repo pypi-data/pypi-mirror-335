b4b67cd102c5c39715fc16ada4ab2efcee8b0a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:07:48 +0000

    Add package pysteamauth2/1.1.5/pysteamauth2-1.1.5.tar.gz

---
+++ packages/pysteamauth2/pysteamauth2-1.1.5.tar.gz/pysteamauth2-1.1.5/pysteamauth/abstract/request.py
@@ -0,0 +1,28 @@
+from abc import (
+    ABC,
+    abstractmethod,
+)
+from typing import (
+    Any,
+    Mapping,
+)
+
+from aiohttp import ClientResponse
+
+
+class RequestStrategyAbstract(ABC):
+    @abstractmethod
+    async def request(self, url: str, method: str, **kwargs: Any) -> ClientResponse:  # noqa:U100
+        ...
+
+    @abstractmethod
+    async def text(self, url: str, method: str, **kwargs: Any) -> str:  # noqa:U100
+        ...
+
+    @abstractmethod
+    async def bytes(self, url: str, method: str, **kwargs: Any) -> bytes:  # noqa:U100
+        ...
+
+    @abstractmethod
+    def cookies(self, domain: str = "steamcommunity.com") -> Mapping[str, str]:  # noqa:U100
+        ...
