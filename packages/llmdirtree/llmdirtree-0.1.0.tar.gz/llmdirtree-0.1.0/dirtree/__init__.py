364b647d3e4c8053b12bc25d80fc6053f5449559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:44 +0000

    Add package llmdirtree/0.1.0/llmdirtree-0.1.0.tar.gz

---
+++ packages/llmdirtree/llmdirtree-0.1.0.tar.gz/llmdirtree-0.1.0/dirtree/__init__.py
@@ -0,0 +1,2 @@
+from .main import main, write_directory_tree
+__version__ = "0.1.0"
---
+++ packages/llmdirtree/llmdirtree-0.1.0.tar.gz/llmdirtree-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="llmdirtree",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "llmdirtree=dirtree.main:main",
+        ],
+    },
+    author="arun",
+    author_email="your.email@example.com",
+    description="A simple directory tree generator for llm",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/arun477/dirtree",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
