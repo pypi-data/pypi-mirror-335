a5e307e26113f66224bdc2fd7164c56c22efa931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:27 +0000

    Add package nautobot-floor-plan/2.7.0/nautobot_floor_plan-2.7.0-py3-none-any.whl

---
+++ packages/nautobot-floor-plan/nautobot_floor_plan-2.7.0-py3-none-any.whl/nautobot_floor_plan/filter_extensions.py
@@ -0,0 +1,101 @@
+"""Extensions to Nautobot core models' filtering functionality."""
+
+import django_filters
+from nautobot.apps.filters import FilterExtension, RelatedMembershipBooleanFilter
+
+from nautobot_floor_plan import models
+
+
+class RackFilterExtension(FilterExtension):
+    """Add a filter to the RackFilterSet in Nautobot core."""
+
+    model = "dcim.rack"
+
+    filterset_fields = {
+        "nautobot_floor_plan_floor_plan": django_filters.ModelChoiceFilter(
+            queryset=models.FloorPlan.objects.all(),
+            field_name="location__floor_plan",
+            label="Floor plan",
+        ),
+        "nautobot_floor_plan_has_floor_plan_tile": RelatedMembershipBooleanFilter(
+            field_name="floor_plan_tile",
+            label="Floor Plan Tile",
+        ),
+    }
+
+
+class RackGroupFilterExtension(FilterExtension):
+    """Add a filter to the RackGroupFilterSet in Nautobot core."""
+
+    model = "dcim.rackgroup"
+
+    filterset_fields = {
+        "nautobot_floor_plan_floor_plan": django_filters.ModelChoiceFilter(
+            queryset=models.FloorPlan.objects.all(),
+            field_name="location__floor_plan",
+            label="Floor plan",
+        ),
+    }
+
+
+class PowerPanelFilterExtension(FilterExtension):
+    """Add a filter to the PowerPanelFilterSet in Nautobot core."""
+
+    model = "dcim.powerpanel"
+
+    filterset_fields = {
+        "nautobot_floor_plan_floor_plan": django_filters.ModelChoiceFilter(
+            queryset=models.FloorPlan.objects.all(),
+            field_name="location__floor_plan",
+            label="Floor plan",
+        ),
+        "nautobot_floor_plan_has_floor_plan_tile": RelatedMembershipBooleanFilter(
+            field_name="floor_plan_tile",
+            label="Floor Plan Tile",
+        ),
+    }
+
+
+class PowerFeedFilterExtension(FilterExtension):
+    """Add a filter to the PowerFeedFilterSet in Nautobot core."""
+
+    model = "dcim.powerfeed"
+
+    filterset_fields = {
+        "nautobot_floor_plan_floor_plan": django_filters.ModelChoiceFilter(
+            queryset=models.FloorPlan.objects.all(),
+            field_name="power_panel__location__floor_plan",
+            label="Floor plan",
+        ),
+        "nautobot_floor_plan_has_floor_plan_tile": RelatedMembershipBooleanFilter(
+            field_name="floor_plan_tile",
+            label="Floor Plan Tile",
+        ),
+    }
+
+
+class DeviceFilterExtension(FilterExtension):
+    """Add a filter to the DeviceFilterSet in Nautobot core."""
+
+    model = "dcim.device"
+
+    filterset_fields = {
+        "nautobot_floor_plan_floor_plan": django_filters.ModelChoiceFilter(
+            queryset=models.FloorPlan.objects.all(),
+            field_name="location__floor_plan",
+            label="Floor plan",
+        ),
+        "nautobot_floor_plan_has_floor_plan_tile": RelatedMembershipBooleanFilter(
+            field_name="floor_plan_tile",
+            label="Floor Plan Tile",
+        ),
+    }
+
+
+filter_extensions = [
+    RackFilterExtension,
+    RackGroupFilterExtension,
+    PowerPanelFilterExtension,
+    PowerFeedFilterExtension,
+    DeviceFilterExtension,
+]
