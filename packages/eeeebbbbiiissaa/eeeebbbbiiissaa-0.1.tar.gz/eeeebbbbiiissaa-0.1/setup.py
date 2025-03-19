dba990f188df212dadd10b4e302f2bc678617fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:07:57 +0000

    Add package eeeebbbbiiissaa/0.1/eeeebbbbiiissaa-0.1.tar.gz

---
+++ packages/eeeebbbbiiissaa/eeeebbbbiiissaa-0.1.tar.gz/eeeebbbbiiissaa-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eeeebbbbiiissaa',  # Name of your package
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # Dependencies (if any)
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
