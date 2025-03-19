bb30f591efe7c8b300184bcb35493b712cfb7905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:24 +0000

    Add package cobweb-launcher/3.1.1/cobweb_launcher-3.1.1-py3-none-any.whl

---
+++ packages/cobweb-launcher/cobweb_launcher-3.1.1-py3-none-any.whl/cobweb/launchers/__init__.py
@@ -0,0 +1,9 @@
+# from .launcher_air import LauncherAir
+# from .launcher_pro import LauncherPro
+# from .launcher_api import LauncherApi
+
+
+from .launcher import Launcher
+from .uploader import Uploader
+from .distributor import Distributor
+
