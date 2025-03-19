2663fbd34e9e5bcd21602bf562ef2f957309287b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:05:19 +0000

    Add package meyigi-scripts/0.4.0/meyigi_scripts-0.4.0.tar.gz

---
+++ packages/meyigi-scripts/meyigi_scripts-0.4.0.tar.gz/meyigi_scripts-0.4.0/meyigi_scripts/__init__.py
@@ -0,0 +1,4 @@
+from .append_to_excel import *
+from .timeit import *   
+from .ChatGPTAPI import *
+from .get_requests import *
---
+++ packages/meyigi-scripts/meyigi_scripts-0.4.0.tar.gz/meyigi_scripts-0.4.0/tests/__init__.py
@@ -0,0 +1,4 @@
+from .test_append_to_excel import *
+from .test_timeit import *
+from .test_ChatGPTAPI import *
+from .test_get_requests import *
