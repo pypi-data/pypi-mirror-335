4e2dd3694b74c50fa682e5aad55ee9875b2ec69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:30 +0000

    Add package SPLDB/0.0.1/spldb-0.0.1.tar.gz

---
+++ packages/SPLDB/spldb-0.0.1.tar.gz/spldb-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages # type: ignore
+
+setup(
+    name='SPLDB',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Alpha-O-Auro SpL',
+    author_email='imr@outlook.in',
+    description='A Dropbox used as a Database.',
+    python_requires='>=3.6',
+)
