6a8b76b3f3212cc6dc14012e286cf77cdfb88e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:05:10 +0000

    Add package super-latex-tool/0.1.1/super_latex_tool-0.1.1.tar.gz

---
+++ packages/super-latex-tool/super_latex_tool-0.1.1.tar.gz/super_latex_tool-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='super_latex_tool',
+    version='0.1.1',
+    packages=find_packages(),
+    author='cormeum',
+    author_email='armus2812@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
