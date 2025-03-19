16becb169d598f3d3419d89455e9ec54220c74fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:05:17 +0000

    Add package DetectAssetPurpose/0.1.1/detectassetpurpose-0.1.1.tar.gz

---
+++ packages/DetectAssetPurpose/detectassetpurpose-0.1.1.tar.gz/detectassetpurpose-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DetectAssetPurpose",
+    version="0.1.1",
+    description="Package provides detection of asset purpose using LLM identification: Brand Building or Conversion",
+    author="Irina White",
+    author_email="i.white@neuronsinc.com",
+    license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},    
+    install_requires=[],
+)
