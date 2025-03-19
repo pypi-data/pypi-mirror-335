a3a0465d7b9f8dd561857905ce28fb111c06b000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:05:34 +0000

    Add package deep-code/0.0.1/deep_code-0.0.1-py3-none-any.whl

---
+++ packages/deep-code/deep_code-0.0.1-py3-none-any.whl/deep_code/tools/new.py
@@ -0,0 +1,5 @@
+"""Logic for initializing repositories
+ Initialize a GitHub repository with the proposed configurations files, an initial
+ workflow notebook template (e.g. workflow.ipynb), a template Python package (code and
+pyproject.toml), and a template setup for documentation (e.g., using mkdocs),
+setup of thebuild pipeline"""
