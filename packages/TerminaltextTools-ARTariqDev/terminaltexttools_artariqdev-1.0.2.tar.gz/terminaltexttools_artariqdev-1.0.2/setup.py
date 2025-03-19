50544889e7cba5f345308ff04dc7014541bd2323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:17 +0000

    Add package TerminaltextTools-ARTariqDev/1.0.2/terminaltexttools_artariqdev-1.0.2.tar.gz

---
+++ packages/TerminaltextTools-ARTariqDev/terminaltexttools_artariqdev-1.0.2.tar.gz/terminaltexttools_artariqdev-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TerminaltextTools_ARTariqDev",
+    version="1.0.2",
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "textual",
+        "python-docx",
+        "chardet",
+    ],
+    entry_points={
+        "console_scripts": [
+            "textSummarizer = textSummarizer.main:main"
+        ]
+    },
+    author="Abdur Rehman Tariq",
+    description="A CLI tool to summarize text files using GPT-3.5-turbo",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
