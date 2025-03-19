7e8939bde43d554d46af3259e6abfae7d2dffda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:48 +0000

    Add package stupidlib/0.1/stupidlib-0.1.tar.gz

---
+++ packages/stupidlib/stupidlib-0.1.tar.gz/stupidlib-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stupidlib',
+      version='0.1',
+      description='Degrade to the maximum',
+      packages=['stupidlib'],
+      author_email='vsevolodrodionov99@gmail.com',
+      zip_safe=False)
+
