89916812640f2ece2c439d4ff88daeff1722e167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:05:10 +0000

    Add package maleo-core/0.1.52/maleo_core-0.1.52.tar.gz

---
+++ packages/maleo-core/maleo_core-0.1.52.tar.gz/maleo_core-0.1.52/maleo_core/models/schemas/request.py
@@ -0,0 +1,6 @@
+from ..transfers.parameter import Expand, StatusUpdate, Get
+
+class Request:
+    ExpandParameter = Expand
+    StatusUpdateQueryParameter = StatusUpdate
+    GetQueryParameters = Get
