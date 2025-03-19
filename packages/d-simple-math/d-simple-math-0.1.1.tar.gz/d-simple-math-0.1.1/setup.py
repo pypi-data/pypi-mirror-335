b9300decf9e5873e1e450d226572d4664925eb21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:35 +0000

    Add package d-simple-math/0.1.1/d-simple-math-0.1.1.tar.gz

---
+++ packages/d-simple-math/d-simple-math-0.1.1.tar.gz/d-simple-math-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="d-simple-math",
+    version="0.1.1",
+    packages=find_packages(),
+    description="A very simple math library",
+    author="deepa_r",
+    author_email="deepa@palmtreeinfotech.com",
+)
