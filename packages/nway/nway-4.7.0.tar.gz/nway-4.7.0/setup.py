8d13987172f714554d565ce388f01603054ff74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:20 +0000

    Add package nway/4.7.0/nway-4.7.0.tar.gz

---
+++ packages/nway/nway-4.7.0.tar.gz/nway-4.7.0/setup.py
@@ -0,0 +1,16 @@
+import os
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+long_description = ""
+with open('README.rst') as f:
+	long_description = f.read()
+
+setup(
+	packages=['nwaylib'],
+	scripts=['nway.py', 'nway-write-header.py', 'nway-explain.py', 'nway-create-fake-catalogue.py', 'nway-create-shifted-catalogue.py', 'nway-calibrate-cutoff.py'],
+	long_description=long_description,
+)
