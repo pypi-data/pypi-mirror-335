97c6d06a5c002ab4a8219b3ff8ab20b5d0cf726f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:07:53 +0000

    Add package botrun-flow-lang/5.3.191/botrun_flow_lang-5.3.191-py3-none-any.whl

---
+++ packages/botrun-flow-lang/botrun_flow_lang-5.3.191-py3-none-any.whl/botrun_flow_lang/constants.py
@@ -0,0 +1,5 @@
+HATCH_STORE_NAME = "hatch"
+CHECKPOINTER_STORE_NAME = "checkpointer"
+HATCH_BUCKET_NAME = "hatch"
+USER_SETTING_STORE_NAME = "user_settings"
+USER_WORKFLOW_STORE_NAME = "user_workflows"
