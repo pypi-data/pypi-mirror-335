660d0c9e37feb83b5402978aa847122729fff487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:05:09 +0000

    Add package batch-plugins/1.0.8/batch_plugins-1.0.8.tar.gz

---
+++ packages/batch-plugins/batch_plugins-1.0.8.tar.gz/batch_plugins-1.0.8/epex_sftp/env_vars.py
@@ -0,0 +1,13 @@
+import os
+
+
+port = os.getenv('SFTP_PORT')
+username = os.getenv('SFTP_USERNAME')
+password = os.getenv('SFTP_PASSWORD')
+private_key = os.getenv('SFTP_PRIVATE_KEY')
+passphrase = os.getenv('SFTP_PASSPHRASE') or password
+
+
+epex_country = os.getenv('EPEX_COUNTRY')
+epex_country_code = os.getenv('EPEX_COUNTRY_CODE')
+epex_year = os.getenv('EPEX_YEAR')
