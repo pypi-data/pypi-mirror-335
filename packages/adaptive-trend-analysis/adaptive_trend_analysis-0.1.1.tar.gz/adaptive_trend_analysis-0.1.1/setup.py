1cc1be49f94a1bd66513504622690ebf009b1144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:42:14 +0000

    Add package adaptive-trend-analysis/0.1.1/adaptive_trend_analysis-0.1.1.tar.gz

---
+++ packages/adaptive-trend-analysis/adaptive_trend_analysis-0.1.1.tar.gz/adaptive_trend_analysis-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="adaptive_trend_analysis",
+    version="0.1.1",
+    description="A library for Gradient-Adaptive Smoothing (GAS) trend analysis.",
+    author="Jherson Aguto",
+    author_email="dalisayfernando4@gmail.com",
+    url="https://github.com/yourusername/adaptive_trend_analysis",  # Update with your actual GitHub repo
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
