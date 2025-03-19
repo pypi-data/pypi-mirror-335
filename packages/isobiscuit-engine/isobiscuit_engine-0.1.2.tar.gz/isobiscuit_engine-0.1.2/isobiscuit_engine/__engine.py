d5cda6b19f09e0e6f869efb008ff85688a72440b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:31 +0000

    Add package isobiscuit-engine/0.1.2/isobiscuit_engine-0.1.2.tar.gz

---
+++ packages/isobiscuit-engine/isobiscuit_engine-0.1.2.tar.gz/isobiscuit_engine-0.1.2/isobiscuit_engine/__engine.py
@@ -0,0 +1,12 @@
+import io
+import engine
+
+
+class Engine:
+    def __init__(self, data_sector, code_sector, mem_sector, zip: io.BytesIO, debug=False, ):
+        self.__engine = engine.Engine(data_sector, code_sector, mem_sector, zip, debug)
+    def kill(self):
+        self.__engine.kill()
+    """Run"""
+    def run(self):
+        return self.__engine.run()
