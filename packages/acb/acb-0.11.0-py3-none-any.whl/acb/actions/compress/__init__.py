f63e08a0208f248429b13509d34e4e319399784e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:07:54 +0000

    Add package acb/0.11.0/acb-0.11.0-py3-none-any.whl

---
+++ packages/acb/acb-0.11.0-py3-none-any.whl/acb/actions/compress/__init__.py
@@ -0,0 +1,48 @@
+import typing as t
+from gzip import GzipFile
+from io import BytesIO
+from pathlib import Path
+
+import brotli
+from pydantic import BaseModel
+
+
+class Compress(BaseModel):
+    @staticmethod
+    def gzip(
+        content: str | bytes,
+        path: t.Optional[str | Path] = None,
+        compresslevel: int = 6,
+    ) -> bytes:
+        gzip_buffer = BytesIO()
+        gz = GzipFile(path, "wb", compresslevel, gzip_buffer)
+        if path:
+            if isinstance(content, bytes):
+                gz.write(content)
+            else:
+                gz.write(content.encode())
+        gz.close()
+        return gzip_buffer.getvalue()
+
+    @staticmethod
+    def brotli(data: bytes | str, level: int = 3) -> bytes:
+        if isinstance(data, str):
+            data = data.encode()
+        return brotli.compress(data, quality=level)
+
+
+compress = Compress()
+
+
+class Decompress(BaseModel):
+    @staticmethod
+    def brotli(data: bytes) -> str:
+        return brotli.decompress(data)
+
+    @staticmethod
+    def gzip(content: t.Any) -> str:
+        gzip_buffer = BytesIO(content)
+        return GzipFile(None, "rb", fileobj=gzip_buffer).read().decode()
+
+
+decompress = Decompress()
---
+++ packages/acb/acb-0.11.0-py3-none-any.whl/acb/adapters/requests/httpx.py
@@ -0,0 +1,68 @@
+import typing as t
+
+from hishel import (
+    AsyncCacheClient,
+    AsyncRedisStorage,
+    Controller,
+)
+from hishel._utils import generate_key
+from httpcore import Request
+from httpx import Response as HttpxResponse
+from redis.asyncio import Redis as AsyncRedis
+from acb.depends import depends
+
+from ._base import RequestsBase, RequestsBaseSettings
+
+
+class RequestsSettings(RequestsBaseSettings): ...
+
+
+class Requests(RequestsBase):
+    storage: AsyncRedisStorage
+    controller: Controller
+
+    def cache_key(self, request: Request, body: bytes) -> str:
+        key = generate_key(request, body)
+        return f"{self.config.app.name}:httpx:{key}"
+
+    async def get(self, url: str, timeout: int = 5) -> HttpxResponse:
+        async with AsyncCacheClient(
+            storage=self.storage, controller=self.controller
+        ) as client:
+            return await client.get(url, timeout=timeout)
+
+    async def post(
+        self, url: str, data: dict[str, t.Any], timeout: int = 5
+    ) -> HttpxResponse:
+        async with AsyncCacheClient(
+            storage=self.storage, controller=self.controller
+        ) as client:
+            return await client.post(url, data=data, timeout=timeout)
+
+    async def put(
+        self, url: str, data: dict[str, t.Any], timeout: int = 5
+    ) -> HttpxResponse:
+        async with AsyncCacheClient(
+            storage=self.storage, controller=self.controller
+        ) as client:
+            return await client.put(url, data=data, timeout=timeout)
+
+    async def delete(self, url: str, timeout: int = 5) -> HttpxResponse:
+        async with AsyncCacheClient(
+            storage=self.storage, controller=self.controller
+        ) as client:
+            return await client.delete(url, timeout=timeout)
+
+    @depends.inject
+    async def init(self) -> None:  # type: ignore
+        self.storage = AsyncRedisStorage(
+            client=AsyncRedis(
+                host=self.config.cache.host.get_secret_value(),
+                port=self.config.cache.port,
+            ),
+            ttl=self.config.requests.cache_ttl,
+        )
+        self.controller = Controller(key_generator=self.cache_key)  # type: ignore
+
+
+depends.set(Requests)
