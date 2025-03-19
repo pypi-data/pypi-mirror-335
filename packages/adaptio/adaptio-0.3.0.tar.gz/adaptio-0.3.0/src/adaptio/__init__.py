13d935f9def6f1264ac148fe5cb0a0c62e3c0e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:07:52 +0000

    Add package adaptio/0.3.0/adaptio-0.3.0.tar.gz

---
+++ packages/adaptio/adaptio-0.3.0.tar.gz/adaptio-0.3.0/src/adaptio/__init__.py
@@ -0,0 +1,17 @@
+from .adaptive_async_concurrency_limiter import (
+    AdaptiveAsyncConcurrencyLimiter,
+    ServiceOverloadError,
+)
+from .adjustable_semaphore import AdjustableSemaphore
+from .raise_on_aiohttp_overload import raise_on_aiohttp_overload
+from .with_adaptive_retry import with_adaptive_retry
+from .with_async_control import with_async_control
+
+__all__ = [
+    "AdaptiveAsyncConcurrencyLimiter",
+    "AdjustableSemaphore",
+    "raise_on_aiohttp_overload",
+    "ServiceOverloadError",
+    "with_adaptive_retry",
+    "with_async_control",
+]
