a5d4a3ce54672eb1d195cbe721063fa0da603285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:15:27 +0000

    Add package fmu-sumo/2.3.7/fmu_sumo-2.3.7-py3-none-any.whl

---
+++ packages/fmu-sumo/fmu_sumo-2.3.7-py3-none-any.whl/fmu/sumo/explorer/objects/__init__.py
@@ -0,0 +1,18 @@
+"""Sumo cases and child objects"""
+
+from fmu.sumo.explorer.objects._child import Child
+from fmu.sumo.explorer.objects._metrics import Metrics
+from fmu.sumo.explorer.objects._search_context import SearchContext
+from fmu.sumo.explorer.objects.case import Case
+from fmu.sumo.explorer.objects.cases import Cases
+from fmu.sumo.explorer.objects.cpgrid import CPGrid
+from fmu.sumo.explorer.objects.cpgrid_property import CPGridProperty
+from fmu.sumo.explorer.objects.cube import Cube
+from fmu.sumo.explorer.objects.dictionary import Dictionary
+from fmu.sumo.explorer.objects.iteration import Iteration
+from fmu.sumo.explorer.objects.iterations import Iterations
+from fmu.sumo.explorer.objects.polygons import Polygons
+from fmu.sumo.explorer.objects.realization import Realization
+from fmu.sumo.explorer.objects.realizations import Realizations
+from fmu.sumo.explorer.objects.surface import Surface
+from fmu.sumo.explorer.objects.table import Table
---
+++ packages/fmu-sumo/fmu_sumo-2.3.7-py3-none-any.whl/fmu/sumo/explorer/objects/iterations.py
@@ -0,0 +1,61 @@
+"""Module for searchcontext for collection of iterations."""
+
+from typing import Dict, List
+
+from fmu.sumo.explorer.objects._search_context import SearchContext
+
+
+class Iterations(SearchContext):
+    def __init__(self, sc, uuids):
+        super().__init__(sc._sumo, must=[{"ids": {"values": uuids}}])
+        self._hits = uuids
+        return
+
+    def _maybe_prefetch(self, index):
+        return
+
+    async def _maybe_prefetch_async(self, index):
+        return
+
+    def get_object(self, uuid: str, select: List[str] = None) -> Dict:
+        """Get metadata object by uuid
+
+        Args:
+            uuid (str): uuid of metadata object
+            select (List[str]): list of metadata fields to return
+
+        Returns:
+            Dict: a metadata object
+        """
+        obj = self._cache.get(uuid)
+        if obj is None:
+            obj = self.get_iteration_by_uuid(uuid)
+            self._cache.put(uuid, obj)
+            pass
+
+        return obj
+
+    async def get_object_async(
+        self, uuid: str, select: List[str] = None
+    ) -> Dict:
+        """Get metadata object by uuid
+
+        Args:
+            uuid (str): uuid of metadata object
+            select (List[str]): list of metadata fields to return
+
+        Returns:
+            Dict: a metadata object
+        """
+
+        obj = self._cache.get(uuid)
+        if obj is None:
+            obj = await self.get_iteration_by_uuid_async(uuid)
+            self._cache.put(uuid, obj)
+
+        return obj
+
+    def filter(self, **kwargs):
+        sc = super().filter(**kwargs)
+        uuids = sc.uuids
+        return Iterations(self, uuids)
