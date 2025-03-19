464e3c184ac335090928db2f9f2aa142df561b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:48 +0000

    Add package feedbackintelligence/1.0.3/feedbackintelligence-1.0.3.tar.gz

---
+++ packages/feedbackintelligence/feedbackintelligence-1.0.3.tar.gz/feedbackintelligence-1.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="feedbackintelligence",
+    version="1.0.3",
+    author="Tigran Kostanyan",
+    author_email="tigran@manot.ai",
+    description="An SDK for Feedback Intelligence data insertion",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'httpx',
+        'pydantic',
+        'boto3',
+    ],
+    python_requires='>=3.6',
+)
