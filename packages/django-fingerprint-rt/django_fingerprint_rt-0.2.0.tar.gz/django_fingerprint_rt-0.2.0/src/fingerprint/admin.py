d3f7511926c32370cfb6927d987a57052e0f11e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:15 +0000

    Add package django-fingerprint-rt/0.2.0/django_fingerprint_rt-0.2.0.tar.gz

---
+++ packages/django-fingerprint-rt/django_fingerprint_rt-0.2.0.tar.gz/django_fingerprint_rt-0.2.0/src/fingerprint/admin.py
@@ -0,0 +1,302 @@
+from __future__ import annotations
+
+from collections.abc import Iterator
+from datetime import timedelta
+from functools import wraps
+from itertools import chain, islice
+from typing import Callable
+
+from django.contrib import admin
+from django.db.models import Count, Max, Prefetch, Q
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.timezone import now
+
+from .models import BrowserFingerprint, RequestFingerprint, UserFingerprint, UserSession
+
+try:
+    from itertools import pairwise
+except ImportError:
+
+    def pairwise(items):  # type: ignore
+        return zip(items, items[1:])
+
+
+class html_objects_list:
+    def __init__(self, format_string: str, max_items: int = 10):
+        self.format_string = format_string
+        self.max_items = max_items
+
+    def __call__(self, fn: Callable) -> Callable:
+        @wraps(fn)
+        def wrapped(*args, **kwargs):
+            results = list(islice(fn(*args, **kwargs), self.max_items))
+            return format_html("<br>".join(self.format_string for _ in results), *chain.from_iterable(results))
+
+        return wrapped
+
+
+@admin.register(UserSession)
+class UserSessionAdmin(admin.ModelAdmin):
+    list_display = (
+        "session_key",
+        "user",
+        "browser_fingerprints",
+        "request_fingerprints",
+        "created",
+    )
+    list_filter = ("created",)
+    search_fields = (
+        "user__username",
+        "session_key",
+    )
+    ordering = ("-created",)
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .select_related("user")
+            .prefetch_related(
+                Prefetch(
+                    "browserfingerprints",
+                    queryset=BrowserFingerprint.objects.order_by("visitor_id", "-created").distinct("visitor_id"),
+                ),
+                Prefetch(
+                    "requestfingerprints",
+                    queryset=RequestFingerprint.objects.order_by("user_session", "user_agent", "-created").distinct(
+                        "user_session", "user_agent"
+                    ),
+                ),
+            )
+        )
+
+    @html_objects_list('{} &nbsp;&nbsp; <a href="{}"><code>{}</code></a>')
+    def browser_fingerprints(self, instance) -> Iterator[tuple]:
+        for fingerprint in instance.browserfingerprints.all():
+            yield (
+                fingerprint.created.date(),
+                reverse("admin:fingerprint_browserfingerprint_changelist") + f"?user_session={instance.id}",
+                fingerprint.get_value_display(),
+            )
+
+    @html_objects_list('{} &nbsp;&nbsp; <a href="{}"><code>{}</code></a>')
+    def request_fingerprints(self, instance) -> Iterator[tuple]:
+        for fingerprint in instance.requestfingerprints.all():
+            yield (
+                fingerprint.created.date(),
+                reverse("admin:fingerprint_requestfingerprint_changelist") + f"?user_session={instance.id}",
+                fingerprint.get_value_display(),
+            )
+
+
+class FingerprintBaseAdmin(admin.ModelAdmin):
+    list_display: tuple[str, ...] = (
+        "id",
+        "user_session",
+        "get_user",
+    )
+    list_filter: tuple[str, ...] = ("created",)
+    ordering: tuple[str, ...] = ("-created",)
+    search_fields: tuple[str, ...] = (
+        "user_session__session_key",
+        "user_session__user__email",
+        "user_session__user__username",
+    )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("user_session__user")
+
+    @admin.display(description="user")
+    def get_user(self, instance):
+        return instance.user_session.user
+
+
+@admin.register(BrowserFingerprint)
+class BrowserFingerprintAdmin(FingerprintBaseAdmin):
+    list_display = (
+        *FingerprintBaseAdmin.list_display,
+        "visitor_id",
+        "created",
+    )
+    search_fields = (
+        "visitor_id",
+        *FingerprintBaseAdmin.search_fields,
+    )
+
+
+@admin.register(RequestFingerprint)
+class RequestFingerprintAdmin(FingerprintBaseAdmin):
+    list_display = (
+        *FingerprintBaseAdmin.list_display,
+        "ip",
+        "user_agent",
+        "created",
+    )
+
+    search_fields = (
+        "ip",
+        "user_agent",
+        "accept",
+        "content_encoding",
+        "content_language",
+        *FingerprintBaseAdmin.search_fields,
+    )
+
+
+class NumFingerprintsListFilter(admin.SimpleListFilter):
+    groups = list(pairwise((1, 4, 6, 11, 21, 10_001)))
+    query_parameter: str
+
+    def lookups(self, request, model_admin):
+        return [(i, f"{min_}-{max_ - 1}") for i, (min_, max_) in enumerate(self.groups)]
+
+    def queryset(self, request, queryset):
+        if not (value := self.value()):
+            return
+
+        try:
+            min_, max_ = self.groups[int(value)]
+        except (KeyError, ValueError):
+            return
+
+        return queryset.filter(
+            **{
+                f"{self.query_parameter}__gte": min_,
+                f"{self.query_parameter}__lt": max_,
+            }
+        )
+
+
+class NumBrowserFingerprintsListFilter(NumFingerprintsListFilter):
+    title = "unique browser fingerprints"
+    parameter_name = "num_browser_fingerprints_group"
+    query_parameter = "num_browser_fingerprints"
+
+
+class NumRequestFingerprintsListFilter(NumFingerprintsListFilter):
+    title = "unique request fingerprints"
+    parameter_name = "num_request_fingerprints_group"
+    query_parameter = "num_request_fingerprints"
+
+
+class LastFingerprintCreatedListFilter(admin.SimpleListFilter):
+    title = "last fingerprint capture"
+    parameter_name = "fingerprint_captured_days"
+
+    def lookups(self, request, model_admin):
+        return [
+            (1, "1 day"),
+            (7, "1 week"),
+            (30, "1 month"),
+            (90, "3 months"),
+            (365, "12 months"),
+        ]
+
+    def queryset(self, request, queryset):
+        if not (value := self.value()):
+            return
+
+        try:
+            days = timedelta(days=int(value))
+        except ValueError:
+            return
+
+        since = now() - days
+        return queryset.annotate(
+            last_browser_fingerprint=Max("sessions__browserfingerprints__created"),
+            last_request_fingerprint=Max("sessions__requestfingerprints__created"),
+        ).filter(Q(last_browser_fingerprint__gte=since) | Q(last_request_fingerprint__gte=since))
+
+
+@admin.register(UserFingerprint)
+class UserFingerprintAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "sessions",
+        "num_browser_fingerprints",
+        "browser_fingerprints",
+        "num_request_fingerprints",
+        "request_fingerprints",
+    )
+    list_filter = (
+        LastFingerprintCreatedListFilter,
+        NumBrowserFingerprintsListFilter,
+        NumRequestFingerprintsListFilter,
+    )
+    search_fields = (
+        "username",
+        "email",
+    )
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .prefetch_related(
+                Prefetch("sessions", queryset=UserSession.objects.all().order_by("-created")),
+                Prefetch(
+                    "sessions__browserfingerprints",
+                    queryset=BrowserFingerprint.objects.order_by("visitor_id", "-created").distinct("visitor_id"),
+                ),
+                Prefetch(
+                    "sessions__requestfingerprints",
+                    queryset=RequestFingerprint.objects.order_by("user_session", "user_agent", "-created").distinct(
+                        "user_session", "user_agent"
+                    ),
+                ),
+            )
+            .annotate(
+                num_browser_fingerprints=Count("sessions__browserfingerprints__visitor_id", distinct=True),
+                num_request_fingerprints=Count("sessions__requestfingerprints__user_agent", distinct=True),
+            )
+        )
+
+    @admin.display(description="#")
+    def num_browser_fingerprints(self, instance):
+        return format_html(
+            '<a href="{}">{}</a>',
+            reverse("admin:fingerprint_browserfingerprint_changelist") + f"?q={instance.email}",
+            instance.num_browser_fingerprints,
+        )
+
+    @admin.display(description="#")
+    def num_request_fingerprints(self, instance):
+        return format_html(
+            '<a href="{}">{}</a>',
+            reverse("admin:fingerprint_requestfingerprint_changelist") + f"?q={instance.email}",
+            instance.num_request_fingerprints,
+        )
+
+    @admin.display(description="last sessions")
+    @html_objects_list('{} &nbsp;&nbsp; <a href="{}"><code>{}</code></a>')
+    def sessions(self, instance) -> Iterator[tuple]:
+        for session in instance.sessions.all():
+            yield (
+                session.created.date(),
+                reverse("admin:fingerprint_usersession_changelist") + f"?id={instance.id}",
+                session.get_value_display(),
+            )
+
+    @admin.display(description="last browsers")
+    @html_objects_list('{} &nbsp;&nbsp; <a href="{}"><code>{}</code></a>')
+    def browser_fingerprints(self, instance) -> Iterator[tuple]:
+        for session in instance.sessions.all():
+            for fingerprint in session.browserfingerprints.all():
+                yield (
+                    fingerprint.created.date(),
+                    reverse("admin:fingerprint_browserfingerprint_changelist") + f"?id={fingerprint.id}",
+                    fingerprint.get_value_display(),
+                )
+
+    @admin.display(description="last requests")
+    @html_objects_list('{} &nbsp;&nbsp; <a href="{}"><code>{}</code></a>')
+    def request_fingerprints(self, instance) -> Iterator[tuple]:
+        for session in instance.sessions.all():
+            for fingerprint in session.requestfingerprints.all():
+                yield (
+                    fingerprint.created.date(),
+                    reverse("admin:fingerprint_requestfingerprint_changelist") + f"?id={fingerprint.id}",
+                    fingerprint.get_value_display(),
+                )
---
+++ packages/django-fingerprint-rt/django_fingerprint_rt-0.2.0.tar.gz/django_fingerprint_rt-0.2.0/src/fingerprint/views.py
@@ -0,0 +1,159 @@
+from contextlib import suppress
+from datetime import timedelta
+from functools import wraps
+from logging import getLogger
+from typing import Optional
+
+from django.conf import settings
+from django.core.exceptions import BadRequest, DisallowedRedirect
+from django.db import transaction
+from django.http.response import HttpResponse
+from django.utils.datastructures import MultiValueDictKeyError
+from django.utils.decorators import method_decorator
+from django.utils.encoding import iri_to_uri
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.utils.timezone import now
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
+from ipware import get_client_ip
+
+from .models import BrowserFingerprint, RequestFingerprint, Url, UserSession
+
+log = getLogger(__name__)
+
+UTM_PARAMS = (
+    "utm_source",
+    "utm_campaign",
+    "utm_term",
+    "utm_medium",
+    "utm_content",
+)
+
+
+def get_or_create_session_key(request) -> str:
+    if not request.session or not request.session.session_key:
+        request.session.create()
+    return request.session.session_key
+
+
+def fingerprint(fn):
+    """A decorator which creates a backend Fingerprint object for the current request."""
+
+    max_length = {
+        field_name: RequestFingerprint._meta.get_field(field_name).max_length
+        for field_name in ("user_agent", "accept", "content_encoding", "content_language", "referer", "cf_ipcountry")
+    }
+    max_length["url"] = Url._meta.get_field("value").max_length
+
+    @wraps(fn)
+    def wrapper(request, *args, **kwargs):
+        session_key = get_or_create_session_key(request)
+        debounce_period = getattr(settings, "FINGERPRINT_DEBOUNCE_PERIOD", timedelta(seconds=10))
+
+        url_value = request.build_absolute_uri()[: max_length["url"]]
+        url, _ = Url.objects.get_get_or_create(value=url_value)
+
+        referer = request.META.get("HTTP_REFERER", "")[: max_length["referer"]]
+
+        session_defaults = {param: request.GET.get(param, "") for param in UTM_PARAMS}
+        session_defaults["referer"] = referer
+
+        with suppress(RequestFingerprint.MultipleObjectsReturned), transaction.atomic():
+            fingerprint, created = RequestFingerprint.objects.get_or_create(
+                user_session=UserSession.objects.get_or_create(session_key=session_key, defaults=session_defaults)[0],
+                url=url,
+                created__gte=now() - debounce_period,
+                defaults=dict(
+                    ip=get_client_ip(request)[0],
+                    user_agent=request.META.get("HTTP_USER_AGENT", "")[: max_length["user_agent"]],
+                    accept=request.META.get("HTTP_ACCEPT", "")[: max_length["accept"]],
+                    content_encoding=request.META.get("HTTP_CONTENT_ENCODING", "")[: max_length["content_encoding"]],
+                    content_language=request.META.get("HTTP_CONTENT_LANGUAGE", "")[: max_length["content_language"]],
+                    referer=referer,
+                    cf_ipcountry=request.META.get("HTTP_CF_IPCOUNTRY", "")[: max_length["cf_ipcountry"]],
+                ),
+            )
+            log.debug("Fingerprint %s, created=%s", fingerprint, created)
+        return fn(request, *args, **kwargs)
+
+    return wrapper
+
+
+def remember_user_session(fn):
+    """
+    A decorator to match a user to a session.
+
+    When database session backend is used, this is not necessary, as UserSession creation
+    is triggered by Session's post_save signal. However, with other session backends this
+    signal may not be triggered, thus we decorate some view with this decorator, and it
+    remembers UserSession if user is logged in.
+    """
+
+    @wraps(fn)
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            UserSession.objects.update_or_create(
+                session_key=get_or_create_session_key(request),
+                defaults=dict(user=request.user),
+            )
+        return fn(request, *args, **kwargs)
+
+    return wrapper
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+@method_decorator(remember_user_session, name="get")
+@method_decorator(fingerprint, name="get")
+class FingerprintView(TemplateView):
+    """
+    Multi-purpose fingerprinting view.
+
+    If user makes a GET request to this page, a page with both a server and browser fingerprinting is displayed.
+    Server fingerprinting is done when processing GET request in the view (by using @fingerprint decorator),
+    and browser fingerprinting is done by javascript code in the template. Javascript code calculates visitor_id
+    based on browser fingerprint, and then sends it to this view via POST request.
+
+    If there is `next` parameter in the query string, user is redirected to that page after fingerprinting. Otherwise
+    user will be redirected to main page.
+
+    If user sends POST request to this page, it is assumed that the request is coming from javascript code and
+    contains `id` post parameter, which is effectively a browser fingerprint (visitor_id).
+
+    If redirect to other domains is required, then this view should be subclassed and
+    `allowed_hosts` class variable should contain a set of allowed domains, say, `{'google.com'}`.
+    By default only relative paths are allowed.
+    """
+
+    template_name = "fingerprint/fingerprint.html"
+    redirect_in = timedelta(seconds=3)
+    allowed_hosts: Optional[set] = None
+
+    def get_context_data(self, **kwargs):
+        redirect_url = iri_to_uri(self.request.GET.get("next", "/"))
+        if not url_has_allowed_host_and_scheme(redirect_url, self.allowed_hosts):
+            raise DisallowedRedirect()
+
+        return {
+            "redirect_in": self.redirect_in,
+            "redirect_url": redirect_url,
+        }
+
+    def post(self, request, *args, **kwargs):
+        try:
+            visitor_id = request.POST["id"]
+        except MultiValueDictKeyError:
+            raise BadRequest()
+
+        session_key = get_or_create_session_key(request)
+
+        url_max_length = Url._meta.get_field("value").max_length
+        url_value = request.META.get("HTTP_REFERER", "")[:url_max_length]
+        url, _ = Url.objects.get_get_or_create(value=url_value)
+
+        with transaction.atomic():
+            BrowserFingerprint.objects.create(
+                user_session=UserSession.objects.get_or_create(session_key=session_key)[0],
+                url=url,
+                visitor_id=visitor_id,
+            )
+        return HttpResponse(status=200)
