224266fd8ae1743db09fbee1cc45643ec0c80eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:26 +0000

    Add package jaxsim/0.6.2.dev194/jaxsim-0.6.2.dev194-py3-none-any.whl

---
+++ packages/jaxsim/jaxsim-0.6.2.dev194-py3-none-any.whl/jaxsim/rbda/contacts/__init__.py
@@ -0,0 +1,5 @@
+from . import relaxed_rigid
+from .common import ContactModel, ContactsParams
+from .relaxed_rigid import RelaxedRigidContacts, RelaxedRigidContactsParams
+
+ContactParamsTypes = RelaxedRigidContactsParams
