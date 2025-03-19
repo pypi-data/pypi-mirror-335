ae995cad1cc110f449f8d89cc73f39da5e701fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:34 +0000

    Add package gps-toolkit-py/0.1.20/gps_toolkit_py-0.1.20.tar.gz

---
+++ packages/gps-toolkit-py/gps_toolkit_py-0.1.20.tar.gz/gps_toolkit_py-0.1.20/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+# This setup.py is a thin wrapper around the configuration in pyproject.toml
+# We keep it for compatibility with older tools
+
+setup(
+    name="gps-toolkit-py",
+    version="0.1.20",
+    description="A Python package for working with GPS coordinates",
+    author="GPS Tools Team",
+    author_email="example@example.com",
+    url="https://github.com/username/gps-toolkit-py",
+    package_dir={"": "src"},
+    packages=find_packages(where="src", exclude=["tests*", "examples*", "documentation*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pyproj",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-cov",
+            "black",
+            "flake8",
+            "mypy",
+            "pre-commit",
+        ],
+        "viz": [
+            "matplotlib",
+            "folium",
+        ],
+        "gis": [
+            "shapely",
+            "geopandas",
+        ],
+    },
+) 
