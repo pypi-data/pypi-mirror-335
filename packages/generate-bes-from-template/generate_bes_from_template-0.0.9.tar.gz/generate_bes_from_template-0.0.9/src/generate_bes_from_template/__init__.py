363d761c2abaf2106f82a0b9dceb8a1c9a5d5aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:56 +0000

    Add package generate-bes-from-template/0.0.9/generate_bes_from_template-0.0.9.tar.gz

---
+++ packages/generate-bes-from-template/generate_bes_from_template-0.0.9.tar.gz/generate_bes_from_template-0.0.9/src/generate_bes_from_template/__init__.py
@@ -0,0 +1,9 @@
+"""
+import functions so they can be run as the module
+"""
+
+from . import (
+    action_createfile_from_file,
+    action_prefetch_from_template,
+    generate_bes_from_template,
+)
