887efba5c72705fa0d2e04f30eae5f2ff8d8c6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:29 +0000

    Add package dseagull/0.0.28/dseagull-0.0.28.tar.gz

---
+++ packages/dseagull/dseagull-0.0.28.tar.gz/dseagull-0.0.28/tests/test_checks.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.test import TestCase, override_settings
+
+from dseagull.checks import jwt_check
+
+
+class TestChecks(TestCase):
+
+    @override_settings(JWT_KEY=None, JWT_EXP=None)
+    def test_pagination_settings(self):
+        self.assertIsNone(settings.JWT_KEY)
+        self.assertIsNone(settings.JWT_EXP)
+
+        errors = jwt_check(app_configs=None)
+        errors = [error.msg for error in errors]
+        self.assertEqual(3, len(errors))
+        self.assertIn('请配置 jwt 的加密秘钥 JWT_KEY', errors[0])
+        self.assertIn('请配置 jwt 的过期时间(单位秒) JWT_EXP', errors[1])
+        self.assertIn('请配置 DJANGO_REQUEST_ERROR_WEBHOOK', errors[2])
+
+    @override_settings(DJANGO_REQUEST_ERROR_WEBHOOK="test")
+    def test_django_request_error_webhook(self):
+        errors = jwt_check(app_configs=None)
+        errors = [error.msg for error in errors]
+        self.assertEqual(0, len(errors))
+        self.assertIn("django.request", settings.LOGGING['loggers'])
