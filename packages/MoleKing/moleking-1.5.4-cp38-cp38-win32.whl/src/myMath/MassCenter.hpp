8a6bb25ca5807d7b11212bebf76bf6aafb5e0861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:12:54 +0000

    Add package MoleKing/1.5.4/moleking-1.5.4-cp38-cp38-win32.whl

---
+++ packages/MoleKing/moleking-1.5.4-cp38-cp38-win32.whl/src/myMath/MassCenter.hpp
@@ -0,0 +1,37 @@
+//   MoleKing //
+//
+//   File:        [MassCenter.hpp]
+//
+//   Author(s):   ['LEEDMOL Reserch Group']
+//   Site(s):     ['https://www.researchgate.net/lab/LEEDMOL-Heibbe-Cristhians-Research-Group-Heibbe-Cristhian-B-De-Oliveira']
+//   Email(s):    ['heibbe@ufg.br']
+//   Credits:     ['Copyright © 2023 LEEDMOL. All rights reserved.']
+//   Date:        ['17.01.2023']
+//   Version:     ['1.5.4']
+//   Status:      ['Development']
+//   Language:    ['C++','Python']
+//   Description: ['A python module written in C++ for theoretical chemestry']
+
+
+#ifndef MassCenter_hpp
+#define MassCenter_hpp
+#include <stdio.h>
+#include <vector>
+#include "Geometry.hpp"
+
+using namespace std;
+
+class MassCenter{
+    private:
+    vector <double> massList;
+    Point massCenterPoint;
+    double axisMassCenter(vector <double> coords);
+
+    public:
+    MassCenter(vector <double> massList, vector <double> xCoords, vector <double> yCoords, vector <double> zCoords);
+    ~MassCenter();
+    Point getMassCenter();
+};
+
+
+#endif /* MassCenter_hpp */
