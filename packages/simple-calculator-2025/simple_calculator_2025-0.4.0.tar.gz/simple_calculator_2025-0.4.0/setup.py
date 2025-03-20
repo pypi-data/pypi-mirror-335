a2bcfe0c2b6bb13729152702939b81e036b0ebd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:06:10 +0000

    Add package simple-calculator-2025/0.4.0/simple_calculator_2025-0.4.0.tar.gz

---
+++ packages/simple-calculator-2025/simple_calculator_2025-0.4.0.tar.gz/simple_calculator_2025-0.4.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simple-calculator-2025",
+    version="0.4.0",
+    author="Ganesan Selvaraj",
+    author_email="ganesanluna@yahoo.in",
+    description="A simple calculator package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
