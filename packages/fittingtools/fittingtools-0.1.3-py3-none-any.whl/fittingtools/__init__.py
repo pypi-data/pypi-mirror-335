b8c74285a50a01c2ca2004ea9c067bad75fac28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:14 +0000

    Add package fittingtools/0.1.3/fittingtools-0.1.3-py3-none-any.whl

---
+++ packages/fittingtools/fittingtools-0.1.3-py3-none-any.whl/fittingtools/__init__.py
@@ -0,0 +1,6 @@
+"""
+Basic tools to generate confidence intervals and prediction confidence
+intervals based on `scipy.optimize.least_squares`
+"""
+__version__ = "0.1.3"
+__author__ = 'Erick Martinez Loran'
