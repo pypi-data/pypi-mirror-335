1a16477bb1dca649a5b325254e35745bfaec815e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:12:52 +0000

    Add package MoleKing/1.5.4/moleking-1.5.4-pp310-pypy310_pp73-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/MoleKing/moleking-1.5.4-pp310-pypy310_pp73-manylinux_2_17_i686.manylinux2014_i686.whl/src/berny/Hessian.hpp
@@ -0,0 +1,46 @@
+//   MoleKing //
+//
+//   File:        [Hessian.hpp]
+//
+//   Author(s):   ['LEEDMOL Reserch Group']
+//   Site(s):     ['https://www.researchgate.net/lab/LEEDMOL-Heibbe-Cristhians-Research-Group-Heibbe-Cristhian-B-De-Oliveira']
+//   Email(s):    ['heibbe@ufg.br']
+//   Credits:     ['Copyright © 2023 LEEDMOL. All rights reserved.']
+//   Date:        ['17.01.2023']
+//   Version:     ['1.5.4']
+//   Status:      ['Development']
+//   Language:    ['C++','Python']
+//   Description: ['A python module written in C++ for theoretical chemestry']
+
+
+#ifndef Hessian_hpp
+#define Hessian_hpp
+
+#include <stdio.h>
+#include "../myMath/Matrix.hpp"
+#include "../myMath/Geometry.hpp"
+#include <cmath>
+#include <vector>
+#include "../chemicalUnits/Molecule.hpp"
+#include "../chemicalUnits/AtomicScale.hpp"
+#include <iostream>
+
+using namespace std;
+
+class Hessian{
+    
+private:
+    Matrix bondHessian, angleHessian, dihedralHessian;
+    Molecule molecule;
+    
+public:
+    Hessian(Molecule molecule);
+    Matrix doInitialGuess();
+    double rho(vector <int> atoms);
+    double wAngle(vector <int> atoms);
+    double wDihedral(vector <int> atoms);
+    
+};
+
+
+#endif /* Hessian_hpp */
---
+++ packages/MoleKing/moleking-1.5.4-pp310-pypy310_pp73-manylinux_2_17_i686.manylinux2014_i686.whl/src/myMath/Matrix.hpp
@@ -0,0 +1,55 @@
+//   MoleKing //
+//
+//   File:        [Matrix.hpp]
+//
+//   Author(s):   ['LEEDMOL Reserch Group']
+//   Site(s):     ['https://www.researchgate.net/lab/LEEDMOL-Heibbe-Cristhians-Research-Group-Heibbe-Cristhian-B-De-Oliveira']
+//   Email(s):    ['heibbe@ufg.br']
+//   Credits:     ['Copyright © 2023 LEEDMOL. All rights reserved.']
+//   Date:        ['17.01.2023']
+//   Version:     ['1.5.4']
+//   Status:      ['Development']
+//   Language:    ['C++','Python']
+//   Description: ['A python module written in C++ for theoretical chemestry']
+
+
+#ifndef Matrix_hpp
+#define Matrix_hpp
+#include <cmath>
+#include <vector>
+//#include <../../include/Eigen/Core>
+
+
+#include <iostream>
+
+using namespace std;
+
+class Matrix{
+    private:
+    vector < vector <double> > matrix;
+    vector < vector <double> > getCofactor(vector< vector <double> > mat, int p, int q, long n);
+    double det(vector< vector <double> > mat, long n);
+
+    public:
+
+    //double eigenvalue();
+
+    Matrix(vector < vector <double> > matrix);
+    Matrix();
+    Matrix(int i, int j);
+    ~Matrix();
+    void setMatrix(vector < vector <double> > matrix);
+    Matrix sum(Matrix matrixB);
+    Matrix multiplication(double scalar);
+    vector < vector < double > > toVector();
+    Matrix multiplication(Matrix matrixB);
+    double determinant();
+    void replace(int i, int j, double newValue);
+    vector<double> getLine(int i);
+    vector <long> getDimensions();
+    double element(long i, long j);
+    void print();
+    string toStr();
+};
+
+#endif /* Matrix_hpp */
