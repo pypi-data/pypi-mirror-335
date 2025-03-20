87809c416eaf7538a3d7b640528940020cfa4116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:31 +0000

    Add package syncode/0.4.6/syncode-0.4.6.tar.gz

---
+++ packages/syncode/syncode-0.4.6.tar.gz/syncode-0.4.6/syncode/__init__.py
@@ -0,0 +1,6 @@
+from syncode.infer import Syncode
+from syncode.grammar_decoder import SyncodeLogitsProcessor
+from syncode.parsers.grammars import Grammar
+import syncode.common as common
+
+common.setup_logging()
