799a5bd96d1f516dfe739ccb525f08422aede77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:29 +0000

    Add package memory-foam/0.2.1/memory_foam-0.2.1.tar.gz

---
+++ packages/memory-foam/memory_foam-0.2.1.tar.gz/memory_foam-0.2.1/src/memory_foam/asyn.py
@@ -0,0 +1,38 @@
+import asyncio
+
+from fsspec.asyn import get_loop
+from typing import AsyncIterable, Awaitable, Iterator, TypeVar
+
+T = TypeVar("T")
+
+
+async def queue_task_result(
+    coro: Awaitable[T], queue: asyncio.Queue, loop=get_loop()
+) -> asyncio.Task:
+    task = asyncio.ensure_future(coro, loop=loop)
+    result = await task
+    await queue.put(result)
+    return task
+
+
+def sync_iter_async(
+    ait: AsyncIterable[T], loop: asyncio.AbstractEventLoop = get_loop()
+) -> Iterator[T]:
+    """Wrap an asynchronous iterator into a synchronous one"""
+
+    ait = ait.__aiter__()
+
+    # helper async fn that just gets the next element from the async iterator
+    async def get_next():
+        try:
+            obj = await ait.__anext__()
+            return False, obj
+        except StopAsyncIteration:
+            return True, None
+
+    # actual sync iterator
+    while True:
+        done, obj = asyncio.run_coroutine_threadsafe(get_next(), loop).result()
+        if done:
+            break
+        yield obj
