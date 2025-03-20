eabd59f68098b3806e7ca533e6b70a8d90353ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:05:37 +0000

    Add package yundownload/0.6.7/yundownload-0.6.7.tar.gz

---
+++ packages/yundownload/yundownload-0.6.7.tar.gz/yundownload-0.6.7/yundownload/utils/config.py
@@ -0,0 +1,11 @@
+DEFAULT_HEADERS = {
+    'User-Agent': 'Wget/1.12 (linux-gnu)',
+    'Content-Encoding': 'identity',
+    'Accept-Encoding': 'identity'
+}
+DEFAULT_CHUNK_SIZE = 1024 * 1024
+DEFAULT_SLICED_CHUNK_SIZE = 100 * 1024 * 1024
+DEFAULT_TIMEOUT = 60
+DEFAULT_MAX_RETRY = 3
+DEFAULT_RETRY_DELAY = 1
+DEFAULT_SLICED_FILE_SUFFIX = '.ydstf'
