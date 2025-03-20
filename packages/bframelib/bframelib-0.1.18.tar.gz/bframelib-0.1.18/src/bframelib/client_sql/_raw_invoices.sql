c1df25f51d7add15ea11fa38d843cfd722b8284f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:11 +0000

    Add package bframelib/0.1.18/bframelib-0.1.18.tar.gz

---
+++ packages/bframelib/bframelib-0.1.18.tar.gz/bframelib-0.1.18/src/bframelib/client_sql/_raw_invoices.sql
@@ -0,0 +1,8 @@
+SELECT *
+FROM src.invoices
+WHERE org_id = _BF_ORG_ID
+    AND env_id = _BF_ENV_ID
+    AND branch_id = _BF_BRANCH_ID
+    AND created_at <= _BF_SYSTEM_DT
+    AND started_at >= date_trunc('month', CAST(_BF_STORED_RATING_RANGE_START as TIMESTAMP))
+    AND started_at < _BF_STORED_RATING_RANGE_END
