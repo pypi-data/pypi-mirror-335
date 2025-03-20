65cff12746a037fd4d2698f71e9bcbe4e332ff74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 04:04:12 +0000

    Add package osbot-utils/2.48.0/osbot_utils-2.48.0-py3-none-any.whl

---
+++ packages/osbot-utils/osbot_utils-2.48.0-py3-none-any.whl/osbot_utils/helpers/llms/schemas/Schema__LLM_Request.py
@@ -0,0 +1,7 @@
+from osbot_utils.helpers.llms.schemas.Schema__LLM_Request__Data  import Schema__LLM_Request__Data
+from osbot_utils.type_safe.Type_Safe                             import Type_Safe
+
+
+class Schema__LLM_Request(Type_Safe):
+    #request_id   : Obj_Id                      # can't put this id here or we won't be able to cache this data
+    request_data : Schema__LLM_Request__Data
