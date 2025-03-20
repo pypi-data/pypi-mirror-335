4755259ed820aa01fd0d0b7686160a1478b980cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:12 +0000

    Add package gemini-webapi/1.9.1/gemini_webapi-1.9.1.tar.gz

---
+++ packages/gemini-webapi/gemini_webapi-1.9.1.tar.gz/gemini_webapi-1.9.1/src/gemini_webapi/utils/upload_file.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from httpx import AsyncClient
+from pydantic import validate_call
+
+from ..constants import Endpoint, Headers
+
+
+@validate_call
+async def upload_file(file: bytes | str | Path, proxy: str | None = None) -> str:
+    """
+    Upload a file to Google's server and return its identifier.
+
+    Parameters
+    ----------
+    file : `bytes` | `str` | `Path`
+        File data in bytes, or path to the file to be uploaded.
+    proxy: `str`, optional
+        Proxy URL.
+
+    Returns
+    -------
+    `str`
+        Identifier of the uploaded file.
+        E.g. "/contrib_service/ttl_1d/1709764705i7wdlyx3mdzndme3a767pluckv4flj"
+
+    Raises
+    ------
+    `httpx.HTTPStatusError`
+        If the upload request failed.
+    """
+
+    if not isinstance(file, bytes):
+        with open(file, "rb") as f:
+            file = f.read()
+
+    async with AsyncClient(http2=True, proxy=proxy) as client:
+        response = await client.post(
+            url=Endpoint.UPLOAD.value,
+            headers=Headers.UPLOAD.value,
+            files={"file": file},
+            follow_redirects=True,
+        )
+        response.raise_for_status()
+        return response.text
