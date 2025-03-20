5a0433d4c94dd6a4d7e5da27dc0c0778944869b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:05:26 +0000

    Add package bfg-llmutil/0.1.1/bfg_llmutil-0.1.1.tar.gz

---
+++ packages/bfg-llmutil/bfg_llmutil-0.1.1.tar.gz/bfg_llmutil-0.1.1/src/llmutil/__init__.py
@@ -0,0 +1,13 @@
+from .llm import chat, gen
+from .schema import gen_arr, gen_bool, gen_num, gen_obj, gen_schema, gen_str
+
+__all__ = [
+    "chat",
+    "gen",
+    "gen_arr",
+    "gen_bool",
+    "gen_num",
+    "gen_obj",
+    "gen_schema",
+    "gen_str",
+]
