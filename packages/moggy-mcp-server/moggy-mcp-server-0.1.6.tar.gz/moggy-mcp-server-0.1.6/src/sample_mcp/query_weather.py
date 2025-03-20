4e698a9d0557071816490e025929e0241618cbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:09 +0000

    Add package moggy-mcp-server/0.1.6/moggy-mcp-server-0.1.6.tar.gz

---
+++ packages/moggy-mcp-server/moggy-mcp-server-0.1.6.tar.gz/moggy-mcp-server-0.1.6/src/sample_mcp/query_weather.py
@@ -0,0 +1,26 @@
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+async def main():
+    """Main entry point for the application."""
+    await mcp.run_async()
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
