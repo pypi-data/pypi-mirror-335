68f1fc4abe007553f088f630c639d0a4ce33293c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 04:03:54 +0000

    Add package optimap/0.3.1/optimap-0.3.1-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/optimap/optimap-0.3.1-cp312-cp312-macosx_11_0_arm64.whl/optimap/video/_play.py
@@ -0,0 +1,319 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import animation
+from matplotlib.colors import Colormap
+
+from ..utils import deprecated, interactive_backend
+from ._export import iter_alpha_blend_videos
+from ._player import InteractivePlayer
+
+
+@deprecated("use optimap.show_video instead")
+def play(video, skip_frame=1, title="", vmin=None, vmax=None, cmap="gray", interval=10, **kwargs):
+    """Deprecated alias for :py:func:`optimap.video.show_video`."""
+    return show_video(video, skip_frame=skip_frame, title=title, vmin=vmin, vmax=vmax,
+                      cmap=cmap, interval=interval, **kwargs)
+
+
+@deprecated("use optimap.show_video_pair instead")
+def play2(video1, video2, skip_frame=1, title1="", title2="", vmin1=None, vmax1=None, vmin2=None, vmax2=None, cmap1="gray", cmap2="gray", interval=10, **kwargs):
+    """Deprecated alias for :py:func:`optimap.video.show_video_pair`."""
+    return show_video_pair(video1, video2, skip_frame=skip_frame, title1=title1, title2=title2,
+                           vmin1=vmin1, vmax1=vmax1, vmin2=vmin2, vmax2=vmax2, cmap1=cmap1, cmap2=cmap2,
+                           interval=interval, **kwargs)
+
+
+@deprecated("use optimap.show_videos instead")
+def playn(videos, skip_frame=1, titles=None, cmaps="gray", vmins=None, vmaxs=None, interval=10, **kwargs):
+    """Deprecated alias for :py:func:`optimap.video.show_videos`."""
+    return show_videos(videos, skip_frame=skip_frame, titles=titles, cmaps=cmaps, vmins=vmins, vmaxs=vmaxs, interval=interval, **kwargs)
+
+
+@deprecated("use optimap.show_videos instead")
+def play_with_overlay(*args, **kwargs):
+    """Deprecated alias for :py:func:`optimap.video.show_video_overlay`."""
+    return show_video_overlay(*args, **kwargs)
+
+
+def show_video(video, skip_frame=1, title="", vmin=None, vmax=None, cmap="gray", interval=10, **kwargs):
+    """Simple video player based on matplotlib's animate function.
+
+    See :py:func:`optimap.video.show_video_pair` for a player for two videos side-by-side,
+    and :py:func:`optimap.video.show_videos` for a player for `n` videos side-by-side.
+
+    .. note::
+        Using Monochrome is an alternative to this function, which allows for more control over the video player.
+
+        See :py:func:`monochrome.show_array` for more information.
+
+        >>> import monochrome as mc
+        >>> mc.show_array(video)
+
+    Parameters
+    ----------
+    video : {t, x, y} np.ndarray
+        Video to play.
+    skip_frame : int, optional
+        Skip every n-th frame, by default 1
+    title : str, optional
+        Title of the video, by default ""
+    vmin : float, optional
+        Minimum value for the colorbar, by default None
+    vmax : float, optional
+        Maximum value for the colorbar, by default None
+    cmap : str, optional
+        Colormap to use, by default "gray"
+    interval : int, optional
+        Delay between frames in ms, by default 10.
+        This is not the actual framerate, but the delay between frames.
+    **kwargs
+        Additional keyword arguments passed to :func:`matplotlib.pyplot.subplots`
+
+    Returns
+    -------
+    InteractivePlayer
+    """
+    return show_videos(
+        [video],
+        skip_frame=skip_frame,
+        titles=[title],
+        cmaps=[cmap],
+        vmins=[vmin],
+        vmaxs=[vmax],
+        interval=interval,
+        **kwargs,
+    )
+
+
+def show_video_pair(
+    video1,
+    video2,
+    skip_frame=1,
+    title1="video1",
+    title2="video2",
+    cmap1="gray",
+    cmap2="gray",
+    interval=10,
+    vmin1=None,
+    vmax1=None,
+    vmin2=None,
+    vmax2=None,
+    **kwargs,
+):
+    """Video player for two videos side-by-side based on matplotlib's animate function.
+
+    Parameters
+    ----------
+    video1 : {t, x, y} np.ndarray
+        First video to play.
+    video2 : {t, x, y} np.ndarray
+        Second video to play.
+    skip_frame : int, optional
+        Skip every n-th frame, by default 1
+    title1 : str, optional
+        Title of the first video, by default "video1"
+    title2 : str, optional
+        Title of the second video, by default "video2"
+    cmap1 : str, optional
+        Colormap to use for the first video, by default "gray"
+    cmap2 : str, optional
+        Colormap to use for the second video, by default "gray"
+    vmin1 : float, optional
+        Minimum value for the colorbar of the first video, by default None
+    vmax1 : float, optional
+        Maximum value for the colorbar of the first video, by default None
+    vmin2 : float, optional
+        Minimum value for the colorbar of the second video, by default None
+    vmax2 : float, optional
+        Maximum value for the colorbar of the second video, by default None
+    interval : int, optional
+        Delay between frames in ms, by default 10. This is not the actual framerate, but the delay between frames.
+    **kwargs
+        Additional keyword arguments passed to :func:`matplotlib.pyplot.subplots`
+
+    Returns
+    -------
+    InteractivePlayer
+    """
+    if vmin1 is None and vmin2 is None:
+        vmins = None
+    else:
+        vmins = [vmin1, vmin2]
+    if vmax1 is None and vmax2 is None:
+        vmaxs = None
+    else:
+        vmaxs = [vmax1, vmax2]
+    return show_videos([video1, video2],
+                 skip_frame=skip_frame,
+                 titles=[title1, title2],
+                 cmaps=[cmap1, cmap2],
+                 vmins=vmins,
+                 vmaxs=vmaxs,
+                 interval=interval,
+                 **kwargs)
+
+
+@interactive_backend
+def show_videos(videos, skip_frame=1, titles=None, cmaps="gray", vmins=None, vmaxs=None, interval=10, **kwargs):
+    """Video player for `n` side-by-side videos based on matplotlib's animate function.
+
+    Parameters
+    ----------
+    videos: list of {t, x, y} np.ndarray
+        Videos to play.
+    skip_frame : int, optional
+        Skip every n-th frame, by default 1
+    titles : list of str, optional
+        Titles of the videos, by default None
+    cmaps : str or list of str, optional
+        Colormaps to use for the videos, by default "gray"
+    vmins : float or list of floats, optional
+        Minimum values for the colorbars, by default None
+    vmaxs : float or list of floats, optional
+        Maximum values for the colorbars, by default None
+    interval : int, optional
+        Delay between frames in ms, by default 10. This is not the actual framerate, but the delay between frames.
+    **kwargs
+        Additional keyword arguments passed to :func:`matplotlib.pyplot.subplots`
+
+    Returns
+    -------
+    InteractivePlayer
+    """
+    n = len(videos)
+    nt = len(videos[0])
+    for i in range(n):
+        if videos[i].ndim < 3 or videos[i].ndim > 4:
+            msg = f"Video {i} is not a video, it has shape {videos[i].shape}! Use show_image or show_traces instead."
+            raise ValueError(msg)
+        if len(videos[i]) < nt:
+            msg = "videos have to be same length!"
+            raise ValueError(msg)
+
+    if titles is None:
+        titles = [f"Video {i}" for i in range(n)]
+    if isinstance(cmaps, (Colormap, str)):
+        cmaps = [cmaps for i in range(n)]
+
+    if vmins is None:
+        vmins = [np.nanmin(videos[i][0]) for i in range(n)]
+    elif isinstance(vmins, (int, float)):
+        vmins = [vmins for _ in range(n)]
+    if vmaxs is None:
+        vmaxs = [np.nanmax(videos[i][0]) for i in range(n)]
+    elif isinstance(vmaxs, (int, float)):
+        vmaxs = [vmaxs for _ in range(n)]
+
+    fig, axs = plt.subplots(1, n, **kwargs)
+    if n == 1:
+        axs = [axs]
+
+    # needed to set the correct spacing between GUI elements for Player
+    fig.suptitle("  ", font="monospace")
+
+    imshows = []
+    for i in range(n):
+        imshows.append(
+            axs[i].imshow(videos[i][0], cmap=cmaps[i], interpolation="none", vmin=vmins[i], vmax=vmaxs[i])
+        )
+        axs[i].set_title(f"{titles[i]}")
+        axs[i].axis("off")
+    fig.tight_layout()
+
+    def update(i):
+        for j in range(n):
+            imshows[j].set_data(videos[j][i])
+
+    ani = InteractivePlayer(
+        fig=fig,
+        func=update,
+        start=0,
+        end=len(videos[0]),
+        step=skip_frame,
+        interval=interval,
+    )
+    plt.show(block=True)
+    return ani
+
+
+@interactive_backend
+def show_video_overlay(
+    base: np.ndarray,
+    overlay: np.ndarray,
+    alpha: np.ndarray = None,
+    skip_frames: int = 1,
+    cmap_base="gray",
+    cmap_overlay="Purples",
+    vmin_base=None,
+    vmax_base=None,
+    vmin_overlay=None,
+    vmax_overlay=None,
+    interval=10,
+    **kwargs,
+):
+    """Play a video with an overlay. See :func:`export_video_with_overlay` and :func:`iter_alpha_blend_videos` for more information.
+
+    Parameters
+    ----------
+    base : np.ndarray
+        Base video to play.
+    overlay : np.ndarray
+        Video to overlay on the base video.
+    alpha : np.ndarray, optional
+        Alpha channel for the overlay, by default None
+    skip_frames : int, optional
+        Show every n-th frame, by default 1
+    cmap_base : str or matplotlib.colors.Colormap, optional
+        Colormap to use for the base video, by default "gray"
+    cmap_overlay : str or matplotlib.colors.Colormap, optional
+        Colormap to use for the overlay video, by default "Purples"
+    vmin_base : float, optional
+        Minimum value for the colorbar of the base video, by default None
+    vmax_base : float, optional
+        Maximum value for the colorbar of the base video, by default None
+    vmin_overlay : float, optional
+        Minimum value for the colorbar of the overlay video, by default None
+    vmax_overlay : float, optional
+        Maximum value for the colorbar of the overlay video, by default None
+    interval : int, optional
+        Delay between frames in ms, by default 10. This is not the actual framerate, but the delay between frames.
+    **kwargs
+        Additional keyword arguments passed to :func:`matplotlib.pyplot.subplots`
+
+    Returns
+    -------
+    matplotlib.animation.FuncAnimation
+    """
+    fig, ax = plt.subplots(**kwargs)
+
+    suptitle = fig.suptitle(f"Frame {0:4d}", font="monospace")
+
+    imshow = ax.imshow(base[0], vmin=0, vmax=1)
+    ax.axis("off")
+    fig.tight_layout()
+
+    def update(frame):
+        i, frame = frame
+        imshow.set_data(frame)
+        suptitle.set_text(f"Frame {i*skip_frames:4d}")
+
+    ani = animation.FuncAnimation(
+        fig,
+        update,
+        frames=enumerate(iter_alpha_blend_videos(
+            base=base,
+            overlay=overlay,
+            alpha=alpha,
+            skip_frames=skip_frames,
+            cmap_base=cmap_base,
+            cmap_overlay=cmap_overlay,
+            vmin_base=vmin_base,
+            vmax_base=vmax_base,
+            vmin_overlay=vmin_overlay,
+            vmax_overlay=vmax_overlay,
+        )),
+        interval=interval,
+        save_count=base.shape[0],
+    )
+    plt.show(block=True)
+    return ani
---
+++ packages/optimap/optimap-0.3.1-cp312-cp312-macosx_11_0_arm64.whl/optimap/video/_player.py
@@ -0,0 +1,202 @@
+import matplotlib.widgets
+from matplotlib.animation import FuncAnimation
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
+
+
+class InteractivePlayer(FuncAnimation):
+    """Generic interactive looping player with play, pause, forward, backward, and slider controls based on :class:`matplotlib.animation.FuncAnimation`.
+
+    Calls a function at each frame to update the figure content with the current frame index. Can be used to create custom video players or other interactive animations, see :func:`optimap.show_video` as an example of how to use this class.
+
+    .. table:: **Keyboard Shortcuts**
+
+        ========================= ===========================
+        Key                       Action
+        ========================= ===========================
+        ``Space``                 Play/Pause
+        ``Left``                  Last frame
+        ``Right``                 Next frame
+        ``Up``                    Increase step size
+        ``Down``                  Decrease step size
+        ``q``                     Quit
+        ========================= ===========================
+
+
+    Example
+    -------
+
+    .. code-block:: python
+
+        import numpy as np
+        import matplotlib.pyplot as plt
+        from optimap.video import InteractivePlayer
+        fig, ax = plt.subplots()
+        x = np.linspace(0, 2*np.pi, 100)
+        line, = ax.plot(x, np.sin(x))
+        def update(i):
+            line.set_ydata(np.sin(x + i / 10))
+        player = InteractivePlayer(fig, update, end=len(x))
+        plt.show()
+
+    Parameters
+    ----------
+    fig : matplotlib.figure.Figure
+        Figure to plot on.
+    func : callable
+        Function to call at each frame. Must accept a single integer argument (frame index).
+    start : int, optional
+        Start frame, by default 0
+    end : int, optional
+        End frame (exclusive), by default 100
+    step : int, optional
+        Step size for forward and backward buttons, by default 1
+    interval : int, optional
+        Delay between frames in milliseconds, by default 25
+    gui_pos : tuple, optional
+        Position of the GUI elements (play, slider, etc.), by default (0.125, 0.92)
+    **kwargs
+        Additional keyword arguments passed to :class:`matplotlib.animation.FuncAnimation`
+    """
+
+    def __init__(
+        self,
+        fig,
+        func,
+        start=0,
+        end=100,
+        step=1,
+        interval=25,
+        gui_pos=(0.125, 0.92),
+        **kwargs,
+    ):
+        self.i = start - step
+        self.min = start
+        self.max = end
+        self.runs = True
+        self.forwards = True
+        self.step = step
+        self.fig = fig
+        self.func = func
+        self.saving = False
+        self.suptitle = fig.suptitle("  ", font="monospace")
+        fig.canvas.mpl_connect("key_press_event", self.on_key_press)
+
+        self.setup_gui(gui_pos)
+        FuncAnimation.__init__(
+            self,
+            self.fig,
+            self.update,
+            frames=self.play(),
+            cache_frame_data=False,
+            repeat=False,
+            interval=interval,
+            **kwargs,
+        )
+
+    def play(self):
+        while self.runs:
+            self.i = self.i + self.step
+            if self.i >= self.min and self.i < self.max:
+                yield self.i
+            else:
+                if self.saving:
+                    break
+                elif self.i >= self.max:
+                    yield self.min
+                else:
+                    yield self.min
+
+    def update(self, i):
+        self.slider.set_val(i)
+        if self.saving:
+            self.suptitle.set_text(f"Frame {i:4d}")
+
+    def set_pos(self, i):
+        self.i = int(self.slider.val)
+        self.func(self.i)
+
+    def toggle_play(self, event=None):
+        if self.runs:
+            self.runs = False
+            self.event_source.stop()
+            self.button_stop.label.set_text("▶")
+        else:
+            self.runs = True
+            self.event_source.start()
+            self.button_stop.label.set_text("■")
+        self.fig.canvas.draw_idle()
+
+    def one_forward(self, event=None):
+        self.onestep(True)
+
+    def one_backward(self, event=None):
+        self.onestep(False)
+
+    def onestep(self, forwards):
+        x = 2 * forwards - 1
+        self.step = abs(self.step) * x
+        self.i += x
+        if self.i >= self.max:
+            self.i = self.min
+        elif self.i < self.min:
+            self.i = self.max - 1
+        self.func(self.i)
+        self.slider.set_val(self.i)
+        self.fig.canvas.draw_idle()
+
+    def on_key_press(self, event):
+        if event.key == " ":
+            self.toggle_play()
+        elif event.key == "right":
+            self.one_forward()
+        elif event.key == "left":
+            self.one_backward()
+        elif event.key == "up":
+            self.step += 1
+        elif event.key == "down":
+            self.step -= 1
+
+    def setup_gui(self, pos):
+        self.ax_player = self.fig.add_axes([pos[0], pos[1], 0.64, 0.04])
+        divider = make_axes_locatable(self.ax_player)
+        self.ax_slider = divider.append_axes("right", size="500%", pad=0.07)
+        self.button_stop = matplotlib.widgets.Button(self.ax_player, label="■")
+        self.button_stop.on_clicked(self.toggle_play)
+        self.slider = matplotlib.widgets.Slider(
+            self.ax_slider, "", self.min, self.max - 1, valinit=self.min,
+        )
+        self.slider.on_changed(self.set_pos)
+
+    def save(self, *args, hide_slider=True, hide_buttons=True, hide_framecounter=False, **kwargs):
+        """Save the animation as a movie file, but hide the GUI buttons and slider.
+
+        Parameters
+        ----------
+        *args :
+            See FuncAnimation's :meth:`~matplotlib.animation.Animation.save` for arguments.
+        hide_slider : bool, optional
+            Hide the slider, by default True
+        hide_buttons : bool, optional
+            Hide the play/pause button, by default True
+        hide_framecounter : bool, optional
+            Hide the frame counter, by default False
+        **kwargs :
+            See FuncAnimation's :meth:`~matplotlib.animation.Animation.save` for arguments.
+        """
+        self.i = self.min - self.step
+        self.saving = True
+        self._save_count = (self.max - self.min) // self.step
+        self.ax_player.set_visible(not hide_buttons)
+        self.ax_slider.set_visible(not hide_slider)
+        self.suptitle.set_visible(not hide_framecounter)
+
+        super().save(*args, **kwargs)
+
+        self.saving = False
+        # reset frame generator, it's exhausted now
+        self._iter_gen = self.play
+        self._save_count = None
+
+        self.ax_player.set_visible(True)
+        self.ax_slider.set_visible(True)
+        self.suptitle.set_visible(True)
