ff7c1db20918b9d69cb033d33b65e61adc441307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:13:41 +0000

    Add package ifnumberequals123/0.0.3/ifnumberequals123-0.0.3-py3-none-any.whl

---
+++ packages/ifnumberequals123/ifnumberequals123-0.0.3-py3-none-any.whl/ifnumberequals123/__init__.py
@@ -0,0 +1,23 @@
+def CreateCode(amount: int, numberVariableName: str = "number", fileName: str = "mycode") -> str:
+    """
+    Creates the code and writes it to a file.
+
+    Parameters:
+    amount (int): Amount of if number == amount.
+    numberVariableName (str): The name of the variable that the number will have.
+    fileName (str): the name of the file (.py is added automatically).
+    
+    Returns:
+    str: The code.
+    """
+    finalCode = numberVariableName + " = int(input('input your number'))\n"
+    for i in range(0, amount + 1):
+        if i != amount:
+            finalCode += f"if {numberVariableName} == {i}:\n\tprint('Your number is {i}')\n"
+        else:
+            finalCode += f"if {numberVariableName} == {i}:\n\tprint('Your number is {i}')"
+    with open(fileName + ".py", "w") as file:
+        file.write(finalCode)
+    return finalCode
+
+CreateCode(3, "eee", "mahcode")
