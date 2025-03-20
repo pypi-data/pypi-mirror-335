ea1bf51cd1ada2fa9f35311bcc540a308bcba5de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:09 +0000

    Add package computer-gym/0.1.0/computer_gym-0.1.0.tar.gz

---
+++ packages/computer-gym/computer_gym-0.1.0.tar.gz/computer_gym-0.1.0/README.md
@@ -0,0 +1,17 @@
+# computer-gym
+
+A placeholder package for computer-gym.
+
+## Installation
+
+```bash
+pip install computer-gym
+```
+
+## Usage
+
+This is a placeholder package.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ packages/computer-gym/computer_gym-0.1.0.tar.gz/computer_gym-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="computer-gym",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Xiangyi Li",
+    author_email="xiangyi@benchflow.ai",
+    description="A placeholder package for computer-gym",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/placeholder/computer-gym",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+) 
