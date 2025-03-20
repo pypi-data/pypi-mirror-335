cdf5e114abea633c7b4a895c1bbdff1eefe5b15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:13 +0000

    Add package reques-smm/0.0.0.5/reques_smm-0.0.0.5.tar.gz

---
+++ packages/reques-smm/reques_smm-0.0.0.5.tar.gz/reques_smm-0.0.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='reques_smm',
+    version='0.0.0.5',
+    author='Mahdi Hasan Shuvo',
+    author_email='shuvobbhh@gmail.com',
+    description='A library for manipulating memory in a Windows process',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Ensure this matches your file format
+    url='https://github.com/Mahdi-hasan-shuvo/PyMemoryRW',  # Project homepage
+    project_urls={
+        'Homepage': 'https://github.com/Mahdi-hasan-shuvo/PyMemoryRW',
+        'Bug Reports': 'https://github.com/Mahdi-hasan-shuvo/PyMemoryRW/issues',
+        'Source': 'https://github.com/Mahdi-hasan-shuvo/PyMemoryRW',
+    },
+    packages=find_packages(),
+    install_requires=[
+        'some_dependency',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',  # Adjusted to your requirement
+    license='MIT',  # License type
+    keywords='memory manipulation Windows memory management process memory Windows API memory injection memory editing Windows process memory library process hacking Windows utilities memory access system programming memory tools Win32 API Python memory library process manipulation memory read/write Windows programming memory management library Python Windows API',
+)
