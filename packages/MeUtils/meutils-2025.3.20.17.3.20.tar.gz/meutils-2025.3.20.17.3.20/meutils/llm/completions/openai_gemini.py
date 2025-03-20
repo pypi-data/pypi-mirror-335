f7073c675a28e03bbe352498b8d89937c9636425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:18 +0000

    Add package MeUtils/2025.3.20.17.3.20/meutils-2025.3.20.17.3.20.tar.gz

---
+++ packages/MeUtils/meutils-2025.3.20.17.3.20.tar.gz/meutils-2025.3.20.17.3.20/meutils/llm/completions/openai_gemini.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : openai_gemini
+# @Time         : 2025/3/20 12:24
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+from meutils.pipe import *
+
+"https://api.aiguoguo199.com/v1/images/generations"
