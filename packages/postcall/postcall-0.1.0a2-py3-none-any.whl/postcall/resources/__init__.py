21a49bbfc960f16a0d3f5e36f8eaff61d8d59e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:12 +0000

    Add package postcall/0.1.0a2/postcall-0.1.0a2-py3-none-any.whl

---
+++ packages/postcall/postcall-0.1.0a2-py3-none-any.whl/postcall/resources/__init__.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .extract_details import (
+    ExtractDetailsResource,
+    AsyncExtractDetailsResource,
+    ExtractDetailsResourceWithRawResponse,
+    AsyncExtractDetailsResourceWithRawResponse,
+    ExtractDetailsResourceWithStreamingResponse,
+    AsyncExtractDetailsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "ExtractDetailsResource",
+    "AsyncExtractDetailsResource",
+    "ExtractDetailsResourceWithRawResponse",
+    "AsyncExtractDetailsResourceWithRawResponse",
+    "ExtractDetailsResourceWithStreamingResponse",
+    "AsyncExtractDetailsResourceWithStreamingResponse",
+]
