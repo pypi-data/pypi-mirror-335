1d1957a37d495e48fedcc8f4fd869faacf9c7de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:47 +0000

    Add package readwise-sdk/0.1.0a1/readwise_sdk-0.1.0a1.tar.gz

---
+++ packages/readwise-sdk/readwise_sdk-0.1.0a1.tar.gz/readwise_sdk-0.1.0a1/src/readwise_sdk/types/__init__.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .book_list_params import BookListParams as BookListParams
+from .book_list_response import BookListResponse as BookListResponse
+from .highlight_list_params import HighlightListParams as HighlightListParams
+from .highlight_list_response import HighlightListResponse as HighlightListResponse
