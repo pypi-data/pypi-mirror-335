844bcec1d5ce95e5044973f572ffa684093604ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:38 +0000

    Add package readwise-sdk/0.1.0a2/readwise_sdk-0.1.0a2-py3-none-any.whl

---
+++ packages/readwise-sdk/readwise_sdk-0.1.0a2-py3-none-any.whl/readwise_sdk/resources/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .auth import (
+    AuthResource,
+    AsyncAuthResource,
+    AuthResourceWithRawResponse,
+    AsyncAuthResourceWithRawResponse,
+    AuthResourceWithStreamingResponse,
+    AsyncAuthResourceWithStreamingResponse,
+)
+from .books import (
+    BooksResource,
+    AsyncBooksResource,
+    BooksResourceWithRawResponse,
+    AsyncBooksResourceWithRawResponse,
+    BooksResourceWithStreamingResponse,
+    AsyncBooksResourceWithStreamingResponse,
+)
+from .highlights import (
+    HighlightsResource,
+    AsyncHighlightsResource,
+    HighlightsResourceWithRawResponse,
+    AsyncHighlightsResourceWithRawResponse,
+    HighlightsResourceWithStreamingResponse,
+    AsyncHighlightsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "AuthResource",
+    "AsyncAuthResource",
+    "AuthResourceWithRawResponse",
+    "AsyncAuthResourceWithRawResponse",
+    "AuthResourceWithStreamingResponse",
+    "AsyncAuthResourceWithStreamingResponse",
+    "HighlightsResource",
+    "AsyncHighlightsResource",
+    "HighlightsResourceWithRawResponse",
+    "AsyncHighlightsResourceWithRawResponse",
+    "HighlightsResourceWithStreamingResponse",
+    "AsyncHighlightsResourceWithStreamingResponse",
+    "BooksResource",
+    "AsyncBooksResource",
+    "BooksResourceWithRawResponse",
+    "AsyncBooksResourceWithRawResponse",
+    "BooksResourceWithStreamingResponse",
+    "AsyncBooksResourceWithStreamingResponse",
+]
