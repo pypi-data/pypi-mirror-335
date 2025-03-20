be9095e7b87dab47189b1f39f3b11dcb1d21bb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:00 +0000

    Add package py-text-scan/0.1.0/py_text_scan-0.1.0.tar.gz

---
+++ packages/py-text-scan/py_text_scan-0.1.0.tar.gz/py_text_scan-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='py_text_scan',        # Package name as it will appear on PyPI
+    version='0.1.0',           # Version number
+    py_modules=['py_text_scan'], # List of module files
+    author='Sidvilas',
+    author_email='sdevilgv@gmail.com',
+    description='A UNKNOWN py_text_scan',
+    url='https://github.com/Sidvilas/py_text_scan_package',  # URL to your project (GitHub, etc.)
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
