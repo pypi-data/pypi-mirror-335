5c10536c3951652717baca6ec6a4872a3f51ac8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:45 +0000

    Add package lyricsgenius/3.3.1/lyricsgenius-3.3.1.tar.gz

---
+++ packages/lyricsgenius/lyricsgenius-3.3.1.tar.gz/lyricsgenius-3.3.1/lyricsgenius/names.py
@@ -0,0 +1,22 @@
+john = {"Hollyhock",
+"Real Food",
+"Bathwater",
+"Michigan July",
+"Receding Gum",
+"Drowning in Snake Oil",
+"Almost Joy",
+"Hummin’",
+"Sweet Routine"}
+
+phil = {"Hollyhock",
+"Receding Gum",
+"Half Feral Half Bourgeois",
+"Drowning in Snake Oil",
+"November Water",
+"When the Rabbits Come",
+"Hummin’",
+"Sweet Routine",
+"Linoleum Fields"}
+
+intersection = john.intersection(phil)
+print("\n".join(sorted(intersection)))
