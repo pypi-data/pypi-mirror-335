8e9c890865d27151b7fde21c3b7e84de828dfb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:09 +0000

    Add package lukoshkin-lego/0.0.23.post9/lukoshkin_lego-0.0.23.post9.tar.gz

---
+++ packages/lukoshkin-lego/lukoshkin_lego-0.0.23.post9.tar.gz/lukoshkin_lego-0.0.23.post9/lego/db/redshift/errors.py
@@ -0,0 +1,83 @@
+"""
+Exceptions raised during Redshift query execution with the RedshiftData API.
+
+
+Currently relies on definitions used in psycopg2.errors and includes only:
+- SyntaxError
+- AmbiguousColumn
+- UndefinedColumn
+- GroupingError
+- InternalError
+"""
+
+import re
+
+
+class RedshiftQueryAbortedError(Exception):
+    """Raise when a Redshift query was aborted"""
+
+
+class RedshiftDataError(Exception):
+    """Base class for RedshiftData errors"""
+
+    error_patterns = [
+        (
+            "UndefinedColumn",
+            re.compile(r"column .*? does not exist"),
+            "pattern",
+        ),
+        (
+            "AmbiguousColumn",
+            re.compile(r"column reference .*? is ambiguous"),
+            "pattern",
+        ),
+        (
+            "InternalError",
+            re.compile("internal_? ?error", re.IGNORECASE),
+            "pattern",
+        ),
+        ("SyntaxError", "syntax error at or near", "occurrence"),
+        ("GroupingError", "in the GROUP BY clause", "occurrence"),
+    ]
+
+    def __init__(self, desc: dict, orig: Exception | None = None):
+        self.desc = desc
+        self.message = desc.get("Error", "")
+        self.orig = orig or self.classify_error(self.message)
+        super().__init__(self.message)
+
+    def classify_error(self, message: str) -> "RedshiftDataError":
+        for error_type, pattern, kind in self.error_patterns:
+            if kind == "pattern" and pattern.search(message):
+                return globals()[error_type](self.desc)
+            if kind == "occurrence" and pattern in message:
+                return globals()[error_type](self.desc)
+        return UnknownError(self.desc, Exception(self.message))
+
+
+class UnknownError(RedshiftDataError):
+    """Raise when an unknown error occurs"""
+
+
+class SyntaxError(RedshiftDataError):
+    """Raise when a syntax error occurs"""
+
+
+class AmbiguousColumn(RedshiftDataError):
+    """Raise when a column reference is ambiguous"""
+
+
+class UndefinedColumn(RedshiftDataError):
+    """Raise when a column does not exist"""
+
+
+class GroupingError(RedshiftDataError):
+    """Raise when an error occurs in the GROUP BY clause"""
+
+
+class InternalError(RedshiftDataError):
+    """Raise when an internal error occurs"""
+
+
+# Just an alias for InternalError
+InternalError_ = InternalError
