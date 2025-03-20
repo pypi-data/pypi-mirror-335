32eb20d208de916aba4aeb18339bec917d0ee150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:06 +0000

    Add package Q-XenonSign/1.0.1/q_xenonsign-1.0.1.tar.gz

---
+++ packages/Q-XenonSign/q_xenonsign-1.0.1.tar.gz/q_xenonsign-1.0.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+from setuptools.extension import Extension
+import glob
+import os
+
+# Cython을 적용할 파일 찾기
+source_files = glob.glob("fourtest/*.py") + [r"C:\Users\ekgml\lim\QXenonSign\qxenonsign\core.c"]
+
+# 확장 모듈 설정
+extensions = [
+    Extension(
+        name=os.path.splitext(os.path.basename(file))[0],
+        sources=[file],
+    )
+    for file in source_files
+]
+
+setup(
+    name="Q-XenonSign",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "requests",
+        "pydantic",
+        "pyngrok",
+        "flask"
+        
+    ],
+    ext_modules=cythonize(extensions, language_level="3"),
+    author="Q-XenonSign",
+    author_email="qxenonsign@gmail.com",
+    description="Q-XenonSign library",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
