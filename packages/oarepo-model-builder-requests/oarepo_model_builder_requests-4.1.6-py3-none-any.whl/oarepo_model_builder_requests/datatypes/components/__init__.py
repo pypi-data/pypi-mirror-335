c16ed7792fd21b5ba65b93ce54ae4e5914803d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:05:27 +0000

    Add package oarepo-model-builder-requests/4.1.6/oarepo_model_builder_requests-4.1.6-py3-none-any.whl

---
+++ packages/oarepo-model-builder-requests/oarepo_model_builder_requests-4.1.6-py3-none-any.whl/oarepo_model_builder_requests/datatypes/components/__init__.py
@@ -0,0 +1,21 @@
+from .requests_model import (
+    RecordRequestsResourceComponent,
+    RecordRequestTypesResourceComponent,
+    RecordResolverComponent,
+    RequestsComponent,
+    RequestsRecordItemModelComponent,
+    RequestsTestComponent,
+    RequestsUIMarshmallowModelComponent,
+    UIRecordResolverComponent,
+)
+
+requests_components = [
+    RequestsComponent,
+    RecordResolverComponent,
+    RequestsTestComponent,
+    RequestsUIMarshmallowModelComponent,
+    UIRecordResolverComponent,
+    RequestsRecordItemModelComponent,
+    RecordRequestsResourceComponent,
+    RecordRequestTypesResourceComponent,
+]
---
+++ packages/oarepo-model-builder-requests/oarepo_model_builder_requests-4.1.6-py3-none-any.whl/oarepo_model_builder_requests/datatypes/components/requests_model/__init__.py
@@ -0,0 +1,21 @@
+from .record_item import RequestsRecordItemModelComponent
+from .requests import (
+    RecordRequestsResourceComponent,
+    RecordRequestTypesResourceComponent,
+    RequestsComponent,
+)
+from .resolver import RecordResolverComponent
+from .tests import RequestsTestComponent
+from .ui_marshmallow import RequestsUIMarshmallowModelComponent
+from .ui_resolver import UIRecordResolverComponent
+
+__all__ = [
+    "RequestsTestComponent",
+    "RequestsComponent",
+    "RecordResolverComponent",
+    "RequestsUIMarshmallowModelComponent",
+    "UIRecordResolverComponent",
+    "RequestsRecordItemModelComponent",
+    "RecordRequestsResourceComponent",
+    "RecordRequestTypesResourceComponent",
+]
