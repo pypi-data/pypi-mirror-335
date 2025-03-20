5719596c6fdde77f7272023a23bd1d8d7ab55e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:19 +0000

    Add package pydorm/0.1.6/pydorm-0.1.6-py3-none-any.whl

---
+++ packages/pydorm/pydorm-0.1.6-py3-none-any.whl/pydorm/__init__.py
@@ -0,0 +1,20 @@
+from ._dorm import dorm
+from ._initializer import init
+from ._insert import insert, insert_bulk, upsert, upsert_bulk
+from ._query import query, dict_query
+from ._update import update
+
+__author__ = 'melon'
+__version__ = '0.1.6'
+
+__all__ = [
+    'init',
+    'dorm',
+    'query',
+    'update',
+    'insert',
+    'insert_bulk',
+    'upsert',
+    'upsert_bulk',
+    'dict_query',
+]
