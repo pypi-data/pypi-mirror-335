302304c6ef79635e6e3f5091c2ce3dd6c689b5aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:19 +0000

    Add package deasy-client/0.1.0a9/deasy_client-0.1.0a9.tar.gz

---
+++ packages/deasy-client/deasy_client-0.1.0a9.tar.gz/deasy_client-0.1.0a9/src/deasy_client/types/suggest_hierarchy_create_params.py
@@ -0,0 +1,37 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List, Optional
+from typing_extensions import Required, TypedDict
+
+__all__ = ["SuggestHierarchyCreateParams"]
+
+
+class SuggestHierarchyCreateParams(TypedDict, total=False):
+    vdb_profile_name: Required[str]
+
+    condition: Optional["ConditionInputParam"]
+
+    context_level: Optional[str]
+
+    current_tree: Optional[object]
+
+    dataslice_id: Optional[str]
+
+    file_names: Optional[List[str]]
+
+    llm_profile_name: Optional[str]
+
+    max_height: Optional[int]
+
+    node: Optional[object]
+
+    use_existing_tags: Optional[bool]
+
+    use_extracted_tags: Optional[bool]
+
+    user_context: Optional[str]
+
+
+from .condition_input_param import ConditionInputParam
