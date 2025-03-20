b31942100e7f3552427a6d38d1d3b2ed95d3e0f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:14 +0000

    Add package pyAgrum-nightly/1.17.2.9.dev202503201739452835/pyAgrum_nightly-1.17.2.9.dev202503201739452835-cp310-cp310-win_amd64.whl

---
+++ packages/pyAgrum-nightly/pyAgrum_nightly-1.17.2.9.dev202503201739452835-cp310-cp310-win_amd64.whl/pyAgrum/causal/_types.py
@@ -0,0 +1,39 @@
+# (c) Copyright 2020-2024 by Pierre-Henri Wuillemin(@LIP6)  (pierre-henri.wuillemin@lip6.fr)
+# Permission to use, copy, modify, and distribute this
+# software and its documentation for any purpose and
+# without fee or royalty is hereby granted, provided
+# that the above copyright notice appear in all copies
+# and that both that copyright notice and this permission
+# notice appear in supporting documentation or portions
+# thereof, including modifications, that you make.
+# THE AUTHOR P.H. WUILLEMIN  DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, INDIRECT
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+# RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+# IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+# OR PERFORMANCE OF THIS SOFTWARE!
+
+"""
+This file defines the types for the causal package
+"""
+from typing import NewType
+from typing import Set, List, Tuple, Union
+
+# pylint: disable=unused-import
+import pyAgrum as gum
+import pyAgrum.causal  # for annotations
+
+DirectedModel = Union['pyAgrum.BayesNet', 'pyAgrum.DAG', 'pyAgrum.causal.CausalModel']
+
+NodeId = NewType("NodeId", int)
+NodeList = List[NodeId]
+NodeSet = Set[NodeId]
+
+ArcSet = Set[Tuple[NodeId, NodeId]]
+NameSet = Set[str]
+
+LatentDescriptor = Tuple[str, Tuple[str, str]]
+LatentDescriptorList = List[LatentDescriptor]
---
+++ packages/pyAgrum-nightly/pyAgrum_nightly-1.17.2.9.dev202503201739452835-cp310-cp310-win_amd64.whl/pyAgrum/causal/notebook.py
@@ -0,0 +1,138 @@
+# (c) Copyright 2020-2024 by Pierre-Henri Wuillemin(@LIP6)  (pierre-henri.wuillemin@lip6.fr)
+# Permission to use, copy, modify, and distribute this
+# software and its documentation for any purpose and
+# without fee or royalty is hereby granted, provided
+# that the above copyright notice appear in all copies
+# and that both that copyright notice and this permission
+# notice appear in supporting documentation or portions
+# thereof, including modifications, that you make.
+# THE AUTHOR P.H. WUILLEMIN  DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, INDIRECT
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+# RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+# IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+# OR PERFORMANCE OF THIS SOFTWARE!
+
+"""
+This file defines some helpers for handling causal concepts in notebooks
+"""
+from typing import Union, Optional, Dict, Tuple
+import IPython
+
+import pyAgrum
+import pyAgrum.lib.notebook as gnb
+import pyAgrum.causal as csl
+
+from pyAgrum.causal._types import NameSet
+
+
+def getCausalModel(cm: csl.CausalModel, size=None) -> str:
+  """
+  return a HTML representing the causal model
+
+  Parameters
+  ----------
+  cm: CausalModel
+    the causal model
+  size: int|str
+    the size of the rendered graph
+
+  Returns
+  -------
+  pydot.Dot
+    the dot representation
+  """
+  if size is None:
+    size = pyAgrum.config['causal', 'default_graph_size']
+  return gnb.getDot(cm.toDot(), size)
+
+
+def showCausalModel(cm: csl.CausalModel, size=None):
+  """
+  Shows a pydot svg representation of the causal DAG
+
+  Parameters
+  ----------
+  cm: CausalModel
+    the causal model
+  size: int|str
+    the size of the rendered graph
+  """
+  if size is None:
+    size = pyAgrum.config['causal', 'default_graph_size']
+  gnb.showDot(cm.toDot(), size=size)
+
+
+def getCausalImpact(model: csl.CausalModel, on: Union[str, NameSet], doing: Union[str, NameSet],
+                    knowing: Optional[NameSet] = None, values: Optional[Dict[str, int]] = None):
+  """
+  return a HTML representing of the three values defining a causal impact : formula, value, explanation
+
+  Parameters
+  ----------
+  model: CausalModel
+    the causal model
+  on: str | Set[str]
+    the impacted variable(s)
+  doing: str | Set[str]
+    the interventions
+  knowing: str | Set[str]
+    the observations
+  values: Dict[str,int] default=None
+    value for certain variables
+
+  Returns
+  -------
+  HTML
+  """
+  formula, impact, explanation = csl.causalImpact(model, on, doing, knowing, values)
+
+  gnb.flow.clear()
+  gnb.flow.add(getCausalModel(model), caption="Causal Model")
+
+  if formula is None:
+    gnb.flow.add(explanation, caption="Impossible")
+  else:
+    gnb.flow.add('$$\\begin{equation*}' + formula.toLatex() + '\\end{equation*}$$',
+                 caption="Explanation : " + explanation)
+
+  if formula is None:
+    res="No result"
+  else:
+    if impact.variable(0).domainSize()<5:
+      res=impact
+    else:
+      res=gnb.getProba(impact)
+  gnb.flow.add(res,caption="Impact")
+
+  return gnb.flow.html()
+
+
+def showCausalImpact(model: csl.CausalModel, on: Union[str, NameSet], doing: Union[str, NameSet],
+                     knowing: Optional[NameSet] = None, values: Optional[Dict[str, int]] = None):
+  """
+  display a HTML representing of the three values defining a causal impact :  formula, value, explanation
+
+  Parameters
+  ----------
+  model: CausalModel
+    the causal model
+  on: str | Set[str]
+    the impacted variable(s)
+  doing: str | Set[str]
+    the interventions
+  knowing: str | Set[str]
+    the observations
+  values: Dict[str,int] default=None
+    value for certain variables
+  """
+  html = getCausalImpact(model, on, doing, knowing, values)
+  IPython.display.display(html)
+
+
+csl.CausalModel._repr_html_ = lambda self: gnb.getDot(
+  self.toDot(), size=pyAgrum.config['causal', 'default_graph_size'])
+csl.CausalFormula._repr_html_ = lambda self: f"$${self.toLatex()}$$"
