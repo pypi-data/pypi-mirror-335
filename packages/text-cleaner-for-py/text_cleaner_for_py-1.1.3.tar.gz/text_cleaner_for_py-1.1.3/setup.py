149069678b758623672c30b4847a7390753193ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:12 +0000

    Add package text-cleaner-for-py/1.1.3/text_cleaner_for_py-1.1.3.tar.gz

---
+++ packages/text-cleaner-for-py/text_cleaner_for_py-1.1.3.tar.gz/text_cleaner_for_py-1.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="text_cleaner_for_py",
+    version="1.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "beautifulsoup4>=4.12.0",
+        "requests>=2.31.0",
+        "nltk",
+    ],
+    author="Roberto Lima",
+    author_email="robertolima.izphera@gmail.com",
+    description="🧹 Um pacote para limpeza e normalização de texto em Python.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/robertolima-dev/text-cleaner-for-py",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+    license="MIT",
+)
