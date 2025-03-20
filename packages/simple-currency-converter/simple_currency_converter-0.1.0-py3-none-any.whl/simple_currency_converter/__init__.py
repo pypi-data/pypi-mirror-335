2d0734d0f4122417bbf25027b85f8a68f0e3497a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:15 +0000

    Add package simple-currency-converter/0.1.0/simple_currency_converter-0.1.0-py3-none-any.whl

---
+++ packages/simple-currency-converter/simple_currency_converter-0.1.0-py3-none-any.whl/simple_currency_converter/__init__.py
@@ -0,0 +1,4 @@
+from .currency_codes import all_codes, common_codes, crypto_codes
+from .core import convert, get_currency_data
+
+__all__ = ["convert", "get_currency_data", "all_codes", "common_codes", "crypto_codes"]
