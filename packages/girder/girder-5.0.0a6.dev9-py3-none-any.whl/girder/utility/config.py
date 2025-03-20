2f339ee23cde6435996f92d0f258173baa9d5445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:12:38 +0000

    Add package girder/5.0.0a6.dev9/girder-5.0.0a6.dev9-py3-none-any.whl

---
+++ packages/girder/girder-5.0.0a6.dev9-py3-none-any.whl/girder/utility/config.py
@@ -0,0 +1,33 @@
+import cherrypy
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+
+def loadConfig():
+    # Calling this has the side effect of populating the cherrypy.config object with
+    # values from the environment. It is called at import time of the girder package.
+    cherrypy.config['server.socket_host'] = os.getenv('GIRDER_HOST', '127.0.0.1')
+    cherrypy.config['server.socket_port'] = int(os.getenv('GIRDER_PORT', 8080))
+    cherrypy.config['tools.proxy.on'] = True
+
+    if 'database' not in cherrypy.config:
+        cherrypy.config['database'] = {}
+
+    if 'GIRDER_TEST_DB' in os.environ:
+        cherrypy.config['database']['uri'] = os.environ['GIRDER_TEST_DB'].replace('.', '_')
+    else:
+        cherrypy.config['database']['uri'] = os.getenv(
+            'GIRDER_MONGO_URI', 'mongodb://localhost:27017/girder')
+
+
+def getConfig():
+    if 'database' not in cherrypy.config:
+        loadConfig()
+    # When in Sphinx, cherrypy may be mocked and returning None
+    return cherrypy.config or {}
+
+
+def getServerMode():
+    return getConfig()['server']['mode']
