15ad0ce8d9e44dd31b8ad795676f7b3c1011154d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:12:46 +0000

    Add package madcubapy/0.4.0/madcubapy-0.4.0-py3-none-any.whl

---
+++ packages/madcubapy/madcubapy-0.4.0-py3-none-any.whl/madcubapy/coordinates/__init__.py
@@ -0,0 +1,9 @@
+# madcubapy/coordinates/__init__.py
+
+"""
+The `madcubapy.coordinates` package provides functions to help the user
+work with coordinates and transformations.
+"""
+
+from .offsets import *
+from .transforms import *
