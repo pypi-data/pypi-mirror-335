65ecc524c9b22bbdb597d33a37becbdf7f2525a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 04:04:02 +0000

    Add package json-graph-traversal/0.1.0/json_graph_traversal-0.1.0-py3-none-any.whl

---
+++ packages/json-graph-traversal/json_graph_traversal-0.1.0-py3-none-any.whl/json_graph_traversal/__init__.py
@@ -0,0 +1,9 @@
+"""
+json_graph_traversal: A Python package to traverse nested JSON data structures 
+using BFS or DFS algorithms.
+"""
+
+from .traversal import JSONGraphTraversal
+
+__version__ = "0.1.0"
+__all__ = ['JSONGraphTraversal']
