2ee73c5a12d2d6fd3a6947429926fc6a3684714f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:35 +0000

    Add package modelmerge/1.0.21/modelmerge-1.0.21.tar.gz

---
+++ packages/modelmerge/modelmerge-1.0.21.tar.gz/modelmerge-1.0.21/setup.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+setup(
+    name="modelmerge",
+    version="1.0.21",
+    description="modelmerge is a multi-large language model API aggregator.",
+    long_description=Path.open(Path("README.md"), encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=Path.open(Path("requirements.txt"), encoding="utf-8").read().splitlines(),
+    include_package_data=True,
+)
