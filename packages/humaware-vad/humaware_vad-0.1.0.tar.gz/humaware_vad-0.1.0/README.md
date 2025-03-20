75e36988374e9aee6daeda5128a8b75e528aac9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:33 +0000

    Add package humaware-vad/0.1.0/humaware_vad-0.1.0.tar.gz

---
+++ packages/humaware-vad/humaware_vad-0.1.0.tar.gz/humaware_vad-0.1.0/README.md
@@ -0,0 +1,12 @@
+---
+title: HumAwareVAD-Whisper Realtime Transcription
+emoji: 👂
+colorFrom: purple
+colorTo: red
+sdk: gradio
+sdk_version: 5.16.0
+app_file: app.py
+pinned: false
+license: mit
+short_description: Transcribe audio in realtime with Whisper
+tags: [webrtc, websocket, gradio, secret|TWILIO_ACCOUNT_SID, secret|TWILIO_AUTH_TOKEN, secret|GROQ_API_KEY]
---
+++ packages/humaware-vad/humaware_vad-0.1.0.tar.gz/humaware_vad-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+# print(os.getcwd())
+def read_requirements():
+    req_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
+    if os.path.exists(req_file):
+        with open(req_file) as f:
+            return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+    return []
+setup(
+    name="humaware-vad",
+    version="0.1.0",
+    author="Sourabh Saini",
+    author_email="Sourabh72101@gmail.com",
+    description="HumAwareVAD: A optimized voice activity detection model to better distinguish humming and speech",
+    url="https://github.com/CuriousMonkey7/humaware-vad",  # Replace with actual URL
+    packages=find_packages(),
+    install_requires= read_requirements(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Multimedia :: Sound/Audio :: Speech",
+    ],
+    python_requires=">=3.12",
+)
