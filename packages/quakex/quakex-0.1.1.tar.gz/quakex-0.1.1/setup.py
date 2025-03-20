e537ce8d03fe8e9568b7165ebd54578ded7e31bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:13:00 +0000

    Add package quakex/0.1.1/quakex-0.1.1.tar.gz

---
+++ packages/quakex/quakex-0.1.1.tar.gz/quakex-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="QuakeX",
+    version="0.1.1",
+    long_description="",
+    long_description_content_type="text/markdown",
+    packages=["quakex"],
+    install_requires=["numpy",  "h5py", "matplotlib", "pandas"],
+)
