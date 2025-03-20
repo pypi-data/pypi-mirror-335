c5bcda9af15340d6603d737066a777956c6c5300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:06:05 +0000

    Add package liteauto/0.2.13/liteauto-0.2.13-py3-none-any.whl

---
+++ packages/liteauto/liteauto-0.2.13-py3-none-any.whl/liteauto/arxivlite/__init__.py
@@ -0,0 +1,6 @@
+from .main import get_todays_arxiv_papers,get_arxiv_papers
+from .analysis import research_paper_analysis
+from .schema import Paper, ArxivTags, CSTag, BaseArXivCSTags
+
+from .const import *
+from .utils import *
