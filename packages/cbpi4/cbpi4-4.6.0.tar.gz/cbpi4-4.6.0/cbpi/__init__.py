0ccc0806c8a8f6975ef016a4bbb7ba6dbc95cff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:30 +0000

    Add package cbpi4/4.6.0/cbpi4-4.6.0.tar.gz

---
+++ packages/cbpi4/cbpi4-4.6.0.tar.gz/cbpi4-4.6.0/cbpi/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "4.6.0"
+__codename__ = "Hop Master"
+
+
+# 4.6.0 - Hop Master
+# 4.5.0 - Cross Country
+# 4.4.0 - Yeast Starter
+# 4.3.0 - Winter Storm
+# 4.2.0 - Indian Summer
+# 4.1.0 - Groundhog Day
+# 4.0.7 - November Rain
+# 4.0.0 - Spring Break
