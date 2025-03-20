39fb00ecbb61fadae1fa62a4db2f50555b5ecb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:12:53 +0000

    Add package MoleKing/1.5.4/moleking-1.5.4-cp310-cp310-win32.whl

---
+++ packages/MoleKing/moleking-1.5.4-cp310-cp310-win32.whl/src/chemicalUnits/OPLSff.hpp
@@ -0,0 +1,43 @@
+//   MoleKing //
+//
+//   File:        [OPLSff.hpp]
+//
+//   Author(s):   ['LEEDMOL Reserch Group']
+//   Site(s):     ['https://www.researchgate.net/lab/LEEDMOL-Heibbe-Cristhians-Research-Group-Heibbe-Cristhian-B-De-Oliveira']
+//   Email(s):    ['heibbe@ufg.br']
+//   Credits:     ['Copyright © 2023 LEEDMOL. All rights reserved.']
+//   Date:        ['17.01.2023']
+//   Version:     ['1.5.4']
+//   Status:      ['Development']
+//   Language:    ['C++','Python']
+//   Description: ['A python module written in C++ for theoretical chemestry']
+
+
+#ifndef OPLSff_hpp
+#define OPLSff_hpp
+
+#include <stdio.h>
+#include <map>
+#include <iterator>
+#include <string>
+//#include "Molecule.hpp"
+
+using namespace std;
+
+class OPLSff{
+    private:
+    map<string, string> opls;
+    map<string, double> epsilon;
+    map<string, double> sigma;
+    
+    public:
+    OPLSff();
+    ~OPLSff();
+    
+    string getBondType(string opls_str);
+    double getEpsilon(string opls_str);
+    double getSigma(string opls_str);
+
+};
+
+#endif /* PeriodicTable_hpp */
