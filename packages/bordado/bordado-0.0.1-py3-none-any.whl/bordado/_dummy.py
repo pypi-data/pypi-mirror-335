a68a5b614ee961f2909d5ed6d8bdf43a4c1485d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:13:46 +0000

    Add package bordado/0.0.1/bordado-0.0.1-py3-none-any.whl

---
+++ packages/bordado/bordado-0.0.1-py3-none-any.whl/bordado/_dummy.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2025 The Bordado Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
+"""
+Dummy module so tests don't fail.
+"""
+
+
+def dummy():
+    """Calculate nothing."""
+    return "bla"
