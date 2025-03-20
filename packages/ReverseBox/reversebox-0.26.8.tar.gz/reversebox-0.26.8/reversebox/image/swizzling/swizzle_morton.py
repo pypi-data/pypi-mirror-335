a6c6e838fa249429fa51a9221eaabce7b33d974b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:24 +0000

    Add package ReverseBox/0.26.8/reversebox-0.26.8.tar.gz

---
+++ packages/ReverseBox/reversebox-0.26.8.tar.gz/reversebox-0.26.8/reversebox/image/swizzling/swizzle_morton.py
@@ -0,0 +1,65 @@
+"""
+Copyright © 2024  Bartłomiej Duda
+License: GPL-3.0 License
+"""
+
+from reversebox.image.common import convert_bpp_to_bytes_per_pixel
+
+# Morton Order Texture Swizzling
+# https://en.wikipedia.org/wiki/Z-order_curve
+# Used in XBOX CLASSIC and PS3 games (e.g. EA XSH files)
+
+# fmt: off
+
+
+def calculate_morton_index(x: int, y: int, img_width: int, img_height: int) -> int:
+    nx: int = 0
+    ny: int = 0
+    xpos: int = 0
+    ypos: int = 0
+    tpix: int = 1
+    morton_index: int = y * img_width + x
+
+    while tpix < img_width or tpix < img_height:
+        if tpix < img_width:
+            nx |= ((morton_index & 1) << xpos)
+            xpos += 1
+            morton_index >>= 1
+        if tpix < img_height:
+            ny |= ((morton_index & 1) << ypos)
+            ypos += 1
+            morton_index >>= 1
+        tpix <<= 1
+
+    morton_index = ny * img_width + nx
+    return morton_index
+
+
+def unswizzle_morton(image_data: bytes, img_width: int, img_height: int, bpp: int) -> bytes:
+    unswizzled_data = bytearray(len(image_data))
+    bytes_per_pixel: int = convert_bpp_to_bytes_per_pixel(bpp)
+    source_index: int = 0
+
+    for y in range(img_height):
+        for x in range(img_width):
+            morton_index = calculate_morton_index(x, y, img_width, img_height)
+            destination_index = bytes_per_pixel * morton_index
+            unswizzled_data[destination_index: destination_index + bytes_per_pixel] = image_data[source_index: source_index + bytes_per_pixel]
+            source_index += bytes_per_pixel
+
+    return unswizzled_data
+
+
+def swizzle_morton(image_data: bytes, img_width: int, img_height: int, bpp: int) -> bytes:
+    swizzled_data = bytearray(len(image_data))
+    bytes_per_pixel: int = convert_bpp_to_bytes_per_pixel(bpp)
+    source_index: int = 0
+
+    for y in range(img_height):
+        for x in range(img_width):
+            morton_index = calculate_morton_index(x, y, img_width, img_height)
+            destination_index = bytes_per_pixel * morton_index
+            swizzled_data[source_index: source_index + bytes_per_pixel] = image_data[destination_index: destination_index + bytes_per_pixel]
+            source_index += bytes_per_pixel
+
+    return swizzled_data
---
+++ packages/ReverseBox/reversebox-0.26.8.tar.gz/reversebox-0.26.8/reversebox/image/swizzling/swizzle_psvita_dreamcast.py
@@ -0,0 +1,124 @@
+"""
+Copyright © 2024-2025  Bartłomiej Duda
+License: GPL-3.0 License
+"""
+
+import numpy as np
+
+# fmt: off
+
+# Dreamcast and PS Vita Texture Swizzling
+# Morton Order (+ rotate by 90 degrees)
+# https://en.wikipedia.org/wiki/Z-order_curve
+# https://dreamcast.wiki/Twiddling
+
+# Same algorithm is used in Dreamcast and PS Vita consoles
+# I've seen it used in Dreamcast DTEX files and in PS Vita GXT files
+# example games:
+# - Danganronpa: Trigger Happy Havoc (PS Vita) (*.GXT)
+# - Danganronpa 2: Goodbye Despair (PS Vita) (*.GXT)
+# - Senran Kagura: Shinovi Versus (PS Vita) (*.GXT)
+
+
+def bsr(value: int) -> int:
+    """bit scan reverse"""
+    return int(np.floor(np.log2(value))) if value > 0 else 0
+
+
+def enclosing_power_of_2(x: int) -> int:
+    """find the smallest power of 2 equal or bigger than x"""
+    return 1 << int(np.ceil(np.log2(x)))
+
+
+def align(value: int, alignment: int) -> int:
+    """power of two alignment"""
+    return (value + (alignment - 1)) & ~(alignment - 1)
+
+
+def get_morton_index_psvita_dreamcast(x: int, y: int, width: int, height: int) -> int:
+    log_w = bsr(width)
+    log_h = bsr(height)
+    d = min(log_w, log_h)
+    m = 0
+
+    for i in range(d):
+        m |= ((x & (1 << i)) << (i + 1)) | ((y & (1 << i)) << i)
+
+    if width < height:
+        m |= ((y & ~(width - 1)) << d)
+    else:
+        m |= ((x & ~(height - 1)) << d)
+
+    return m
+
+
+def _convert_psvita_dreamcast_4bpp(pixel_data: bytes, img_width: int, img_height: int, swizzle_flag: bool) -> bytes:
+    converted_data: bytearray = bytearray(len(pixel_data))
+    mx = get_morton_index_psvita_dreamcast(img_width - 1, 0, img_width, img_height)
+    my = get_morton_index_psvita_dreamcast(0, img_height - 1, img_width, img_height)
+
+    line_stride = align(img_width, 2)
+
+    oy = 0
+    for y in range(img_height):
+        ox = 0
+        for x in range(img_width):
+            if not swizzle_flag:
+                src_ofs_n = ox + oy
+                tgt_ofs_n = y * line_stride + x
+            else:
+                src_ofs_n = y * line_stride + x
+                tgt_ofs_n = ox + oy
+
+            src_ofs = src_ofs_n >> 1
+            tgt_ofs = tgt_ofs_n >> 1
+
+            src_shift = (src_ofs_n & 1) << 2
+            dst_shift = (tgt_ofs_n & 1) << 2
+
+            n = (pixel_data[src_ofs] >> src_shift) & 0xF
+            converted_data[tgt_ofs] = (converted_data[tgt_ofs] & (0xF0 >> dst_shift)) | (n << dst_shift)
+
+            ox = (ox - mx) & mx
+        oy = (oy - my) & my
+
+    return converted_data
+
+
+def _convert_psvita_dreamcast(pixel_data: bytes, img_width: int, img_height: int, bpp: int, swizzle_flag: bool) -> bytes:
+    converted_data: bytearray = bytearray(len(pixel_data))
+    width_pow2 = enclosing_power_of_2(img_width)
+    height_pow2 = enclosing_power_of_2(img_height)
+
+    mx = get_morton_index_psvita_dreamcast(width_pow2 - 1, 0, width_pow2, height_pow2)
+    my = get_morton_index_psvita_dreamcast(0, height_pow2 - 1, width_pow2, height_pow2)
+
+    pixel_size = bpp // 8
+
+    oy = 0
+    for y in range(img_height):
+        ox = 0
+        for x in range(img_width):
+            src_offset = (ox + oy) * pixel_size
+            dest_offset = (y * img_width + x) * pixel_size
+            if not swizzle_flag:
+                converted_data[dest_offset:dest_offset + pixel_size] = pixel_data[src_offset:src_offset + pixel_size]
+            else:
+                converted_data[src_offset:src_offset + pixel_size] = pixel_data[dest_offset:dest_offset + pixel_size]
+
+            ox = (ox - mx) & mx
+        oy = (oy - my) & my
+
+    return converted_data
+
+
+def unswizzle_psvita_dreamcast(pixel_data: bytes, img_width: int, img_height: int, bpp: int) -> bytes:
+    if bpp == 4:
+        return _convert_psvita_dreamcast_4bpp(pixel_data, img_width, img_height, False)
+    return _convert_psvita_dreamcast(pixel_data, img_width, img_height, bpp, False)
+
+
+def swizzle_psvita_dreamcast(pixel_data: bytes, img_width: int, img_height: int, bpp: int) -> bytes:
+    if bpp == 4:
+        return _convert_psvita_dreamcast_4bpp(pixel_data, img_width, img_height, True)
+    return _convert_psvita_dreamcast(pixel_data, img_width, img_height, bpp, True)
