5bf52624a4116d92bac752d6e2f783e5f3d93ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:04:51 +0000

    Add package pcffont/0.0.19/pcffont-0.0.19.tar.gz

---
+++ packages/pcffont/pcffont-0.0.19.tar.gz/pcffont-0.0.19/src/pcffont/internal/stream.py
@@ -0,0 +1,172 @@
+from collections.abc import Iterable
+from typing import BinaryIO
+
+
+class Stream:
+    buffer: BinaryIO
+
+    def __init__(self, buffer: BinaryIO):
+        self.buffer = buffer
+
+    def read(self, size: int, ignore_eof: bool = False) -> bytes:
+        data = self.buffer.read(size)
+        if len(data) < size and not ignore_eof:
+            raise EOFError()
+        return data
+
+    def read_int(self, size: int, ms_byte_first: bool = False) -> int:
+        return int.from_bytes(self.read(size), 'big' if ms_byte_first else 'little', signed=True)
+
+    def read_int_list(self, size: int, length: int, ms_byte_first: bool = False) -> list[int]:
+        return [self.read_int(size, ms_byte_first) for _ in range(length)]
+
+    def read_int8(self) -> int:
+        return self.read_int(1)
+
+    def read_int8_list(self, length: int) -> list[int]:
+        return self.read_int_list(1, length)
+
+    def read_int16(self, ms_byte_first: bool = False) -> int:
+        return self.read_int(2, ms_byte_first)
+
+    def read_int16_list(self, length: int, ms_byte_first: bool = False) -> list[int]:
+        return self.read_int_list(2, length, ms_byte_first)
+
+    def read_int32(self, ms_byte_first: bool = False) -> int:
+        return self.read_int(4, ms_byte_first)
+
+    def read_int32_list(self, length: int, ms_byte_first: bool = False) -> list[int]:
+        return self.read_int_list(4, length, ms_byte_first)
+
+    def read_uint(self, size: int, ms_byte_first: bool = False) -> int:
+        return int.from_bytes(self.read(size), 'big' if ms_byte_first else 'little', signed=False)
+
+    def read_uint_list(self, size: int, length: int, ms_byte_first: bool = False) -> list[int]:
+        return [self.read_uint(size, ms_byte_first) for _ in range(length)]
+
+    def read_uint8(self) -> int:
+        return self.read_uint(1)
+
+    def read_uint8_list(self, length: int) -> list[int]:
+        return self.read_uint_list(1, length)
+
+    def read_uint16(self, ms_byte_first: bool = False) -> int:
+        return self.read_uint(2, ms_byte_first)
+
+    def read_uint16_list(self, length: int, ms_byte_first: bool = False) -> list[int]:
+        return self.read_uint_list(2, length, ms_byte_first)
+
+    def read_uint32(self, ms_byte_first: bool = False) -> int:
+        return self.read_uint(4, ms_byte_first)
+
+    def read_uint32_list(self, length: int, ms_byte_first: bool = False) -> list[int]:
+        return self.read_uint_list(4, length, ms_byte_first)
+
+    def read_binary(self, ms_bit_first: bool = False) -> list[int]:
+        binary = [int(c) for c in f'{self.read(1)[0]:08b}']
+        if not ms_bit_first:
+            binary.reverse()
+        return binary
+
+    def read_binary_list(self, length: int, ms_bit_first: bool = False) -> list[list[int]]:
+        return [self.read_binary(ms_bit_first) for _ in range(length)]
+
+    def read_string(self) -> str:
+        data = bytearray()
+        while True:
+            b = self.read(1)
+            if b == b'\x00' or b == b'':
+                break
+            data.extend(b)
+        return data.decode()
+
+    def read_string_list(self, length: int) -> list[str]:
+        return [self.read_string() for _ in range(length)]
+
+    def read_bool(self) -> bool:
+        return self.read(1) != b'\x00'
+
+    def write(self, data: bytes) -> int:
+        return self.buffer.write(data)
+
+    def write_int(self, data: int, size: int, ms_byte_first: bool = False) -> int:
+        return self.write(data.to_bytes(size, 'big' if ms_byte_first else 'little', signed=True))
+
+    def write_int_list(self, data: Iterable[int], size: int, ms_byte_first: bool = False) -> int:
+        return sum([self.write_int(value, size, ms_byte_first) for value in data])
+
+    def write_int8(self, data: int) -> int:
+        return self.write_int(data, 1)
+
+    def write_int8_list(self, data: Iterable[int]) -> int:
+        return self.write_int_list(data, 1)
+
+    def write_int16(self, data: int, ms_byte_first: bool = False) -> int:
+        return self.write_int(data, 2, ms_byte_first)
+
+    def write_int16_list(self, data: Iterable[int], ms_byte_first: bool = False) -> int:
+        return self.write_int_list(data, 2, ms_byte_first)
+
+    def write_int32(self, data: int, ms_byte_first: bool = False) -> int:
+        return self.write_int(data, 4, ms_byte_first)
+
+    def write_int32_list(self, data: Iterable[int], ms_byte_first: bool = False) -> int:
+        return self.write_int_list(data, 4, ms_byte_first)
+
+    def write_uint(self, data: int, size: int, ms_byte_first: bool = False) -> int:
+        return self.write(data.to_bytes(size, 'big' if ms_byte_first else 'little', signed=False))
+
+    def write_uint_list(self, data: Iterable[int], size: int, ms_byte_first: bool = False) -> int:
+        return sum([self.write_uint(value, size, ms_byte_first) for value in data])
+
+    def write_uint8(self, data: int) -> int:
+        return self.write_uint(data, 1)
+
+    def write_uint8_list(self, data: Iterable[int]) -> int:
+        return self.write_uint_list(data, 1)
+
+    def write_uint16(self, data: int, ms_byte_first: bool = False) -> int:
+        return self.write_uint(data, 2, ms_byte_first)
+
+    def write_uint16_list(self, data: Iterable[int], ms_byte_first: bool = False) -> int:
+        return self.write_uint_list(data, 2, ms_byte_first)
+
+    def write_uint32(self, data: int, ms_byte_first: bool = False) -> int:
+        return self.write_uint(data, 4, ms_byte_first)
+
+    def write_uint32_list(self, data: Iterable[int], ms_byte_first: bool = False) -> int:
+        return self.write_uint_list(data, 4, ms_byte_first)
+
+    def write_binary(self, binary: list[int], ms_bit_first: bool = False) -> int:
+        if not ms_bit_first:
+            binary = binary[::-1]
+        return self.write(bytes([int(''.join(map(str, binary)), 2)]))
+
+    def write_binary_list(self, data: Iterable[list[int]], ms_bit_first: bool = False) -> int:
+        return sum([self.write_binary(value, ms_bit_first) for value in data])
+
+    def write_string(self, data: str) -> int:
+        return self.write(data.encode()) + self.write_nulls(1)
+
+    def write_string_list(self, data: Iterable[str]) -> int:
+        return sum([self.write_string(value) for value in data])
+
+    def write_bool(self, value: bool) -> int:
+        return self.write(b'\x01' if value else b'\x00')
+
+    def write_nulls(self, size: int) -> int:
+        for _ in range(size):
+            self.write(b'\x00')
+        return size
+
+    def skip(self, size: int):
+        self.seek(self.tell() + size)
+
+    def seek(self, offset: int):
+        self.buffer.seek(offset)
+
+    def tell(self) -> int:
+        return self.buffer.tell()
+
+    def align_to_bit32_with_nulls(self) -> int:
+        return self.write_nulls(3 - (self.tell() + 3) % 4)
---
+++ packages/pcffont/pcffont-0.0.19.tar.gz/pcffont-0.0.19/tests/test_type.py
@@ -0,0 +1,165 @@
+from pcffont import PcfTableFormat, PcfMetric, PcfTable, PcfProperties, PcfAccelerators, PcfMetrics, PcfBitmaps, PcfBdfEncodings, PcfScalableWidths, PcfGlyphNames
+
+
+def test_table_format_value():
+    assert PcfTableFormat().value == 12
+    assert PcfTableFormat(glyph_pad_index=2).value == 14
+    assert PcfTableFormat(glyph_pad_index=2, ink_or_compressed_metrics=True).value == 270
+    assert PcfTableFormat(ms_byte_first=False, ms_bit_first=False).value == 0
+    assert PcfTableFormat(ms_byte_first=False, ms_bit_first=False, glyph_pad_index=2).value == 2
+    assert PcfTableFormat(ms_byte_first=False, ms_bit_first=False, glyph_pad_index=2, ink_or_compressed_metrics=True).value == 258
+
+
+def test_table_format_eq():
+    table_format_1 = PcfTableFormat(ms_byte_first=False)
+    table_format_2 = PcfTableFormat(ms_byte_first=False)
+    table_format_3 = PcfTableFormat(glyph_pad_index=2)
+    assert table_format_1 == table_format_2
+    assert table_format_1 != table_format_3
+    assert table_format_1 != 1
+    assert table_format_1 != 'Hello World!'
+
+
+def test_metric_eq():
+    metric_1 = PcfMetric(
+        left_side_bearing=-3,
+        right_side_bearing=8,
+        character_width=4,
+        ascent=9,
+        descent=-5,
+    )
+    metric_2 = PcfMetric(
+        left_side_bearing=-3,
+        right_side_bearing=8,
+        character_width=4,
+        ascent=9,
+        descent=-5,
+    )
+    metric_3 = PcfMetric(
+        left_side_bearing=-2,
+        right_side_bearing=8,
+        character_width=4,
+        ascent=9,
+        descent=-5,
+    )
+    assert metric_1 == metric_2
+    assert metric_1 != metric_3
+    assert metric_1 != 1
+    assert metric_1 != 'Hello World!'
+
+
+def test_metric_compressible():
+    metric = PcfMetric(
+        left_side_bearing=0,
+        right_side_bearing=0,
+        character_width=0,
+        ascent=0,
+        descent=-0,
+    )
+    assert metric.compressible
+
+    metric.left_side_bearing = -129
+    assert not metric.compressible
+    metric.left_side_bearing = -128
+    assert metric.compressible
+    metric.left_side_bearing = 128
+    assert not metric.compressible
+    metric.left_side_bearing = 127
+    assert metric.compressible
+
+    metric.right_side_bearing = -129
+    assert not metric.compressible
+    metric.right_side_bearing = -128
+    assert metric.compressible
+    metric.right_side_bearing = 128
+    assert not metric.compressible
+    metric.right_side_bearing = 127
+    assert metric.compressible
+
+    metric.character_width = -129
+    assert not metric.compressible
+    metric.character_width = -128
+    assert metric.compressible
+    metric.character_width = 128
+    assert not metric.compressible
+    metric.character_width = 127
+    assert metric.compressible
+
+    metric.ascent = -129
+    assert not metric.compressible
+    metric.ascent = -128
+    assert metric.compressible
+    metric.ascent = 128
+    assert not metric.compressible
+    metric.ascent = 127
+    assert metric.compressible
+
+    metric.descent = -129
+    assert not metric.compressible
+    metric.descent = -128
+    assert metric.compressible
+    metric.descent = 128
+    assert not metric.compressible
+    metric.descent = 127
+    assert metric.compressible
+
+
+def test_table_isinstance():
+    assert isinstance(PcfProperties(), PcfTable)
+    assert isinstance(PcfAccelerators(), PcfTable)
+    assert isinstance(PcfMetrics(), PcfTable)
+    assert isinstance(PcfBitmaps(), PcfTable)
+    assert isinstance(PcfBdfEncodings(), PcfTable)
+    assert isinstance(PcfScalableWidths(), PcfTable)
+    assert isinstance(PcfGlyphNames(), PcfTable)
+
+
+def test_metrics_calculate():
+    metrics = PcfMetrics(metrics=[
+        PcfMetric(
+            left_side_bearing=-3,
+            right_side_bearing=8,
+            character_width=4,
+            ascent=9,
+            descent=-5,
+        ),
+        PcfMetric(
+            left_side_bearing=7,
+            right_side_bearing=3,
+            character_width=1,
+            ascent=-6,
+            descent=0,
+        ),
+        PcfMetric(
+            left_side_bearing=1,
+            right_side_bearing=0,
+            character_width=2,
+            ascent=5,
+            descent=4,
+        ),
+        PcfMetric(
+            left_side_bearing=-5,
+            right_side_bearing=-1,
+            character_width=7,
+            ascent=-3,
+            descent=-9,
+        ),
+    ])
+    assert metrics.calculate_min_bounds() == PcfMetric(
+        left_side_bearing=-5,
+        right_side_bearing=-1,
+        character_width=1,
+        ascent=-6,
+        descent=-9,
+    )
+    assert metrics.calculate_max_bounds() == PcfMetric(
+        left_side_bearing=7,
+        right_side_bearing=8,
+        character_width=7,
+        ascent=9,
+        descent=4,
+    )
+    assert metrics.calculate_max_overlap() == 4
+    assert metrics.calculate_compressible()
+    metrics[0].left_side_bearing = 128
+    assert not metrics.calculate_compressible()
