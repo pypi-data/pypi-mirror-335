2ca250a7aa42e2cf27ecd420ede7909be123e7c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:13:13 +0000

    Add package PySCHARE/0.0.0.0.1/pyschare-0.0.0.0.1.tar.gz

---
+++ packages/PySCHARE/pyschare-0.0.0.0.1.tar.gz/pyschare-0.0.0.0.1/PySCHARE/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PySCHARE',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'ipywidgets',
+        'IPython',
+'numpy',
+        'seaborn',
+        'matplotlib',
+
+
+    ],
+)
