6e2bf5d9d84eed4370f0b868a498613f56aeeab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:40 +0000

    Add package simplecalculator-ganesan/0.1.0/simplecalculator_ganesan-0.1.0.tar.gz

---
+++ packages/simplecalculator-ganesan/simplecalculator_ganesan-0.1.0.tar.gz/simplecalculator_ganesan-0.1.0/README.md
@@ -0,0 +1,16 @@
+# Simple Calculator
+
+A simple Python calculator package.
+
+## Installation
+```bash
+pip install simplecalculator
+```
+
+## Usage
+``` python
+from calculator import Calculator
+
+calc = Calculator()
+print(calc.add(5, 3))  # Output: 8
+```
---
+++ packages/simplecalculator-ganesan/simplecalculator_ganesan-0.1.0.tar.gz/simplecalculator_ganesan-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simplecalculator-ganesan",
+    version="0.1.0",
+    author="Ganesan Selvaraj",
+    author_email="ganesan@example.com",
+    description="A simple calculator package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
