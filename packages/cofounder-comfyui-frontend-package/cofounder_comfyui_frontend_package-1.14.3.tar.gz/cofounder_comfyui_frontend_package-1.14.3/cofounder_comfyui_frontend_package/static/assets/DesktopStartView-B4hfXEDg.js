b37b707353c573c226d88b28a09744376a7f6757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:35 +0000

    Add package cofounder-comfyui-frontend-package/1.14.3/cofounder_comfyui_frontend_package-1.14.3.tar.gz

---
+++ packages/cofounder-comfyui-frontend-package/cofounder_comfyui_frontend_package-1.14.3.tar.gz/cofounder_comfyui_frontend_package-1.14.3/cofounder_comfyui_frontend_package/static/assets/DesktopStartView-B4hfXEDg.js
@@ -0,0 +1,19 @@
+import { d as defineComponent, o as openBlock, y as createBlock, z as withCtx, k as createVNode, j as unref, bT as script } from "./index-CZ6DeBqz.js";
+import { _ as _sfc_main$1 } from "./BaseViewTemplate-g5TaNDJy.js";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "DesktopStartView",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createBlock(_sfc_main$1, { dark: "" }, {
+        default: withCtx(() => [
+          createVNode(unref(script), { class: "m-8 w-48 h-48" })
+        ]),
+        _: 1
+      });
+    };
+  }
+});
+export {
+  _sfc_main as default
+};
+//# sourceMappingURL=DesktopStartView-B4hfXEDg.js.map
