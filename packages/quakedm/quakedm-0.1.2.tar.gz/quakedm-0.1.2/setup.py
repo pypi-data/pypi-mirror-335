d853c54cb0edc67f77263d3984ce2322c295071c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:13:31 +0000

    Add package quakedm/0.1.2/quakedm-0.1.2.tar.gz

---
+++ packages/quakedm/quakedm-0.1.2.tar.gz/quakedm-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="quakedm",
+    version="0.1.2",
+    long_description="",
+    long_description_content_type="text/markdown",
+    packages=["quakedm"],
+    install_requires=["numpy",  "h5py", "matplotlib", "pandas"],
+)
