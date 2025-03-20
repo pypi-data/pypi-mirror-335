60627bd6865c77646d8854d6df804c18cc988c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:36 +0000

    Add package pyclmuapp/0.0.1/pyclmuapp-0.0.1-py3-none-any.whl

---
+++ packages/pyclmuapp/pyclmuapp-0.0.1-py3-none-any.whl/pyclmuapp/__init__.py
@@ -0,0 +1,11 @@
+from pyclmuapp import *
+from pyclmuapp.container import *
+from pyclmuapp.usp import *
+from pyclmuapp.pts import *
+#from pyclmuapp.clmu import get_clmuapp_frocing, get_urban_params, get_soil_params
+from pyclmuapp.clmu import get_soil_params, get_urban_params, get_forcing
+from pyclmuapp.era5_forcing import era5_to_forcing, era5_download, arco_era5_to_forcing
+
+__all__ = ['clumapp', 'usp_clmu', 'pts_clmu', 'clmu']
+
+__version__ = "0.0.0"
