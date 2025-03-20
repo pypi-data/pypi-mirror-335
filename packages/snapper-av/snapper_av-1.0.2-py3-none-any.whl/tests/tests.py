24824fe99e77077ea0db589794e0287af561e8e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:30 +0000

    Add package snapper-av/1.0.2/snapper_av-1.0.2-py3-none-any.whl

---
+++ packages/snapper-av/snapper_av-1.0.2-py3-none-any.whl/tests/tests.py
@@ -0,0 +1,10 @@
+# test_core.py
+import unittest
+from snapper.core import Snapper
+
+class TestSnapper(unittest.TestCase):
+    def test(self):
+        assert True
+
+if __name__ == "__main__":
+    unittest.main()
