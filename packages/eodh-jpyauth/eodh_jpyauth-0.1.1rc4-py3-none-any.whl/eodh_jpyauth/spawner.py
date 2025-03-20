3904e7a2a14a3db078ea992dd6e6b7b40ed8e165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:33 +0000

    Add package eodh-jpyauth/0.1.1rc4/eodh_jpyauth-0.1.1rc4-py3-none-any.whl

---
+++ packages/eodh-jpyauth/eodh_jpyauth-0.1.1rc4-py3-none-any.whl/eodh_jpyauth/spawner.py
@@ -0,0 +1,40 @@
+from kubespawner.slugs import safe_slug
+from kubespawner.spawner import KubeSpawner
+from traitlets import Unicode
+
+
+class EODHSpawner(KubeSpawner):
+    workspace = Unicode(
+        None,
+        allow_none=False,
+        config=True,
+        help="The workspace of the notebook",
+    )
+
+    def _expand_user_properties(self, template, slug_scheme=None):
+        """
+        Expand user properties in template strings
+        """
+        safe_username = safe_slug(self.user.name)
+        safe_workspace = safe_slug(self.workspace)
+        servername = f"{safe_username}--{safe_workspace}"
+        ns = dict(
+            username=safe_slug(self.user.name),
+            workspace=safe_slug(self.workspace),
+            servername=servername,
+            user_server=servername,
+        )
+        for attr_name in ("pod_name", "pvc_name", "namespace"):
+            ns[attr_name] = getattr(self, attr_name, f"{attr_name}_unavailable!")
+        return template.format(**ns)
+
+    options_form = """
+        Set your key:
+        <input name="key" val="default_key"></input>
+        <br>
+        Choose a letter:
+        <select name="letter" multiple="true">
+            <option value="A">The letter A</option>
+            <option value="B">The letter B</option>
+        </select>
+    """
