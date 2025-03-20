0dccbc66b3e8aef0b247bee91dcc9468a7e5eef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:56 +0000

    Add package mcp-python-sdk-aliyun/0.2.1.6/mcp_python_sdk_aliyun-0.2.1.6-py3-none-any.whl

---
+++ packages/mcp-python-sdk-aliyun/mcp_python_sdk_aliyun-0.2.1.6-py3-none-any.whl/src/aliyun/http_caller.py
@@ -0,0 +1,20 @@
+import requests
+
+def call_http_tool(tool_info: {}, arguments: {}):
+    if "protocol" not in tool_info or not isinstance(tool_info["protocol"], dict):
+        raise Exception("openapi工具调用信息protocol不可为空")
+    protocol = tool_info["protocol"]
+
+    url = protocol["url"] if "url" in protocol else ""
+    params = arguments["params"] if "params" in arguments else ""
+    headers = arguments["headers"] if "headers" in arguments else ""
+    data = arguments["data"] if "data" in arguments else ""
+
+    if protocol["methodType"].upper() == "POST":
+        response = requests.post(url, headers=headers, json=data)
+    else:
+        response = requests.get(url, params=params, headers=headers)
+
+    if response.status == 200:
+        return response.json()
+    return {}
