ca9978e1b9c53578c030324c4de0cdad39d421ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:13 +0000

    Add package ap-gym/0.2.0/ap_gym-0.2.0.tar.gz

---
+++ packages/ap-gym/ap_gym-0.2.0.tar.gz/ap_gym-0.2.0/ap_gym/__init__.py
@@ -0,0 +1,34 @@
+from .active_classification_env import (
+    ActiveClassificationEnv,
+    ActiveClassificationVectorEnv,
+)
+from .active_perception_env import (
+    ActivePerceptionEnv,
+    BaseActivePerceptionEnv,
+    ActivePerceptionWrapper,
+    ActivePerceptionRestoreWrapper,
+    ActivePerceptionActionSpace,
+    PseudoActivePerceptionWrapper,
+    ensure_active_perception_env,
+    NoActivePerceptionEnvError,
+)
+from .active_perception_vector_env import (
+    ActivePerceptionVectorEnv,
+    BaseActivePerceptionVectorEnv,
+    ActivePerceptionVectorWrapper,
+    ActivePerceptionVectorRestoreWrapper,
+    PseudoActivePerceptionVectorWrapper,
+    ensure_active_perception_vector_env,
+)
+from .active_regression_env import ActiveRegressionEnv, ActiveRegressionVectorEnv
+from .image_space import ImageSpace
+from .loss_fn import LossFn, LambdaLossFn, ZeroLossFn, CrossEntropyLossFn, MSELossFn
+from .time_limit import TimeLimit
+from .vector_to_single_wrapper import (
+    VectorToSingleWrapper,
+    ActivePerceptionVectorToSingleWrapper,
+)
+
+from .envs.registration import make, make_vec, register_envs
+
+register_envs()
