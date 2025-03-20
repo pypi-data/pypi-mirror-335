6b8e954d709eb0320fcfb6f111611dc7e94dc3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:18 +0000

    Add package requestssmm/0.0.0.7/requestssmm-0.0.0.7.tar.gz

---
+++ packages/requestssmm/requestssmm-0.0.0.7.tar.gz/requestssmm-0.0.0.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='requestssmm',
+    version='0.0.0.7',
+    packages=find_packages(),
+    install_requires=[
+        'requests',  'fb_atm','requestssmm'# Replace 'some_dependency' with the actual dependency
+    ],
+)
