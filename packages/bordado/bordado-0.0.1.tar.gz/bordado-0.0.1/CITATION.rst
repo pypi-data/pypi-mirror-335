86bf2897472b387e5c483d726b53226fc77f885a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 03:13:46 +0000

    Add package bordado/0.0.1/bordado-0.0.1.tar.gz

---
+++ packages/bordado/bordado-0.0.1.tar.gz/bordado-0.0.1/CITATION.rst
@@ -0,0 +1,8 @@
+Citing Bordado
+==============
+
+This is research software **made by scientists**. Citations help us justify the
+effort that goes into building and maintaining this project.
+
+If you used it in your research, please consider citing the latest Zenodo
+archive: https://doi.org/TO_BE_DETERMINED
---
+++ packages/bordado/bordado-0.0.1.tar.gz/bordado-0.0.1/src/bordado/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2025 The Bordado Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
+"""
+These are the functions and classes that make up the Bordado API.
+"""
+
+from ._version import __version__
+
+# Append a leading "v" to the generated version by setuptools_scm
+__version__ = f"v{__version__}"
