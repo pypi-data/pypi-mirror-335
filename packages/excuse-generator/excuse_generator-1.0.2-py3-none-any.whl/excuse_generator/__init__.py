a34c15bcf771b01c74b484a13b9ffe0c46a797b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:59 +0000

    Add package excuse-generator/1.0.2/excuse_generator-1.0.2-py3-none-any.whl

---
+++ packages/excuse-generator/excuse_generator-1.0.2-py3-none-any.whl/excuse_generator/__init__.py
@@ -0,0 +1,2 @@
+from .excuses import generate_excuse, random_excuse, add_custom_excuse, list_categories, help
+__all__ = ["generate_excuse", "random_excuse", "add_custom_excuse", "list_categories", "help"]
