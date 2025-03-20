39a95e72d02c82453db7e6e8dc3bf7c2f59af781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:07 +0000

    Add package ssb-pubmd/0.0.3/ssb_pubmd-0.0.3.tar.gz

---
+++ packages/ssb-pubmd/ssb_pubmd-0.0.3.tar.gz/ssb_pubmd-0.0.3/src/ssb_pubmd/functions.py
@@ -0,0 +1,37 @@
+"""A collection of useful functions.
+
+The template and this example uses Google style docstrings as described at:
+https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+
+"""
+
+
+def example_function(number1: int, number2: int) -> str:
+    """Compare two integers.
+
+    This is merely an example function can be deleted. It is used to show and test generating
+    documentation from code, type hinting, testing, and testing examples
+    in the code.
+
+
+    Args:
+        number1: The first number.
+        number2: The second number, which will be compared to number1.
+
+    Returns:
+        A string describing which number is the greatest.
+
+    Examples:
+        Examples should be written in doctest format, and should illustrate how
+        to use the function.
+
+        >>> example_function(1, 2)
+        1 is less than 2 by 1
+
+    """
+    if number1 < number2:
+        return f"{number1} is less than {number2} by {number2 - number1}"
+    elif number1 > number2:
+        return f"{number1} is greater than {number2} by {number1 - number2}"
+    else:
+        return f"{number1} is equal to {number2}"
