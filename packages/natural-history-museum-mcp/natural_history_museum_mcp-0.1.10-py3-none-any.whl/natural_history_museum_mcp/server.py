940c3642da59e4bf809803f1549b1901b4577d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 04:04:18 +0000

    Add package natural-history-museum-mcp/0.1.10/natural_history_museum_mcp-0.1.10-py3-none-any.whl

---
+++ packages/natural-history-museum-mcp/natural_history_museum_mcp-0.1.10-py3-none-any.whl/natural_history_museum_mcp/server.py
@@ -0,0 +1,33 @@
+import sys
+from typing import Literal
+
+import json
+import logging
+
+from natural_history_museum_mcp import nhm_api
+from mcp.server.fastmcp import FastMCP
+
+
+COMMUNICATION_TYPE: Literal["stdio", "sse"] = "stdio" # For talking to clients locally
+mcp = FastMCP("Natural History Museum Data API")
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    )
+logger = logging.getLogger("BlenderMCPServer")
+
+@mcp.tool()
+def search_specimens(query: str, limit: int) -> str:
+    print("Starting search specimens", file=sys.stderr)
+    nhm_api_result = nhm_api.get_by_query(query, limit)
+
+    return json.dumps(nhm_api_result)
+
+
+def main():
+    print("MCP run starting", file=sys.stderr)
+    mcp.run(COMMUNICATION_TYPE)
+
+if __name__ == "__main__":
+    print("MCP run starting", file=sys.stderr)
+    main()
