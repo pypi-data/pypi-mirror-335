af6dee6b54c7cf7f38fce59b5ea6987d413077ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:41 +0000

    Add package flowcept/0.8.3/flowcept-0.8.3.tar.gz

---
+++ packages/flowcept/flowcept-0.8.3.tar.gz/flowcept-0.8.3/resources/mofka/consumer.py
@@ -0,0 +1,54 @@
+import mochi.mofka.client as mofka
+from mochi.mofka.client import ThreadPool, AdaptiveBatchSize
+import json
+import time
+import csv
+print("about to start", flush=True)
+driver = mofka.MofkaDriver("mofka.json")
+batch_size = AdaptiveBatchSize
+thread_pool = ThreadPool(0)
+# create a topic
+topic_name = "interception"
+topic = driver.open_topic(topic_name)
+consumer_name = "flowcept"
+consumer = topic.consumer(name=consumer_name,
+                            thread_pool=thread_pool,
+                            batch_size=batch_size)
+
+pulls = []
+events = []
+count = 0
+print("about to start", flush=True)
+while True:
+    data = []
+    metadata = []
+    t1 = time.time()
+    f = consumer.pull()
+    event = f.wait()
+    t2 = time.time()
+    e = json.loads(event.metadata)
+
+
+    events.append(e)
+    pulls.append(t2 - t1)
+    # print("h: ", e.keys(),flush=True)
+    
+    # break
+
+    if "message" in e.keys():
+        print(e['message'],flush=True)
+        count += 1
+        with open("data.json", 'w') as f:
+            json.dump(events, f, indent=4)
+        with open("pulls.csv", "w", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(pulls)
+        
+    if count == 4:
+        break
+
+
+    
+
+    
+    
