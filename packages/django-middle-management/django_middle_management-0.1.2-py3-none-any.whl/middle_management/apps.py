2854390e141dbfbdd74af4c0691205fccbb4e53f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:11 +0000

    Add package django-middle-management/0.1.2/django_middle_management-0.1.2-py3-none-any.whl

---
+++ packages/django-middle-management/django_middle_management-0.1.2-py3-none-any.whl/middle_management/apps.py
@@ -0,0 +1,10 @@
+"""Middle Management app configuration."""
+
+from django.apps import AppConfig
+
+
+class MiddleManagementConfig(AppConfig):
+    """Middle Management app configuration."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "middle_management"
