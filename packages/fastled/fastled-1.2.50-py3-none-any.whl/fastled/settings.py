b124f03f28fe1bf81c75503aa6922b568b1a3998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:26 +0000

    Add package fastled/1.2.50/fastled-1.2.50-py3-none-any.whl

---
+++ packages/fastled/fastled-1.2.50-py3-none-any.whl/fastled/settings.py
@@ -0,0 +1,13 @@
+import os
+import platform
+
+MACHINE = platform.machine().lower()
+IS_ARM: bool = "arm" in MACHINE or "aarch64" in MACHINE
+PLATFORM_TAG: str = "-arm64" if IS_ARM else ""
+CONTAINER_NAME = f"fastled-wasm-compiler{PLATFORM_TAG}"
+DEFAULT_URL = str(os.environ.get("FASTLED_URL", "https://fastled.onrender.com"))
+SERVER_PORT = 9021
+
+IMAGE_NAME = "niteris/fastled-wasm"
+DEFAULT_CONTAINER_NAME = "fastled-wasm-compiler"
+# IMAGE_TAG = "latest"
