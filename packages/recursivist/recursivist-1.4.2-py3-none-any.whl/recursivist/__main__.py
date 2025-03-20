9a76699d5c1e6c922ef43c499f70894c44dd9d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:04:55 +0000

    Add package recursivist/1.4.2/recursivist-1.4.2-py3-none-any.whl

---
+++ packages/recursivist/recursivist-1.4.2-py3-none-any.whl/recursivist/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Entry point for recursivist.
+This allows the package to be run directly using `python -m recursivist`.
+"""
+from recursivist.cli import main
+
+if __name__ == "__main__":
+    main()
