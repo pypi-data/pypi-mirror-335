ad0174a91d9cfea3e5bf71de39ef77223fcfe26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:04:39 +0000

    Add package r2r/3.5.3/r2r-3.5.3-py3-none-any.whl

---
+++ packages/r2r/r2r-3.5.3-py3-none-any.whl/core/parsers/__init__.py
@@ -0,0 +1,36 @@
+from .media import *
+from .structured import *
+from .text import *
+
+__all__ = [
+    "AudioParser",
+    "BMPParser",
+    "DOCParser",
+    "DOCXParser",
+    "ImageParser",
+    "ODTParser",
+    "VLMPDFParser",
+    "BasicPDFParser",
+    "PDFParserUnstructured",
+    "VLMPDFParser",
+    "PPTParser",
+    "PPTXParser",
+    "RTFParser",
+    "CSVParser",
+    "CSVParserAdvanced",
+    "EMLParser",
+    "EPUBParser",
+    "JSONParser",
+    "MSGParser",
+    "ORGParser",
+    "P7SParser",
+    "RSTParser",
+    "TIFFParser",
+    "TSVParser",
+    "XLSParser",
+    "XLSXParser",
+    "XLSXParserAdvanced",
+    "MDParser",
+    "HTMLParser",
+    "TextParser",
+]
