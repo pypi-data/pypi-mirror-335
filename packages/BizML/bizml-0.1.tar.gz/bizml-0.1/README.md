2e8574d23f403bbc0434827206022b1cdf59c66b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 16:06:07 +0000

    Add package BizML/0.1/bizml-0.1.tar.gz

---
+++ packages/BizML/bizml-0.1.tar.gz/bizml-0.1/README.md
@@ -0,0 +1,20 @@
+# BizML
+
+**BizML** is a Python library for predictive analytics and business forecasting. It provides tools for customer churn prediction, sales forecasting, automated business reporting, and interactive dashboards.
+
+---
+
+## Features
+- **Customer Churn Prediction**: Predict which customers are likely to stop using your product or service.
+- **Sales Forecasting**: Forecast future sales using advanced time series models.
+- **Automated Reporting**: Generate business reports in PDF or Excel format.
+- **Interactive Dashboards**: Create visualizations for sales and churn analysis.
+
+---
+
+## Installation
+
+You can install BizML using pip:
+
+```bash
+pip install BizML
---
+++ packages/BizML/bizml-0.1.tar.gz/bizml-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BizML",  
+    version="0.1",  
+    author="karisham m patel",  
+    author_email="karishma2o@gmil.com",  
+    description="A Python library for predictive analytics and business forecasting.", 
+    long_description=open("README.md").read(), 
+    long_description_content_type="text/markdown",  
+    url="https://github.com/MOON11kr/BizML",
+    packages=find_packages(), 
+    install_requires=[  # List of dependencies
+        "pandas",
+        "scikit-learn",
+        "prophet",
+        "statsmodels",
+        "reportlab",
+        "openpyxl",
+        "plotly",
+        "dash",
+    ],
+    classifiers=[  
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Minimum Python version required
+)
