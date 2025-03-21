556a6301fa382eaa73e2a0f3d514728221d3116c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 14:05:51 +0000

    Add package kyakx/1.1.1/kyakx-1.1.1-py3-none-any.whl

---
+++ packages/kyakx/kyakx-1.1.1-py3-none-any.whl/kyakx/core/banner.py
@@ -0,0 +1,27 @@
+import time
+import sys
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+def animated_banner():
+    banner_text = "SUKA NOW OWNS YOU ♥"
+    for char in banner_text:
+        sys.stdout.write(Fore.RED + char)
+        sys.stdout.flush()
+        time.sleep(0.1)  # Adjust speed of animation
+    print(Style.RESET_ALL)  # Reset color
+
+def funky_loading():
+    loading_text = "LOADING KyakX..."
+    for char in loading_text:
+        sys.stdout.write(Fore.YELLOW + char)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    time.sleep(0.5)
+    print("\n")
+    animated_banner()
+    time.sleep(1)
+
+if __name__ == "__main__":
+    funky_loading()
