bf837559392449f8855c5b6e6eae3df573a7d0fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:05:31 +0000

    Add package tgram/1.12.7/tgram-1.12.7-py3-none-any.whl

---
+++ packages/tgram/tgram-1.12.7-py3-none-any.whl/tgram/__init__.py
@@ -0,0 +1,24 @@
+__all__ = [
+    "types",
+    "TgBot",
+    "handlers",
+    "filters",
+    "compose",
+    "StopPropagation",
+    "utils",
+    "storage",
+]
+
+__version__ = "1.12.7"
+
+from . import (
+    filters,
+    utils,
+    storage,
+    handlers,
+    types,
+)
+
+from .client import TgBot
+from .sync import compose
+from .errors import StopPropagation
