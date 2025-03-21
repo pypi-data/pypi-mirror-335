58da43aecf5f8394365741e808e97898f25398e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:15:10 +0000

    Add package poocr/0.1.20/poocr-0.1.20.tar.gz

---
+++ packages/poocr/poocr-0.1.20.tar.gz/poocr-0.1.20/tests/test_code/test_ocr2excel.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+
+from poocr.api.ocr2excel import RecognizeGeneralInvoiceOCR2Excel, VatInvoiceOCR2Excel
+
+
+class Ocr2Excel(unittest.TestCase):
+
+    def setUp(self):
+        self.SecretId = os.getenv("SecretId", None)
+        self.SecretKey = os.getenv("SecretKey", None)
+
+    def test_RecognizeGeneralInvoiceOCR2Excel(self):
+        RecognizeGeneralInvoiceOCR2Excel(input_path='../test_files/RecognizeGeneralInvoiceOCR/样例.png',
+                                         output_path='../test_files/RecognizeGeneralInvoiceOCR',
+                                         id=self.SecretId,
+                                         key=self.SecretKey, sub_type=2)
+        self.assertTrue(
+            os.path.exists("../test_files/RecognizeGeneralInvoiceOCR/RecognizeGeneralInvoiceOCR2Excel.xlsx"))
+        os.remove('../test_files/RecognizeGeneralInvoiceOCR/RecognizeGeneralInvoiceOCR2Excel.xlsx')
+
+    def test_VatInvoiceOCR2Excel(self):
+        VatInvoiceOCR2Excel(input_path='../test_files/VatInvoiceOCR/test.pdf',
+                            output_path=r'../test_files/VatInvoiceOCR',
+                            id=self.SecretId,
+                            key=self.SecretKey)
