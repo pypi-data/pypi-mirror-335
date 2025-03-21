7e915a65032ae60fcf666e677e47dde294c5dbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 16:05:59 +0000

    Add package ICECipher/1.0/icecipher-1.0.tar.gz

---
+++ packages/ICECipher/icecipher-1.0.tar.gz/icecipher-1.0/README.md
@@ -0,0 +1,15 @@
+# ICECipher
+ICE cipher for Python
+
+# Example
+```python
+from ICECipher import IceKey
+
+data = b'Hello, World!'
+
+ice = IceKey(0, [ 0x45, 0x32, 0x4E, 0x63, 0x55, 0x6B, 0x47, 0x32 ]) # 64 bit key used
+
+encrypted_data = ice.Encrypt(data)
+print(f'Encrypted = {encrypted_data}') # b'@\xff\xf3TQ>X\x04orld!'
+print(f'Decrypted = {ice.Decrypt(encrypted_data)}') # b'Hello, World!'
+```
---
+++ packages/ICECipher/icecipher-1.0.tar.gz/icecipher-1.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+	name = 'ICECipher',
+	packages = ['ICECipher'],
+	version = '1.0',
+	license = 'MIT',
+	description = 'ICE cipher for Python',
+	author = 'RenardDev',
+	author_email = 'zeze839@gmail.com',
+	url = 'https://github.com/RenardDev/ICECipher',
+	download_url = 'https://github.com/RenardDev/ICECipher/releases/latest',
+	classifiers = [
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Topic :: Software Development',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3'
+	]
+)
