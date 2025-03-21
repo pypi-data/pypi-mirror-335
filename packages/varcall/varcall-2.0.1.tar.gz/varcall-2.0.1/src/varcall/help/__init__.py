49da010939766142750cc73dc232b7198d1779af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:05:14 +0000

    Add package varcall/2.0.1/varcall-2.0.1.tar.gz

---
+++ packages/varcall/varcall-2.0.1.tar.gz/varcall-2.0.1/src/varcall/help/__init__.py
@@ -0,0 +1,15 @@
+######################################################################
+# Main app information.
+__author__ = "Vidyasagar"
+__copyright__ = "Copyright 2024, Vidyasagar"
+__credits__ = ["Vidyasagar"]
+__maintainer__ = "Vidyasagar"
+__version__ = "2.0.1"
+__licence__ = "MIT"
+
+from pathlib import Path
+
+help_dir = Path(__file__).parent
+help_path = help_dir.joinpath("help.md").absolute()
+
+### __init__.py ends here
