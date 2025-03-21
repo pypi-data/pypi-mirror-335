479bf9b7ebd4403e377a3ac2a9af3e28f82f82d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:05:18 +0000

    Add package python-datapack/1.21.5.95/python_datapack-1.21.5.95.tar.gz

---
+++ packages/python-datapack/python_datapack-1.21.5.95.tar.gz/python_datapack-1.21.5.95/copy_in_local.py
@@ -0,0 +1,12 @@
+
+import shutil
+import os
+
+source = os.path.dirname(os.path.realpath(__file__)).replace("\\","/") + "/src/python_datapack"
+destination = "C:/Users/Alexandre-PC/AppData/Local/Programs/Python/Python312/Lib/site-packages/python_datapack"
+
+shutil.rmtree(destination, ignore_errors=True)
+shutil.copytree(source, destination)
+os.system("clear")
+print("\nCopied python_datapack to local Python's site-packages\n")
+
