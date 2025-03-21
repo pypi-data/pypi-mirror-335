08470ac9936365b0c247f2896e20cd2bd627abf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 07:04:55 +0000

    Add package structureddataX/0.0.1/structureddataX-0.0.1.tar.gz

---
+++ packages/structureddataX/structureddataX-0.0.1.tar.gz/structureddataX-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="structureddataX",
+    version="0.0.1",
+    description="A simple package for math tricks",
+    # package_dir={"": "app"},
+    packages=find_packages(),
+    install_requires=[],  # No external dependencies
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/ArjanCodes/2023-package",
+    author="structured data",
+    author_email="structureddatadrive@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    # install_requires=["bson >= 0.5.10"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.6",
+)
