f13e7d38731cb50f3333c3e62ad8ef414d1c0220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:15:10 +0000

    Add package wirepas-networktool-messaging/4.4.3/wirepas_networktool_messaging-4.4.3.tar.gz

---
+++ packages/wirepas-networktool-messaging/wirepas_networktool_messaging-4.4.3.tar.gz/wirepas_networktool_messaging-4.4.3/README.md
@@ -0,0 +1,18 @@
+# wirepas-networktool-messaging-python
+
+This repository contains WNT API protocol buffer Python bindings used for the real time situation data, and WNT API usage examples.
+
+## Installation
+
+From [PyPi][wirepas_networktool_messaging_python_pypi]
+```shell
+    pip3 install wirepas-networktool-messaging
+```
+
+## License
+
+Licensed under the Apache License, Version 2.0.
+See [LICENSE][here_license] for the full license text.
+
+[here_license]: LICENSE
+[wirepas_networktool_messaging_python_pypi]: https://pypi.org/project/wirepas-networktool-messaging
---
+++ packages/wirepas-networktool-messaging/wirepas_networktool_messaging-4.4.3.tar.gz/wirepas_networktool_messaging-4.4.3/wirepas_networktool_messaging/__about__.py
@@ -0,0 +1,23 @@
+"""
+    .. Copyright:
+        Copyright Wirepas Ltd 2021 licensed under Apache License, Version 2.0
+        See file LICENSE for full license details.
+"""
+
+__author__ = "Wirepas Ltd"
+__author_email__ = "opensource@wirepas.com"
+__classifiers__ = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Topic :: Software Development :: Libraries",
+    "Programming Language :: Python :: 3",
+]
+__copyright__ = "2021 Wirepas Ltd"
+__description__ = "WNT protocol buffers bindings."
+__keywords__ = "wirepas WNT"
+__license__ = "Apache-2"
+__pkg_name__ = "wirepas_networktool_messaging"
+__title__ = "Wirepas Messaging For WNT"
+__url__ = "https://github.com/wirepas/wirepas-networktool-messaging-python"
+__version__ = "v4.4.3"
