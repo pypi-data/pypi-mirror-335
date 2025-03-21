27dbbf09cb5209c6303732b49029e9ab77a466bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:15:11 +0000

    Add package fortitude-lint/0.7.1/fortitude_lint-0.7.1.tar.gz

---
+++ packages/fortitude-lint/fortitude_lint-0.7.1.tar.gz/fortitude_lint-0.7.1/fortitude/src/rules/correctness/init_decls.rs
@@ -0,0 +1,109 @@
+use crate::ast::FortitudeNode;
+use crate::settings::Settings;
+use crate::{AstRule, FromAstNode};
+use ruff_diagnostics::{Diagnostic, Violation};
+use ruff_macros::{derive_message_formats, ViolationMetadata};
+use ruff_source_file::SourceFile;
+use tree_sitter::Node;
+
+/// ## What it does
+/// Checks for local variables with implicit `save`
+///
+/// ## Why is this bad?
+/// Initialising procedure local variables in their declaration gives them an
+/// implicit `save` attribute: the initialisation is only done on the first call
+/// to the procedure, and the variable retains its value on exit.
+///
+/// ## Examples
+/// For example, this subroutine:
+///
+/// ```f90
+/// subroutine example()
+///   integer :: var = 1
+///   print*, var
+///   var = var + 1
+/// end subroutine example
+/// ```
+///
+/// when called twice:
+///
+/// ```f90
+/// call example()
+/// call example()
+/// ```
+///
+/// prints `1 2`, when it might be expected to print `1 1`.
+///
+/// Adding the `save` attribute makes it clear that this is the intention:
+///
+/// ```f90
+/// subroutine example()
+///   integer, save :: var = 1
+///   print*, var
+///   var = var + 1
+/// end subroutine example
+/// ```
+///
+/// Unfortunately, in Fortran there is no way to disable this behaviour, and so if it
+/// is not intended, it's necessary to have a separate assignment statement:
+///
+/// ```f90
+/// subroutine example()
+///   integer :: var
+///   var = 1
+///   print*, var
+///   var = var + 1
+/// end subroutine example
+/// ```
+///
+/// If the variable's value is intended to be constant, then use the `parameter`
+/// attribute instead:
+///
+/// ```f90
+/// subroutine example()
+///   integer, parameter :: var = 1
+///   print*, var
+/// end subroutine example
+/// ```
+#[derive(ViolationMetadata)]
+pub(crate) struct InitialisationInDeclaration {
+    name: String,
+}
+
+impl Violation for InitialisationInDeclaration {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        let Self { name } = self;
+        format!("'{name}' is initialised in its declaration and has no explicit `save` or `parameter` attribute")
+    }
+}
+
+impl AstRule for InitialisationInDeclaration {
+    fn check(_settings: &Settings, node: &Node, src: &SourceFile) -> Option<Vec<Diagnostic>> {
+        let src = src.source_text();
+        // Only check in procedures
+        node.ancestors().find(|parent| {
+            ["function", "subroutine", "module_procedure"].contains(&parent.kind())
+        })?;
+
+        let declaration = node
+            .ancestors()
+            .find(|parent| parent.kind() == "variable_declaration")?;
+
+        // Init in declaration ok for save and parameter
+        if declaration
+            .children_by_field_name("attribute", &mut declaration.walk())
+            .filter_map(|attr| attr.to_text(src))
+            .any(|attr_name| ["save", "parameter"].contains(&attr_name.to_lowercase().as_str()))
+        {
+            return None;
+        }
+
+        let name = node.child_by_field_name("left")?.to_text(src)?.to_string();
+        some_vec![Diagnostic::from_node(Self { name }, node)]
+    }
+
+    fn entrypoints() -> Vec<&'static str> {
+        vec!["init_declarator"]
+    }
+}
---
+++ packages/fortitude-lint/fortitude_lint-0.7.1.tar.gz/fortitude_lint-0.7.1/fortitude/src/rules/modernisation/double_precision.rs
@@ -0,0 +1,76 @@
+use crate::ast::FortitudeNode;
+use crate::settings::Settings;
+use crate::{AstRule, FromAstNode};
+use ruff_diagnostics::{Diagnostic, Violation};
+use ruff_macros::{derive_message_formats, ViolationMetadata};
+use ruff_source_file::SourceFile;
+use tree_sitter::Node;
+
+// TODO rule to prefer 1.23e4_sp over 1.23e4, and 1.23e4_dp over 1.23d4
+
+/// ## What it does
+/// Checks for use of `double precision` and `double complex` types.
+///
+/// ## Why is this bad?
+/// The `double precision` type does not guarantee a 64-bit floating point number
+/// as one might expect, and instead is only required to have a higher decimal
+/// precision than the default `real`, which may vary depending on your system
+/// and can be modified by compiler arguments.
+///
+/// In modern Fortran, it is preferred to use `real` and `complex` and instead set
+/// the required precision using 'kinds'. For portability, it is recommended to use
+/// `real(dp)`, with `dp` set in one of the following ways:
+///
+/// - `use, intrinsic :: iso_fortran_env, only: dp => real64`
+/// - `integer, parameter :: dp = selected_real_kind(15, 307)`
+///
+/// For code that should be compatible with C, you should instead use
+/// `real(c_double)`, which may be found in the intrinsic module `iso_c_binding`.
+///
+/// ## References
+/// - Metcalf, M., Reid, J. and Cohen, M., 2018, _Modern Fortran Explained: Incorporating Fortran
+///   2018_, Oxford University Press, Appendix A 'Deprecated Features'
+/// - [Fortran-Lang Best Practices on Floating Point Numbers](https://fortran-lang.org/learn/best_practices/floating_point/)
+#[derive(ViolationMetadata)]
+pub(crate) struct DoublePrecision {
+    original: String, // TODO: could be &'static str
+    preferred: String,
+}
+
+impl DoublePrecision {
+    fn try_new<S: AsRef<str>>(original: S) -> Option<Self> {
+        match original.as_ref() {
+            "double precision" => Some(Self {
+                original: original.as_ref().to_string(),
+                preferred: "real(real64)".to_string(),
+            }),
+            "double complex" => Some(Self {
+                original: original.as_ref().to_string(),
+                preferred: "complex(real64)".to_string(),
+            }),
+            _ => None,
+        }
+    }
+}
+
+impl Violation for DoublePrecision {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        let DoublePrecision {
+            original,
+            preferred,
+        } = self;
+        format!("prefer '{preferred}' to '{original}' (see 'iso_fortran_env')")
+    }
+}
+
+impl AstRule for DoublePrecision {
+    fn check(_settings: &Settings, node: &Node, src: &SourceFile) -> Option<Vec<Diagnostic>> {
+        let txt = node.to_text(src.source_text())?.to_lowercase();
+        some_vec![Diagnostic::from_node(DoublePrecision::try_new(txt)?, node)]
+    }
+
+    fn entrypoints() -> Vec<&'static str> {
+        vec!["intrinsic_type"]
+    }
+}
