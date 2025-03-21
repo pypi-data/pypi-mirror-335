76e95b75d45fb4018e0d8e607587cb378e24e339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:05:42 +0000

    Add package proxsuite/0.7.2.post1/proxsuite-0.7.2.post1-0-cp310-cp310-win_amd64.whl

---
+++ packages/proxsuite/proxsuite-0.7.2.post1-0-cp310-cp310-win_amd64.whl/cmeel.prefix/lib/cmake/proxsuite/proxsuiteTargets-debug.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "proxsuite::instructionset" for configuration "Debug"
+set_property(TARGET proxsuite::instructionset APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(proxsuite::instructionset PROPERTIES
+  IMPORTED_COMMON_LANGUAGE_RUNTIME_DEBUG ""
+  IMPORTED_LOCATION_DEBUG "C:/Users/runneradmin/AppData/Local/Temp/cmeel-y0w_ing0/whl/cmeel.prefix/lib/python3.10/site-packages/proxsuite/instructionset.cp310-win_amd64.pyd"
+  )
+
+list(APPEND _cmake_import_check_targets proxsuite::instructionset )
+list(APPEND _cmake_import_check_files_for_proxsuite::instructionset "C:/Users/runneradmin/AppData/Local/Temp/cmeel-y0w_ing0/whl/cmeel.prefix/lib/python3.10/site-packages/proxsuite/instructionset.cp310-win_amd64.pyd" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
