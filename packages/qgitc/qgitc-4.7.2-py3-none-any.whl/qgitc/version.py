3b366b38f95a6970b5c9f19ffc9b406e7fe16434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 14:06:26 +0000

    Add package qgitc/4.7.2/qgitc-4.7.2-py3-none-any.whl

---
+++ packages/qgitc/qgitc-4.7.2-py3-none-any.whl/qgitc/version.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+VERSION_MAJOR = 4
+VERSION_MINOR = 7
+VERSION_PATCH = 2
+VERSION = "{}.{}.{}".format(VERSION_MAJOR,
+                            VERSION_MINOR,
+                            VERSION_PATCH)
