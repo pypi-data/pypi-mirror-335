d678b579b1ab9576eed6af32a60e2966db6047a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:04:56 +0000

    Add package fds.sdk.OFDB/0.32.1/fds.sdk.OFDB-0.32.1.tar.gz

---
+++ packages/fds.sdk.OFDB/fds.sdk.OFDB-0.32.1.tar.gz/fds.sdk.OFDB-0.32.1/fds/sdk/OFDB/models/__init__.py
@@ -0,0 +1,64 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from fds.sdk.OFDB.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from fds.sdk.OFDB.model.add_dates import AddDates
+from fds.sdk.OFDB.model.add_fields import AddFields
+from fds.sdk.OFDB.model.add_symbols import AddSymbols
+from fds.sdk.OFDB.model.bulk_delete import BulkDelete
+from fds.sdk.OFDB.model.bulk_delete_array import BulkDeleteArray
+from fds.sdk.OFDB.model.bulk_delete_array_item import BulkDeleteArrayItem
+from fds.sdk.OFDB.model.bulk_delete_array_item_array import BulkDeleteArrayItemArray
+from fds.sdk.OFDB.model.bulk_delete_array_item_nested import BulkDeleteArrayItemNested
+from fds.sdk.OFDB.model.bulk_delete_array_item_nested_array import BulkDeleteArrayItemNestedArray
+from fds.sdk.OFDB.model.bulk_delete_array_item_nested_object import BulkDeleteArrayItemNestedObject
+from fds.sdk.OFDB.model.bulk_delete_array_item_object import BulkDeleteArrayItemObject
+from fds.sdk.OFDB.model.bulk_delete_item import BulkDeleteItem
+from fds.sdk.OFDB.model.content_with_date import ContentWithDate
+from fds.sdk.OFDB.model.content_with_symbol import ContentWithSymbol
+from fds.sdk.OFDB.model.create_database import CreateDatabase
+from fds.sdk.OFDB.model.create_database_data import CreateDatabaseData
+from fds.sdk.OFDB.model.data_value import DataValue
+from fds.sdk.OFDB.model.database_schema import DatabaseSchema
+from fds.sdk.OFDB.model.database_schema_data import DatabaseSchemaData
+from fds.sdk.OFDB.model.date_and_content import DateAndContent
+from fds.sdk.OFDB.model.date_and_fields import DateAndFields
+from fds.sdk.OFDB.model.dates import Dates
+from fds.sdk.OFDB.model.dates_with_id import DatesWithId
+from fds.sdk.OFDB.model.error_object import ErrorObject
+from fds.sdk.OFDB.model.error_object_with_source import ErrorObjectWithSource
+from fds.sdk.OFDB.model.error_object_with_source_source import ErrorObjectWithSourceSource
+from fds.sdk.OFDB.model.field_properties import FieldProperties
+from fds.sdk.OFDB.model.fields import Fields
+from fds.sdk.OFDB.model.inline_response200 import InlineResponse200
+from fds.sdk.OFDB.model.inline_response2001 import InlineResponse2001
+from fds.sdk.OFDB.model.inline_response2001_data import InlineResponse2001Data
+from fds.sdk.OFDB.model.inline_response2002 import InlineResponse2002
+from fds.sdk.OFDB.model.inline_response2003 import InlineResponse2003
+from fds.sdk.OFDB.model.inline_response2004 import InlineResponse2004
+from fds.sdk.OFDB.model.inline_response2005 import InlineResponse2005
+from fds.sdk.OFDB.model.inline_response2006 import InlineResponse2006
+from fds.sdk.OFDB.model.inline_response200_data import InlineResponse200Data
+from fds.sdk.OFDB.model.inline_response201 import InlineResponse201
+from fds.sdk.OFDB.model.inline_response201_data import InlineResponse201Data
+from fds.sdk.OFDB.model.inline_response202 import InlineResponse202
+from fds.sdk.OFDB.model.inline_response202_data import InlineResponse202Data
+from fds.sdk.OFDB.model.inline_response400 import InlineResponse400
+from fds.sdk.OFDB.model.inline_response403 import InlineResponse403
+from fds.sdk.OFDB.model.success_post_response import SuccessPostResponse
+from fds.sdk.OFDB.model.success_post_response_data import SuccessPostResponseData
+from fds.sdk.OFDB.model.symbol_and_content import SymbolAndContent
+from fds.sdk.OFDB.model.symbol_and_fields import SymbolAndFields
+from fds.sdk.OFDB.model.symbols import Symbols
+from fds.sdk.OFDB.model.symbols_with_id import SymbolsWithId
+from fds.sdk.OFDB.model.update_date import UpdateDate
+from fds.sdk.OFDB.model.update_dates import UpdateDates
+from fds.sdk.OFDB.model.update_symbol import UpdateSymbol
+from fds.sdk.OFDB.model.update_symbols import UpdateSymbols
