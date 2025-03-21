ed9f6082a8bde3f8062a00bbad71a6d3c129806b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:06:00 +0000

    Add package mcp-run/0.3.0/mcp_run-0.3.0.tar.gz

---
+++ packages/mcp-run/mcp_run-0.3.0.tar.gz/mcp_run-0.3.0/mcp_run/plugin.py
@@ -0,0 +1,39 @@
+import base64
+import json
+import extism as ext
+
+from .types import Servlet, Content, CallResult
+
+
+class InstalledPlugin:
+    _install: Servlet
+    _plugin: ext.Plugin
+
+    def __init__(self, install, plugin):
+        self._install = install
+        self._plugin = plugin
+
+    def call(self, tool: str | None = None, input: dict = {}) -> CallResult:
+        """
+        Call a tool with the given input
+        """
+        if tool is None:
+            tool = self._install.name
+        j = json.dumps({"params": {"arguments": input, "name": tool}})
+        r = self._plugin.call("call", j)
+        r = json.loads(r)
+
+        out = []
+        for c in r["content"]:
+            ty = c["type"]
+            if ty == "text":
+                out.append(Content(type=ty, _data=c["text"].encode()))
+            elif ty == "image":
+                out.append(
+                    Content(
+                        type=ty,
+                        _data=base64.b64decode(c["data"]),
+                        mime_type=c["mimeType"],
+                    )
+                )
+        return CallResult(content=out)
