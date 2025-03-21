f61468178247f9517b7c1c8ab7c2045ef1f708f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:05:34 +0000

    Add package gaze-tracker/0.1.2/gaze_tracker-0.1.2.tar.gz

---
+++ packages/gaze-tracker/gaze_tracker-0.1.2.tar.gz/gaze_tracker-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gaze_tracker", 
+    version="0.1.2",
+    author="UNKNOWN",
+    author_email="UNKNOWN",
+    description="UNKNOWN",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/antoinelame/GazeTracking.git", 
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        # "numpy>=1.21.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
