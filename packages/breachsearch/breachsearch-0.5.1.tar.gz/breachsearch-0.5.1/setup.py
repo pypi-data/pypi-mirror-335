08890ba0e5260af8329673a527e0dca55010192f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:05:30 +0000

    Add package breachsearch/0.5.1/breachsearch-0.5.1.tar.gz

---
+++ packages/breachsearch/breachsearch-0.5.1.tar.gz/breachsearch-0.5.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="breachsearch",
+    version="0.5.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    entry_points={
+        "console_scripts": [
+            "breachsearch=breachsearch.main:main",
+        ],
+    },
+    author="Tintumon",
+    author_email="",
+    description="A tool to check if an data is part of a data breach.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/appuachu/breachsearch",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
