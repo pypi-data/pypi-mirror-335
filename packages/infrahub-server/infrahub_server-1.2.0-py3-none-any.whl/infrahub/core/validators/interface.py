bcd2c849c07d64f5ab9eaab70f80cf49a3892954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 14:06:28 +0000

    Add package infrahub-server/1.2.0/infrahub_server-1.2.0-py3-none-any.whl

---
+++ packages/infrahub-server/infrahub_server-1.2.0-py3-none-any.whl/infrahub/core/validators/interface.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+from infrahub.core.path import GroupedDataPaths
+
+from .model import SchemaConstraintValidatorRequest
+
+
+class ConstraintCheckerInterface(ABC):
+    @property
+    @abstractmethod
+    def name(self) -> str: ...
+
+    @abstractmethod
+    def supports(self, request: SchemaConstraintValidatorRequest) -> bool: ...
+
+    @abstractmethod
+    async def check(self, request: SchemaConstraintValidatorRequest) -> list[GroupedDataPaths]: ...
