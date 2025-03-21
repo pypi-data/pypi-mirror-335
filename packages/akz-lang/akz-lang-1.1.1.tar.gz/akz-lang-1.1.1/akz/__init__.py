f3ce28b54db63a1ef7a4376a62ff09f5ee1200c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:15:07 +0000

    Add package akz-lang/1.1.1/akz-lang-1.1.1.tar.gz

---
+++ packages/akz-lang/akz-lang-1.1.1.tar.gz/akz-lang-1.1.1/akz/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "1.1.1"
+__author__ = "Azzam Jauzi"
+__description__ = "Bahasa pemrograman Akz untuk pemula"
---
+++ packages/akz-lang/akz-lang-1.1.1.tar.gz/akz-lang-1.1.1/akz/transpiler.py
@@ -0,0 +1,32 @@
+import re
+
+class AKZTranspiler:
+    @staticmethod
+    def transpile(code: str) -> str:
+        # Bersihkan kode
+        code = re.sub(r'@@\s*|\s*\$\$', '', code)
+        
+        # Konversi sintaks dasar
+        replacements = [
+            (r'jika (.*?) \{', r'if \1:'),
+            (r'atau jika (.*?) \{', r'elif \1:'),
+            (r'lainnya \{', r'else:'),
+            (r'ulangi (\d+) kali \{', r'for _ in range(\1):'),
+            (r'masukkan "(.*?)" -> (\w+)', r'\2 = input("\1")'),
+            (r'tampilkan "(.*?)"', r'print("\1")'),
+            (r'tampilkan (.*?)\n', r'print(\1)\n'),
+            (r'//.*', ''),  # Hapus komentar
+            (r'\+', '+'),    # Tambah
+            (r'\-', '-'),    # Kurang
+            (r'\*', '*'),    # Kali
+            (r'\/', '/'),    # Bagi
+            (r'\^', '**'),   # Pangkat
+        ]
+        
+        for pattern, repl in replacements:
+            code = re.sub(pattern, repl, code)
+            
+        # Hapus kurung kurawal
+        code = code.replace('{', '').replace('}', '')
+        
+        return code
