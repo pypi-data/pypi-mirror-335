9efc93a3d39b432c3ec8ebabb26c41c65a0df1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 16:05:46 +0000

    Add package duc193test/0.1/duc193test-0.1.tar.gz

---
+++ packages/duc193test/duc193test-0.1.tar.gz/duc193test-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="duc193test",
+    version="0.1",
+    description="Thư viện test của duc193",
+    author="duc193",
+    packages=["duc193test"],
+    install_requires=[],
+)
