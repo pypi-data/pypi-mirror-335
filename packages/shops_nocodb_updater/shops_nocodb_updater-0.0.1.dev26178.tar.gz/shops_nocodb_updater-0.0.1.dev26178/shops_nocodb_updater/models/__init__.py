0adfa41dea53acd4c2bac3b1dd9b91f993fa2397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:05:20 +0000

    Add package shops_nocodb_updater/0.0.1.dev26178/shops_nocodb_updater-0.0.1.dev26178.tar.gz

---
+++ packages/shops_nocodb_updater/shops_nocodb_updater-0.0.1.dev26178.tar.gz/shops_nocodb_updater-0.0.1.dev26178/shops_nocodb_updater/models/__init__.py
@@ -0,0 +1,8 @@
+from shops_nocodb_updater.models.base import NocodbModel, AttachmentObject
+from shops_nocodb_updater.models.types import LongText
+
+__all__ = [
+    'NocodbModel',
+    'AttachmentObject',
+    'LongText'
+]
