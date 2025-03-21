f1db36f0fa9be1e710afc665f3568c4c6607f3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:05:45 +0000

    Add package dataroom/1.0.0/dataroom-1.0.0.tar.gz

---
+++ packages/dataroom/dataroom-1.0.0.tar.gz/dataroom-1.0.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dataroom",
+    version="1.0.0",
+    author="Mohammad Taha Gorji",
+    author_email="MohammadTahaGorjiProfile@gmail.com",
+    description="A powerful and easy-to-use data processing library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "requests",
+        "sqlalchemy",
+        "scikit-learn",
+    ],
+)
