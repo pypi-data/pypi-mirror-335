7f86a250513aed74d5168d0151cb402701a5e7bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:05:12 +0000

    Add package fortunecookiecravaggio/1.0.4/fortunecookiecravaggio-1.0.4.tar.gz

---
+++ packages/fortunecookiecravaggio/fortunecookiecravaggio-1.0.4.tar.gz/fortunecookiecravaggio-1.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fortunecookiecravaggio",
+    version="1.0.5",
+    author="Cravaggio",
+    description="A package that generates “fortune cookie” messages, programming wisdom, or absurd advice when called.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/software-students-spring2025/3-python-package-cravaggio", 
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True, 
+    package_data={"fortunecookiecravaggio": ["fortunes.txt"]}, 
+)
