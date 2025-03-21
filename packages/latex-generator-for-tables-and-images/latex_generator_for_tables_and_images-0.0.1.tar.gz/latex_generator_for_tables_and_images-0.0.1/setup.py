195bbb42f4d6d6f4f3c813875afd9d4f8808181a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:05:00 +0000

    Add package latex-generator-for-tables-and-images/0.0.1/latex_generator_for_tables_and_images-0.0.1.tar.gz

---
+++ packages/latex-generator-for-tables-and-images/latex_generator_for_tables_and_images-0.0.1.tar.gz/latex_generator_for_tables_and_images-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='latex_generator_for_tables_and_images',
+  version='0.0.1',
+  author='Artem',
+  author_email='Artems2311@mail.ru',
+  description='A library to generate code for tables and images.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://example.com',
+  packages=find_packages(),
+  install_requires=['requests>=2.25.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  python_requires='>=3.6'
+)
