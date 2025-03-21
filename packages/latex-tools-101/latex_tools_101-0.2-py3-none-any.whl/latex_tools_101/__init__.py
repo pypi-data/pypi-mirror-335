cc5e4137d47cec36b4625191d3e615670bdeb933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:05:57 +0000

    Add package latex-tools-101/0.2/latex_tools_101-0.2-py3-none-any.whl

---
+++ packages/latex-tools-101/latex_tools_101-0.2-py3-none-any.whl/latex_tools_101/__init__.py
@@ -0,0 +1,13 @@
+from latex_tools_101.latex_generators import (
+    execute_functions,
+    execute_tex,
+    generate_table_tex,
+    generate_image_tex
+)
+
+__all__ = [
+    'execute_functions',
+    'execute_tex',
+    'generate_table_tex',
+    'generate_image_tex'
+]
