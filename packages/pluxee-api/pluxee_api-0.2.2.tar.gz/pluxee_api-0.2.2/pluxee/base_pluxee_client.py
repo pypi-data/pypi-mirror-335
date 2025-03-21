12d4a66781c705af4f6099b5145094bf26847d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 07:04:49 +0000

    Add package pluxee-api/0.2.2/pluxee_api-0.2.2.tar.gz

---
+++ packages/pluxee-api/pluxee_api-0.2.2.tar.gz/pluxee_api-0.2.2/pluxee/base_pluxee_client.py
@@ -0,0 +1,189 @@
+import os
+from datetime import date, datetime
+from enum import Enum
+from typing import Optional, List, Union, Type
+
+from bs4 import BeautifulSoup
+
+from .exceptions import PluxeeAPIError, PluxeeLoginError
+
+import requests
+try:
+    import aiohttp
+    Session_Type = Type[Union[aiohttp.ClientSession, requests.Session]]
+except ImportError:
+    Session_Type = Type[requests.Session]
+
+
+class PassType(str, Enum):
+    """The different types of pass that are provided."""
+    LUNCH = "LUNCH"
+    ECO = "ECO"
+    CONSO = "CONSO"
+    GIFT = "GIFT"
+
+
+class PluxeeBalance:
+    """The balance of each pass."""
+    def __init__(self, lunch_pass: float, eco_pass: float, gift_pass: float, conso_pass: float):
+        self.lunch_pass = lunch_pass
+        self.eco_pass = eco_pass
+        self.gift_pass = gift_pass
+        self.conso_pass = conso_pass
+
+    def __str__(self):
+        return f"lunch_pass: {self.lunch_pass}\neco_pass: {self.eco_pass}\ngift_pass: {self.gift_pass}\nconso_pass: {self.conso_pass}"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class PluxeeTransaction:
+    """A payment or the reception of your pass."""
+    def __init__(self, date: date, amount: float, detail: str, merchant: str):
+        self.date = date
+        self.amount = amount
+        self.detail = detail
+        self.merchant = merchant
+
+    def __str__(self):
+        return f"date: {self.date}\namount: {self.amount}\ndetail: {self.detail}\nmerchant: {self.merchant}"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class _ResponseWrapper:
+    def __init__(self, content: str, status_code: int):
+        self.content = content
+        self.status_code = status_code
+
+
+class _PluxeeClient:
+    """
+    The business logic, how to parse and what information to extract.
+
+    Args:
+        username: The pluxee username.
+        password: The pluxee password.
+        language: The pluxee website language (either 'fr' or 'nl', defaults to 'fr').
+
+    Attrs:
+        username: The pluxee username.
+        password: The pluxee password.
+        language: The pluxee website language (either 'fr' or 'nl', defaults to 'fr').
+    """
+
+    DOMAIN = "users.pluxee.be"
+
+    LUNCH_PASS_SELECTOR = (
+        'body > div > header > div.header-fixed > div.balance-block > div > ul > li > a[href*="LUNCH"] > span.balance--price'
+    )
+    ECO_PASS_SELECTOR = (
+        'body > div > header > div.header-fixed > div.balance-block > div > ul > li > a[href*="ECO"] > span.balance--price'
+    )
+    GIFT_PASS_SELECTOR = (
+        'body > div > header > div.header-fixed > div.balance-block > div > ul > li > a[href*="GIFT"] > span.balance--price'
+    )
+    CONSO_PASS_SELECTOR = (
+        'body > div > header > div.header-fixed > div.balance-block > div > ul > li > a[href*="CONSO"] > span.balance--price'
+    )
+    TRANSACTION_SELECTOR = "body > div.dialog-off-canvas-main-canvas > div > div > div.transaction--section > div.transaction-list--section > div.transactions-list--table > div > table > tbody > tr"
+    TRANSACTION_TABLE_SELECTOR = "body > div.dialog-off-canvas-main-canvas > div > div > div.transaction--section > div.transaction-list--section > div.transactions-list--table > div > table"
+
+    def __init__(self, username: str, password: str, language: str = 'fr', session: Optional[Session_Type] = None):
+        self._username = username or os.environ.get("PLUXEE_USERNAME")
+        self._password = password or os.environ.get("PLUXEE_PASSWORD")
+        self._language = language
+        self._base_url_localized = f"https://{_PluxeeClient.DOMAIN}/{self._language}"
+        self._base_url_transactions = f"{self._base_url_localized}/" + ('mijn-sodexo-card-saldo' if self._language == 'nl' else 'mon-solde-sodexo-card')
+        self._session = session
+
+    @staticmethod
+    def _price_to_float(price) -> float:
+        return float(price.replace("€", "").replace(",", ".").replace("EUR", "").strip().replace(" ", ""))
+
+    def _parse_balance_from_response(self, response: _ResponseWrapper) -> PluxeeBalance:
+        soup = BeautifulSoup(response.content, features="html.parser")
+
+        lunch_tag = soup.select_one(self.LUNCH_PASS_SELECTOR)
+        lunch = self._price_to_float(lunch_tag.text) if lunch_tag is not None else 0
+
+        eco_tag = soup.select_one(self.ECO_PASS_SELECTOR)
+        eco = self._price_to_float(eco_tag.text) if eco_tag is not None else 0
+
+        gift_tag = soup.select_one(self.GIFT_PASS_SELECTOR)
+        gift = self._price_to_float(gift_tag.text) if gift_tag is not None else 0
+
+        conso_tag = soup.select_one(self.CONSO_PASS_SELECTOR)
+        conso = self._price_to_float(conso_tag.text) if conso_tag is not None else 0
+
+        if (lunch_tag, eco_tag, gift_tag, conso_tag) == (None, None, None, None):
+            raise PluxeeAPIError("Could not find the balance in the response")
+
+        return PluxeeBalance(lunch, eco, gift, conso)
+
+    def _parse_transactions_from_reponse(
+        self, response: _ResponseWrapper,
+        transactions: List[PluxeeTransaction],
+        since: Optional[date] = None,
+        until: Optional[date] = None,
+    ) -> bool:
+        dom = BeautifulSoup(response.content, features="html.parser")
+
+        table = dom.select_one(self.TRANSACTION_TABLE_SELECTOR)
+        if not table:
+            if not transactions:
+                # If there is not table, it means something unexpected appen.
+                raise PluxeeAPIError()
+            else:
+                # In the case where we already have some transactions in the list, it means we have reach an empty page.
+                return True
+
+        entries = dom.select(self.TRANSACTION_SELECTOR)
+        complete = len(entries) < 10
+        for entry in entries:
+            date_dom = entry.select_one("td.views-field-date")
+            merchant_dom = entry.select_one("td.views-field-description")
+            description_dom = entry.select_one("td.views-field-detail")
+            amount_dom = entry.select_one("td.views-field-amount > span")
+
+            if date_dom is None or merchant_dom is None or description_dom is None or amount_dom is None:
+                raise PluxeeAPIError("Could not find the transactions in the response")
+
+            date = datetime.strptime(date_dom.text.strip(), "%d.%m.%Y").date()
+            merchant = merchant_dom.text.strip()
+            description = description_dom.text.strip()
+            amount = self._price_to_float(amount_dom.text)
+            if since and date < since:
+                complete = True
+                break
+            if until and date < until:
+                transactions.append(PluxeeTransaction(date, amount, description, merchant))
+        return complete
+
+    def gen_login_post_args(self):
+        return {
+            "url": self._base_url_localized + "/frontpage",
+            "params": {
+                "destination": f"/{self._language}/frontpage",
+            },
+            "allow_redirects": False,
+            "data": {
+                "name": self._username,
+                "pass": self._password,
+                "form_build_id": "form_build_id",
+                "form_id": "user_login_form",
+                "op": "Se connecter",
+            },
+        }
+
+    def get_language(self):
+        return self._language
+
+    @staticmethod
+    def handle_login_status(status):
+        if status != 303:
+            if status == 302:
+                raise PluxeeLoginError(f"Bad username/password. {status}")
+            raise PluxeeAPIError(f"Pluxee webpage did not respond with the expected status. {status}")
---
+++ packages/pluxee-api/pluxee_api-0.2.2.tar.gz/pluxee_api-0.2.2/tests/test_async_pluxee_api.py
@@ -0,0 +1,261 @@
+import pathlib
+from datetime import date
+import pytest
+from pytest_mock import MockerFixture
+import aiohttp
+import ssl
+
+from pluxee import PassType, PluxeeAPIError, PluxeeBalance, PluxeeAsyncClient, PluxeeLoginError, PluxeeTransaction
+
+from .conftest import AsyncMockAPIResponse, async_mock
+
+test_data_dir = pathlib.Path(__file__).parent / "test_data"
+
+CONTENT_BALANCE = open(test_data_dir / "content_balance.html", "r", encoding="utf-8").read()
+CONTENT_EXPIRED_COOKIES = open(test_data_dir / "content_empty_balance.html", "r", encoding="utf-8").read()
+CONTENT_TRANSACTIONS = open(test_data_dir / "content_transactions.html", "r", encoding="utf-8").read()
+CONTENT_MALFORMED_TRANSACTIONS = open(test_data_dir / "content_malformed_transactions.html", "r", encoding="utf-8").read()
+
+
+
+
+@pytest.fixture(scope="function")
+def client():
+    return PluxeeAsyncClient("Foo", "Bar")
+
+
+class TestPluxeeAsyncClient:
+    def test_aiohttp_not_present(self, mocker):
+        mocker.patch('builtins.__import__', side_effect=ImportError)
+        try:
+            from pluxee import PluxeeAsyncClient # noqa
+            assert False
+        except ImportError:
+            pass
+        
+
+
+    @pytest.mark.asyncio
+    async def test_login(self, mocker, client: PluxeeAsyncClient):
+        async with aiohttp.ClientSession() as session:
+            mock_post: MockerFixture = mocker.patch(
+                "aiohttp.ClientSession.post",
+                return_value=AsyncMockAPIResponse(303, content="coucou", headers={"set-cookie": "key=value;..."}),
+            )
+            await client._login(session) 
+            mock_post.assert_called_once()
+            assert session.cookie_jar
+
+
+    @pytest.mark.asyncio
+    async def test_login_bad_password(self, mocker, client: PluxeeAsyncClient):
+        async with aiohttp.ClientSession() as session:
+            mock_post = mocker.patch(
+                "aiohttp.ClientSession.post",
+                return_value=AsyncMockAPIResponse(302, content="coucou", headers={"set-cookie": "key=value;..."}),
+            )
+
+            with pytest.raises(PluxeeLoginError):
+                await client._login(session)
+            mock_post.assert_called_once()
+
+
+    @pytest.mark.asyncio
+    async def test_login_unknown_error(self, mocker, client: PluxeeAsyncClient):
+        async with aiohttp.ClientSession() as session:
+            mock_post = mocker.patch(
+                "aiohttp.ClientSession.post",
+                return_value=AsyncMockAPIResponse(200, content="coucou", headers={"set-cookie": "key=value;..."}),
+            )
+
+            with pytest.raises(PluxeeAPIError):
+                await client._login(session)
+            mock_post.assert_called_once()
+
+
+    @pytest.mark.asyncio
+    async def test_login_bad_cookie(self, mocker, client: PluxeeAsyncClient):
+        async with aiohttp.ClientSession() as session:
+            mock_post = mocker.patch(
+                "aiohttp.ClientSession.post",
+                return_value=AsyncMockAPIResponse(303, content="coucou", headers={"set-cookie": "...;..."}),
+            )
+
+            with pytest.raises(PluxeeLoginError):
+                await client._login(session)
+            mock_post.assert_called_once()
+
+
+    @pytest.mark.asyncio
+    async def test_get_balance(self, mocker, client: PluxeeAsyncClient):
+        async with aiohttp.ClientSession() as _:
+            mock_get = mocker.patch(
+                "aiohttp.ClientSession.get",
+                return_value=AsyncMockAPIResponse(303, content=CONTENT_BALANCE),
+            )
+            mock_aia: MockerFixture = mocker.patch(
+                "pluxee.PluxeeAsyncClient.get_ssl_context",
+                side_effect=async_mock,
+            )
+
+            result = await client.get_balance()
+            mock_get.assert_called_once()
+            mock_aia.assert_called_once()
+            assert isinstance(result, PluxeeBalance)
+            assert result.lunch_pass == 1
+            assert result.eco_pass == 2
+            assert result.gift_pass == 3
+            assert result.conso_pass == 4
+
+
+    @pytest.mark.asyncio
+    async def test_get_balance_expired_cookie(self, mocker, client: PluxeeAsyncClient):
+        async with aiohttp.ClientSession() as _:
+            mock_get = mocker.patch(
+                "aiohttp.ClientSession.get",
+                side_effect=[
+                    AsyncMockAPIResponse(200, content=CONTENT_EXPIRED_COOKIES),
+                    AsyncMockAPIResponse(200, content=CONTENT_BALANCE),
+                ],
+            )
+            mock_aia: MockerFixture = mocker.patch(
+                "pluxee.PluxeeAsyncClient.get_ssl_context",
+                side_effect=async_mock,
+            )
+            mock_login = mocker.patch("pluxee.PluxeeAsyncClient._login", side_effect=async_mock)
+
+            result = await client.get_balance()
+            assert mock_get.call_count == 2
+            mock_login.assert_called_once()
+            mock_aia.assert_called_once()
+            assert isinstance(result, PluxeeBalance)
+            assert result.lunch_pass == 1
+            assert result.eco_pass == 2
+            assert result.gift_pass == 3
+            assert result.conso_pass == 4
+
+
+    @pytest.mark.asyncio
+    async def test_login_balances_not_found(self, mocker, client: PluxeeAsyncClient):
+        async with aiohttp.ClientSession() as _:
+            mock_get = mocker.patch(
+                "aiohttp.ClientSession.get",
+                return_value=AsyncMockAPIResponse(200, content=f"href=\"/{client.get_language()}/user/logout\""),
+            )
+            mock_aia: MockerFixture = mocker.patch(
+                "pluxee.PluxeeAsyncClient.get_ssl_context",
+                side_effect=async_mock,
+            )
+
+            with pytest.raises(PluxeeAPIError):
+                await client.get_balance()
+            mock_get.assert_called_once()
+            mock_aia.assert_called_once()
+
+
+    @pytest.mark.asyncio
+    async def test_get_transactions(self, mocker, client: PluxeeAsyncClient):
+        async with aiohttp.ClientSession() as _:
+            mock_get = mocker.patch(
+                "aiohttp.ClientSession.get",
+                return_value=AsyncMockAPIResponse(303, content=CONTENT_TRANSACTIONS),
+            )
+            mock_aia: MockerFixture = mocker.patch(
+                "pluxee.PluxeeAsyncClient.get_ssl_context",
+                side_effect=async_mock,
+            )
+
+            transactions = await client.get_transactions(PassType.LUNCH, date(2024, 1, 25), date(2024, 3, 1))
+            mock_get.assert_called_once()
+            mock_aia.assert_called_once()
+
+            assert isinstance(transactions, list)
+            assert len(transactions) == 2
+
+            transaction = transactions[1]
+            assert isinstance(transaction, PluxeeTransaction)
+            assert transaction.date == date(2024, 2, 6)
+            assert transaction.amount == -6.10
+            assert transaction.detail == "Paiement detail"
+            assert transaction.merchant == "THE MERCHANT"
+
+            transaction = transactions[0]
+            assert isinstance(transaction, PluxeeTransaction)
+            assert transaction.date == date(2024, 1, 28)
+            assert transaction.amount == 144
+            assert transaction.detail.replace("\r\n", "\n") == "18 cheques de 8 € = 144 €. Expiry date: \n28.01.2025"
+            assert transaction.merchant == "YOUR EMPLOYER"
+
+
+    @pytest.mark.asyncio
+    async def test_get_transactions_expired_cookie(self, mocker, client: PluxeeAsyncClient):
+        async with aiohttp.ClientSession() as _:
+            mock_get = mocker.patch(
+                "aiohttp.ClientSession.get",
+                side_effect=[
+                    AsyncMockAPIResponse(200, content=CONTENT_EXPIRED_COOKIES),
+                    AsyncMockAPIResponse(200, content=CONTENT_TRANSACTIONS),
+                ],
+            )
+            mock_aia: MockerFixture = mocker.patch(
+                "pluxee.PluxeeAsyncClient.get_ssl_context",
+                side_effect=async_mock,
+            )
+            mock_login = mocker.patch("pluxee.PluxeeAsyncClient._login", side_effect=async_mock)
+
+            transactions = await client.get_transactions(PassType.LUNCH, date(2024, 1, 25), date(2024, 3, 1))
+            assert mock_get.call_count == 2
+            mock_login.assert_called_once()
+            mock_aia.assert_called_once()
+            assert isinstance(transactions, list)
+            assert len(transactions) == 2
+
+            transaction = transactions[1]
+            assert isinstance(transaction, PluxeeTransaction)
+            assert transaction.date == date(2024, 2, 6)
+            assert transaction.amount == -6.10
+            assert transaction.detail == "Paiement detail"
+            assert transaction.merchant == "THE MERCHANT"
+
+            transaction = transactions[0]
+            assert isinstance(transaction, PluxeeTransaction)
+            assert transaction.date == date(2024, 1, 28)
+            assert transaction.amount == 144
+            assert transaction.detail.replace("\r\n", "\n") == "18 cheques de 8 € = 144 €. Expiry date: \n28.01.2025"
+            assert transaction.merchant == "YOUR EMPLOYER"
+
+
+    @pytest.mark.asyncio
+    async def test_login_transactions_not_found(self, mocker, client: PluxeeAsyncClient):
+        async with aiohttp.ClientSession() as _:
+            mock_get = mocker.patch(
+                "aiohttp.ClientSession.get",
+                return_value=AsyncMockAPIResponse(200, content=f"href=\"/{client.get_language()}/user/logout\""),
+            )
+            mock_aia: MockerFixture = mocker.patch(
+                "pluxee.PluxeeAsyncClient.get_ssl_context",
+                side_effect=async_mock,
+            )
+
+            with pytest.raises(PluxeeAPIError):
+                await client.get_transactions(PassType.LUNCH, date(2024, 1, 25), date(2024, 3, 1))
+            mock_get.assert_called_once()
+            mock_aia.assert_called_once()
+
+
+    @pytest.mark.asyncio
+    async def test_login_transactions_malformed(self, mocker, client: PluxeeAsyncClient):
+        async with aiohttp.ClientSession() as _:
+            mock_get = mocker.patch(
+                "aiohttp.ClientSession.get",
+                return_value=AsyncMockAPIResponse(200, content=CONTENT_MALFORMED_TRANSACTIONS),
+            )
+            mock_aia: MockerFixture = mocker.patch(
+                "pluxee.PluxeeAsyncClient.get_ssl_context",
+                side_effect=async_mock,
+            )
+
+            with pytest.raises(PluxeeAPIError):
+                await client.get_transactions(PassType.LUNCH, date(2024, 1, 25), date(2024, 3, 1))
+            mock_get.assert_called_once()
+            mock_aia.assert_called_once()
