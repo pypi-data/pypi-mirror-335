4d94117c46cfa354f76372a6e37db0ecf536980b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 07:04:55 +0000

    Add package latex-generator-VILGIZ/0.1.6/latex_generator_VILGIZ-0.1.6.tar.gz

---
+++ packages/latex-generator-VILGIZ/latex_generator_VILGIZ-0.1.6.tar.gz/latex_generator_VILGIZ-0.1.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="latex_generator_VILGIZ",
+    version="0.1.6",
+    packages=find_packages(),
+    install_requires=[],
+    author="VILGIZ",
+    description="LaTeX table and image generator",
+    long_description=open("README.md").read()
+)
