9d915be93d690b165a5da103821a896de9ae6293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 15:05:45 +0000

    Add package esrganenhance/0.4.1/esrganenhance-0.4.1.tar.gz

---
+++ packages/esrganenhance/esrganenhance-0.4.1.tar.gz/esrganenhance-0.4.1/esrganenhance/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa
+from .inference_realesrgan_video import *
+from .version import *
---
+++ packages/esrganenhance/esrganenhance-0.4.1.tar.gz/esrganenhance-0.4.1/esrganenhance/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Fri Mar 21 12:02:45 2025
+__version__ = '0.4.1'
+__gitsha__ = 'f6e601e'
+version_info = (0, 4, 1)
