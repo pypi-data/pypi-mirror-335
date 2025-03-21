e62d680589f310bcf4ad638486d1e4d2128cef07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:04:58 +0000

    Add package quantcrypt/1.0.1/quantcrypt-1.0.1-cp313-cp313-win_amd64.whl

---
+++ packages/quantcrypt/quantcrypt-1.0.1-cp313-cp313-win_amd64.whl/quantcrypt/internal/cli/commands/enc_dec.py
@@ -0,0 +1,109 @@
+#
+#   MIT License
+#
+#   Copyright (c) 2024, Mattias Aabmets
+#
+#   The contents of this file are subject to the terms and conditions defined in the License.
+#   You may not use, modify, or distribute this file except in compliance with the License.
+#
+#   SPDX-License-Identifier: MIT
+#
+
+from typer import Typer
+from quantcrypt.cipher import KryptonKEM
+from quantcrypt.internal import constants as const
+from quantcrypt.internal.cli import tools, console, annotations as ats
+
+
+enc_app = Typer(
+    name="encrypt", invoke_without_command=True, no_args_is_help=True,
+    help="Uses an ASCII armored KEM public key to encrypt a file with the Krypton cipher."
+)
+dec_app = Typer(
+    name="decrypt", invoke_without_command=True, no_args_is_help=True,
+    help="Uses an ASCII armored KEM secret key to decrypt a file with the Krypton cipher."
+)
+
+
+@enc_app.callback()
+def command_encrypt(
+        pk_file: ats.PubKeyFile,
+        in_file: ats.EncInFile,
+        out_file: ats.EncOutFile = None,
+        dry_run: ats.DryRun = False,
+        overwrite: ats.Overwrite = False,
+        non_interactive: ats.NonInteractive = False
+) -> None:
+    paths = tools.process_paths(pk_file, in_file, out_file, const.KryptonFileSuffix)
+    _common_flow(paths, dry_run, overwrite, non_interactive, const.PQAKeyType.PUBLIC)
+
+
+@dec_app.callback()
+def command_decrypt(
+        sk_file: ats.SecKeyFile,
+        in_file: ats.DecInFile,
+        out_file: ats.DecOutFile = None,
+        dry_run: ats.DryRun = False,
+        overwrite: ats.Overwrite = False,
+        non_interactive: ats.NonInteractive = False
+) -> None:
+    paths = tools.process_paths(sk_file, in_file, out_file, const.KryptonFileSuffix)
+    _common_flow(paths, dry_run, overwrite, non_interactive, const.PQAKeyType.SECRET)
+
+
+def _common_flow(
+        paths: tools.CommandPaths,
+        dry_run: ats.DryRun,
+        overwrite: ats.Overwrite,
+        non_interactive: ats.NonInteractive,
+        key_type: const.PQAKeyType
+) -> None:
+    console.notify_dry_run(dry_run)
+
+    with paths.key_file.open('r') as file:
+        armored_key = file.read()
+
+    armor_name = tools.validate_armored_key(armored_key, key_type, const.PQAType.KEM)
+
+    files = [paths.in_file, paths.key_file]
+    a, b = [f"[italic sky_blue2]{f.name.lower()}[/]" for f in files]
+
+    if key_type == const.PQAKeyType.PUBLIC:
+        console.styled_print(
+            f"QuantCrypt is about to encrypt the {a} plaintext file with the \n"
+            f"{b} KEM PK file into the following binary ciphertext file: \n"
+            f"[italic tan]{paths.out_file} \n"
+        )
+    else:
+        console.styled_print(
+            f"QuantCrypt is about to decrypt the {a} ciphertext file with \n"
+            f"the {b} KEM SK file into the following plaintext file: \n"
+            f"[italic tan]{paths.out_file} \n"
+        )
+
+    if not non_interactive:
+        console.ask_continue(exit_on_false=True)
+
+    if paths.out_file.exists():
+        console.ask_overwrite_files(non_interactive, overwrite,True)
+
+    if dry_run:
+        console.styled_print("QuantCrypt would have created the following file:")
+        console.pretty_print([paths.out_file.as_posix()])
+        return
+
+    kem_cls = tools.get_pqa_class(armor_name)
+    krypton = KryptonKEM(kem_cls)
+    if key_type == const.PQAKeyType.PUBLIC:
+        krypton.encrypt(
+            public_key=armored_key,
+            data_file=paths.in_file,
+            output_file=paths.out_file
+        )
+    else:
+        krypton.decrypt_to_file(
+            secret_key=armored_key,
+            encrypted_file=paths.in_file,
+            output_file=paths.out_file
+        )
+    console.print_success()
---
+++ packages/quantcrypt/quantcrypt-1.0.1-cp313-cp313-win_amd64.whl/quantcrypt/internal/errors.py
@@ -0,0 +1,167 @@
+#
+#   MIT License
+#   
+#   Copyright (c) 2024, Mattias Aabmets
+#   
+#   The contents of this file are subject to the terms and conditions defined in the License.
+#   You may not use, modify, or distribute this file except in compliance with the License.
+#   
+#   SPDX-License-Identifier: MIT
+#
+
+import platform
+from typing import Literal
+from quantcrypt.internal import constants as const
+
+
+__all__ = [
+	"QuantCryptError",
+	"InvalidUsageError",
+	"InvalidArgsError",
+	"UnsupportedPlatformError",
+
+	"PQAError",
+	"PQAImportError",
+	"PQAUnsupportedAlgoError",
+	"PQAKeyArmorError",
+	"KEMKeygenFailedError",
+	"KEMEncapsFailedError",
+	"KEMDecapsFailedError",
+	"DSSKeygenFailedError",
+	"DSSSignFailedError",
+	"DSSVerifyFailedError",
+
+	"KDFError",
+	"KDFOutputLimitError",
+	"KDFWeakPasswordError",
+	"KDFVerificationError",
+	"KDFInvalidHashError",
+	"KDFHashingError",
+
+	"CipherError",
+	"CipherStateError",
+	"CipherVerifyError",
+	"CipherChunkSizeError",
+	"CipherPaddingError"
+]
+
+
+class QuantCryptError(Exception):
+	"""Base class for all QuantCrypt errors."""
+
+
+class InvalidUsageError(QuantCryptError):
+	def __init__(self, message: str = None):
+		super().__init__(message or "Invalid usage of object.")
+
+
+class InvalidArgsError(QuantCryptError):
+	def __init__(self, message: str = None):
+		super().__init__(message or "Method received invalid arguments.")
+
+
+class UnsupportedPlatformError(QuantCryptError):
+	def __init__(self):
+		super().__init__(f"Operating system '{platform.system()}' not supported.")
+
+
+class PQAError(QuantCryptError):
+	"""Base class for all PQC errors."""
+
+
+class PQAImportError(PQAError):
+	def __init__(self, spec: const.AlgoSpec, variant: const.PQAVariant):
+		super().__init__(f"Failed to import {variant.value} binaries of the {spec.class_name} algorithm.")
+
+
+class PQAUnsupportedAlgoError(PQAError):
+	def __init__(self, cls_name: str):
+		super().__init__(f"Unsupported PQA class '{cls_name}'.")
+
+
+class PQAKeyArmorError(PQAError):
+	def __init__(self, verb: Literal["armor", "dearmor"]):
+		super().__init__(f"QuantCrypt will not {verb} a corrupted key.")
+
+
+class KEMKeygenFailedError(PQAError):
+	def __init__(self):
+		super().__init__("QuantCrypt KEM keygen failed.")
+
+
+class KEMEncapsFailedError(PQAError):
+	def __init__(self):
+		super().__init__("QuantCrypt KEM encaps failed.")
+
+
+class KEMDecapsFailedError(PQAError):
+	def __init__(self):
+		super().__init__("QuantCrypt KEM decaps failed.")
+
+
+class DSSKeygenFailedError(PQAError):
+	def __init__(self):
+		super().__init__("QuantCrypt DSS keygen failed.")
+
+
+class DSSSignFailedError(PQAError):
+	def __init__(self):
+		super().__init__("QuantCrypt DSS sign failed.")
+
+
+class DSSVerifyFailedError(PQAError):
+	def __init__(self):
+		super().__init__("QuantCrypt DSS verify failed.")
+
+
+class KDFError(QuantCryptError):
+	"""Base class for all KDF errors."""
+
+
+class KDFOutputLimitError(KDFError):
+	def __init__(self, limit: int):
+		super().__init__(f"Not allowed to derive more than {limit} bytes of keys from one master key.")
+
+
+class KDFWeakPasswordError(KDFError):
+	def __init__(self):
+		super().__init__("Weak passwords are not allowed.")
+
+
+class KDFVerificationError(KDFError):
+	def __init__(self):
+		super().__init__("KDF failed to verify the password against the provided public hash.")
+
+
+class KDFInvalidHashError(KDFError):
+	def __init__(self):
+		super().__init__("KDF was provided with an invalid hash for verification.")
+
+
+class KDFHashingError(KDFError):
+	def __init__(self):
+		super().__init__("KDF was unable to hash the password due to an internal error.")
+
+
+class CipherError(QuantCryptError):
+	"""Base class for all Cipher errors."""
+
+
+class CipherStateError(CipherError):
+	def __init__(self):
+		super().__init__("Cannot call this method in the current cipher state.")
+
+
+class CipherVerifyError(CipherError):
+	def __init__(self):
+		super().__init__("Cannot verify the decrypted data with the provided digest.")
+
+
+class CipherChunkSizeError(CipherError):
+	def __init__(self):
+		super().__init__("Data is larger than the allowed chunk size.")
+
+
+class CipherPaddingError(CipherError):
+	def __init__(self):
+		super().__init__("The padding of the decrypted plaintext is incorrect.")
