9607657b7d5b9c1e5025db8ca2da8c4914f0b349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 14:06:30 +0000

    Add package esrganenhance/0.3.8/esrganenhance-0.3.8-py3-none-any.whl

---
+++ packages/esrganenhance/esrganenhance-0.3.8-py3-none-any.whl/esrgan_video_enhance/__init__.py
@@ -0,0 +1,2 @@
+# flake8: noqa
+from .inference_realesrgan_video import *
---
+++ packages/esrganenhance/esrganenhance-0.3.8-py3-none-any.whl/esrganenhance/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Fri Mar 21 10:29:53 2025
+__version__ = '0.3.8'
+__gitsha__ = 'fed82cc'
+version_info = (0, 3, 8)
