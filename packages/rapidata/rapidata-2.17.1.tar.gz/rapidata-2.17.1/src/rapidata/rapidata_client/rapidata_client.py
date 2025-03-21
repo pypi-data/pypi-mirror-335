d890f5692146692b0fde890bb621984da5fdb81a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:14:47 +0000

    Add package rapidata/2.17.1/rapidata-2.17.1.tar.gz

---
+++ packages/rapidata/rapidata-2.17.1.tar.gz/rapidata-2.17.1/src/rapidata/rapidata_client/rapidata_client.py
@@ -0,0 +1,48 @@
+from rapidata.service.openapi_service import OpenAPIService
+
+from rapidata.rapidata_client.order.rapidata_order_manager import RapidataOrderManager
+
+from rapidata.rapidata_client.validation.validation_set_manager import ValidationSetManager
+
+from rapidata.rapidata_client.demographic.demographic_manager import DemographicManager
+
+class RapidataClient:
+    """The Rapidata client is the main entry point for interacting with the Rapidata API. It allows you to create orders and validation sets."""
+    
+    def __init__(
+        self,
+        client_id: str | None = None,
+        client_secret: str | None = None,
+        enviroment: str = "rapidata.ai",
+        oauth_scope: str = "openid",
+        cert_path: str | None = None,
+    ):
+        """Initialize the RapidataClient. If both the client_id and client_secret are None, it will try using your credentials under "~/.config/rapidata/credentials.json". 
+        If this is not successful, it will open a browser windown and ask you to log in, then save your new credentials in said json file.
+
+        Args:
+            client_id (str): The client ID for authentication.
+            client_secret (str): The client secret for authentication.
+            enviroment (str, optional): The API endpoint.
+
+        Attributes:
+            order (RapidataOrderManager): The RapidataOrderManager instance.
+            validation (ValidationSetManager): The ValidationSetManager instance.
+        """
+        self._openapi_service = OpenAPIService(
+            client_id=client_id,
+            client_secret=client_secret,
+            enviroment=enviroment,
+            oauth_scope=oauth_scope,
+            cert_path=cert_path
+        )
+        
+        self.order = RapidataOrderManager(openapi_service=self._openapi_service)
+        
+        self.validation = ValidationSetManager(openapi_service=self._openapi_service)
+
+        self._demographic = DemographicManager(openapi_service=self._openapi_service)
+
+    def reset_credentials(self):
+        """Reset the credentials saved in the configuration file for the current enviroment."""
+        self._openapi_service.reset_credentials()
