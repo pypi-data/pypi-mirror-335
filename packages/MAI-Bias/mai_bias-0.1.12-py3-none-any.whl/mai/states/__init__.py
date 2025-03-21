85c046e4d3247543dd422cafb32586474e968ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:15:04 +0000

    Add package MAI-Bias/0.1.12/mai_bias-0.1.12-py3-none-any.whl

---
+++ packages/MAI-Bias/mai_bias-0.1.12-py3-none-any.whl/mai/states/__init__.py
@@ -0,0 +1,5 @@
+from mai.states import step
+from mai.states import style
+from mai.states import dashboard
+from mai.states import results
+from mai.states import steps
---
+++ packages/MAI-Bias/mai_bias-0.1.12-py3-none-any.whl/tests/test_tc8_augmentation_report.py
@@ -0,0 +1,21 @@
+from mammoth import testing
+from catalogue.dataset_loaders.uci_csv import data_uci
+from catalogue.model_loaders.no_model import no_model
+from catalogue.metrics.augmentation_report import (
+    augmentation_report,
+)
+
+
+def test_augmentation_report():
+    with testing.Env(no_model, data_uci, augmentation_report) as env:
+        dataset_name = "credit"
+        target = "Y"
+        dataset = env.data_uci(dataset_name=dataset_name, target=target)
+        model = env.no_model()
+        sensitive = ["X2", "X3"]
+        html_result = env.augmentation_report(dataset, model, sensitive=sensitive)
+        html_result.show()
+
+
+if __name__ == "__main__":
+    test_augmentation_report()
