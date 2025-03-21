371453ad7f6ab8706ce59f607ad78fe8cc680877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 08:05:24 +0000

    Add package bricklink-py/0.1.2b0/bricklink_py-0.1.2b0-py3-none-any.whl

---
+++ packages/bricklink-py/bricklink_py-0.1.2b0-py3-none-any.whl/bricklink_py/__init__.py
@@ -0,0 +1,9 @@
+from .bricklink import Bricklink
+
+__all__ = ["Bricklink"]
+
+# Package metadata
+__version__ = "0.1.2-beta"
+__author__ = "Juan Franco"
+__email__ = "bricklink_py@juanfg.es"
+__description__ = "A wrapper for interacting with the Bricklink API."
