e10bb61e178377fe53c50eac4d71515dcfe3ccbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 15:06:22 +0000

    Add package daft-lts/0.4.8/daft_lts-0.4.8-cp39-abi3-win_amd64.whl

---
+++ packages/daft-lts/daft_lts-0.4.8-cp39-abi3-win_amd64.whl/daft/viz/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from .dataframe_display import DataFrameDisplay
+from .html_viz_hooks import register_viz_hook
+
+__all__ = ["DataFrameDisplay", "register_viz_hook"]
