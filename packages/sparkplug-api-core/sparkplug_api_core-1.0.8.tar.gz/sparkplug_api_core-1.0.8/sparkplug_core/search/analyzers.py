696c86d44c55a55c2d99fb55490841d275be208d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:05:39 +0000

    Add package sparkplug-api-core/1.0.8/sparkplug_api_core-1.0.8.tar.gz

---
+++ packages/sparkplug-api-core/sparkplug_api_core-1.0.8.tar.gz/sparkplug_api_core-1.0.8/sparkplug_core/search/analyzers.py
@@ -0,0 +1,31 @@
+# contrib
+from elasticsearch_dsl import analyzer
+
+# app
+from .token_filters import (
+    edge_ngram_filter,
+    trigram_filter,
+)
+
+edge_ngram = analyzer(
+    "edge_ngram",
+    tokenizer="standard",
+    char_filter=["html_strip"],
+    filter=[
+        "lowercase",
+        "asciifolding",
+        edge_ngram_filter,
+    ],
+)
+
+
+trigram = analyzer(
+    "trigram",
+    tokenizer="standard",
+    char_filter=["html_strip"],
+    filter=[
+        "lowercase",
+        "asciifolding",
+        trigram_filter,
+    ],
+)
