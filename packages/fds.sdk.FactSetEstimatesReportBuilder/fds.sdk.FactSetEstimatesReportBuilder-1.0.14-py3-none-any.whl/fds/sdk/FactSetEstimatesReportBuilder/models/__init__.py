f73f3229d67775acbef47302a2ed34820861bcb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:05:06 +0000

    Add package fds.sdk.FactSetEstimatesReportBuilder/1.0.14/fds.sdk.FactSetEstimatesReportBuilder-1.0.14-py3-none-any.whl

---
+++ packages/fds.sdk.FactSetEstimatesReportBuilder/fds.sdk.FactSetEstimatesReportBuilder-1.0.14-py3-none-any.whl/fds/sdk/FactSetEstimatesReportBuilder/models/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from fds.sdk.FactSetEstimatesReportBuilder.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from fds.sdk.FactSetEstimatesReportBuilder.model.error_object import ErrorObject
+from fds.sdk.FactSetEstimatesReportBuilder.model.error_object_links import ErrorObjectLinks
+from fds.sdk.FactSetEstimatesReportBuilder.model.error_object_source import ErrorObjectSource
+from fds.sdk.FactSetEstimatesReportBuilder.model.error_response import ErrorResponse
+from fds.sdk.FactSetEstimatesReportBuilder.model.meta import Meta
+from fds.sdk.FactSetEstimatesReportBuilder.model.response import Response
