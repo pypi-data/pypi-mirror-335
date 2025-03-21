462a486654a02b72519c3b0083df93e9e7c72be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:04:54 +0000

    Add package dbformatter/0.0.2/dbformatter-0.0.2.tar.gz

---
+++ packages/dbformatter/dbformatter-0.0.2.tar.gz/dbformatter-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="dbformatter",
+    version="0.0.2",
+    author="bruteforcer",
+    author_email="admin@coopers1337.xyz",
+    description="A trash database formatter.",
+    long_description="Trash project",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
