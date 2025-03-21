6c3d97bb2fa3dd0727a491dc29a58432702a09cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:05:35 +0000

    Add package ezoff/0.1.29/ezoff-0.1.29.tar.gz

---
+++ packages/ezoff/ezoff-0.1.29.tar.gz/ezoff-0.1.29/ezoff/ezoff.py
@@ -0,0 +1,13 @@
+"""
+Module for interacting with the EZOffice Inventory API
+"""
+
+from .assets import *
+from .exceptions import *
+from .groups import *
+from .inventory import *
+from .locations import *
+from .members import *
+from .projects import *
+from .workorders import *
+from .checklist import *
