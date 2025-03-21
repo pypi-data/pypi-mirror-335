b31ac1d77d70840605afb02fc69cb5bcf746e9ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 15:06:57 +0000

    Add package ddapi/0.9.3/ddapi-0.9.3.tar.gz

---
+++ packages/ddapi/ddapi-0.9.3.tar.gz/ddapi-0.9.3/ddapi/ddapi/__init__.py
@@ -0,0 +1,7 @@
+"""the module is made for the convenience of users to download and use"""
+from .main import DDnetApi, DDstats, Status
+from .scheme import *
+
+__version__ = "0.9.3"
+__author__ = 'ByFox'
+__LICENSE__ = "MIT"
