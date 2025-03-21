b981f4132aabeec2a9dc53506476ace1a757c640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:04:59 +0000

    Add package cryptogamax/0.1.0/cryptogamax-0.1.0.tar.gz

---
+++ packages/cryptogamax/cryptogamax-0.1.0.tar.gz/cryptogamax-0.1.0/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="cryptogamax",
+    version="0.1.0",
+    description="A Super Strong Cipher for secure encryption and decryption.",
+    author="Alfisene Keita",
+    packages=find_packages(),
+    install_requires=[
+        'pycryptodome',
+        'cryptography',  # Ensure this dependency is installed
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
