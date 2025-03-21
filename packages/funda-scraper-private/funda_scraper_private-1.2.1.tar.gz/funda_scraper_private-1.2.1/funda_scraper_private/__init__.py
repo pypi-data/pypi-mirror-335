f574e8567c1f122728852a0304d2f54c1fd4f627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:05:29 +0000

    Add package funda-scraper-private/1.2.1/funda_scraper_private-1.2.1.tar.gz

---
+++ packages/funda-scraper-private/funda_scraper_private-1.2.1.tar.gz/funda_scraper_private-1.2.1/funda_scraper_private/__init__.py
@@ -0,0 +1,5 @@
+"""Access the directory in python"""
+
+from funda_scraper_private.scrape import FundaScraper
+
+__all__ = ["FundaScraper"]
---
+++ packages/funda-scraper-private/funda_scraper_private-1.2.1.tar.gz/funda_scraper_private-1.2.1/funda_scraper_private/config/core.py
@@ -0,0 +1,17 @@
+"""Configuration used for scraping"""
+
+from pathlib import Path
+
+import yaml
+from diot import Diot
+from yaml.loader import SafeLoader
+
+import funda_scraper
+
+PACKAGE_ROOT = Path(funda_scraper.__file__).resolve().parent
+CONFIG_PATH = PACKAGE_ROOT / "config/config.yaml"
+
+with open(CONFIG_PATH) as f:
+    data = yaml.load(f, Loader=SafeLoader)
+
+config = Diot(data)
