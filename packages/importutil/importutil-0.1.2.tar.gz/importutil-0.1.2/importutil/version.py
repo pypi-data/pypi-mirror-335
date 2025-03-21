9cbb834cb534f300afabb02696b899d8389a09c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 16:05:26 +0000

    Add package importutil/0.1.2/importutil-0.1.2.tar.gz

---
+++ packages/importutil/importutil-0.1.2.tar.gz/importutil-0.1.2/importutil/version.py
@@ -0,0 +1,8 @@
+"""
+v0.1.0      20241128    1. first release
+v0.1.1      20241129    1. add import_file()
+v0.1.2      20250321    1. add get_sys_path() and add_sys_path()
+"""
+
+__version__ = "0.1.2"
+__repository__ = r"https://github.com/Hom-Wang/importutil.git"
