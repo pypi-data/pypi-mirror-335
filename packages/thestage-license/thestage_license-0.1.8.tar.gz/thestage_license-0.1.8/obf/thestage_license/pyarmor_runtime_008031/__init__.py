c6d7ba23749f00278aed82bfe4ec4a1437c14f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 15:05:51 +0000

    Add package thestage-license/0.1.8/thestage_license-0.1.8.tar.gz

---
+++ packages/thestage-license/thestage_license-0.1.8.tar.gz/thestage_license-0.1.8/obf/thestage_license/pyarmor_runtime_008031/__init__.py
@@ -0,0 +1,3 @@
+# Pyarmor 9.1.1 (ci), 008031, 2025-03-21T06:04:07.932802
+from sys import version_info as py_version
+__pyarmor__ = __import__("py%d%d.pyarmor_runtime" % py_version[:2], globals(), locals(), ["__pyarmor__"], 1).__pyarmor__
