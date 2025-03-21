4a62b0836c3e04d6bd10a20e730e51b89f57f88d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:05:36 +0000

    Add package imputegap/1.0.6/imputegap-1.0.6-py3-none-any.whl

---
+++ packages/imputegap/imputegap-1.0.6-py3-none-any.whl/imputegap/wrapper/AlgoPython/DeepMVI/recoveryDeepMVI.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import numpy as np
+
+from imputegap.wrapper.AlgoPython.DeepMVI.transformer import transformer_recovery
+
+def recover_matrix(matrix, max_epoch=1000, patience=2, lr=1e-3):
+    temp = transformer_recovery(matrix, max_epoch, patience, lr)
+    return temp
+
+# end function
+
+
+def deep_mvi_recovery(input, max_epoch=1000, patience=2, lr=1e-3):
+
+    print("\t\t(PYTHON) DEEP MVI: Matrix Shape: (", input.shape[0], ", ", input.shape[1], ") "
+            "for max_epoch ", max_epoch, ", patience ", patience,
+            ", lr ", lr, "...")
+
+    # read input matrix
+    matrix = input
+    
+    matrix_imputed = recover_matrix(matrix, max_epoch, patience, lr)
+    
+    # verification
+    nan_mask = np.isnan(matrix_imputed)
+    matrix_imputed[nan_mask] = np.sqrt(np.finfo('d').max / 100000.0)
+
+    return matrix_imputed
+    # end if
+
+# end function
