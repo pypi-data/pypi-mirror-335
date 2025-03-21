d09a298b6002ab2ca9cb2a7922939517af6e7b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:05:23 +0000

    Add package kvcommon/0.2.9/kvcommon-0.2.9-py3-none-any.whl

---
+++ packages/kvcommon/kvcommon-0.2.9-py3-none-any.whl/kvcommon/config/env.py
@@ -0,0 +1,72 @@
+import os
+from typing import Collection
+from typing import Generic
+from typing import Type
+
+from kvcommon.types import to_bool
+
+from .var import ConfigVarType
+from .var import ConfigVarImmutable
+from .validators import VarValidator
+
+
+def os_get_env(key: str, default: str = "") -> str:
+    # Typed wrapper
+    if not isinstance(default, str):
+        raise TypeError(
+            f"os_get_env default must be of type 'str' - got: '{default}' (type: '{type(default).__name__}')"
+        )
+    value = os.getenv(key, default=default)
+    if value is None:
+        return ""
+    return value
+
+
+class EnvVar(Generic[ConfigVarType]):
+    _name: str
+    _env_var_key: str
+    _coerce_type: Type[ConfigVarType]
+    _var: ConfigVarImmutable
+
+    def __init__(self, key: str, default: ConfigVarType, coerce_type: Type[ConfigVarType]) -> None:
+
+        self._env_var_key = key
+        self._coerce_type = coerce_type
+
+        value_from_env = os_get_env(key)
+        if not value_from_env:
+            value_from_env = default
+
+        if coerce_type == bool:
+            coerced_value = to_bool(value_from_env)
+        elif coerce_type == int:
+            coerced_value = int(value_from_env)
+        elif coerce_type == float:
+            coerced_value = float(value_from_env)
+        elif coerce_type == str:
+            coerced_value = str(value_from_env)
+        elif coerce_type == list:
+            values = str(value_from_env).split(",")
+            coerced_value = [value.strip() for value in values]
+
+        self._var = ConfigVarImmutable(name=key, value=coerced_value, expected_type=coerce_type)
+
+    def __str__(self) -> str:
+        return str(self._var.value)
+
+    def __repr__(self) -> str:
+        return f"<EnvVar:{self._coerce_type.__name__}:{str(self._var.value)}>"
+
+    @property
+    def name(self) -> str:
+        return self._var._name
+
+    @property
+    def value(self) -> ConfigVarType:
+        return self._var.value
+
+    def add_validator(self, new_validator: VarValidator | Collection[VarValidator]):
+        return self._var.add_validator(new_validator)
+
+    def validate(self):
+        return self._var.validate()
