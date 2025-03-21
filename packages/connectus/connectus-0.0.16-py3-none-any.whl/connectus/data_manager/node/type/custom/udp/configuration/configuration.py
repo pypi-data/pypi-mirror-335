5084c484a79a62b5273a761f962e1ed258619a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:05:01 +0000

    Add package connectus/0.0.16/connectus-0.0.16-py3-none-any.whl

---
+++ packages/connectus/connectus-0.0.16-py3-none-any.whl/connectus/data_manager/node/type/custom/udp/configuration/configuration.py
@@ -0,0 +1,9 @@
+
+class Configuration:
+    def __init__(self, node_config: dict[str, str]):
+        self.type = 'communication'
+        self.id = 'udp'
+        self.timeout = node_config['timeout']
+        self.server_ip = node_config['ip']
+        self.server_port = node_config['port']  # default server port
+        self.node_id = node_config['node_id']
---
+++ packages/connectus/connectus-0.0.16-py3-none-any.whl/connectus/data_manager/node/type/custom/udp/udp.py
@@ -0,0 +1,44 @@
+from ...base import BaseNode
+from .configuration import Configuration
+import asyncio
+import socket
+
+class UDP(BaseNode, Configuration):
+    def __init__(self, node_config: dict[str, str], stop_event: asyncio.Event):
+        BaseNode.__init__(self, stop_event)
+        Configuration.__init__(self, node_config)
+
+    async def connect(self):
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.sock.bind((self.server_ip, self.server_port))
+            self.sock.setblocking(False)
+            self.sock.settimeout(self.timeout)
+            print("UDP Server started with IP:", self.server_ip, "and port:", self.server_port)
+        except Exception as e:
+            print("An error occurred while starting UDP:", e)
+    
+    async def disconnect(self):
+        self.sock.close()
+
+    def write(self, data: list[str], node_params: dict[str, any]):
+        try:
+            addr = (node_params['ip_client'], node_params['port_client'])
+            for msg in data:
+                self.sock.sendto(msg.encode(), addr)
+        except Exception as e:
+            print("An error occurred while sending data with UDP protocol:", e)
+
+    def read(self):
+        try:
+            msg, addr = self.sock.recvfrom(65535)
+            decoded_msg = msg.decode('latin-1')
+            return decoded_msg
+        except TimeoutError:
+            pass
+        except asyncio.TimeoutError:
+            pass
+        except ConnectionResetError:
+            pass
+        except Exception as e:
+            print("An error occurred while receiving data with UDP protocol:", e)
