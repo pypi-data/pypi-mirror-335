57fa1a4b6bdcbccfd2bbe9ae42b44e473370953e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:04:52 +0000

    Add package pydantic_to_flat_dict_and_back/1.0.0/pydantic_to_flat_dict_and_back-1.0.0-py3-none-any.whl

---
+++ packages/pydantic_to_flat_dict_and_back/pydantic_to_flat_dict_and_back-1.0.0-py3-none-any.whl/pydantic_to_flat_dict_and_back/__init__.py
@@ -0,0 +1,11 @@
+from .src import (
+    check_pydantic_model_compatibility,
+    flat_dict_to_pydantic,
+    pydantic_to_flat_dict,
+)
+
+__all__ = [
+    "pydantic_to_flat_dict",
+    "flat_dict_to_pydantic",
+    "check_pydantic_model_compatibility",
+]
