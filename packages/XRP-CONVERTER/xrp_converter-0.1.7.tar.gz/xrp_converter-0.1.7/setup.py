6348bb40a4510d5c0ba8b5998b08159e8918ba17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:04:57 +0000

    Add package XRP-CONVERTER/0.1.7/xrp_converter-0.1.7.tar.gz

---
+++ packages/XRP-CONVERTER/xrp_converter-0.1.7.tar.gz/xrp_converter-0.1.7/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='XRP_converter',
+    version='0.1.7',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+        'numpy',
+        'tqdm',
+        'matplotlib',
+        'scikit-learn',
+        'flask',
+        'beautifulsoup4',
+        'pytest',
+        'pydantic',
+        'asyncio'
+    ],
+    entry_points={
+        'console_scripts': [
+            'xrp_convert=XRP_converter.xrp_w_convert:main'
+        ]
+    },
+    description='A cool XRP conversion library with extra tools.',
+    author='kakz',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.8'
+)
