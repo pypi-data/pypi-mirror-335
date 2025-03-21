4752b4b1fc87283529b9b6052491efbdf1649753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 07:05:18 +0000

    Add package tastytrade/9.11/tastytrade-9.11.tar.gz

---
+++ packages/tastytrade/tastytrade-9.11.tar.gz/tastytrade-9.11/tastytrade/__init__.py
@@ -0,0 +1,27 @@
+import logging
+
+API_URL = "https://api.tastyworks.com"
+BACKTEST_URL = "https://backtester.vast.tastyworks.com"
+CERT_URL = "https://api.cert.tastyworks.com"
+VAST_URL = "https://vast.tastyworks.com"
+VERSION = "9.11"
+
+__version__ = VERSION
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# ruff: noqa: E402
+
+from .account import Account
+from .session import Session
+from .streamer import AlertStreamer, DXLinkStreamer
+from .watchlists import Watchlist
+
+__all__ = [
+    "Account",
+    "AlertStreamer",
+    "DXLinkStreamer",
+    "Session",
+    "Watchlist",
+]
