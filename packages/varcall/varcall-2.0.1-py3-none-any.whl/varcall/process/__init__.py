193cfde8db1d365ef2ce650db0ae4e17e415d467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:05:14 +0000

    Add package varcall/2.0.1/varcall-2.0.1-py3-none-any.whl

---
+++ packages/varcall/varcall-2.0.1-py3-none-any.whl/varcall/process/__init__.py
@@ -0,0 +1,16 @@
+######################################################################
+# Main app information.
+__author__ = "Vidyasagar"
+__copyright__ = "Copyright 2024, Vidyasagar"
+__credits__ = ["Vidyasagar"]
+__maintainer__ = "Vidyasagar"
+__version__ = "2.0.1"
+__licence__ = "MIT"
+
+##############################################################################
+# Local imports.
+
+##############################################################################
+# Export the imports.
+
+### __init__.py ends here
