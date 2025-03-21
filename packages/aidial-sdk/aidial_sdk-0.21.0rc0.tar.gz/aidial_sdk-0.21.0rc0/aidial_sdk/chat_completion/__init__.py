3cb33a15370e6816d8753ffc9aabe28af1f46668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:05:28 +0000

    Add package aidial-sdk/0.21.0rc0/aidial_sdk-0.21.0rc0.tar.gz

---
+++ packages/aidial-sdk/aidial_sdk-0.21.0rc0.tar.gz/aidial_sdk-0.21.0rc0/aidial_sdk/chat_completion/__init__.py
@@ -0,0 +1,43 @@
+from aidial_sdk.chat_completion.base import ChatCompletion
+from aidial_sdk.chat_completion.choice import Choice
+from aidial_sdk.chat_completion.enums import FinishReason, Status
+from aidial_sdk.chat_completion.form import Button, FormMetaclass
+from aidial_sdk.chat_completion.request import (
+    Addon,
+    Attachment,
+    CustomContent,
+    Function,
+    FunctionCall,
+    FunctionChoice,
+    Message,
+    MessageContentImagePart,
+    MessageContentPart,
+    MessageContentTextPart,
+    Request,
+    ResponseFormat,
+    ResponseFormatJsonObject,
+    ResponseFormatJsonSchema,
+    ResponseFormatJsonSchemaObject,
+    ResponseFormatText,
+    Role,
+)
+from aidial_sdk.chat_completion.request import Stage as RequestStage
+from aidial_sdk.chat_completion.request import Tool, ToolCall, ToolChoice
+from aidial_sdk.chat_completion.response import Response
+from aidial_sdk.chat_completion.stage import Stage
+from aidial_sdk.deployment.configuration import (
+    ConfigurationRequest,
+    ConfigurationResponse,
+)
+from aidial_sdk.deployment.tokenize import (
+    TokenizeError,
+    TokenizeRequest,
+    TokenizeResponse,
+    TokenizeSuccess,
+)
+from aidial_sdk.deployment.truncate_prompt import (
+    TruncatePromptError,
+    TruncatePromptRequest,
+    TruncatePromptResponse,
+    TruncatePromptSuccess,
+)
