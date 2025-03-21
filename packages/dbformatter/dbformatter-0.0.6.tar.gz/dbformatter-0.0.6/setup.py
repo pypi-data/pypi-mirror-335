17f5dbf18d522b3632acca95283b6ea8296b1e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:05:08 +0000

    Add package dbformatter/0.0.6/dbformatter-0.0.6.tar.gz

---
+++ packages/dbformatter/dbformatter-0.0.6.tar.gz/dbformatter-0.0.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dbformatter",
+    version="0.0.6",
+    packages=find_packages(),
+    description="A tool to format database entries",
+    author="Bruteforcer",
+    python_requires=">=3.6",
+)
