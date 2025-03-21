fb63ee49f15caf6ae36b45c9ba000f1cad69189b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:04:57 +0000

    Add package haliax/1.4.dev342/haliax-1.4.dev342.tar.gz

---
+++ packages/haliax/haliax-1.4.dev342.tar.gz/haliax-1.4.dev342/tests/test_scan.py
@@ -0,0 +1,166 @@
+import equinox as eqx
+import jax
+import pytest
+
+import haliax as hax
+from haliax.nn.scan import BlockSeq, Stacked
+
+
+def test_unstacked():
+    class Module(eqx.Module):
+        named: hax.NamedArray
+        array: jax.Array
+        static: int = eqx.static_field()
+
+        def __call__(self, x, *, key):
+            return x + self.array + self.static
+
+        @staticmethod
+        def init(named, array, static):
+            return Module(named=named, array=array, static=static)
+
+    Block = hax.Axis("block", 4)
+    E = hax.Axis("E", 10)
+
+    initial_named = hax.random.uniform(jax.random.PRNGKey(0), (Block, E))
+
+    m = Stacked.init(Block, Module)(named=initial_named, array=jax.numpy.ones(Block.size), static=1)
+
+    assert m.stacked.named.axes == (Block, E)
+    assert m.stacked.array.shape == (Block.size,)
+    assert m.stacked.static == 1
+
+    unstacked = m.unstacked()
+
+    assert len(unstacked) == Block.size
+
+    for i, module in enumerate(unstacked):
+        assert module.named.axes == (E,)
+        assert module.array.shape == ()
+        assert module.static == 1
+
+        assert hax.all(module.named == m.stacked.named["block", i])
+        assert hax.all(module.array == m.stacked.array[i])
+
+
+def test_seq_and_stacked_give_same_results():
+    class Module(eqx.Module):
+        named: hax.NamedArray
+        array: jax.Array
+        static: int = eqx.static_field()
+
+        def __call__(self, x, *, key):
+            return x + self.array + self.static + hax.random.normal(key, x.axes)
+
+        @staticmethod
+        def init(named, array, static):
+            return Module(named=named, array=array, static=static)
+
+    Block = hax.Axis("block", 4)
+    E = hax.Axis("E", 10)
+
+    initial_named = hax.random.uniform(jax.random.PRNGKey(0), (Block, E))
+
+    m = Stacked.init(Block, Module)(named=initial_named, array=jax.numpy.ones(Block.size), static=1)
+    m_seq = BlockSeq.init(Block, Module)(named=initial_named, array=jax.numpy.ones(Block.size), static=1)
+
+    x = hax.random.uniform(jax.random.PRNGKey(1), (E,))
+    y = m.fold(x, key=jax.random.split(jax.random.PRNGKey(2), Block.size))
+    y_seq = m_seq.fold(x, key=jax.random.split(jax.random.PRNGKey(2), Block.size))
+    assert hax.all(hax.isclose(y, y_seq, atol=1e-5))
+
+    with pytest.raises(ValueError):
+        m.scan(x, key=jax.random.split(jax.random.PRNGKey(2), Block.size))
+
+    with pytest.raises(ValueError):
+        m_seq.scan(x, key=jax.random.split(jax.random.PRNGKey(2), Block.size))
+
+
+def test_using_scan():
+    class Module(eqx.Module):
+        named: hax.NamedArray
+        array: jax.Array
+        static: int = eqx.static_field()
+
+        def __call__(self, x, *, key):
+            return x + self.array + self.static + hax.random.normal(key, x.axes), x * 2
+
+        @staticmethod
+        def init(named, array, static):
+            return Module(named=named, array=array, static=static)
+
+    Block = hax.Axis("block", 4)
+    E = hax.Axis("E", 10)
+
+    initial_named = hax.random.uniform(jax.random.PRNGKey(0), (Block, E))
+
+    m = Stacked.init(Block, Module)(named=initial_named, array=jax.numpy.ones(Block.size), static=1)
+
+    x = hax.random.uniform(jax.random.PRNGKey(1), (E,))
+    y, intermediates = m.scan(x, key=jax.random.split(jax.random.PRNGKey(2), Block.size))
+
+    assert y.axes == (E,)
+    assert intermediates.axes == (Block, E)
+
+
+def test_scan_with_aux_named_args():
+    class Module(eqx.Module):
+        named: hax.NamedArray
+        array: jax.Array
+        static: int = eqx.static_field()
+
+        def __call__(self, x, y, *, key):
+            return x + self.array + self.static + hax.random.normal(key, x.axes), x * 2 + y
+
+        @staticmethod
+        def init(named, array, static):
+            return Module(named=named, array=array, static=static)
+
+    Block = hax.Axis("block", 4)
+    E = hax.Axis("E", 10)
+
+    initial_named = hax.random.uniform(jax.random.PRNGKey(0), (Block, E))
+    initial_y = hax.random.uniform(jax.random.PRNGKey(1), (E,))
+
+    m = Stacked.init(Block, Module)(named=initial_named, array=jax.numpy.ones(Block.size), static=1)
+    m_seq = BlockSeq.init(Block, Module)(named=initial_named, array=jax.numpy.ones(Block.size), static=1)
+
+    x = hax.random.uniform(jax.random.PRNGKey(1), (E,))
+    z, z_scan = m.scan(x, initial_y, key=jax.random.split(jax.random.PRNGKey(2), Block.size))
+    z_seq, z_seq_scan = m_seq.scan(x, initial_y, key=jax.random.split(jax.random.PRNGKey(2), Block.size))
+    assert hax.all(hax.isclose(z, z_seq, atol=1e-5))
+
+    z_seq_scan = hax.stack(Block, z_seq_scan)
+
+    assert hax.all(hax.isclose(z_scan, z_seq_scan, atol=1e-5))
+
+
+def test_stacked_to_state_dict():
+    class Module(eqx.Module):
+        named: hax.NamedArray
+        array: jax.Array
+        static: int = eqx.static_field()
+
+        def __call__(self, x, *, key):
+            return x + self.array + self.static + hax.random.normal(key, x.axes)
+
+        @staticmethod
+        def init(named, array, static):
+            return Module(named=named, array=array, static=static)
+
+    Block = hax.Axis("block", 4)
+    E = hax.Axis("E", 10)
+
+    initial_named = hax.random.uniform(jax.random.PRNGKey(0), (Block, E))
+
+    m = Stacked.init(Block, Module)(named=initial_named, array=jax.numpy.ones(Block.size), static=1)
+
+    state_dict = m.to_state_dict()
+    m2 = m.from_state_dict(state_dict)
+    input = hax.random.uniform(jax.random.PRNGKey(1), (E,))
+    key = jax.random.split(jax.random.PRNGKey(2), Block.size)
+
+    y = m.fold(input, key=key)
+    y2 = m2.fold(input, key=key)
+
+    assert hax.all(hax.equal(y, y2))
