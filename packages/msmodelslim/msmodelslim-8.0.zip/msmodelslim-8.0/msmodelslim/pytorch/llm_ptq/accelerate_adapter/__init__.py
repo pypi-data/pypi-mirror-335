4eef70ccf4d0f6b19fa97e64db1115990bd84c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 15:06:44 +0000

    Add package msmodelslim/8.0/msmodelslim-8.0.zip

---
+++ packages/msmodelslim/msmodelslim-8.0.zip/msmodelslim-8.0/msmodelslim/pytorch/llm_ptq/accelerate_adapter/__init__.py
@@ -0,0 +1,29 @@
+# Copyright Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
+
+__all__ = [
+    'replace_device_align_hook_if_needed',
+    'move_update_weight_hook_if_need',
+    'get_state_dict_copy',
+    'clear_unused_module',
+    'PrepareWeight',
+
+    'get_offloaded_dataset',
+
+    'copy_offloaded_state_dict',
+    'DiskStateDictConfig',
+    'MemoryStateDictConfig',
+
+    'LazyTensor',
+    'handle_lazy_tensor'
+]
+
+from .hook_adapter import (replace_device_align_hook_if_needed,
+                           move_update_weight_hook_if_need,
+                           get_state_dict_copy,
+                           clear_unused_module,
+                           PrepareWeight)
+from .utils import get_offloaded_dataset
+from .offloaded_state_dict import (DiskStateDictConfig,
+                                   MemoryStateDictConfig,
+                                   copy_offloaded_state_dict)
+from .lazy_handler import LazyTensor, handle_lazy_tensor
