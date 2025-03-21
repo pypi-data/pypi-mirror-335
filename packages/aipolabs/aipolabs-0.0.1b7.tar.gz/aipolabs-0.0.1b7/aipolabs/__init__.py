632c24aac37ea5bd378bd6b8048b0e012dd108a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:05:30 +0000

    Add package aipolabs/0.0.1b7/aipolabs-0.0.1b7.tar.gz

---
+++ packages/aipolabs/aipolabs-0.0.1b7.tar.gz/aipolabs-0.0.1b7/aipolabs/__init__.py
@@ -0,0 +1,6 @@
+from aipolabs._client import ACI
+from aipolabs.utils._logging import setup_logging as _setup_logging
+
+_setup_logging()
+
+__all__ = ["ACI"]
---
+++ packages/aipolabs/aipolabs-0.0.1b7.tar.gz/aipolabs-0.0.1b7/aipolabs/meta_functions/__init__.py
@@ -0,0 +1,13 @@
+import aipolabs.meta_functions._aci_execute_function as ACIExecuteFunction
+import aipolabs.meta_functions._aci_get_function_definition as ACIGetFunctionDefinition
+import aipolabs.meta_functions._aci_search_apps as ACISearchApps
+import aipolabs.meta_functions._aci_search_functions as ACISearchFunctions
+import aipolabs.meta_functions._aci_search_functions_with_intent as ACISearchFunctionsWithIntent
+
+__all__ = [
+    "ACISearchApps",
+    "ACISearchFunctions",
+    "ACISearchFunctionsWithIntent",
+    "ACIGetFunctionDefinition",
+    "ACIExecuteFunction",
+]
