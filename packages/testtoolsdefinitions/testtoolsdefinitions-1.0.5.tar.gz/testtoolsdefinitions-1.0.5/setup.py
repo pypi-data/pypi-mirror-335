f8e0651e0d2ae9d2a042cd670eaead54af236f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 07:04:54 +0000

    Add package testtoolsdefinitions/1.0.5/testtoolsdefinitions-1.0.5.tar.gz

---
+++ packages/testtoolsdefinitions/testtoolsdefinitions-1.0.5.tar.gz/testtoolsdefinitions-1.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="testtoolsdefinitions",
+	version="1.0.5",
+	description="Singlestore tools definitions",
+	author="Jai Khatri",
+	author_email="jkhatri@singlestore.com",
+	url="https://github.com/singlestore-labs/singlestore-ai-tools",
+	packages=find_packages(),
+	install_requires=[
+		"requests",
+		"singlestoredb",
+		"python-dotenv"
+	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
