ba691168364797a8ea83f02e2f78de13c64cdb1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 16:05:46 +0000

    Add package pypots/0.13/pypots-0.13.tar.gz

---
+++ packages/pypots/pypots-0.13.tar.gz/pypots-0.13/pypots/__init__.py
@@ -0,0 +1,31 @@
+"""
+PyPOTS package.
+"""
+
+# Created by Wenjie Du <wenjay.du@gmail.com>
+# License: BSD-3-Clause
+
+
+from . import (
+    imputation,
+    classification,
+    clustering,
+    forecasting,
+    optim,
+    data,
+    utils,
+)
+from .timeseries_ai import TimeSeriesAI
+from .version import __version__
+
+__all__ = [
+    "TimeSeriesAI",
+    "imputation",
+    "classification",
+    "clustering",
+    "forecasting",
+    "optim",
+    "data",
+    "utils",
+    "__version__",
+]
