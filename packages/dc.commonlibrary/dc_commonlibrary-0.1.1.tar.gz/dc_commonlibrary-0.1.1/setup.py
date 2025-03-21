e63dec6d2100bc80eaff6f06c855e9f38bd0ba1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:04:50 +0000

    Add package dc.commonlibrary/0.1.1/dc_commonlibrary-0.1.1.tar.gz

---
+++ packages/dc.commonlibrary/dc_commonlibrary-0.1.1.tar.gz/dc_commonlibrary-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+VERSION = '0.1.1'
+
+setup(
+    name='dc.commonlibrary',  # package name
+    version=VERSION,  # package version
+    description='common library',  # package description
+    packages=find_packages(),
+    zip_safe=False,
+)
