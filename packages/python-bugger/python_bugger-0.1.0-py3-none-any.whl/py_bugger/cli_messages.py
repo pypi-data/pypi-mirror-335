a576f01d6dfae2027c4e5dcc38105bdb2b70a0c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:05:17 +0000

    Add package python-bugger/0.1.0/python_bugger-0.1.0-py3-none-any.whl

---
+++ packages/python-bugger/python_bugger-0.1.0-py3-none-any.whl/py_bugger/cli_messages.py
@@ -0,0 +1,4 @@
+"""Messages longer than one line."""
+
+msg_bare_call = """A bare py-bugger call makes no changes to your project.
+You must be explicit about what kinds of errors you want to induce in the project."""
