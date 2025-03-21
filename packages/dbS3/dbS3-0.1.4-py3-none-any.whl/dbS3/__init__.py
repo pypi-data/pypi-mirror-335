a17473caf637fcb64b7f5f514420ee4add1e2623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 07:04:49 +0000

    Add package dbS3/0.1.4/dbS3-0.1.4-py3-none-any.whl

---
+++ packages/dbS3/dbS3-0.1.4-py3-none-any.whl/dbS3/__init__.py
@@ -0,0 +1,2 @@
+from .s3_storage import S3Storage, get_database
+from .data_operations import MydbS3, save, insert_tinydb, get_all_data_tinydb, get_all_data_env, get_project_data_tinydb, register_call_enpoint
