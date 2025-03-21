c0abbb25cfbd32882f5859706dd825efb1cfb320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:04:54 +0000

    Add package fds.sdk.FixedIncomeCalculation/1.2.2/fds.sdk.FixedIncomeCalculation-1.2.2.tar.gz

---
+++ packages/fds.sdk.FixedIncomeCalculation/fds.sdk.FixedIncomeCalculation-1.2.2.tar.gz/fds.sdk.FixedIncomeCalculation-1.2.2/fds/sdk/FixedIncomeCalculation/models/__init__.py
@@ -0,0 +1,42 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from fds.sdk.FixedIncomeCalculation.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from fds.sdk.FixedIncomeCalculation.model.calculation_info import CalculationInfo
+from fds.sdk.FixedIncomeCalculation.model.calculation_info_root import CalculationInfoRoot
+from fds.sdk.FixedIncomeCalculation.model.calculation_meta import CalculationMeta
+from fds.sdk.FixedIncomeCalculation.model.client_error_response import ClientErrorResponse
+from fds.sdk.FixedIncomeCalculation.model.error import Error
+from fds.sdk.FixedIncomeCalculation.model.error_source import ErrorSource
+from fds.sdk.FixedIncomeCalculation.model.fi_attribution_for_job_settings import FIAttributionForJobSettings
+from fds.sdk.FixedIncomeCalculation.model.fi_attribution_for_securities import FIAttributionForSecurities
+from fds.sdk.FixedIncomeCalculation.model.fi_balloon_extension import FIBalloonExtension
+from fds.sdk.FixedIncomeCalculation.model.fi_bank_loans import FIBankLoans
+from fds.sdk.FixedIncomeCalculation.model.fi_calculation_parameters import FICalculationParameters
+from fds.sdk.FixedIncomeCalculation.model.fi_calculation_parameters_root import FICalculationParametersRoot
+from fds.sdk.FixedIncomeCalculation.model.fi_cashflows import FICashflows
+from fds.sdk.FixedIncomeCalculation.model.fi_convertible_settings import FIConvertibleSettings
+from fds.sdk.FixedIncomeCalculation.model.fi_discount_curve_info import FIDiscountCurveInfo
+from fds.sdk.FixedIncomeCalculation.model.fi_discount_curve_info_root import FIDiscountCurveInfoRoot
+from fds.sdk.FixedIncomeCalculation.model.fi_job_settings import FIJobSettings
+from fds.sdk.FixedIncomeCalculation.model.fi_loss import FILoss
+from fds.sdk.FixedIncomeCalculation.model.fi_market_environment import FIMarketEnvironment
+from fds.sdk.FixedIncomeCalculation.model.fi_municipal_bonds import FIMunicipalBonds
+from fds.sdk.FixedIncomeCalculation.model.fi_municipal_bonds_for_job_settings import FIMunicipalBondsForJobSettings
+from fds.sdk.FixedIncomeCalculation.model.fi_prepay import FIPrepay
+from fds.sdk.FixedIncomeCalculation.model.fi_prepay_lockout import FIPrepayLockout
+from fds.sdk.FixedIncomeCalculation.model.fi_reference_prepay import FIReferencePrepay
+from fds.sdk.FixedIncomeCalculation.model.fi_reference_security import FIReferenceSecurity
+from fds.sdk.FixedIncomeCalculation.model.fi_security import FISecurity
+from fds.sdk.FixedIncomeCalculation.model.fi_servicer_advances import FIServicerAdvances
+from fds.sdk.FixedIncomeCalculation.model.fi_servicer_advances_for_securities import FIServicerAdvancesForSecurities
+from fds.sdk.FixedIncomeCalculation.model.fi_structured_products_for_job_settings import FIStructuredProductsForJobSettings
+from fds.sdk.FixedIncomeCalculation.model.fi_structured_products_for_securities import FIStructuredProductsForSecurities
+from fds.sdk.FixedIncomeCalculation.model.object_root import ObjectRoot
