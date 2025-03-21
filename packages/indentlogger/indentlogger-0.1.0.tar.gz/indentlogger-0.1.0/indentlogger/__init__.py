e909e3dccfcea8ba5c8473040fd39f999c538eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 14:06:35 +0000

    Add package indentlogger/0.1.0/indentlogger-0.1.0.tar.gz

---
+++ packages/indentlogger/indentlogger-0.1.0.tar.gz/indentlogger-0.1.0/indentlogger/__init__.py
@@ -0,0 +1,8 @@
+"""
+indentlogger package
+
+Provides IndentLogger and LogStyle for indentation-based logging.
+"""
+from .indent_logger import IndentLogger, LogStyle
+
+__all__ = ["IndentLogger", "LogStyle"]
