bf5a1e4e4857a3eb56b0798a269457d36eee41a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:06:04 +0000

    Add package dagster-datadog/0.26.6rc0/dagster-datadog-0.26.6rc0.tar.gz

---
+++ packages/dagster-datadog/dagster-datadog-0.26.6rc0.tar.gz/dagster-datadog-0.26.6rc0/dagster_datadog/__init__.py
@@ -0,0 +1,8 @@
+from dagster._core.libraries import DagsterLibraryRegistry
+
+from dagster_datadog.resources import DatadogResource, datadog_resource
+from dagster_datadog.version import __version__
+
+DagsterLibraryRegistry.register("dagster-datadog", __version__)
+
+__all__ = ["DatadogResource", "datadog_resource"]
