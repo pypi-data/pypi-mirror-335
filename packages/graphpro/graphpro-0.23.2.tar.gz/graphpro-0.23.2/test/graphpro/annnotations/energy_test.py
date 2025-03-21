445296eff0fe1270447c009db781b5d9d17063b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 14:05:44 +0000

    Add package graphpro/0.23.2/graphpro-0.23.2.tar.gz

---
+++ packages/graphpro/graphpro-0.23.2.tar.gz/graphpro-0.23.2/test/graphpro/annnotations/energy_test.py
@@ -0,0 +1,27 @@
+import torch
+import MDAnalysis as mda
+
+from graphpro import md_analisys
+from graphpro.graphgen import ContactMap
+from graphpro.annotations import BTPotential, BTEigenCentrality
+
+from MDAnalysis.tests.datafiles import PDB_small
+
+u1 = mda.Universe(PDB_small)
+
+def test_bt_potential_calculation():
+    G = md_analisys(u1).generate(ContactMap(cutoff=6), [BTPotential()])
+    assert len(G.nodes()) == 214
+    assert round(G.node_attr(0)['bt_potential'],2) == -702.08
+
+def test_gnm_encoding():
+    G = md_analisys(u1).generate(ContactMap(cutoff=6), [BTPotential()])
+    data = G.to_data(node_encoders=[BTPotential()])
+
+    assert data.x.size() == (214, 1)
+    assert data.x.dtype == torch.float
+    
+def test_bt_potential_calculation():
+    G = md_analisys(u1).generate(ContactMap(cutoff=6), [BTEigenCentrality()])
+    assert len(G.nodes()) == 214
+    assert round(G.node_attr(90)['bt_eigen_centrality'],10) == 1.24773e-05
