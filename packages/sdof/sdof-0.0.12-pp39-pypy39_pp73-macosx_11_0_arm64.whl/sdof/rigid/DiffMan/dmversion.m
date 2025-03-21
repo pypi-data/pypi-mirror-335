33f6f945cf3456fbc81037e71788c19485e3ad69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 15:06:54 +0000

    Add package sdof/0.0.12/sdof-0.0.12-pp39-pypy39_pp73-macosx_11_0_arm64.whl

---
+++ packages/sdof/sdof-0.0.12-pp39-pypy39_pp73-macosx_11_0_arm64.whl/sdof/rigid/DiffMan/dmversion.m
@@ -0,0 +1,10 @@
+function [version,date] = dmversion()
+% DMVERSION- Returns two strings with version and release date of DiffMan.
+% function [version,date] = dmversion()
+
+% WRITTEN BY       : Kenth Engø, 1999.04.01
+% LAST MODIFIED BY : Kenth Engø, 2000.09.02
+
+version = '2.0';
+date    = '2000.09.15';
+return;
---
+++ packages/sdof/sdof-0.0.12-pp39-pypy39_pp73-macosx_11_0_arm64.whl/sdof/rigid/DiffMan/domain/liegroup/@lgse/setshape.m
@@ -0,0 +1,27 @@
+function [] = setshape(a,sh)
+% SETSHAPE - Sets the shape information in LGSE.
+% function [] = setshape(a,sh)
+
+% WRITTEN BY       : Kenth Engø, 1998.01.15
+% LAST MODIFIED BY : Kenth Engø, 1999.04.12
+
+global DMARGCHK
+
+name = inputname(1);
+if DMARGCHK
+  if isempty(name),
+    error('First argument to set must be a named variable');
+  end;
+  if ~(isinteger(sh)),
+    error('Shape should be an integer!');
+  end;
+  if sh<1,
+    error('Shape should be positive!');
+  end;
+end;
+
+a.shape = sh;
+a.data  = {[] []};
+assignin('caller',name,a);
+return;
+
