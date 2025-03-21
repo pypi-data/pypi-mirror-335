f0e6d93f8d9760b1c0f40e9cc9a641fccde7e80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:05:00 +0000

    Add package dbformatter/0.0.3/dbformatter-0.0.3.tar.gz

---
+++ packages/dbformatter/dbformatter-0.0.3.tar.gz/dbformatter-0.0.3/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="dbformatter",
+    version="0.0.3",
+    packages=find_packages(),  # Finds dbformatter/ with __init__.py
+    description="A tool to format database entries",
+    author="Bruteforcer",
+    author_email="admin@example.com",
+    url="https://github.com",  # Optional
+    python_requires=">=3.6",
+)
