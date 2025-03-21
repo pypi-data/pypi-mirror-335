a3d6e2c5338bba460ce24c73bb9f6786e479c1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 14:06:24 +0000

    Add package actxa-insights-test/0.0.5/actxa_insights_test-0.0.5.tar.gz

---
+++ packages/actxa-insights-test/actxa_insights_test-0.0.5.tar.gz/actxa_insights_test-0.0.5/actxa_insights_test/__init__.py
@@ -0,0 +1,1 @@
+from .bgm import predict_bgm_range_app, predict_bgm_classification_app, bgm_range_result, bgm_classification_result
---
+++ packages/actxa-insights-test/actxa_insights_test-0.0.5.tar.gz/actxa_insights_test-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="actxa_insights_test",
+    version="0.0.5",
+    packages=find_packages(),
+    description="SDK Python package for Actxa Insights",
+    author="Actxa Insights",
+    author_email="hadrian.gunawan@inphosoft.com",
+    # url="https://github.com/yourusername/my-package",
+    python_requires=">=3.9",
+    install_requires=["requests"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
