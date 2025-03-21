a4aaf157b4bf8979bb8819279934a5fa25d0fc74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:15:26 +0000

    Add package py-fh/0.3/py_fh-0.3.tar.gz

---
+++ packages/py-fh/py_fh-0.3.tar.gz/py_fh-0.3/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name= 'py_fh',
+    version= '0.3',
+    author= 'Hasneen Ali',
+    description= 'test 🙂🤍',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
