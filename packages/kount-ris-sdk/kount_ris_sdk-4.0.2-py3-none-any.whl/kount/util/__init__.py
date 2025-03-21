fa9679fd8cfbd6dc2cd72626c77915a7bf907b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:05:33 +0000

    Add package kount-ris-sdk/4.0.2/kount_ris_sdk-4.0.2-py3-none-any.whl

---
+++ packages/kount-ris-sdk/kount_ris_sdk-4.0.2-py3-none-any.whl/kount/util/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of the Kount python sdk project
+# https://github.com/Kount/kount-ris-python-sdk/)
+# Copyright (C) 2025 Kount an Equifax Company All Rights Reserved.
+
+from kount.version import VERSION
+from kount.config import SDKConfig
+
+__author__ = SDKConfig.SDK_AUTHOR
+__version__ = VERSION
+__maintainer__ = SDKConfig.SDK_MAINTAINER
+__email__ = SDKConfig.MAINTAINER_EMAIL
+__status__ = SDKConfig.STATUS
---
+++ packages/kount-ris-sdk/kount_ris_sdk-4.0.2-py3-none-any.whl/kount/util/a85.py
@@ -0,0 +1,2 @@
+# no longer necessary but leaving as is for backwards compatibility
+from base64 import a85encode, a85decode
