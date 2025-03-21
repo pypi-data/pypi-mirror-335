cafa50b7616afe25af31fc507f359545f9a47435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:04:52 +0000

    Add package texcellent-wondertex-dragon-phoenix-ninja-unicorn-texas-ranger-latex-masterpiece-generator/0.1.0/texcellent_wondertex_dragon_phoenix_ninja_unicorn_texas_ranger_latex_masterpiece_generator-0.1.0.tar.gz

---
+++ packages/texcellent-wondertex-dragon-phoenix-ninja-unicorn-texas-ranger-latex-masterpiece-generator/texcellent_wondertex_dragon_phoenix_ninja_unicorn_texas_ranger_latex_masterpiece_generator-0.1.0.tar.gz/texcellent_wondertex_dragon_phoenix_ninja_unicorn_texas_ranger_latex_masterpiece_generator-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="texcellent-wondertex-dragon-phoenix-ninja-unicorn-texas-ranger-latex-masterpiece-generator",
+    version="0.1.0",
+    author="Kalinin Ivan",
+    author_email="jeck5ivk@gmail.com",
+    description="A library to generate LaTeX code for tables and images.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jeck5iv/pythonHW",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    license="MIT",
+)
