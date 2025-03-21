59b192e4d43e047c49b1629bcf04692e9e652f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 15:06:02 +0000

    Add package matchescu-base/0.12.0/matchescu_base-0.12.0-py3-none-any.whl

---
+++ packages/matchescu-base/matchescu_base-0.12.0-py3-none-any.whl/matchescu/typing/_references.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from typing import Hashable, Iterable, Sized, Protocol, Callable
+
+from matchescu.typing._data import Record
+
+
+@dataclass(frozen=True, eq=True)
+class EntityReferenceIdentifier:
+    """Identifies an entity reference.
+
+    Attributes:
+        :label Hashable: unique label identifying an entity reference within a collection
+        :source str: a string describing where the entity reference originated
+    """
+
+    label: Hashable
+    source: str
+
+
+class EntityReference(Record, Protocol):
+    """An entity reference instance allows accessing data by name or index.
+
+    Attributes:
+        :id EntityReferenceIdentifier: identifies the entity reference
+    """
+
+    id: EntityReferenceIdentifier
+
+
+class EntityProfile(Iterable[EntityReference], Sized, Protocol):
+    """Entity profiles are iterable sequences of a finite number of entity
+    references.
+
+    Concrete entity profiles have different representations according to the
+    entity resolution model being used.
+    """
+
+
+EntityReferenceIdFactory = Callable[[Record], EntityReferenceIdentifier]
+RecordAdapter = Callable[[Record], EntityReference]
