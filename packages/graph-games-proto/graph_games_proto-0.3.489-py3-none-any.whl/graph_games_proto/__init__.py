7ce3513f0afe433123d815fc507eb41d129b6d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:05:35 +0000

    Add package graph-games-proto/0.3.489/graph_games_proto-0.3.489-py3-none-any.whl

---
+++ packages/graph-games-proto/graph_games_proto-0.3.489-py3-none-any.whl/graph_games_proto/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py
+from .main import hello
+from .fns import AltAction, Fig, RandoPolicy, autoplay, getstate, getpublicstate, State, PlayerInfo, Action, Fig, getnextaction, getnextstate, initboardconfig, gettoplay, getlegalactionsforplayer, getstateidx, printstate, getinitialstate, printaction, json_serializer, getrng, initgameconfig, GameConfig
