0017229f7711f14d1f7b87a417ac24bbbf1b7d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:05:21 +0000

    Add package coreagent/0.1.0a0/coreagent-0.1.0a0-py2.py3-none-any.whl

---
+++ packages/coreagent/coreagent-0.1.0a0-py2.py3-none-any.whl/coreagent/__init__.py
@@ -0,0 +1,2 @@
+from .config import Config, get_default_config, set_default_config
+from .agent import Agent, Identity
---
+++ packages/coreagent/coreagent-0.1.0a0-py2.py3-none-any.whl/coreagent/config.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+import openai
+from attr import dataclass
+
+@dataclass
+class Config:
+  """
+  # Configuration file for an Agent, can be shared across Agent instances.
+  """
+  # ---- essential settings ----
+  llm: openai.Client                                    # MUST SET!
+  model: str                                            # MUST SET!
+  # ---- optional settings ----
+  temperature: Optional[float] = None
+  frequency_penalty: float = None # generally don't set this, may cause problems.
+  generation_limit: int = 5000
+  show_generation: bool = False                         # Don't use it for now, a bug in vLLM (tested as of <= v0.8.0) caused random junks to be streamed, check out vLLM Issue #15188.
+  # ---- optional settings ----
+  progressbar_length: int = 50                          # Not used for now
+  guided_decoding_backend: str = 'xgrammar:no-fallback' # Tested with vLLM with Engine v0.
+  use_stop_token: bool = False                          # Tested not working with vLLM <= 0.8.0, since stop tokens are also considered during reasoning, see vLLM Issue #14170.
+  chat_template_type: Optional[str] = None              # modified chat templates, only for vLLM, one of ["qwq" or None]
+
+# Default configuration (used internally, do NOT modify directly! )
+default_config: Optional[Config] = None
+
+# Get the default configuration
+def get_default_config() -> Config:
+  global default_config
+  if default_config is None:
+    raise Exception("default config is not set.")
+  return default_config
+
+# Set the default configuration
+def set_default_config(config: Config):
+  global default_config
+  default_config = config
