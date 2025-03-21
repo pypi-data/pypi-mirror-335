91139765262b75c8985dad366657725f086024fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 15:06:33 +0000

    Add package subtitle-corrector/0.1.8/subtitle_corrector-0.1.8.tar.gz

---
+++ packages/subtitle-corrector/subtitle_corrector-0.1.8.tar.gz/subtitle_corrector-0.1.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+
+setup(
+    name="subtitle_corrector",
+    version="0.1.8",
+    author="imuzhangy",
+    author_email="imuzhangying@gmail.com",
+    description="A package for correcting medical subtitles",
+    long_description=open("README.md", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'subtitle_corrector': ['config.json', 'error_terms_library.json'],
+    },
+    install_requires=[
+        "requests",
+        "Cython"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    ext_modules=cythonize("subtitle_corrector/subtitle_corrector.pyx"),
+)
