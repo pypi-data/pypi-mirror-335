6b509ac742c4c85c5028fe70f652b083c32fa5a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:06:04 +0000

    Add package latex-tools-101/0.1/latex_tools_101-0.1.tar.gz

---
+++ packages/latex-tools-101/latex_tools_101-0.1.tar.gz/latex_tools_101-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='latex_tools_101',
+    version='0.1',
+    packages=find_packages(),
+    description='Small lib with tools for Latex',
+)
