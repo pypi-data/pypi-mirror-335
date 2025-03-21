ee98cab0b9718752dd4a75e94599a6c985160028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:05:32 +0000

    Add package archs4py/1.0.3/archs4py-1.0.3.tar.gz

---
+++ packages/archs4py/archs4py-1.0.3.tar.gz/archs4py-1.0.3/archs4py/download.py
@@ -0,0 +1,58 @@
+import wget
+import sys
+import requests
+import archs4py.utils
+import requests
+import os
+
+def bar_progress(current, total, width=80, update_interval=10):
+    current_gb = current / (1024**3)  # Convert current bytes to GB
+    total_gb = total / (1024**3)  # Convert total bytes to GB
+    
+    if current % (update_interval * 1024**2) == 0:  # Update progress every 10 MB
+        progress_message = "Downloading: %d%% [%.2f GB / %.2f GB]" % (current / total * 100, current_gb, total_gb)
+        sys.stdout.write("\r" + progress_message)
+        sys.stdout.flush()
+
+def counts(species, path="", type="GENE_COUNTS", version="latest"):
+    """
+    Download count files for a given species and count type.
+
+    Args:
+        species (str): The species for which count files are being downloaded. ["human", "mouse"]
+        path (str, optional): The path where the downloaded file will be saved. Defaults to "".
+        type (str, optional): The type of count file to be downloaded. Defaults to "GENE_COUNTS".
+        version (str, optional): The version of the count file to be downloaded. Defaults to "latest". Versions can be listed with archs4py.versions()
+
+    Returns:
+        str: The path where the count file is downloaded.
+
+    Raises:
+        Exception: If an error occurs during the download process.
+
+    Notes:
+        The function first tries to download the count file using the primary URL specified in the configuration file.
+        If the download fails, it falls back to the fallback URL specified in the configuration file.
+
+        Supported count types:
+        - GENE_COUNTS: Gene-level count files.
+        - TRANSCRIPT_COUNTS: Transcript-level count files.
+    """
+    conf = archs4py.utils.get_config()
+
+    try:
+        file_name = os.path.basename(conf[type][species.upper()][version]["primary"])
+        download_url = conf["DOWNLOAD_URL"]
+        url = f"{download_url}?&file={file_name}&version=1337"
+        payload = {"category": "download", "entry": file_name, "user_id": 1337}
+        response = requests.post(f"{download_url}/api/log", payload)
+    except:
+        x = "just continue"
+    
+    try:
+        fpath = wget.download(conf[type][species.upper()][version]["primary"], out=path, bar=bar_progress)
+        print("file downloaded to", fpath)
+    except Exception:
+        fpath = wget.download(conf[type][species.upper()][version]["fallback"], out=path, bar=bar_progress)
+        print("file downloaded to", fpath)
+
---
+++ packages/archs4py/archs4py-1.0.3.tar.gz/archs4py-1.0.3/archs4py/utils.py
@@ -0,0 +1,141 @@
+import numpy as np
+import pandas as pd
+import h5py as h5
+import random
+
+import os
+import json
+
+import qnorm
+
+def get_config():
+    config_url = os.path.join(
+        os.path.dirname(__file__),
+        'data/config.json')
+    with open(config_url) as json_file:
+        data = json.load(json_file)
+    return(data)
+
+def versions():
+    """
+    Get the available versions of human gene counts.
+
+    Returns:
+        list: A list of available versions of human gene counts.
+    """
+    config = get_config()
+    versions = config["GENE_COUNTS"]["HUMAN"].keys()
+    return versions
+
+def normalize(counts, method="log_quantile", tmm_outlier=0.05):
+    """
+    Normalize the count matrix using a specified method.
+
+    Args:
+        counts (pd.DataFrame): A pandas DataFrame representing the count matrix.
+        method (str, optional): The normalization method to be applied. Default is "log_quantiles".
+            - "quantile": Perform quantile normalization on the counts.
+            - "log_quantile": Perform quantile normalization on the log-transformed counts.
+            - "cpm": Perform count per million (CPM) normalization.
+            - "tmm": Perform trimmed mean normalization
+
+    Returns:
+        pd.DataFrame: A normalized count matrix as a pandas DataFrame with the same index and columns as the input.
+
+    Raises:
+        ValueError: If an unsupported normalization method is provided.
+    """
+    norm_exp = 0
+    if method == "quantile":
+        norm_exp = qnorm.quantile_normalize(np.array(counts))
+    elif method == "log_quantile":
+        norm_exp = qnorm.quantile_normalize(np.log2(1+np.array(counts)))
+    elif method == "cpm":
+        norm_exp = cpm_normalization(counts)
+    elif method == "tmm":
+        norm_exp = tmm_norm(counts, tmm_outlier)
+    else:
+        raise ValueError("Unsupported normalization method: " + method)
+    norm_exp = pd.DataFrame(norm_exp, index=counts.index, columns=counts.columns, dtype=np.float32)
+    return norm_exp
+
+def tmm_norm(exp, percentage=0.05):
+    lexp = np.log2(1+exp).astype(np.float32)
+    tmm = trimmed_mean(lexp, percentage)
+    nf = pd.DataFrame(np.tile(tmm, (exp.shape[0], 1)), index=lexp.index, columns=lexp.columns)
+    temp = (lexp/nf)
+    return temp
+
+def trimmed_mean(matrix, percentage):
+    matrix = np.array(matrix)
+    trimmed_means = []
+    for col in range(matrix.shape[1]):
+        data = matrix[:, col].copy()
+        data = data[data > 0]
+        n_trim = int(len(data) * percentage)
+        sorted_values = np.sort(data)
+        trimmed_values = sorted_values[n_trim:-n_trim]
+        trimmed_mean = np.mean(trimmed_values)
+        trimmed_means.append(trimmed_mean)
+    return trimmed_means
+
+def cpm_normalization(df):
+    sample_sum = df.sum(axis=0)
+    scaling_factor = sample_sum / 1e6
+    normalized_df = df / scaling_factor
+    return normalized_df
+
+def ls(file):
+    """
+    List all meta data groups and meta data fields in the specified H5 file.
+
+    Args:
+        file: H5 input file
+    """
+    def print_data(name, obj, prefix):
+        if isinstance(obj, h5.Dataset):
+            data_type = str(obj.dtype)
+            if data_type == "object":
+                data_type = "str"
+            shape = obj.shape
+            print("{}{:<20}  {:<6} | {}".format(prefix, name, data_type, shape))
+        else:
+            print("{}{:<26}".format(prefix, name))
+            
+        for key, val in obj.attrs.items():
+            print("{}   {:<11} : {}".format(prefix, key, val))
+
+    with h5.File(file, 'r') as f:
+        for name in f:
+            print_data(name, f[name], "")
+            if isinstance(f[name], h5.Group):
+                for sub_name in f[name]:
+                    print_data(sub_name, f[name][sub_name], "│ ")
+                    if isinstance(f[name][sub_name], h5.Group):
+                        for sub_sub_name in f[name][sub_name]:
+                            print_data(sub_sub_name, f[name][sub_name][sub_sub_name], "│   ")
+
+def aggregate_duplicate_genes(exp):
+    return exp.groupby(exp.index).sum()
+
+def filter_genes(exp, readThreshold: int=20, sampleThreshold: float=0.02, deterministic: bool=True, aggregate=True):
+    '''
+    Returns filtered genes with sufficient read support
+        Parameters:
+                h5file          (string): path to expression h5 file
+                readThreshold      (int): minimum number of reads required for gene filtering
+                sampleThreshold  (float): fraction of samples required with read count larger than _readThreshold
+                filterSamples      (int): number of samples used to identify genes for clustering
+
+        Returns:
+                (List[int]): filtered index of genes passing criteria
+    '''
+    if deterministic:
+        random.seed(42)
+
+    if aggregate:
+        exp = aggregate_duplicate_genes(exp)
+
+    kk = exp[exp > readThreshold].count(axis=1)
+    ii = [idx for idx, val in enumerate(kk) if val >= exp.shape[1]*sampleThreshold]
+    return exp.iloc[ii,:]
