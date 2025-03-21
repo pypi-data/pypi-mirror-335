0653748e14640afc43ef2df552bb0da28a2cb002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:15:02 +0000

    Add package wandelbots-nova/0.47.7/wandelbots_nova-0.47.7-py3-none-any.whl

---
+++ packages/wandelbots-nova/wandelbots_nova-0.47.7-py3-none-any.whl/nova/core/exceptions.py
@@ -0,0 +1,60 @@
+import json
+
+import wandelbots_api_client as wb
+
+
+class ControllerNotFound(Exception):
+    def __init__(self, controller: str):
+        super().__init__(f"Controller {controller} not found.")
+
+
+class PlanTrajectoryFailed(Exception):
+    def __init__(self, error: wb.models.PlanTrajectoryFailedResponse):
+        self._error = error
+        super().__init__(f"Plan trajectory failed: {json.dumps(error.to_dict(), indent=2)}")
+
+    def to_pretty_string(self) -> str:
+        """Give a more lightweight representation of the error, omitting some gritty details."""
+        error_dict = self._error.to_dict()
+        del error_dict["joint_trajectory"]
+        return f"Plan trajectory failed: {json.dumps(error_dict, indent=2)}"
+
+    @property
+    def error(self) -> wb.models.PlanTrajectoryFailedResponse:
+        """Return the original PlanTrajectoryFailedResponse object."""
+        return self._error
+
+
+class InitMovementFailed(Exception):
+    def __init__(self, error: wb.models.InitializeMovementResponseInitResponse):
+        self._error = error
+        super().__init__(f"Initial movement failed: {json.dumps(error.to_dict(), indent=2)}")
+
+    @property
+    def error(self) -> wb.models.InitializeMovementResponseInitResponse:
+        """Return the original InitializeMovementResponseInitResponse object."""
+        return self._error
+
+
+class LoadPlanFailed(Exception):
+    def __init__(self, error: wb.models.PlanSuccessfulResponse):
+        self._error = error
+        super().__init__(f"Load plan failed: {json.dumps(error.to_dict(), indent=2)}")
+
+    @property
+    def error(self) -> wb.models.PlanSuccessfulResponse:
+        """Return the original PlanSuccessfulResponse object."""
+        return self._error
+
+
+class InconsistentCollisionScenes(Exception):
+    """Raised when actions have different collision scenes."""
+
+    def __init__(self, message: str):
+        self._message = message
+        super().__init__(message)
+
+    @property
+    def message(self) -> str:
+        """Return the error message."""
+        return self._message
