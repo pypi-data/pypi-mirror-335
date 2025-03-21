2c23e1343aa1e7923693ba5c73b938ab4a45608e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 08:05:16 +0000

    Add package rtv-solver/0.1.3/rtv_solver-0.1.3.tar.gz

---
+++ packages/rtv-solver/rtv_solver-0.1.3.tar.gz/rtv_solver-0.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rtv_solver",
+    version="0.1.3",
+    description="A solver for real-time vehicle routing problems",
+    author="Danushka Edirimanna",
+    author_email="ke233@cornell.edu",
+    license="MIT",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=["rtv_solver"]),  # Matches `find = { include = ["rtv_solver"] }`
+    install_requires=[],  # No dependencies for now
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
