d04db2157ac1030bb2d862b27615155f7d25d1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 06:05:28 +0000

    Add package tacoreader/0.5.5/tacoreader-0.5.5-py3-none-any.whl

---
+++ packages/tacoreader/tacoreader-0.5.5-py3-none-any.whl/tacoreader/__init__.py
@@ -0,0 +1,8 @@
+from tacoreader.compile import compile
+from tacoreader.loader_dataframe import load
+from tacoreader.loader_metadata import load_metadata
+from tacoreader.sanity import sanity_check
+from tacoreader.TortillaDataFrame import TortillaDataFrame
+
+__all__ = ["load", "load_metadata", "compile", "sanity_check", "TortillaDataFrame"]
+__version__ = "0.5.5"
