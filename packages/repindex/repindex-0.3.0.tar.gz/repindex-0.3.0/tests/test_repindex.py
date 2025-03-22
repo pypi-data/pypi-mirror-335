eeba899a2e13f83301b85470602803f1e2e53dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 15:05:38 +0000

    Add package repindex/0.3.0/repindex-0.3.0.tar.gz

---
+++ packages/repindex/repindex-0.3.0.tar.gz/repindex-0.3.0/tests/test_repindex.py
@@ -0,0 +1,54 @@
+import os
+import shutil
+import tempfile
+import unittest
+from repindex import repindex
+
+class TestRepindex(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+        os.makedirs(os.path.join(self.test_dir, 'subdir'))
+
+        files = {
+            'file1.ts': 'import { func } from "./file2";\nexport const x = 1;',
+            'file2.ts': 'export function func() {}',
+            'file3.py': 'print("Hello, world!")',
+            'file4.txt': 'Just some text.',
+            'subdir/file5.sh': '#!/bin/bash\necho "Hello"',
+        }
+        for filename, content in files.items():
+            fp = os.path.join(self.test_dir, filename)
+            os.makedirs(os.path.dirname(fp), exist_ok=True)
+            with open(fp, 'w') as f:
+                f.write(content)
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_generate_tree(self):
+        tree = repindex.generate_tree_text(self.test_dir, '', langs=[])
+        self.assertIn('file1.ts', tree)
+        self.assertIn('subdir', tree)
+
+    def test_extract_dependencies(self):
+        file1 = os.path.join(self.test_dir, 'file1.ts')
+        deps = repindex.extract_dependencies(file1, langs=['react'])
+        self.assertIn('./file2', deps['imports'])
+        self.assertIn('x', deps['exports'])
+
+    def test_build_dependency_graph(self):
+        graph, _ = repindex.build_dependency_graph(self.test_dir, langs=['react'])
+        self.assertIn('file1.ts', graph['nodes'])
+        self.assertIn('file2.ts', graph['nodes'])
+        self.assertTrue(any(edge['from'] == 'file1.ts' for edge in graph['edges']))
+
+    def test_generate_markdown(self):
+        markdown = repindex.generate_markdown(self.test_dir, langs=[])
+        self.assertIn('### file1.ts', markdown)
+        self.assertIn('import { func } from "./file2";', markdown)
+
+    def test_generate_light_markdown(self):
+        markdown = repindex.generate_light_markdown(self.test_dir, langs=[])
+        self.assertIn('### file1.ts', markdown)
+        self.assertNotIn('### file4.txt', markdown)
+        self.assertIn('```python\nprint("Hello, world!")', markdown)
