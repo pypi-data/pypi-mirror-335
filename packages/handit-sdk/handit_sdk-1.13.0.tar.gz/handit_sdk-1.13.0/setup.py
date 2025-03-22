ead452d6fe8567eca9a486088ac7923a01aae7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:21 +0000

    Add package handit-sdk/1.13.0/handit_sdk-1.13.0.tar.gz

---
+++ packages/handit-sdk/handit_sdk-1.13.0.tar.gz/handit_sdk-1.13.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="handit-sdk",  # The name users will use to install the package
+    version="1.13.0",
+    description="A Python SDK for tracking Model requests and responses.",
+    author="Your Name",
+    author_email="cristhian@handit.ai",
+    url="https://github.com/Handit-AI/handit-sdk",  # Replace with your GitHub repo
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "jsonpickle",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
