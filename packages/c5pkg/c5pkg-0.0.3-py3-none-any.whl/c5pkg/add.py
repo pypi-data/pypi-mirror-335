b39dadb2e9aeaa882b78653ebdb47fa77d8cd5b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 15:06:16 +0000

    Add package c5pkg/0.0.3/c5pkg-0.0.3-py3-none-any.whl

---
+++ packages/c5pkg/c5pkg-0.0.3-py3-none-any.whl/c5pkg/add.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    """
+    Adds two numbers together.
+
+    Parameters:
+    a (int, float): The first number.
+    b (int, float): The second number.
+
+    Returns:
+    int, float: The sum of a and b.
+    """
+    return a + b
