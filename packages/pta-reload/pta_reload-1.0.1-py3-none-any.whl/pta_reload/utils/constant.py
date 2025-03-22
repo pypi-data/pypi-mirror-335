95903c64c4b9bb13a2b47d4e26b0ae0be18b0a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 06:05:13 +0000

    Add package pta-reload/1.0.1/pta_reload-1.0.1-py3-none-any.whl

---
+++ packages/pta-reload/pta_reload-1.0.1-py3-none-any.whl/pta_reload/utils/constant.py
@@ -0,0 +1,18 @@
+EXCHANGE_TZ = {
+    "NZSX": 12, "ASX": 11,
+    "TSE": 9, "HKE": 8, "SSE": 8, "SGX": 8,
+    "NSE": 5.5, "DIFX": 4, "RTS": 3,
+    "JSE": 2, "FWB": 1, "LSE": 1,
+    "BMF": -2, "NYSE": -4, "TSX": -4
+}
+
+RATE = {
+    "DAYS_PER_MONTH": 21,
+    "MINUTES_PER_HOUR": 60,
+    "MONTHS_PER_YEAR": 12,
+    "QUARTERS_PER_YEAR": 4,
+    "TRADING_DAYS_PER_YEAR": 252,  # Keep even
+    "TRADING_HOURS_PER_DAY": 6.5,
+    "WEEKS_PER_YEAR": 52,
+    "YEARLY": 1,
+}
