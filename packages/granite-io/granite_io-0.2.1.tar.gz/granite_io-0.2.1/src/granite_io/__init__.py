110ce00ac2dee5683fc781322c12f73a8332ea41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 06:05:17 +0000

    Add package granite-io/0.2.1/granite_io-0.2.1.tar.gz

---
+++ packages/granite-io/granite_io-0.2.1.tar.gz/granite_io-0.2.1/src/granite_io/__init__.py
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: Apache-2.0
+
+__doc__ = f"""
+The {__package__} library provides a common programming abstraction for using
+the advanced control instructions with Granite models.
+"""
+
+# Local
+from granite_io.backend import backend, make_backend  # noqa: F401
+from granite_io.io import (  # noqa: F401
+    get_input_processor,
+    get_output_processor,
+    input_processor,
+    io_processor,
+    make_io_processor,
+    output_processor,
+)
