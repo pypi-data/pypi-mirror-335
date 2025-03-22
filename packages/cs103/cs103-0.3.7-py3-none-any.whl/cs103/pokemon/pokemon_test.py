f9430b6ff34e6bb0a1f9a5127ed503cc373ffb15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 06:05:42 +0000

    Add package cs103/0.3.7/cs103-0.3.7-py3-none-any.whl

---
+++ packages/cs103/cs103-0.3.7-py3-none-any.whl/cs103/pokemon/pokemon_test.py
@@ -0,0 +1,25 @@
+from pokemon import *
+import unittest
+
+class PokemonTest(unittest.TestCase):
+    """
+    Make sure the functions work for every combination of Pokemon
+    """
+    def test_get_pokemon_name(self):
+        for i in range(1, 801):
+            self.assertTrue(get_pokemon_name(i))
+
+    def test_get_pokemon_stats(self):
+        for i in range(1, 801):
+            n = get_pokemon_name(i)
+            get_pokemon_attack(n)
+            get_pokemon_defense(n)
+            get_pokemon_height(n)
+            get_pokemon_num_types(n)
+            get_pokemon_type1(n)
+            get_pokemon_type2(n)
+            get_pokemon_weight(n)
+
+
+if __name__ == '__main__':
+    unittest.main()
