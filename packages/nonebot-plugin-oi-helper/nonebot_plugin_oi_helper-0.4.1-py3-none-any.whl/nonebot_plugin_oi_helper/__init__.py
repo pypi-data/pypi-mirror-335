2b0b88f8cfc39f24f0e5c40e0244eea867433450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 04:05:23 +0000

    Add package nonebot-plugin-oi-helper/0.4.1/nonebot_plugin_oi_helper-0.4.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-oi-helper/nonebot_plugin_oi_helper-0.4.1-py3-none-any.whl/nonebot_plugin_oi_helper/__init__.py
@@ -0,0 +1,57 @@
+# none_plugin_oi_helper for NoneBot2 plugin
+# Copyright (C) 2024  talentestors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from nonebot import require, get_plugin_config
+from nonebot.log import logger
+from nonebot.plugin import PluginMetadata
+from .config import Config
+
+require("nonebot_plugin_localstore")
+require("nonebot_plugin_apscheduler")
+
+__plugin_meta__ = PluginMetadata(
+    name="none_plugin_oi_helper",
+    description="none_plugin_oi_helper for NoneBot2 plugin",
+    usage="This is a helper library for the none-plugin-oi. Use this library to assist with various operations related to the none-plugin-oi.",
+    config=Config,
+    homepage="https://github.com/talentestors/none-plugin-oi-helper",
+    type="library",
+    supported_adapters=None,
+    extra={
+        "author": "talentestors",
+        "version": "0.1.0",
+        "tags": ["nonebot", "plugin", "oi-helper"],
+    },
+)
+
+
+config = get_plugin_config(Config)
+
+api_config = config.clist
+
+logger.info("api_config loaded")
+logger.info("username: " + api_config.username)
+logger.info(
+    "user_key: " + api_config.user_key[:6] + ("*" * (len(api_config.user_key) - 6))
+)
+logger.info("request url: " + config.clist.req_url)
+
+__all__ = ["api_config", "config"]
+
+from .command import *  # noqa: E402, F403
+from .scheduler import *  # noqa: E402, F403
+
+logger.info("scheduler first controller loaded")
