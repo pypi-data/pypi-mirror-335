012f333fd0c117448a615d434ef7c4f22f227682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 04:06:07 +0000

    Add package arviz-plots/0.5.0/arviz_plots-0.5.0-py3-none-any.whl

---
+++ packages/arviz-plots/arviz_plots-0.5.0-py3-none-any.whl/arviz_plots/plots/__init__.py
@@ -0,0 +1,43 @@
+"""Batteries-included ArviZ plots."""
+
+from .bfplot import plot_bf
+from .compareplot import plot_compare
+from .convergencedistplot import plot_convergence_dist
+from .distplot import plot_dist
+from .ecdfplot import plot_ecdf_pit
+from .energyplot import plot_energy
+from .essplot import plot_ess
+from .evolutionplot import plot_ess_evolution
+from .forestplot import plot_forest
+from .pavacalibrationplot import plot_ppc_pava
+from .ppcdistplot import plot_ppc_dist
+from .ppcpitplot import plot_ppc_pit
+from .ppcrootogramplot import plot_ppc_rootogram
+from .psensedistplot import plot_psense_dist
+from .psensequantitiesplot import plot_psense_quantities
+from .rankplot import plot_rank
+from .ridgeplot import plot_ridge
+from .tracedistplot import plot_trace_dist
+from .traceplot import plot_trace
+
+__all__ = [
+    "plot_bf",
+    "plot_compare",
+    "plot_convergence_dist",
+    "plot_dist",
+    "plot_forest",
+    "plot_trace",
+    "plot_trace_dist",
+    "plot_ecdf_pit",
+    "plot_energy",
+    "plot_ess",
+    "plot_ess_evolution",
+    "plot_ppc_dist",
+    "plot_ppc_rootogram",
+    "plot_rank",
+    "plot_ridge",
+    "plot_ppc_pava",
+    "plot_ppc_pit",
+    "plot_psense_dist",
+    "plot_psense_quantities",
+]
