2154bf842144191df9c926a8f1a8a326f40b7f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:45 +0000

    Add package endplay/0.5.12/endplay-0.5.12-cp310-cp310-musllinux_1_2_aarch64.whl

---
+++ packages/endplay/endplay-0.5.12-cp310-cp310-musllinux_1_2_aarch64.whl/endplay/dealer/actions/__init__.py
@@ -0,0 +1,14 @@
+"""
+The Actions classes provide a common interface for producing different
+types of output from a dealer script. When a script is run, an appropriate
+Actions object is constructed, and any time output is requested one of the
+methods is called to format it correctly.
+"""
+
+__all__ = ["BaseActions", "TerminalActions", "LaTeXActions", "HTMLActions"]
+
+from endplay.dealer.actions.base import BaseActions
+from endplay.dealer.actions.html import HTMLActions
+from endplay.dealer.actions.latex import LaTeXActions
+from endplay.dealer.actions.pdf import PDFActions
+from endplay.dealer.actions.terminal import TerminalActions
