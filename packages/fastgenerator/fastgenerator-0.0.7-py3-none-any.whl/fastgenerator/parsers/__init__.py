461285030cc1d4f0d9a51c504abb1a99cedb6948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 03:14:16 +0000

    Add package fastgenerator/0.0.7/fastgenerator-0.0.7-py3-none-any.whl

---
+++ packages/fastgenerator/fastgenerator-0.0.7-py3-none-any.whl/fastgenerator/parsers/__init__.py
@@ -0,0 +1,6 @@
+from fastgenerator.parsers.config import getconfig
+from fastgenerator.parsers.config import getcontent
+from fastgenerator.parsers.variables import getvariables
+from fastgenerator.parsers.variables import replacevariables
+
+__all__ = ["getvariables", "replacevariables", "getconfig", "getcontent"]
