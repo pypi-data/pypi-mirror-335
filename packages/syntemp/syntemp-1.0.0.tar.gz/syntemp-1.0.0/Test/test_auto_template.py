b7fd7a1bd71cc7382a10f2d9aaccb03485fb19ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 03:15:00 +0000

    Add package syntemp/1.0.0/syntemp-1.0.0.tar.gz

---
+++ packages/syntemp/syntemp-1.0.0.tar.gz/syntemp-1.0.0/Test/test_auto_template.py
@@ -0,0 +1,43 @@
+import unittest
+from pathlib import Path
+from syntemp.auto_template import AutoTemp
+from synkit.IO.data_io import load_database
+
+
+root_dir = Path(__file__).parents[1]
+
+
+class TestAutoTemp(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.data = load_database(f"{root_dir}/Data/Testcase/demo.json.gz")[:20]
+        self.auto = AutoTemp(
+            rebalancing=True,
+            mapper_types=["rxn_mapper", "graphormer", "local_mapper"],
+            id="R-id",
+            rsmi="reactions",
+            n_jobs=1,
+            verbose=2,
+            batch_size=50,
+            job_timeout=None,
+            safe_mode=False,
+            save_dir=f"{root_dir}/Data/Testcase/Test",
+            fix_hydrogen=True,
+        )
+
+    def test_temp_extract(self):
+        (rules, _, _, _, _, _) = self.auto.temp_extract(self.data, lib_path=None)
+        self.assertIn("ruleID", rules[0][0])
+        self.assertEqual(len(rules[0]), 9)
+
+    # def test_temp_extract_lib(self):
+    #     print(f"{root_dir}/Data/Testcase/Compose/SingleRule")
+    #     (rules, _, _, _, _, _) = self.auto.temp_extract(
+    #         self.data, lib_path=f"{root_dir}/Data/Testcase/Compose/SingleRule"
+    #     )  # 1 rules exist
+    #     self.assertIn("ruleID", rules[0][0])
+    #     self.assertEqual(len(rules[0]), 7)
+
+
+if __name__ == "__main__":
+    unittest.main()
