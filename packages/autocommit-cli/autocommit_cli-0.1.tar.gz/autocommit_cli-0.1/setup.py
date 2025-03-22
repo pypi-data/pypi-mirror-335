688b667a3ee4478231dbf074df48d9cc92ae646b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 03:14:39 +0000

    Add package autocommit-cli/0.1/autocommit_cli-0.1.tar.gz

---
+++ packages/autocommit-cli/autocommit_cli-0.1.tar.gz/autocommit_cli-0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="autocommit-cli",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+        "gitpython",
+        "python-dotenv",
+        "google-generativeai"
+    ],
+    entry_points={
+        "console_scripts": [
+            "autocommit=autocommit:cli"
+        ]
+    },
+    author="Pranjal Kishor",
+    description="A CLI tool for AI-powered Git commit messages using Google Gemini API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Pranjal-88/Autocommit-CLI",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
