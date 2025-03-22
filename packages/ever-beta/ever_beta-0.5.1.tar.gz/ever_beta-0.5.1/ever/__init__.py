f561bb4f8747d89ab389b198bf4f14d2527e261c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 15:05:39 +0000

    Add package ever-beta/0.5.1/ever_beta-0.5.1.tar.gz

---
+++ packages/ever-beta/ever_beta-0.5.1.tar.gz/ever_beta-0.5.1/ever/__init__.py
@@ -0,0 +1,27 @@
+from ever.core import registry
+from ever.core import builder
+from ever.core import config
+from ever.core import to
+from ever.core import dist
+from ever.core.logger import info
+
+from ever import opt, trainer, metric, preprocess
+
+from ever.interface import *
+
+from ever.util import param_util
+from ever.util.seedlib import seed_torch
+from ever.core.device import auto_device
+
+from ever.api import infer_tool
+
+__all__ = [
+    'registry', 'builder', 'config', 'to',
+    'param_util', 'auto_device', 'data', 'metric', 'preprocess', 'infer_tool',
+    'ERDataLoader', 'LearningRateBase', 'ERModule',
+    'Transform', 'MultiTransform', 'Callback',
+    'seed_torch'
+]
+
+__internal_version__ = '2.5.1'
+__version__ = "0.5.1"
