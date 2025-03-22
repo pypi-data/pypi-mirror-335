747e4581b72897bfa94fd7bf06182f109acc8853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 04:05:25 +0000

    Add package sideko-py/1.1.0/sideko_py-1.1.0.tar.gz

---
+++ packages/sideko-py/sideko_py-1.1.0.tar.gz/sideko_py-1.1.0/sideko/src/cmds/api/version/create.rs
@@ -0,0 +1,75 @@
+use camino::Utf8PathBuf;
+use sideko_rest_api::{models::VersionOrBump, resources::api::spec::CreateRequest, UploadFile};
+use tabled::settings::{object::Rows, Color};
+
+use crate::{
+    cmds::DisplayOutput,
+    result::{CliError, CliResult},
+    utils::{self, get_sideko_client},
+};
+
+use super::tabled::TabledApiSpec;
+
+#[derive(clap::Args)]
+pub struct ApiVersionCreateCommand {
+    /// api name or id e.g. my-api
+    #[arg(long)]
+    pub name: String,
+
+    /// semantic version (e.g. `2.1.5`) or version bump (`patch`, `minor`, `major`, `rc`)
+    #[arg(long)]
+    pub version: String,
+
+    /// path to openapi specification (YAML or JSON format)
+    #[arg(
+        long,
+        value_parser = crate::utils::validators::validate_file_json_yaml,
+    )]
+    pub spec: Utf8PathBuf,
+
+    /// disable mock server for new version [default: enabled]
+    #[arg(long)]
+    pub disable_mock: bool,
+
+    /// display result as a raw json or prettified
+    #[arg(long, default_value = "pretty")]
+    pub display: DisplayOutput,
+}
+impl ApiVersionCreateCommand {
+    pub async fn handle(&self) -> CliResult<()> {
+        let mut client = get_sideko_client();
+        let new_version = client
+            .api()
+            .spec()
+            .create(CreateRequest {
+                api_name: self.name.clone(),
+                version: VersionOrBump::Str(self.version.clone()),
+                mock_server_enabled: Some(!self.disable_mock),
+                openapi: UploadFile::from_path(self.spec.as_str()).map_err(|e| {
+                    CliError::io_custom(
+                        format!("failed reading openapi from path: {}", &self.spec),
+                        e,
+                    )
+                })?,
+                notes: None,
+            })
+            .await?;
+
+        match &self.display {
+            DisplayOutput::Raw => utils::logging::log_json_raw(&new_version),
+            DisplayOutput::Pretty => {
+                let org = client.org().get().await?;
+                let mut table = tabled::Table::new([TabledApiSpec {
+                    version: new_version,
+                    org_subdomain: org.subdomain.clone(),
+                }]);
+                utils::tabled::header_panel(&mut table, "new api version");
+                table.modify(Rows::single(1), Color::BOLD);
+
+                utils::logging::log_table(table);
+            }
+        }
+
+        Ok(())
+    }
+}
