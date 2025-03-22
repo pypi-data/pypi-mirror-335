fd96050e37bfa2b0c678278ecc0b291ae0014870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 04:05:53 +0000

    Add package python-plus-tools/1.0.4/python_plus_tools-1.0.4.tar.gz

---
+++ packages/python-plus-tools/python_plus_tools-1.0.4.tar.gz/python_plus_tools-1.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-plus-tools",
+    version="1.0.4",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.10,<=3.14",
+    author="xystudio",
+    author_email="173288240@qq.com",
+    description="For the expansion of python content, it covers science, debugging and other fields.",
+    long_description=open("README.md").read(),
+    license="MIT",
+    url="https://github.com/xystudio889/pyplus",
+)
