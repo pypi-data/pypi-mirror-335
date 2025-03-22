1aabf7679f3302972ba7f26b7e51cf6f0fdc90fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 14:05:33 +0000

    Add package dekshell/0.1.84/dekshell-0.1.84-py3-none-any.whl

---
+++ packages/dekshell/dekshell-0.1.84-py3-none-any.whl/dekshell/core/contexts/__init__.py
@@ -0,0 +1,13 @@
+from dektools.dict import assign
+from dektools.attr import DeepObject
+from ..plugin import get_contexts_from_modules
+from .methods import default_methods
+from .properties import default_properties
+
+
+def get_all_context(**kwargs):
+    return DeepObject(assign(
+        default_methods,
+        default_properties,
+        *get_contexts_from_modules(**kwargs)[::-1]
+    )).__dict__
