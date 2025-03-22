199cbf36f79c3515683440951d41714ca890820c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 03:14:26 +0000

    Add package mglyph/0.5.2/mglyph-0.5.2.tar.gz

---
+++ packages/mglyph/mglyph-0.5.2.tar.gz/mglyph-0.5.2/src/__init__.py
@@ -0,0 +1,10 @@
+'''
+mglyph
+
+The Malleable Glyph librarty.
+'''
+
+__author__ = 'Vojtech Bartl, Adam Herout'
+__credits__ = 'FIT BUT'
+
+from .mglyph import Canvas, export, interact, lerp, orbit, show, show_video, cubic_bezier_for_x
