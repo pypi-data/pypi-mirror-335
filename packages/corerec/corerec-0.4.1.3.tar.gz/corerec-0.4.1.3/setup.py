0bb485878a3a9b02786ee9124441a89368686afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 14:06:22 +0000

    Add package corerec/0.4.1.3/corerec-0.4.1.3.tar.gz

---
+++ packages/corerec/corerec-0.4.1.3.tar.gz/corerec-0.4.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='corerec',
+    version='0.4.1.3',
+    description='A Framework to built custom recommendors.',
+    author='Vishesh Yadav',
+    author_email='vishesh@corerec.tech',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'networkx',
+        'scikit_learn',
+        'cr_learn',
+        'torch',
+        'tqdm',
+        'memory_profiler',
+        'pandas',
+        'torch_geometric',
+        'dask',
+        'gensim',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
