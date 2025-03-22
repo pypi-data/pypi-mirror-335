ed4d1baa6a47e330453591657e51242437302bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:47 +0000

    Add package endplay/0.5.12/endplay-0.5.12-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/endplay/endplay-0.5.12-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/endplay/__init__.py
@@ -0,0 +1,15 @@
+"""
+Endplay - A bridge tools library with generating, analysing and scoring.
+Released under the MIT licence (see the LICENCE file provided with this
+distribution)
+"""
+
+from endplay import _dds as _dds
+from endplay.config import __author__, __version__, __version_info__
+from endplay.config import suppress_unicode as suppress_unicode
+from endplay.dds import *
+from endplay.dealer import *
+from endplay.evaluate import *
+from endplay.interact import *
+from endplay.parsers import *
+from endplay.types import *
