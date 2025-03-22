73747309c6371e88e03ff9152645209d55d6eea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 03:14:42 +0000

    Add package wowool-common/3.0.2.dev20/wowool_common-3.0.2.dev20-py3-none-any.whl

---
+++ packages/wowool-common/wowool_common-3.0.2.dev20-py3-none-any.whl/wowool/annotation/__init__.py
@@ -0,0 +1,4 @@
+from wowool.annotation.annotation import Annotation, ErrorAnnotationNotFound
+from wowool.annotation.token import Token
+from wowool.annotation.concept import Concept
+from wowool.annotation.sentence import Sentence
