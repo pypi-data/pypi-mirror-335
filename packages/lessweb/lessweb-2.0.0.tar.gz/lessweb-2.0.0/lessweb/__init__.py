9e292cb99f1615a76ffc068c6e1dc64e40ad7469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:08 +0000

    Add package lessweb/2.0.0/lessweb-2.0.0.tar.gz

---
+++ packages/lessweb/lessweb-2.0.0.tar.gz/lessweb-2.0.0/lessweb/__init__.py
@@ -0,0 +1,5 @@
+from .bridge import Bridge, load_module_config
+from .event import EventEmitter
+from .ioc import (Middleware, Module, Service, autowire, get_request_stack,
+                  push_request_stack, rest_error, rest_response)
+from .typecast import TypeCast, inspect_type, is_typeddict
