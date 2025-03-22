a151c3d993eaf176ab37cb18d1d4d570029a629e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 06:05:32 +0000

    Add package HanTa/1.1.2/hanta-1.1.2.tar.gz

---
+++ packages/HanTa/hanta-1.1.2.tar.gz/hanta-1.1.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="HanTa",
+    version="1.1.2",
+    author="Christian Wartena",
+    author_email="Christian.Wartena@hs-hannover.de",
+    description="Hannover Tagger: Morphological Analysis and POS Tagging",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wartaal/HanTa",
+    packages=setuptools.find_packages(),
+    package_data = {'HanTa': ['*.pgz']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Natural Language :: Dutch",
+        "Natural Language :: English",
+        "Natural Language :: German",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers"
+    ],
+)
