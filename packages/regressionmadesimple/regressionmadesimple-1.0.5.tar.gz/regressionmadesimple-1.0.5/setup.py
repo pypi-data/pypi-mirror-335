5278ba4ad1493904594e471e2c283701edfe77ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 14:05:42 +0000

    Add package regressionmadesimple/1.0.5/regressionmadesimple-1.0.5.tar.gz

---
+++ packages/regressionmadesimple/regressionmadesimple-1.0.5.tar.gz/regressionmadesimple-1.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+    name="regressionmadesimple",
+    version="1.0.5",
+    description="Automate some Linear Regression tasks and plot them using Plotly. The alias is `rms`",
+    install_requires=[
+        'pandas',
+        'numpy',
+        'plotly',
+        'scikit-learn',
+    ],
+    packages=['regressionmadesimple'],
+    author='Unknownuserfrommars',
+    python_requires='>=3.10',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
