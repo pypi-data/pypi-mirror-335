b02c24162498334f913e02d06615a85288b06917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 04:05:58 +0000

    Add package qase-python-commons/3.3.2/qase_python_commons-3.3.2.tar.gz

---
+++ packages/qase-python-commons/qase_python_commons-3.3.2.tar.gz/qase_python_commons-3.3.2/src/qase/commons/models/config/testops.py
@@ -0,0 +1,32 @@
+from .api import ApiConfig
+from .batch import BatchConfig
+from .plan import PlanConfig
+from .run import RunConfig
+from ..basemodel import BaseModel
+from ... import QaseUtils
+
+
+class TestopsConfig(BaseModel):
+    project: str = None
+    defect: bool = None
+    api: ApiConfig = None
+    run: RunConfig = None
+    plan: PlanConfig = None
+    batch: BatchConfig = None
+
+    def __init__(self):
+        self.api = ApiConfig()
+        self.run = RunConfig()
+        self.batch = BatchConfig()
+        self.plan = PlanConfig()
+        self.defect = False
+        self.use_v2 = True
+
+    def set_project(self, project: str):
+        self.project = project
+
+    def set_defect(self, defect):
+        self.defect = QaseUtils.parse_bool(defect)
+
+    def set_use_v2(self, use_v2):
+        self.use_v2 = QaseUtils.parse_bool(use_v2)
