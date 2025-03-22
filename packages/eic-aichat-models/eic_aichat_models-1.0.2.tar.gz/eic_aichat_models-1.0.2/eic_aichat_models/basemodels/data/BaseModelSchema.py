3ddf4145cb4f203f75520af2a92ee98905fb6c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 03:14:37 +0000

    Add package eic-aichat-models/1.0.2/eic_aichat_models-1.0.2.tar.gz

---
+++ packages/eic-aichat-models/eic_aichat_models-1.0.2.tar.gz/eic_aichat_models-1.0.2/eic_aichat_models/basemodels/data/BaseModelSchema.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from pip_services4_commons.convert import TypeCode
+from pip_services4_data.validate import ObjectSchema
+
+
+class BaseModelSchema(ObjectSchema):
+    def __init__(self):
+        super().__init__()
+
+        self.with_optional_property('id', TypeCode.String)
+        self.with_required_property('site_id', TypeCode.String)
+        self.with_optional_property('type', TypeCode.String)
+        self.with_optional_property('name', TypeCode.String)
+        self.with_optional_property('content', TypeCode.String)
