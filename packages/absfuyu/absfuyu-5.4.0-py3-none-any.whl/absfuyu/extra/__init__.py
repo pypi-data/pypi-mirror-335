a449994fbaa1bb5a39e1c18f271380513f43192c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 04:05:36 +0000

    Add package absfuyu/5.4.0/absfuyu-5.4.0-py3-none-any.whl

---
+++ packages/absfuyu/absfuyu-5.4.0-py3-none-any.whl/absfuyu/extra/__init__.py
@@ -0,0 +1,12 @@
+"""
+Absfuyu: Extra
+--------------
+Features that require additional libraries
+
+Version: 5.4.0
+Date updated: 21/03/2025 (dd/mm/yyyy)
+"""
+
+
+def is_loaded():
+    return True
---
+++ packages/absfuyu/absfuyu-5.4.0-py3-none-any.whl/absfuyu/general/__init__.py
@@ -0,0 +1,23 @@
+"""
+Absfuyu: General
+----------------
+Collection of useful classes
+
+Version: 5.4.0
+Date updated: 21/03/2025 (dd/mm/yyyy)
+
+Features:
+---------
+- content
+- generator
+- human
+"""
+
+# Module level
+# ---------------------------------------------------------------------------
+__all__ = ["content", "shape", "human"]
+
+
+# Libary
+# ---------------------------------------------------------------------------
+from absfuyu.general import content, human, shape
