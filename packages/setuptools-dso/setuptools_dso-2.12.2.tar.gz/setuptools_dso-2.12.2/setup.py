b6f7a83f0d3ba988772315b93f81ce6676da2a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:23 +0000

    Add package setuptools-dso/2.12.2/setuptools_dso-2.12.2.tar.gz

---
+++ packages/setuptools-dso/setuptools_dso-2.12.2.tar.gz/setuptools_dso-2.12.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# Copyright 2022  Michael Davidsaver
+# SPDX-License-Identifier: BSD
+# See LICENSE
+
+from setuptools import setup
+
+with open('README.md', 'r') as F:
+    long_description = F.read()
+
+setup(
+    name='setuptools_dso',
+    version="2.12.2",
+    description="setuptools extension to build non-python shared libraries",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mdavidsaver/setuptools_dso',
+    project_urls={
+        'Documentation':'https://mdavidsaver.github.io/setuptools_dso',
+        'Release Notes':'https://mdavidsaver.github.io/setuptools_dso/releasenotes.html',
+    },
+
+    author='Michael Davidsaver',
+    author_email='mdavidsaver@gmail.com',
+    license='BSD',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Archiving :: Packaging',
+        'Framework :: Setuptools Plugin',
+        'License :: OSI Approved :: BSD License',
+    ],
+    python_requires='>=2.7',
+    install_requires = ['setuptools'],
+
+    packages=['setuptools_dso', 'setuptools_dso.test'],
+    package_dir={'':'src'},
+)
