601b7ac595c52180c2af8a3f41be17ae71984204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 04:05:23 +0000

    Add package esperanto/1.1.1/esperanto-1.1.1-py3-none-any.whl

---
+++ packages/esperanto/esperanto-1.1.1-py3-none-any.whl/esperanto/types.py
@@ -0,0 +1,13 @@
+"""Common type definitions for Esperanto."""
+
+from esperanto.types.model import Model
+from esperanto.types.response import Usage
+from esperanto.types.stt import TranscriptionResponse
+from esperanto.types.tts import AudioResponse
+
+__all__ = [
+    "Model",
+    "Usage",
+    "TranscriptionResponse",
+    "AudioResponse",
+]
