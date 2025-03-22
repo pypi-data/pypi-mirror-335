6d5c4883868c066598ad58f2cf5e20455e29cb1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:30 +0000

    Add package redisw/0.1.3/redisw-0.1.3.tar.gz

---
+++ packages/redisw/redisw-0.1.3.tar.gz/redisw-0.1.3/redisw/pubsub.py
@@ -0,0 +1,36 @@
+import json
+
+from redis import Redis
+from redis.asyncio import Redis as ARedis
+
+
+class RedisPubSub:
+    def __init__(self, redis: Redis):
+        self.redis = redis
+
+    def publish(self, topic: str, value: str | dict) -> None:
+        if isinstance(value, dict):
+            value = json.dumps(value)
+
+        self.redis.publish(topic, value)
+
+
+class AsyncRedisPubSub:
+    def __init__(self, redis: ARedis):
+        self.redis = redis
+
+    async def publish(self, topic: str, value: str | dict) -> None:
+        if isinstance(value, dict):
+            value = json.dumps(value)
+
+        await self.redis.publish(topic, value)
+
+    async def subscribe(self, topic, callback):
+        pubsub = self.redis.pubsub()
+        await pubsub.subscribe(topic)
+
+        async for msg in pubsub.listen():
+            if msg["type"] != "message":
+                continue
+            data = msg["data"]
+            await callback(json.loads(data.decode("utf-8")))
