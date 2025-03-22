d6e1ef3083a1da561970c25911362c489f38002c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 15:06:18 +0000

    Add package levup/0.2/levup-0.2.tar.gz

---
+++ packages/levup/levup-0.2.tar.gz/levup-0.2/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+from setuptools.command.test import test
+__dir__ = os.path.dirname(__file__)
+about = {}
+with open(os.path.join(__dir__, 'made-iraq', '__about__.py')) as f:
+    exec(f.read(), about)
+def run_tests(self):
+    raise SystemExit(__import__('pytest').main(['-v']))
+test.run_tests = run_tests
+setup(
+    name='levup',
+    version=about['__version__'],
+    packages=['made-iraq'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    description=about['__description__'],
+    keywords=about['__keywords__'],
+    url=about['__url__'],
+    install_requires=['six'],
+    tests_require=['pytest'],
+)
