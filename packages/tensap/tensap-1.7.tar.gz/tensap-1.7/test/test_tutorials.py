b6575725d1d2a3f6e7ef7623da8d677da7bc787f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 04:05:59 +0000

    Add package tensap/1.7/tensap-1.7.tar.gz

---
+++ packages/tensap/tensap-1.7.tar.gz/tensap-1.7/test/test_tutorials.py
@@ -0,0 +1,30 @@
+import glob
+import pytest
+import time
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use("Agg")
+
+
+def execfile(filepath, globals=None, locals=None):
+    if globals is None:
+        globals = {}
+    globals.update(
+        {
+            "__file__": filepath,
+            "__name__": "__main__",
+        }
+    )
+    with open(filepath, "rb") as file:
+        exec(compile(file.read(), filepath, "exec"), globals, locals)
+
+
+@pytest.mark.parametrize("f", glob.glob("tutorials/**/*.py", recursive=True))
+def test_tutorial(f):
+    print(f"-- running {f}", flush=True)
+    t0 = time.time()
+    execfile(f)
+    dt = time.time() - t0
+    print(f"-- {f} ran in {dt:.2f} s", flush=True)
+    plt.close("all")
