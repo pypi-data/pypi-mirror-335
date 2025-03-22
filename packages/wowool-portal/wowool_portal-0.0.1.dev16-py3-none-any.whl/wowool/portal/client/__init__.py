6a1b33088c6b9f19fdc331a64ec9959f50acf617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 04:05:58 +0000

    Add package wowool-portal/0.0.1.dev16/wowool_portal-0.0.1.dev16-py3-none-any.whl

---
+++ packages/wowool-portal/wowool_portal-0.0.1.dev16-py3-none-any.whl/wowool/portal/client/__init__.py
@@ -0,0 +1,16 @@
+# from .compiler import *
+from wowool.portal.client.error import ServerError, ClientError  # noqa
+from wowool.portal.client.pipeline import Pipeline  # noqa
+from wowool.portal.client.portal import Portal  # noqa
+
+
+def get_version() -> str:
+    import pkg_resources
+
+    try:
+        version = pkg_resources.require("wowool-portal")[0].version
+    except pkg_resources.DistributionNotFound:
+        from wowool.build.git import get_version
+
+        version = get_version()
+    return version
