1e611c5036d5f5eac57ab1561203baece5729da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 03:14:23 +0000

    Add package jsz/2025.3.21/jsz-2025.3.21.tar.gz

---
+++ packages/jsz/jsz-2025.3.21.tar.gz/jsz-2025.3.21/jsz/__init__.py
@@ -0,0 +1,15 @@
+"""
+# jsz
+
+金手指-我的工具库
+"""
+
+from .tools import ua
+from .tools import *  # noqa: F403
+from .html import *  # noqa: F403
+from .db import *  # noqa: F403
+from newpool import *  # noqa: F403
+from .richx import *  # noqa: F403
+from .http import *  # noqa: F403
+
+headers = ua()
