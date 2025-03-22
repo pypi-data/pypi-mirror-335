4cb1f99a16a80671c8e199e50c4ab6d69639fbe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 15:05:41 +0000

    Add package freamon/0.3.3/freamon-0.3.3.tar.gz

---
+++ packages/freamon/freamon-0.3.3.tar.gz/freamon-0.3.3/setup.py
@@ -0,0 +1,91 @@
+"""
+Setup script for the freamon package.
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="freamon",
+    version="0.3.3",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "matplotlib",
+        "seaborn",
+        "joblib",
+        "category_encoders",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "black",
+            "mypy",
+            "isort",
+            "flake8",
+        ],
+        "all": [
+            "lightgbm",
+            "xgboost",
+            "catboost",
+            "spacy",
+            "polars",
+            "dask[dataframe]",
+            "shapiq",
+            "shap",
+            "optuna>=3.0.0",
+            "plotly",
+        ],
+        "full": [
+            "lightgbm",
+            "xgboost",
+            "catboost",
+            "spacy",
+            "polars",
+            "dask[dataframe]",
+            "shapiq",
+            "shap",
+            "optuna>=3.0.0",
+            "plotly",
+            "pytest",
+            "pytest-cov",
+            "black",
+            "mypy",
+            "isort",
+            "flake8",
+        ],
+        "lightgbm": [
+            "lightgbm",
+            "optuna>=3.0.0",
+        ],
+        "tuning": [
+            "optuna>=3.0.0",
+            "plotly",
+        ],
+        "xgboost": [
+            "xgboost",
+        ],
+        "catboost": [
+            "catboost",
+        ],
+        "nlp": [
+            "spacy",
+        ],
+        "polars": [
+            "polars",
+        ],
+        "dask": [
+            "dask[dataframe]",
+        ],
+        "explainability": [
+            "shapiq",
+            "shap",
+        ],
+        "visualization": [
+            "plotly",
+            "graphviz",
+        ],
+    },
+    python_requires=">=3.10",
+)
