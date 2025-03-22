b70e5355fdec1ada7af880b62b178fadb9cf88f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 03:14:38 +0000

    Add package confit/0.7.5/confit-0.7.5-py3-none-any.whl

---
+++ packages/confit/confit-0.7.5-py3-none-any.whl/confit/__init__.py
@@ -0,0 +1,12 @@
+from .cli import Cli
+from .config import Config
+from .registry import (
+    validate_arguments,
+    Registry,
+    get_default_registry,
+    set_default_registry,
+    RegistryCollection,
+    VisibleDeprecationWarning,
+)
+
+__version__ = "0.7.5"
