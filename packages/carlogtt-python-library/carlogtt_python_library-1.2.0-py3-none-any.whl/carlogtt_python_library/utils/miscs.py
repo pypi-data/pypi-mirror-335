1459416ac6cc9328193f51830c29210a426cea5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 07:06:00 +0000

    Add package carlogtt-python-library/1.2.0/carlogtt_python_library-1.2.0-py3-none-any.whl

---
+++ packages/carlogtt-python-library/carlogtt_python_library-1.2.0-py3-none-any.whl/carlogtt_python_library/utils/miscs.py
@@ -0,0 +1,48 @@
+# ======================================================================
+# MODULE DETAILS
+# This section provides metadata about the module, including its
+# creation date, author, copyright information, and a brief description
+# of the module's purpose and functionality.
+# ======================================================================
+
+#   __|    \    _ \  |      _ \   __| __ __| __ __|
+#  (      _ \     /  |     (   | (_ |    |      |
+# \___| _/  _\ _|_\ ____| \___/ \___|   _|     _|
+
+# miscs.py
+# Created 8/9/23 - 11:34 AM UK Time (London) by carlogtt
+# Copyright (c) Amazon.com Inc. All Rights Reserved.
+# AMAZON.COM CONFIDENTIAL
+
+"""
+This module provides a set of various utilities.
+"""
+
+# ======================================================================
+# EXCEPTIONS
+# This section documents any exceptions made or code quality rules.
+# These exceptions may be necessary due to specific coding requirements
+# or to bypass false positives.
+# ======================================================================
+#
+
+# ======================================================================
+# IMPORTS
+# Importing required libraries and modules for the application.
+# ======================================================================
+
+# Standard Library Imports
+import ctypes
+
+# END IMPORTS
+# ======================================================================
+
+
+# List of public names in the module
+__all__ = [
+    'ref_count',
+]
+
+
+def ref_count(obj_id: int) -> int:
+    return ctypes.c_long.from_address(obj_id).value
