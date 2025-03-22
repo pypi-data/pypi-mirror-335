31f8e58398e5256b128c943a15da3ce60d1f308f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:30 +0000

    Add package nixt/220/nixt-220.tar.gz

---
+++ packages/nixt/nixt-220.tar.gz/nixt-220/nixt/modules/tbl.py
@@ -0,0 +1,68 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "ask": "llm",
+    "brk": "dbg",
+    "cfg": "irc",
+    "dbg": "dbg",
+    "dis": "mdl",
+    "dne": "tdo",
+    "dpl": "rss",
+    "eml": "mbx",
+    "err": "err",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "man": "man",
+    "mbx": "mbx",
+    "mod": "mod",
+    "mre": "irc",
+    "nme": "rss",
+    "now": "mdl",
+    "pwd": "irc",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "slg": "slg",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "udp": "udp",
+    "upt": "upt",
+    "wsd": "wsd"
+}
+
+
+MD5 = {
+    "dbg": "3e8f37c77b8e0a68a3812b8f2d91673e",
+    "err": "c41b0a0b1aaaae11249a767e3fb365d4",
+    "flt": "7657a391f5bc4ecc823c0dc248427ed1",
+    "fnd": "abbf7f2601b5d14f4f80e87c25fbe4d0",
+    "irc": "b65970704659bca5e8d759e555bb65e8",
+    "llm": "984a197166d2636cf878a883ea1d5e39",
+    "log": "799118a89d0649453e30b23d2570ef02",
+    "man": "1182aec1049fc74607eb9efd3e00d9d7",
+    "mbx": "b7e038a6c88e9a6bad396261739b538f",
+    "mdl": "99a1e5441f6e8fcad3104a41f36f1a57",
+    "mod": "82d9b50b3a2a446fbb857de7192b8f28",
+    "req": "a0013d5e8582b1cb56854d17e9c66a95",
+    "rss": "6baac83d5d9ad009fd83cceb3433983e",
+    "rst": "7a9d271930a3a4af80dde0340ea9ec8a",
+    "slg": "3f73218a8d689b60fce8de76af10846a",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "ff882598f1958a571d68726a876dd66b",
+    "thr": "e24a99fce3e81eff1fe58f78ff49ab5a",
+    "tmr": "a920b43a7347f881de0ff83264e124e6",
+    "udp": "0a14a64ed847a3e0a2d583dcec819661",
+    "upt": "a673a80f2085ea2705a391c245c0ab57",
+    "web": "093b007ee6369d23812c72c780b0b708",
+    "wsd": "7bb44c895580b00d0f6bf083e0881267",
+}
