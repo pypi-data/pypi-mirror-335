04711b842b6e6772c59e440eaf6dc24fbbba4d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:17 +0000

    Add package kilight-client/0.3.2/kilight_client-0.3.2.tar.gz

---
+++ packages/kilight-client/kilight_client-0.3.2.tar.gz/kilight_client-0.3.2/src/kilight/client/__init__.py
@@ -0,0 +1,21 @@
+"""kilight.client package for interfacing with KiLight open hardware light controllers."""
+
+__version__ = "0.3.2"
+
+__all__ = [
+    "DEFAULT_PORT",
+    "MIN_COLOR_TEMP",
+    "MAX_COLOR_TEMP",
+    "Connector",
+    "Device",
+    "OutputIdentifier",
+    "OutputState",
+    "DeviceState"
+]
+
+from kilight.protocol import OutputIdentifier
+
+from .const import DEFAULT_PORT, MIN_COLOR_TEMP, MAX_COLOR_TEMP
+from .connector import Connector
+from .device import Device
+from .models import OutputState, DeviceState
