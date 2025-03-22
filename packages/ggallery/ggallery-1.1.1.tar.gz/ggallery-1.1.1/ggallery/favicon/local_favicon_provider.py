df8f1063d96e22e1c81da6a6e1245c35e87d3178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:54 +0000

    Add package ggallery/1.1.1/ggallery-1.1.1.tar.gz

---
+++ packages/ggallery/ggallery-1.1.1.tar.gz/ggallery-1.1.1/ggallery/favicon/local_favicon_provider.py
@@ -0,0 +1,10 @@
+from ..model import FaviconConfig
+from .base_favicon_provider import BaseFaviconProvider, Favicon
+import os
+
+
+class LocalFaviconProvider(BaseFaviconProvider):
+    def get_favicon(self, config: FaviconConfig) -> Favicon:
+        with open(config.name, "rb") as file:
+            file_extension = config.name.split(".")[-1]
+            return Favicon(url=None, content=file.read(), file_type=file_extension)
