a37c6c451be6cab1e8e6621b61a15039f26d89b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:48 +0000

    Add package omlish/0.0.0.dev260/omlish-0.0.0.dev260.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev260.tar.gz/omlish-0.0.0.dev260/omlish/testing/pytest/helpers.py
@@ -0,0 +1,11 @@
+import contextlib
+
+
+@contextlib.contextmanager
+def assert_raises_star(et):
+    num_caught = 0
+    try:
+        yield
+    except* et as eg:  # noqa
+        num_caught += 1
+    assert num_caught > 0
