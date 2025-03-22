2c6d344ea3b6417de1f6140bb2ae281a2c713dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 15:06:14 +0000

    Add package smart-babylon-library/0.4.2/smart_babylon_library-0.4.2-py3-none-any.whl

---
+++ packages/smart-babylon-library/smart_babylon_library-0.4.2-py3-none-any.whl/smart_babylon_library/__init__.py
@@ -0,0 +1,25 @@
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright © 2018-2025, A.A Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+# https://github.com/smartlegionlab/
+# --------------------------------------------------------
+"""Smart Babylon Library"""
+from .library_structure import LibraryStructure
+from .text_encoder import TextEncoder
+from .tools import timeit
+from .babylon_library import BabylonLibrary, BabylonLibraryIterator
+from .babylon_lib_with_books import BabylonLibraryWithBooks
+
+__version__ = '0.4.2'
+__author__ = 'A.A. Suvorov'
+__all__ = [
+    "LibraryStructure",
+    "TextEncoder",
+    "timeit",
+    "BabylonLibrary",
+    "BabylonLibraryWithBooks",
+    "BabylonLibraryIterator"
+]
