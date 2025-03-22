4b73556d290f463d32d2192e5994b02473f97af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 04:05:37 +0000

    Add package gradio-networkgraph/0.0.2/gradio_networkgraph-0.0.2.tar.gz

---
+++ packages/gradio-networkgraph/gradio_networkgraph-0.0.2.tar.gz/gradio_networkgraph-0.0.2/frontend/Network.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+    import type { Gradio } from "@gradio/utils";
+    import { onMount } from "svelte";
+    import { Network } from "vis-network";
+
+    export let gradio: Gradio;
+    export let value;
+    export let selectedNodes = []
+    export let selectedEdges = []
+    export let nodePositions = []
+
+    let container;
+    let network;
+
+    function renderNetwork() {
+      if (!container) return;
+  
+      // Set up the network
+      var nodes = value.nodes
+      var edges = value.edges
+      var options = value.options
+      var data = { nodes, edges, options };
+      network = new Network(container, data);
+
+      function sendData(params) {
+        selectedNodes = params.nodes;
+        selectedEdges = params.edges.map(
+          (edgeId) => network.getConnectedNodes(edgeId)
+        );
+        nodePositions = network.getPositions();
+        return {
+          "nodes": selectedNodes,
+          "edges": selectedEdges,
+          "nodePositions": nodePositions
+        }
+      }
+
+      function selectNode(params) {
+        let selectedNodes = params.nodes;
+        if (selectedNodes.length > 0) {
+          const selectedNodeId = selectedNodes[0];
+          network.focus(
+            selectedNodeId,
+            {
+              scale: 1.5,
+              animation: {
+                duration: 500,
+                easingFunction: "easeInOutQuad",
+              }
+            }
+          )
+          gradio.dispatch("selectNode", sendData(params))
+        }
+      }
+
+      function deselectNode(params) {
+        gradio.dispatch("deselectNode", sendData(params))
+      }
+
+      function selectEdge(params) {
+        gradio.dispatch("selectEdge", sendData(params))
+      }
+
+      function deselectEdge(params) {
+        gradio.dispatch("deselectEdge", sendData(params))
+      }
+
+      function getPositions() {
+        nodePositions = network.getPositions();
+        return {
+          "nodes": [],
+          "edges": [],
+          "nodePositions": nodePositions
+        }
+      }
+
+      function postDraw() {
+        gradio.dispatch("afterDrawing", getPositions())
+      }
+
+      function stabilizationStep() {
+        gradio.dispatch("stabilizationStep", getPositions())
+      }
+
+      // Event Listeners
+      network.on("selectNode", selectNode);
+      network.on("deselectNode", deselectNode);
+      network.on("selectEdge", selectEdge);
+      network.on("deselectEdge", deselectEdge);
+      network.on("afterDrawing", postDraw)
+      network.on("stabilizationStep", postDraw)
+  
+      // Adjust canvas size
+      container.style.width = options.width || "100%";
+      container.style.height = options.height || "500px";
+      network.redraw();
+    };
+  
+    onMount(() => {
+      renderNetwork();
+    });
+
+    $: if (value) {
+      renderNetwork()
+    }
+
+  </script>
+  
+  <style>
+    #vis-network-container {
+      width: 100%;
+      height: 100%;
+      /* border: 1px solid lightgray; */
+    }
+  </style>
+  
+  <div id="vis-network-container" bind:this={container}></div>
+  
