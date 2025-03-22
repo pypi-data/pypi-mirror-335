160b449a2462e744494ccee004c9b7fc0db594a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 07:06:52 +0000

    Add package g2m-snowflake-sdk-python/1.0.12/g2m_snowflake_sdk_python-1.0.12.tar.gz

---
+++ packages/g2m-snowflake-sdk-python/g2m_snowflake_sdk_python-1.0.12.tar.gz/g2m_snowflake_sdk_python-1.0.12/README.md
@@ -0,0 +1,50 @@
+# Python SDK for the G2M Platform API
+
+## Overview
+This Python client will give you access to the G2M API. See files in the `examples` folder
+for examples showing how to use the client. Note that a `client_id` should always be provided when querying the API; it is used for reporting purposes.
+* For general information please see https://g2m.ai.
+* For help and support see https://support.g2m.ai.
+* For SDK reference documentation see 'Coming Soon'.
+
+## Installation instructions
+Getting the client set up will require the following:
+
+1. Install the latest version of the client on your local machine:
+```
+pip install g2m-snowflake-sdk-python -U
+```
+
+2. Get an API username and password from your G2M admin (you may need SSO credentials from your local admin instead).
+
+3. Confirm you are able to connect to the API, and check the API version
+as follows from a Python session:
+```
+>>> from g2mclient import Analyzer
+>>> analyzer = Analyzer(host="<your host>")
+>>> analyzer.login(
+    user="snowflakeuser",
+    account="account-idenfitier",
+    endpoint="xyz.snowflakecomputing.com",
+    role="snowflakerole",
+    snowflake_account_url="xyz.snowflakecomputing.com",
+    lifetime=59,
+    renewal_delay=54
+)
+Login successful
+>>> Analyzer().version()
+{'status': 200, 'response': {'version': 'x.x.xxx', 'tenant': <your tenant name>, 'copyright': '2024 (c) Go2Market Insights Inc. All rights reserved.'}}
+```
+
+## Testing instructions
+If you are developing the SDK and would like to test the repo, clone it locally using git then 
+run the following from the root directory:
+```
+python -m unittest tests.test_all -v   # all tests
+python -m unittest tests.test_quick -v # quick tests
+```
+Make sure you update the `config.json` file first to include the name of your API tenant. 
+To run a single test case do:
+```
+python -m unittest tests.test_all.PropensityTest.test_logistic_regression_classifier -v
+```
---
+++ packages/g2m-snowflake-sdk-python/g2m_snowflake_sdk_python-1.0.12.tar.gz/g2m_snowflake_sdk_python-1.0.12/g2mclient/client_basic_auth.py
@@ -0,0 +1,82 @@
+"""
+Copyright (c) 2025 G2M Insights Inc
+All rights reserved.
+https://g2m.ai
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+import requests
+import json
+
+from .constants import *
+
+
+class BasicAuthClient:
+    """
+    The BasicAuthClient class manages low-level interactions with the Analyzr API
+    using basic authentication (when applicable).
+
+    """
+    def __init__(self, user=None, pwd=None, host=None):
+        """
+        :param user:
+        :param pwd:
+        :param host:
+        :return:
+        """
+        if host is None:
+            print('ERROR! Please provide a valid host, e.g. host=acme.api.g2m.ai')
+            exit(1)
+        self.__user = user
+        self.__pwd = pwd
+        self._base_url = 'https://{}/api/v1'.format(host)
+        return
+
+    ############################################################################
+    #                           Basic API calls                                #
+    ############################################################################
+
+    def _get(self, uri, params=None):
+        """
+        Returns standard GET response for URI. Note that this
+        function is protected, not private.
+        :param uri:
+        :param params:
+        :return res:
+        """
+        if params is None:
+            r = requests.get(uri, auth=(self.__user, self.__pwd))
+        else:
+            r = requests.get(uri, auth=(self.__user, self.__pwd), params=params)
+        return self._response(r)
+
+    def _post(self, uri, json_obj, compressed=False):
+        """
+        Returns standard POST response for URI. Note that this
+        function is protected, not private.
+        :param uri:
+        :param json_obj:
+        :param compressed: not used at this time
+        :return res:
+        """
+        data = json.dumps(json_obj)
+        r = requests.post(uri, auth=(self.__user, self.__pwd), json=data)
+        return self._response(r)
+
+    def _response(self, r):
+        """
+        """
+        res = {}
+        res['status'] = r.status_code
+        if r.status_code>=200 and r.status_code<300:
+            res['response'] = r.json()
+        else:
+            print('WARNING! Request returned status code: {}'.format(r.status_code))
+            res['response'] = None
+        return res
