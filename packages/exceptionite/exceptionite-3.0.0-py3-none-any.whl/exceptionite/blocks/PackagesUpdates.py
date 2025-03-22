ec370a30b095f6940e9445b0304b9320a0a6d402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 03:14:48 +0000

    Add package exceptionite/3.0.0/exceptionite-3.0.0-py3-none-any.whl

---
+++ packages/exceptionite/exceptionite-3.0.0-py3-none-any.whl/exceptionite/blocks/PackagesUpdates.py
@@ -0,0 +1,47 @@
+import importlib.metadata
+import requests
+
+from ..Block import Block
+
+
+def get_latest_version(name):
+    r = requests.get(f"https://pypi.org/pypi/{name}/json")
+    if r.status_code == 200:
+        version = r.json()["info"]["version"]
+        return version
+    return None
+
+
+class PackagesUpdates(Block):
+    id = "packages_updates"
+    name = "Packages to update"
+    icon = "ArrowCircleUpIcon"
+    component = "PackagesUpdatesBlock"
+    empty_msg = "Selected packages are up to date !"
+    disable_scrubbing = True
+
+    def build(self):
+        installed_packages = {
+            dist.metadata["Name"]: dist.version for dist in importlib.metadata.distributions()
+        }
+
+        packages_to_check = self.options.get("list", ["exceptionite"])
+        packages = {}
+        if packages_to_check:
+            for package_name in packages_to_check:
+                current_version = installed_packages.get(package_name)
+                latest_version = get_latest_version(package_name)
+                if current_version != latest_version:
+                    packages.update(
+                        {
+                            package_name: {
+                                "current": installed_packages.get(package_name),
+                                "latest": latest_version,
+                            }
+                        }
+                    )
+
+        return packages
+
+    def has_content(self):
+        return len(self.data.keys()) > 0
