de38a4b2100e685ab7f5f91429c0eaf94a4efb95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 15:05:56 +0000

    Add package chatsynth-vanshajr/0.3.0/chatsynth_vanshajr-0.3.0-py3-none-any.whl

---
+++ packages/chatsynth-vanshajr/chatsynth_vanshajr-0.3.0-py3-none-any.whl/chatsynth_vanshajr/__init__.py
@@ -0,0 +1,8 @@
+from .retriever import ChatSynthRetriever
+from .json_creator import json_creator
+from .faiss_creator import faiss_creator
+from .github_deploy import github_deploy
+
+__version__ = "0.1.0"
+__author__ = "Vanshaj Raghuvanshi vanshajraghuvanshi@email.com"
+__license__ = "BSD-3.0"
