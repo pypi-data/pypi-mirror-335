6e4f33deca2c89f6207f610943c1aaa9b67e0be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 06:05:17 +0000

    Add package streamingcommunity-unofficialapi/2.0.5/streamingcommunity_unofficialapi-2.0.5.tar.gz

---
+++ packages/streamingcommunity-unofficialapi/streamingcommunity_unofficialapi-2.0.5.tar.gz/streamingcommunity_unofficialapi-2.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="streamingcommunity-unofficialapi",
+    version="2.0.5",
+    author="BluTiger",
+    description="A simple unofficial api for the italian StreamingCommunity website.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Blu-Tiger/streamingcommunity-unofficialapi",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "beautifulsoup4>=4.12.3",
+    ],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+    ],
+    python_requires=">= 3.10",
+)
