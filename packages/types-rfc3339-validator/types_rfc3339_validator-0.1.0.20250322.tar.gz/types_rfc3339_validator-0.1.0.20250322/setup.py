e14c2031eb578b2074bd596899793d95a087e66b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 14:06:10 +0000

    Add package types-rfc3339-validator/0.1.0.20250322/types_rfc3339_validator-0.1.0.20250322.tar.gz

---
+++ packages/types-rfc3339-validator/types_rfc3339_validator-0.1.0.20250322.tar.gz/types_rfc3339_validator-0.1.0.20250322/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+name = "types-rfc3339-validator"
+description = "Typing stubs for rfc3339-validator"
+long_description = '''
+## Typing stubs for rfc3339-validator
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`rfc3339-validator`](https://github.com/naimetti/rfc3339-validator) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+[Pyre](https://pyre-check.org/),
+PyCharm, etc. to check code that uses `rfc3339-validator`. This version of
+`types-rfc3339-validator` aims to provide accurate annotations for
+`rfc3339-validator==0.1.*`.
+
+This package is part of the [typeshed project](https://github.com/python/typeshed).
+All fixes for types and metadata should be contributed there.
+See [the README](https://github.com/python/typeshed/blob/main/README.md)
+for more details. The source for this package can be found in the
+[`stubs/rfc3339-validator`](https://github.com/python/typeshed/tree/main/stubs/rfc3339-validator)
+directory.
+
+This package was tested with
+mypy 1.15.0,
+pyright 1.1.397,
+and pytype 2024.10.11.
+It was generated from typeshed commit
+[`494a5d1b98b3522173dd7e0f00f14a32be00456b`](https://github.com/python/typeshed/commit/494a5d1b98b3522173dd7e0f00f14a32be00456b).
+'''.lstrip()
+
+setup(name=name,
+      version="0.1.0.20250322",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/rfc3339-validator.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['rfc3339_validator-stubs'],
+      package_data={'rfc3339_validator-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0",
+      python_requires=">=3.9",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
