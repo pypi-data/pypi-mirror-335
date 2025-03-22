7f3840cc9066685f756b013f3ba05d584a8563c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:38 +0000

    Add package tsp/1.7.7/tsp-1.7.7-py3-none-any.whl

---
+++ packages/tsp/tsp-1.7.7-py3-none-any.whl/tsp/__meta__.py
@@ -0,0 +1,3 @@
+# Automatically created. Please do not edit.
+__version__ = '1.7.7'
+__author__ = 'Nick Brown'
---
+++ packages/tsp/tsp-1.7.7-py3-none-any.whl/tsp/scratch.py
@@ -0,0 +1,6 @@
+from tsp import read_geotop
+
+t = read_geotop(r"C:\Users\Nick\Desktop\soil0032.txt")
+import pdb;pdb.set_trace()
+
+print('ok')
