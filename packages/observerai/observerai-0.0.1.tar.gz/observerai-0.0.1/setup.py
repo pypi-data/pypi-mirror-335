f690c9c961f2f34ba50550682842d0e9a7dc0513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 14:06:30 +0000

    Add package observerai/0.0.1/observerai-0.0.1.tar.gz

---
+++ packages/observerai/observerai-0.0.1.tar.gz/observerai-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="observerai",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Observability and evaluation software for AI Models.",
+    author="Nelson Frugeri, Jr",
+    author_email="f18.cloud@gmail.com",
+    license="MIT",
+    url="https://github.com/nelsonfrugeri-tech/observerai",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.10",
+)
