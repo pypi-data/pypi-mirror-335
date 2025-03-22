73248a5acf920138d1543968e8796fc9f0cd4c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 14:05:32 +0000

    Add package call-serverless/0.1.6/call_serverless-0.1.6.tar.gz

---
+++ packages/call-serverless/call_serverless-0.1.6.tar.gz/call_serverless-0.1.6/call_serverless/formatter.py
@@ -0,0 +1,36 @@
+import json
+import os
+from typing import Union
+
+
+def base_template():
+    with open(os.path.join(os.path.dirname(__file__), "template.json"), "r") as file:
+        request = json.load(file)
+    return request
+
+
+def format_request(
+    stage: str,
+    path: str,
+    method: str,
+    body: Union[dict, None] = None,
+    headers: Union[dict, None] = None,
+):
+    """
+    @param headers: dict
+    @param path: str - the path of the resource and it starts with a forward slash
+    """
+    if not path.startswith("/"):
+        path = f"/{path}"
+
+    template = base_template()
+    template["resource"] = path
+    template["path"] = path
+    template["headers"] = headers
+    template["requestContext"]["resourcePath"] = path
+    template["requestContext"]["path"] = f"/{stage}{path}"
+    template["requestContext"]["stage"] = stage
+    template["body"] = body
+    template["httpMethod"] = method
+    template["requestContext"]["httpMethod"] = method
+    return template
