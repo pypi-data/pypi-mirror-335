7b82716d297825dbd0daece9c07c2ca777f646c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:18 +0000

    Add package brainmaze-eeg/0.1.0/brainmaze_eeg-0.1.0.tar.gz

---
+++ packages/brainmaze-eeg/brainmaze_eeg-0.1.0.tar.gz/brainmaze_eeg-0.1.0/docs/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.dev8+g39e20f4.d20250321',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
---
+++ packages/brainmaze-eeg/brainmaze_eeg-0.1.0.tar.gz/brainmaze_eeg-0.1.0/docs_src/source/brainmaze_eeg.rst
@@ -0,0 +1,18 @@
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Projects
+
+   project_wave_detector
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Modules
+
+   classifiers
+   crp
+   hypnogram
+   scikit_modules
+   features
+
+
