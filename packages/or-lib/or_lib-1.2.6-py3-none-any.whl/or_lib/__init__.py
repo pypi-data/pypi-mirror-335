c8c890071403844d6a912965d84c431adf598ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 14:05:53 +0000

    Add package or-lib/1.2.6/or_lib-1.2.6-py3-none-any.whl

---
+++ packages/or-lib/or_lib-1.2.6-py3-none-any.whl/or_lib/__init__.py
@@ -0,0 +1,5 @@
+from .lightrag import LightRAG as LightRAG, QueryParam as QueryParam
+
+__version__ = "1.2.6"
+__author__ = "ANKITDHARIWAL-Tech"
+__url__ = "https://github.com/AIStudioShorthills/optimizeRagLibrary"
