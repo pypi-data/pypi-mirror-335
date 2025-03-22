a320c9c3836d6463eff5a87c3ce23cbf0b9fd594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 15:06:08 +0000

    Add package applocker/1.1.2/applocker-1.1.2.tar.gz

---
+++ packages/applocker/applocker-1.1.2.tar.gz/applocker-1.1.2/applocker/__init__.py
@@ -0,0 +1,3 @@
+from applocker.utils import dump, dumps, load, loads
+
+__all__ = ["dump", "dumps", "load", "loads"]
---
+++ packages/applocker/applocker-1.1.2.tar.gz/applocker-1.1.2/tests/test_utils.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from applocker.policy import AppLockerPolicy
+from applocker.utils import dumps, load, loads
+
+
+class TestUtils(TestCase):
+    def test_dump(self):
+        pass
+
+    def test_dumps(self):
+        policy = AppLockerPolicy()
+        self.assertEqual(dumps(policy), '<AppLockerPolicy Version="1" />')
+
+    def test_load(self):
+        with open("tests/files/policy.xml", "r") as file:
+            self.assertIsInstance(load(file), AppLockerPolicy)
+
+    def test_loads(self):
+        policy = loads('<AppLockerPolicy Version="1" />')
+        self.assertIsInstance(policy, AppLockerPolicy)
