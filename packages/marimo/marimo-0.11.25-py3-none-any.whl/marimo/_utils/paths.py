1ed4f60c83b1a5d53633418e87171f8b41c1f448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 04:05:19 +0000

    Add package marimo/0.11.25/marimo-0.11.25-py3-none-any.whl

---
+++ packages/marimo/marimo-0.11.25-py3-none-any.whl/marimo/_utils/paths.py
@@ -0,0 +1,39 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+import os
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+def import_files(filename: str) -> Any:
+    from importlib.resources import files as importlib_files
+
+    return importlib_files(filename)
+
+
+def pretty_path(filename: str) -> str:
+    """
+    If it's an absolute path, shorten to relative path if
+    we don't go outside the current directory.
+    Otherwise, return the filename as is.
+    """
+    if os.path.isabs(filename):
+        try:
+            relpath = os.path.relpath(filename)
+        except ValueError:
+            # Windows: relpath doesn't work if filename is on a different drive
+            # than current drive
+            return filename
+        if not relpath.startswith(".."):
+            return relpath
+    return filename
+
+
+def maybe_make_dirs(filepath: Path) -> None:
+    """
+    Create directories if they don't exist.
+    """
+    filepath.parent.mkdir(parents=True, exist_ok=True)
