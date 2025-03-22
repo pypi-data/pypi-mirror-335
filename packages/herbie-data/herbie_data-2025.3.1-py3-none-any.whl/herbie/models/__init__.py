171c6f1d7a71eae7e0f36f4bb34d80b7756820e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 15:06:12 +0000

    Add package herbie-data/2025.3.1/herbie_data-2025.3.1-py3-none-any.whl

---
+++ packages/herbie-data/herbie_data-2025.3.1-py3-none-any.whl/herbie/models/__init__.py
@@ -0,0 +1,55 @@
+"""
+Import all the model template classes.
+
+A Herbie object specifies the
+template by setting ``model='template_class_name'``. For example:
+
+    Herbie('2022-01-01', model='hrrr')
+
+Where "hrrr" is the name of the template class located in models/hrrr.py.
+"""
+
+import sys
+
+from herbie import _config_path
+from herbie.misc import ANSI
+
+# ======================================================================
+#                     Import Public Model Templates
+# ======================================================================
+from .ecmwf import *
+from .gdps import *
+from .gefs import *
+from .gfs import *
+from .hafs import *
+from .hiresw import *
+from .hrdps import *
+from .href import *
+from .hrrr import *
+from .nam import *
+from .nbm import *
+from .nexrad import *
+from .rap import *
+from .rdps import *
+from .rrfs import *
+from .rtma import *
+from .usnavy import *
+from .urma import *
+from .cfs import *
+
+# ======================================================================
+#                     Import Private Model Templates
+# ======================================================================
+_custom_template_file = _config_path / "custom_template.py"
+
+try:
+    if _custom_template_file.exists():
+        sys.path.insert(1, str(_custom_template_file.parent))
+        from custom_template import *
+except Exception:
+    print(
+        f" ╭─{ANSI.herbie}─────────────────────────────────────────────╮\n"
+        f" │ WARNING: Could not load custom template from         │\n"
+        f" │ {ANSI.orange}{str(_custom_template_file):^50s}{ANSI.reset}   │\n"
+        f" ╰──────────────────────────────────────────────────────╯\n"
+    )
