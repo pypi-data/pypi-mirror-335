775c52a1d9ff652662fa3f01b0363d8acd2578f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 04:06:04 +0000

    Add package codablellm/1.0.1/codablellm-1.0.1-py3-none-any.whl

---
+++ packages/codablellm/codablellm-1.0.1-py3-none-any.whl/codablellm/__init__.py
@@ -0,0 +1,25 @@
+'''
+codablellm is a framework for creating and curating high-quality code datasets tailored for large language models
+'''
+
+import logging
+
+from rich.logging import RichHandler
+
+from codablellm.core import extractor, decompiler, ExtractConfig, DecompileConfig, rate_limiter
+from codablellm.dataset import SourceCodeDatasetConfig, DecompiledCodeDatasetConfig
+from codablellm.repoman import (create_source_dataset, create_decompiled_dataset,
+                                compile_dataset, ManageConfig)
+
+__version__ = '1.0.0'
+__all__ = ['create_source_dataset',
+           'create_decompiled_dataset', 'compile_dataset',
+           'extractor', 'decompiler', 'ExtractConfig', 'DecompileConfig',
+           'ManageConfig', 'SourceCodeDatasetConfig', 'DecompiledCodeDatasetConfig',
+           'rate_limiter']
+
+# Configure logger
+logging.basicConfig(
+    level=logging.INFO, format='%(message)s', datefmt='[%X]', handlers=[RichHandler()]
+)
+logger = logging.getLogger('rich')
