f23ca94fb020377aeb4f60bc90cc7e30611642cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 03:14:34 +0000

    Add package test-python-codes/0.2/test_python_codes-0.2.tar.gz

---
+++ packages/test-python-codes/test_python_codes-0.2.tar.gz/test_python_codes-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test_python_codes',
+    version='0.2',
+    packages=find_packages(),  # This should automatically include your folder
+    install_requires=[],
+)
