b8b70a6a39168f230eac901a1e34d087b42de52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 03:14:53 +0000

    Add package python-hello-world-project-prasanta/0.7.0/python_hello_world_project_prasanta-0.7.0.tar.gz

---
+++ packages/python-hello-world-project-prasanta/python_hello_world_project_prasanta-0.7.0.tar.gz/python_hello_world_project_prasanta-0.7.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python_hello_world_project_prasanta",
+    version="0.7.0",
+    packages=find_packages(include=["python_hello_world_project_prasanta", "python_hello_world_project_prasanta.*"]),
+    install_requires=[],
+)
