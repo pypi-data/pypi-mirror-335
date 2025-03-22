b8b820340741f0bf307022487977c5845c243f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 14:05:51 +0000

    Add package hcli-core/3.6.1/hcli_core-3.6.1-py3-none-any.whl

---
+++ packages/hcli-core/hcli_core-3.6.1-py3-none-any.whl/hcli_core/package.py
@@ -0,0 +1,5 @@
+__version__ = "3.6.1"
+dependencies = ["falcon==4.0.2",
+                "portalocker==2.10.1",
+                "huckle>=5.5.6,<6.0.0",
+                "hcli_problem_details==0.1.1"]
