9fbe7bc23f8b30c33e9c0ed39c622921ca095957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 14:06:21 +0000

    Add package panlit/0.1.1/panlit-0.1.1.tar.gz

---
+++ packages/panlit/panlit-0.1.1.tar.gz/panlit-0.1.1/README.md
@@ -0,0 +1,10 @@
+# Pan Lit
+
+Panlit for StreamLit
+
+## Installation
+
+Instructions on how to install the project:
+
+```bash
+pip install panlit
---
+++ packages/panlit/panlit-0.1.1.tar.gz/panlit-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='panlit',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'streamlit>=1.0.0',
+    ],
+    author='Pan Lancer',
+    description='A Python package that depends on Streamlit'
+)
