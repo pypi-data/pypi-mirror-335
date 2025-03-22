19c2c3a1721ea0269a957d152146a02f2e31a491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 15:06:15 +0000

    Add package pybase8/0.7/pybase8-0.7.tar.gz

---
+++ packages/pybase8/pybase8-0.7.tar.gz/pybase8-0.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name='pybase8', 
+    version='0.7',
+    packages=find_packages(),
+    author='Nasr',
+    author_email='nasr2python@gmail.com',
+    description='Simple',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+        ]
+) 
