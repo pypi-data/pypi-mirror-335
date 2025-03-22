f31da51bbb325dfc8e5d84172c515f85cf04f23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:14 +0000

    Add package cryptocipherx/5.0/cryptocipherx-5.0.tar.gz

---
+++ packages/cryptocipherx/cryptocipherx-5.0.tar.gz/cryptocipherx-5.0/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="cryptocipherx",
+    version="5.0",
+    description="...",
+    author="Alfisene Keita",
+    packages=find_packages(),
+    install_requires=[
+        'pycryptodome',
+        'cryptography',  # Ensure this dependency is installed
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
