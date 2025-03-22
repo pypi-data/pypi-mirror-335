7dbbe2c026e3257f2318ab69961361381a342650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 06:05:33 +0000

    Add package encord-agents/0.1.11/encord_agents-0.1.11.tar.gz

---
+++ packages/encord-agents/encord_agents-0.1.11.tar.gz/encord_agents-0.1.11/encord_agents/fastapi/__init__.py
@@ -0,0 +1,9 @@
+from .dependencies import dep_client, dep_label_row, dep_single_frame
+from .utils import verify_auth
+
+__all__ = [
+    "dep_single_frame",
+    "dep_label_row",
+    "dep_client",
+    "verify_auth",
+]
