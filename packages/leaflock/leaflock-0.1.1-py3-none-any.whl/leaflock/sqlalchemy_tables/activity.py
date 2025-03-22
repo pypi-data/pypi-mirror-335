c11ea806bb1252e90df12b99555a4376ba0d4a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 14:05:47 +0000

    Add package leaflock/0.1.1/leaflock-0.1.1-py3-none-any.whl

---
+++ packages/leaflock/leaflock-0.1.1-py3-none-any.whl/leaflock/sqlalchemy_tables/activity.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import uuid
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, MappedAsDataclass, mapped_column, relationship
+
+from .base import Base
+from .joins import module_activity
+from .textbook import Textbook
+
+if TYPE_CHECKING:
+    from .module import Module
+
+
+class Activity(MappedAsDataclass, Base):
+    __tablename__ = "activities"
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    guid: Mapped[uuid.UUID] = mapped_column(init=False, insert_default=uuid.uuid4)
+
+    name: Mapped[str]
+    description: Mapped[str]
+    prompt: Mapped[str]
+
+    textbook_id: Mapped[int] = mapped_column(ForeignKey("textbooks.id"), init=False)
+    textbook: Mapped[Textbook] = relationship(back_populates="activities", init=False)
+
+    modules: Mapped[set[Module]] = relationship(
+        default_factory=set,
+        back_populates="activities",
+        secondary=module_activity,
+    )
+
+    def __hash__(self) -> int:
+        return hash(f"{self.id}{self.name}{self.textbook_id}")
