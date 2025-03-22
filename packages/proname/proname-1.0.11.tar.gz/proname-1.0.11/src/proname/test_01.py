8f87195b6d17876a08c5c97f3a3cec6067a7c910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 05:05:13 +0000

    Add package proname/1.0.11/proname-1.0.11.tar.gz

---
+++ packages/proname/proname-1.0.11.tar.gz/proname-1.0.11/src/proname/test_01.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import ctypes
+
+
+def test_01():
+    print("test_01")
+    print(os.getcwd())
+    print(sys.path)
+
+def test_02():
+    # 获取当前py文件路径
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    print(current_path, "3")
+    # 切换到当前路径
+    dll_path = os.path.join(current_path, r'.\include\VnHardwareConf.dll')
+
+    HdConfigLib = ctypes.CDLL(dll_path)
+    HdConfigLib.getVersion.restype = ctypes.c_char_p
+    result = HdConfigLib.getVersion()
+    print(f"Result: {result.decode('utf-8')}")
+
+
+if __name__ == '__main__':
+    test_01()
+    test_02()
