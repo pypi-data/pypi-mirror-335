8e1ef3558ff3a001ba402f8abc048ed15513eb61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 14:05:36 +0000

    Add package nullzbqhyr/1.0.0/nullzbqhyr-1.0.0.tar.gz

---
+++ packages/nullzbqhyr/nullzbqhyr-1.0.0.tar.gz/nullzbqhyr-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nullzbqhyr",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3,<=4",
+    author="xystudio",
+    author_email="173288240@qq.com",
+)
