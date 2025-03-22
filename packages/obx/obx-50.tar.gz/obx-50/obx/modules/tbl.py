ad922b10f4f8b97f67f221fce6210a1653899670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-22 14:05:54 +0000

    Add package obx/50/obx-50.tar.gz

---
+++ packages/obx/obx-50.tar.gz/obx-50/obx/modules/tbl.py
@@ -0,0 +1,57 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "brk": "dbg",
+    "cfg": "irc",
+    "dbg": "dbg",
+    "dis": "mdl",
+    "dne": "tdo",
+    "dpl": "rss",
+    "err": "err",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "man": "man",
+    "mod": "mod",
+    "mre": "irc",
+    "nme": "rss",
+    "now": "mdl",
+    "pwd": "irc",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "slg": "slg",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "upt": "upt"
+}
+
+
+MD5 = {
+    "dbg": "3e8f37c77b8e0a68a3812b8f2d91673e",
+    "err": "c41b0a0b1aaaae11249a767e3fb365d4",
+    "flt": "7657a391f5bc4ecc823c0dc248427ed1",
+    "fnd": "abbf7f2601b5d14f4f80e87c25fbe4d0",
+    "irc": "4d528e48a802ad50d9308523ec57c23c",
+    "log": "799118a89d0649453e30b23d2570ef02",
+    "man": "1182aec1049fc74607eb9efd3e00d9d7",
+    "mdl": "99a1e5441f6e8fcad3104a41f36f1a57",
+    "mod": "82d9b50b3a2a446fbb857de7192b8f28",
+    "req": "a0013d5e8582b1cb56854d17e9c66a95",
+    "rss": "6baac83d5d9ad009fd83cceb3433983e",
+    "slg": "3f73218a8d689b60fce8de76af10846a",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "ff882598f1958a571d68726a876dd66b",
+    "thr": "e24a99fce3e81eff1fe58f78ff49ab5a",
+    "tmr": "c6fd7a8d9b3e97c31a16873c3b5df5b2",
+    "upt": "a673a80f2085ea2705a391c245c0ab57",
+}
