b40e7737bb402feded666d4aea6d4bf074b2428a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 15:06:38 +0000

    Add package MENACE-ANN/1.2/menace_ann-1.2.tar.gz

---
+++ packages/MENACE-ANN/menace_ann-1.2.tar.gz/menace_ann-1.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+with open("README.md", 'r') as readme:
+    long_description = readme.read()
+
+
+
+setup(
+    name="MENACE_ANN",
+    version="1.2",
+    description="An implementation of the first Noughts and Crosses Artificial Neural Network, made with matchboxes and MENACE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LordUbuntu/MENACE",
+    keywords=["python", "tic tac toe", "noughts and crosses", "Matchbox Enabled Noughts and Crosses Engine", "MENACE", "Neural Network", "AI"],
+    license="MIT",
+    author="Jacobus Burger",
+    author_email="therealjacoburger@gmail.com",
+    packages=["menace_ann"],
+    extras_require={
+        "dev": ["pytest>=7.2", "twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
+    platforms=["any"],
+    py_modules=["menace_ann"],
+    entry_points={
+        "console_scripts": ["menace_ann=menace_ann.__main__:main"]
+    },
+    classifiers=[
+        "Development Status :: 7 - Inactive",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "Topic :: Games/Entertainment",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Education",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
