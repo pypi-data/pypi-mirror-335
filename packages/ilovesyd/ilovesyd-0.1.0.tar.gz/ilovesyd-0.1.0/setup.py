69c3143fdc0f6039a9d36850b96cb2d4e06503dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 16:06:17 +0000

    Add package ilovesyd/0.1.0/ilovesyd-0.1.0.tar.gz

---
+++ packages/ilovesyd/ilovesyd-0.1.0.tar.gz/ilovesyd-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ilovesyd",
+    version="0.1.0",
+    author="KrishVi",
+    author_email="krish@ilovesyd.xyz",
+    description="A Python wrapper for ilovesyd.xyz API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/KrishhVii/ilovesyd",  # Change to your repo
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp",  # Since it's async
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
