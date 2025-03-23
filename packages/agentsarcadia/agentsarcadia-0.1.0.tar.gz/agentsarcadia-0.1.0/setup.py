ece1b1a14e1c68a613bc0c174c6b2e278402cf2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 14:05:46 +0000

    Add package agentsarcadia/0.1.0/agentsarcadia-0.1.0.tar.gz

---
+++ packages/agentsarcadia/agentsarcadia-0.1.0.tar.gz/agentsarcadia-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="agentsarcadia",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "langchain-openai>=0.3.9",
+        "langgraph>=0.3.18",
+        "python-dotenv>=1.0.1",
+    ],
+    author="seiji0906",
+    author_email="yuto.miyashita.info@gmail.com",
+    description="LangGraphによるワークフロー構築を支援するエージェントツール agentsarcadia",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/seiji0906/agentsarcadia",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
