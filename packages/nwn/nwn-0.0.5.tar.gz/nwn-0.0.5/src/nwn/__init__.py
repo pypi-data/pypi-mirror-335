8c23da4292e201bd06b8429883873cd47a3cd6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 05:06:13 +0000

    Add package nwn/0.0.5/nwn-0.0.5.tar.gz

---
+++ packages/nwn/nwn-0.0.5.tar.gz/nwn-0.0.5/src/nwn/__init__.py
@@ -0,0 +1,26 @@
+"""
+nwn
+===
+
+A python package with various modules for accessing Neverwinter Nights: Enhanced Edition
+data formats and functionality.
+
+Stability
+---------
+
+This package is currently in ALPHA state. API stability is not guaranteed.
+
+Installation
+------------
+
+The package is available on PyPI and can be installed with pip:
+
+.. code-block:: bash
+
+    pip install nwn
+
+License
+-------
+
+This package is licensed under the MIT license.
+"""
