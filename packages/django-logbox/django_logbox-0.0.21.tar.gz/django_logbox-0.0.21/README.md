acaccb18e86fa598fd1b0a6f823300894733baa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 15:06:44 +0000

    Add package django-logbox/0.0.21/django_logbox-0.0.21.tar.gz

---
+++ packages/django-logbox/django_logbox-0.0.21.tar.gz/django_logbox-0.0.21/README.md
@@ -0,0 +1,92 @@
+<a name="readme-top"></a>
+
+<div align="center">
+
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+
+</div>
+
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+
+[//]: # (  <a href="https://github.com/tgoddessana/Django-Logbox">)
+
+[//]: # (    <img src="docs/_static/full_logo.svg" style="width:75%";>)
+
+[//]: # (  </a>)
+
+<h3 align="center">Django-Logbox</h3>
+
+  <p align="center">
+    Your small, but useful django log box. 📦
+    <br />
+    <a href="https://tgoddessana.github.io/Django-Logbox/"><strong>Explore the docs »</strong></a>
+    <br />
+  </p>
+</div>
+
+
+<!---------------------------------------------------------------------------------------------->
+
+<br/>
+
+## Django-Logbox
+
+- todo
+
+<br/>
+
+## License
+
+---
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+
+
+<!---------------------------------------------------------------------------------------------->
+
+<br/>
+
+## Contact
+
+---
+
+- Author Email: twicegoddessana1229@gmail.com
+- Project Link: [https://github.com/tgoddessana/Django-Logbox](https://github.com/tgoddessana/Django-Logbox)
+
+<!---------------------------------------------------------------------------------------------->
+<!---------------------------------------------------------------------------------------------->
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+
+[contributors-shield]: https://img.shields.io/github/contributors/tgoddessana/Django-Logbox.svg?style=for-the-badge
+
+[contributors-url]: https://github.com/tgoddessana/Django-Logbox/graphs/contributors
+
+[forks-shield]: https://img.shields.io/github/forks/tgoddessana/Django-Logbox.svg?style=for-the-badge
+
+[forks-url]: https://github.com/tgoddessana/Django-Logbox/network/members
+
+[stars-shield]: https://img.shields.io/github/stars/tgoddessana/Django-Logbox.svg?style=for-the-badge
+
+[stars-url]: https://github.com/tgoddessana/Django-Logbox/stargazers
+
+[issues-shield]: https://img.shields.io/github/issues/tgoddessana/Django-Logbox.svg?style=for-the-badge
+
+[issues-url]: https://github.com/tgoddessana/Django-Logbox/issues
+
+[license-shield]: https://img.shields.io/github/license/tgoddessana/Django-Logbox.svg?style=for-the-badge
+
+[license-url]: https://github.com/tgoddessana/Django-Logbox/blob/master/LICENSE.txt
+
+[Python]: https://img.shields.io/badge/python-306998?style=for-the-badge&logo=python&logoColor=white
+
+
---
+++ packages/django-logbox/django_logbox-0.0.21.tar.gz/django_logbox-0.0.21/django_logbox/models.py
@@ -0,0 +1,122 @@
+from django.db.models import (
+    CharField,
+    DateTimeField,
+    GenericIPAddressField,
+    IntegerField,
+    Model,
+    QuerySet,
+    TextField,
+)
+from django.utils.translation import gettext_lazy as _
+
+
+class ServerLogQuerySet(QuerySet): ...
+
+
+class ServerLog(Model):
+    # http
+    method = CharField(
+        _("method"),
+        help_text=_("HTTP method used for the request, e.g., 'GET', 'POST', 'PUT'."),
+        max_length=10,
+    )
+    path = CharField(
+        _("path"),
+        help_text=_(
+            "The endpoint path requested, excluding the domain, e.g., '/api/v1/users/'."
+        ),
+        max_length=255,
+    )
+    status_code = IntegerField(
+        _("status_code"),
+        help_text=_("HTTP status code of the response, e.g., 200, 404, 500."),
+    )
+    user_agent = TextField(
+        _("user_agent"),
+        help_text=_(
+            "User-Agent string from the client's request header, providing browser and OS details."
+        ),
+        max_length=255,
+        null=True,
+    )
+
+    # for example: Mac(Apple, Mac) , Samsung SM-S711N(Samsung, SM-S711N)
+    device = CharField(
+        _("device"),
+        help_text=_("Device data parsed from User-Agent request header."),
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    # for example: Android(14), iOS(13.2.3), Windows(10)
+    os = CharField(
+        _("os"),
+        help_text=_("OS data parsed from User-Agent request header."),
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    # for example: Yeti(10), Mobile Safari(13.2.3)
+    browser = CharField(
+        _("browser"),
+        help_text=_("Browser data parsed from User-Agent request header."),
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+
+    querystring = TextField(
+        _("querystring"),
+        help_text=_(
+            "Query parameters of the request as a URL-encoded string, e.g., 'param1=value1&ampparam2=value2'."
+        ),
+        null=True,
+    )
+    request_body = TextField(
+        _("request_body"),
+        help_text=_(
+            "Body content of the request, usually JSON or form data. Null if no body was sent."
+        ),
+        null=True,
+    )
+
+    # log
+    timestamp = DateTimeField(
+        _("timestamp"),
+        help_text=_("Date and time when this log entry was created."),
+    )
+    exception_type = CharField(
+        _("exception_type"),
+        help_text=_("Class name or type of the exception, if any occurred."),
+        max_length=255,
+        null=True,
+    )
+    exception_message = TextField(
+        _("exception_message"),
+        help_text=_("Detailed message provided by the exception."),
+        null=True,
+    )
+    traceback = TextField(
+        _("traceback"),
+        help_text=_("Full traceback of the exception for debugging purposes."),
+        null=True,
+    )
+
+    # ip
+    server_ip = GenericIPAddressField(
+        _("server_ip"), help_text=_("IP address of the server handling the request.")
+    )
+    client_ip = GenericIPAddressField(
+        _("client_ip"), help_text=_("IP address of the client making the request.")
+    )
+
+    # object manager
+    objects = ServerLogQuerySet.as_manager()
+
+    def __str__(self) -> str:
+        return f"{self.timestamp} {self.method} {self.path} {self.status_code}"
+
+    class Meta:
+        verbose_name = _("Server Log")
+        verbose_name_plural = _("Server Logs")
+        ordering = ("-timestamp",)
