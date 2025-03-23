e32c7fdd8dec7a0e35e49c7c7412a6f26a2db493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 05:06:26 +0000

    Add package goodvirus/1.2.6/goodvirus-1.2.6-py3-none-any.whl

---
+++ packages/goodvirus/goodvirus-1.2.6-py3-none-any.whl/goodvirus/__about__.py
@@ -0,0 +1,6 @@
+__title__ = "goodvirus"
+__version__ = "1.2.6"
+__author__ = "Nico"
+__license__ = "MIT"
+__description__ = "An ethical, self-updating system watchdog daemon with full-system scanning."
+__email__ = "goodvirus.project@proton.me"
