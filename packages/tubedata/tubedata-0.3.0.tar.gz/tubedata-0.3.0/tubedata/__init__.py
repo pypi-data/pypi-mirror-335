0aa0a26c864064b44cbac8d54ada2b5acd836e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 14:05:44 +0000

    Add package tubedata/0.3.0/tubedata-0.3.0.tar.gz

---
+++ packages/tubedata/tubedata-0.3.0.tar.gz/tubedata-0.3.0/tubedata/__init__.py
@@ -0,0 +1,10 @@
+"""
+Tubedata
+
+Make YouTube queiries and get data from videos ready for Jupyter Notebooks.
+"""
+
+from tubedata.search import Search
+from tubedata.channel_info import ChannelInfo
+
+__all__ = ["Search", "ChannelInfo"]
