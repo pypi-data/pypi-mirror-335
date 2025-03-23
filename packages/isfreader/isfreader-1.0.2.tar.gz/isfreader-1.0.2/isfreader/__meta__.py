1c69c613460ebbf0777f4e61b995a5529b9aeef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 14:05:44 +0000

    Add package isfreader/1.0.2/isfreader-1.0.2.tar.gz

---
+++ packages/isfreader/isfreader-1.0.2.tar.gz/isfreader-1.0.2/isfreader/__meta__.py
@@ -0,0 +1,6 @@
+name = 'isfreader'
+version = '1.0.2'
+description = 'Read tek scope isf files as a numpy array.'
+url = 'https://github.com/justengel/isfreader'
+author = 'Justin Engel'
+author_email = 'jtengel08@gmail.com'
