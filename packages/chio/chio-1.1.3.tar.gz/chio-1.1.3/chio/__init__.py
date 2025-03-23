0285e98ad71cff3322da037ffd8d35b3daf1d178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 03:19:20 +0000

    Add package chio/1.1.3/chio-1.1.3.tar.gz

---
+++ packages/chio/chio-1.1.3.tar.gz/chio-1.1.3/chio/__init__.py
@@ -0,0 +1,11 @@
+
+__author__ = "Lekuru"
+__email__ = "contact@lekuru.xyz"
+__version__ = "1.1.3"
+__license__ = "MIT"
+
+from .clients import select_client, set_protocol_version
+from .chio import BanchoIO
+from .io import Stream
+from .constants import *
+from .types import *
