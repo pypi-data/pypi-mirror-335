c635f7f66e2d38df0def6667e44eaf9e56a37058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 03:18:53 +0000

    Add package diffpy.morph/0.1.4rc1/diffpy_morph-0.1.4rc1-py3-none-any.whl

---
+++ packages/diffpy.morph/diffpy_morph-0.1.4rc1-py3-none-any.whl/diffpy/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/diffpy/diffpy.pdfmorph/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+
+"""morph - tools for manipulating and comparing PDF data.
+
+"""
+
+
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+
+# End of file
