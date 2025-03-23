e7f31ab0c2892300430653a5eed2dfcbfd1908cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 03:19:09 +0000

    Add package PIconnect/0.11.4/piconnect-0.11.4.tar.gz

---
+++ packages/PIconnect/piconnect-0.11.4.tar.gz/piconnect-0.11.4/PIconnect/__init__.py
@@ -0,0 +1,21 @@
+"""PIconnect - Connector to the OSISoft PI and PI-AF databases."""
+
+# pragma pylint: disable=unused-import
+from PIconnect.AFSDK import AF, AF_SDK_VERSION
+from PIconnect.config import PIConfig
+from PIconnect.PI import PIServer
+from PIconnect.PIAF import PIAFDatabase
+
+# pragma pylint: enable=unused-import
+
+__version__ = "0.11.4"
+__sdk_version = tuple(int(x) for x in AF.PISystems().Version.split("."))
+
+__all__ = [
+    "AF",
+    "AF_SDK_VERSION",
+    "PIAFDatabase",
+    "PIConfig",
+    "PIServer",
+    "__sdk_version",
+]
---
+++ packages/PIconnect/piconnect-0.11.4.tar.gz/piconnect-0.11.4/tests/test_PIAF.py
@@ -0,0 +1,59 @@
+"""Test communication with the PI AF system."""
+
+from typing import cast
+
+import pytest
+
+import PIconnect as PI
+from PIconnect._typing import AF
+
+
+class TestAFDatabase:
+    """Test connecting to the AF database."""
+
+    def test_connection(self):
+        """Test creating a PI.PIAFDatabase object without arguments raises no exception."""
+        PI.PIAFDatabase()
+
+    def test_server_name(self):
+        """Test that the server reports the same name as which was connected to."""
+        AFserver = PI.AF.PISystems().DefaultPISystem.Name
+        database = PI.AF.PISystems().DefaultPISystem.Databases.DefaultDatabase.Name
+        server = PI.PIAFDatabase(AFserver, database)
+        assert server.server_name == AFserver
+        assert server.database_name == database
+        assert repr(server) == "PIAFDatabase(\\\\{s}\\{d})".format(s=AFserver, d=database)
+
+    def test_unknown_server_name(self):
+        """Test that the server reports a warning for an unknown server."""
+        AFserver_name = "__".join(list(PI.PIAFDatabase.servers) + ["UnkownServerName"])
+        with pytest.warns(UserWarning):
+            PI.PIAFDatabase(server=AFserver_name)
+
+    def test_unknown_database_name(self):
+        """Test that the server reports a warning for an unknown database."""
+        server = cast(AF.PISystem, PI.PIAFDatabase.default_server["server"])  # type: ignore
+        databases = [db.Name for db in server.Databases]
+        AFdatabase_name = "__".join(databases + ["UnkownDatabaseName"])
+        with pytest.warns(UserWarning):
+            PI.PIAFDatabase(database=AFdatabase_name)
+
+
+class TestDatabaseDescendants:
+    """Test retrieving child elements."""
+
+    def test_children(self):
+        """Test that calling children on the database returns a dict of child elements."""
+        with PI.PIAFDatabase() as db:
+            children = db.children
+        assert isinstance(children, dict)
+
+
+class TestDatabaseSearch:
+    """Test retrieving attributes."""
+
+    def test_search(self):
+        """Test that calling attributes on the database returns a list of attributes."""
+        with PI.PIAFDatabase() as db:
+            attributes = db.search([r"", r""])
+        assert isinstance(attributes, list)
