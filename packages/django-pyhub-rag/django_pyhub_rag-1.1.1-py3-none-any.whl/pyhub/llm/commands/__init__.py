e1c0b5e75ae907974f50b6485441480bbb409528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 15:06:43 +0000

    Add package django-pyhub-rag/1.1.1/django_pyhub_rag-1.1.1-py3-none-any.whl

---
+++ packages/django-pyhub-rag/django_pyhub_rag-1.1.1-py3-none-any.whl/pyhub/llm/commands/__init__.py
@@ -0,0 +1,17 @@
+import typer
+from rich.console import Console
+
+from pyhub import print_for_main
+
+from .ask import ask
+from .embed import app as embed_app
+
+app = typer.Typer()
+console = Console()
+
+app.add_typer(embed_app)
+
+app.command()(ask)
+
+
+app.callback(invoke_without_command=True)(print_for_main)
