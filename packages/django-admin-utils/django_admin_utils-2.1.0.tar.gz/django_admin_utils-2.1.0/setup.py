d546ea0199fae306ac5938bda1a8851b17997f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 03:19:17 +0000

    Add package django-admin-utils/2.1.0/django_admin_utils-2.1.0.tar.gz

---
+++ packages/django-admin-utils/django_admin_utils-2.1.0.tar.gz/django_admin_utils-2.1.0/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import re
+from pathlib import Path
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    with Path(__file__).parent.joinpath(*names).open(encoding=kwargs.get('encoding', 'utf8')) as fh:
+        return fh.read()
+
+
+setup(
+    name='django-admin-utils',
+    version='2.1.0',
+    license='BSD-2-Clause',
+    description='Utility code and patterns.',
+    long_description='{}\n{}'.format(
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
+        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst')),
+    ),
+    author='Ionel Cristian Mărieș',
+    author_email='contact@ionelmc.ro',
+    url='https://github.com/ionelmc/django-admin-utils',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[path.stem for path in Path('src').glob('*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        # uncomment if you test on these interpreters:
+        # "Programming Language :: Python :: Implementation :: IronPython",
+        # "Programming Language :: Python :: Implementation :: Jython",
+        # "Programming Language :: Python :: Implementation :: Stackless",
+        'Topic :: Utilities',
+    ],
+    project_urls={
+        'Changelog': 'https://github.com/ionelmc/django-admin-utils/blob/master/CHANGELOG.rst',
+        'Issue Tracker': 'https://github.com/ionelmc/django-admin-utils/issues',
+    },
+    keywords=[
+        # eg: "keyword1", "keyword2", "keyword3",
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        'django>=4.2',
+    ],
+    extras_require={
+        # eg:
+        #   "rst": ["docutils>=0.11"],
+        #   ":python_version=='3.8'": ["backports.zoneinfo"],
+    },
+)
---
+++ packages/django-admin-utils/django_admin_utils-2.1.0.tar.gz/django_admin_utils-2.1.0/tests/test_app/admin.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from admin_utils import make_admin_class
+from test_app import views
+
+make_admin_class(
+    'test_app',
+    'Test1',
+    [
+        path('', views.root, name='test_app_test1_changelist'),
+        path('level1/', views.level1, name='level-1'),
+        path('level1/level2/', views.level2, name='level-2'),
+    ],
+)
+
+make_admin_class(
+    'test_app',
+    'Test2',
+    [
+        path('', views.root, name='test_app_test2_changelist'),
+    ],
+)
