3ed9dc372fe6543cf37458272b08dac652f5f853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 03:18:52 +0000

    Add package syrius-sdk/0.2.75/syrius_sdk-0.2.75.tar.gz

---
+++ packages/syrius-sdk/syrius_sdk-0.2.75.tar.gz/syrius_sdk-0.2.75/syrius/commands/ReplicateMusicGenCommand.py
@@ -0,0 +1,15 @@
+from syrius.commands.LoopInputCommand import loopType
+from syrius.commands.abstract import AbstractCommand, Command
+
+
+class ReplicateMusicGenCommand(Command):
+    id: int = 89
+    prompt: str | AbstractCommand | loopType
+    model_version: str | AbstractCommand | loopType = "stereo-melody-large"
+    duration: int | AbstractCommand | loopType = 8
+    normalization_strategy: str | AbstractCommand | loopType = "loudness"
+    top_k: int | AbstractCommand | loopType =  250
+    top_p: float | AbstractCommand | loopType = 0
+    temperature: int | AbstractCommand | loopType = 1
+    output_format: str | AbstractCommand | loopType = "mp3"
+    api_key: str | AbstractCommand | loopType
