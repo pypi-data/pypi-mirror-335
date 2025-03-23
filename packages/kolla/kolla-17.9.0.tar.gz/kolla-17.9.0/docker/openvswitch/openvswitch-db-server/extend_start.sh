4d851a56affec65b4743a20a0c5267208f823fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 04:06:18 +0000

    Add package kolla/17.9.0/kolla-17.9.0.tar.gz

---
+++ packages/kolla/kolla-17.9.0.tar.gz/kolla-17.9.0/docker/openvswitch/openvswitch-db-server/extend_start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p "/run/openvswitch"
+if [[ ! -e "/var/lib/openvswitch/conf.db" ]]; then
+    ovsdb-tool create "/var/lib/openvswitch/conf.db"
+fi
+
+if [[ ! -r "/var/log/kolla/openvswitch/ovsdb-server.log" ]]; then
+    touch /var/log/kolla/openvswitch/ovsdb-server.log
+    chown openvswitch:openvswitch /var/log/kolla/openvswitch/ovsdb-server.log
+fi
