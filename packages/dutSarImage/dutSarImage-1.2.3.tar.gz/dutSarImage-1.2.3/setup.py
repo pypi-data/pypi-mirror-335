19eded2bbd3e4fae549f57790b21069f75d5949d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 03:19:20 +0000

    Add package dutSarImage/1.2.3/dutSarImage-1.2.3.tar.gz

---
+++ packages/dutSarImage/dutSarImage-1.2.3.tar.gz/dutSarImage-1.2.3/setup.py
@@ -0,0 +1,18 @@
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='dutSarImage',
+    packages=['dutSarImage'],  # this must be the same as the name above
+    version='1.2.3',
+    entry_points={  # 让你的项目能在命令行运行
+        'console_scripts': [
+            'dutSarImage=dutSarImage.main:__init__',  # 指定运行入口
+        ],
+    },
+    include_package_data=True,
+    description='Algorithms for blackbox falsification of convolutional neural networks',
+    author='Qyy',
+    keywords=['testing', 'safety', 'deep learning', 'computer vision'],  # arbitrary keywords
+    classifiers=[],
+)
