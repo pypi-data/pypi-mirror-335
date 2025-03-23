824b737defb58cd21b802f1bd0f9f9262cd136a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 14:05:52 +0000

    Add package splitter-praveenx23417897/0.1/splitter_praveenx23417897-0.1.tar.gz

---
+++ packages/splitter-praveenx23417897/splitter_praveenx23417897-0.1.tar.gz/splitter_praveenx23417897-0.1/README.md
@@ -0,0 +1,6 @@
+# Math Operations
+
+A simple Python library for basic arithmetic operations.
+
+## Installation
+pip install splitter_praveenx23417897
---
+++ packages/splitter-praveenx23417897/splitter_praveenx23417897-0.1.tar.gz/splitter_praveenx23417897-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="splitter-praveenx23417897",
+    version="0.1",
+    author="praveen",
+    author_email="x23417897@student.ncirl.ie",
+    description="A simple library for addition, subtraction, division",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/praveen/splitter-praveenx23417897",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
