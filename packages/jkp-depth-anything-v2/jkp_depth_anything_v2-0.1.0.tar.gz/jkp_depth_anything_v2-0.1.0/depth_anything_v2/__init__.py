520d96a34bcd6af9c1e545ace9279c8112501afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 14:05:53 +0000

    Add package jkp-depth-anything-v2/0.1.0/jkp_depth_anything_v2-0.1.0.tar.gz

---
+++ packages/jkp-depth-anything-v2/jkp_depth_anything_v2-0.1.0.tar.gz/jkp_depth_anything_v2-0.1.0/depth_anything_v2/__init__.py
@@ -0,0 +1,5 @@
+"""Depth Anything V2: Robust Monocular Depth Estimation"""
+
+__version__ = "0.1.0"
+
+from depth_anything_v2.dpt import DepthAnythingV2
---
+++ packages/jkp-depth-anything-v2/jkp_depth_anything_v2-0.1.0.tar.gz/jkp_depth_anything_v2-0.1.0/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+# This setup.py is maintained for backwards compatibility.
+# For new installations, pyproject.toml will be used by pip and other tools.
+
+setup()
