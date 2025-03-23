ffa5b3bb11c93128c5e08c6fe4df3ab53d0586a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 14:05:48 +0000

    Add package logninja-create-vm/0.1.1.dev0/logninja_create_vm-0.1.1.dev0.tar.gz

---
+++ packages/logninja-create-vm/logninja_create_vm-0.1.1.dev0.tar.gz/logninja_create_vm-0.1.1.dev0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="logninja-create-vm",
+    version="0.1.1.dev0",
+    description="Interactive CLI tool for provisioning virtual machines using virt-install (LOGGIE infrastructure)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="loggie.eth",
+    author_email="founder@loggie.ai",
+    url="https://github.com/loggie-eth/logninja-create-vm",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Emulators",
+        "Topic :: Utilities",
+    ],
+    entry_points={
+        "console_scripts": [
+            "logninja-create-vm = logninja_create_vm.cli:main"
+        ]
+    },
+    include_package_data=True,
+    python_requires='>=3.7',
+    license="MIT",
+)
