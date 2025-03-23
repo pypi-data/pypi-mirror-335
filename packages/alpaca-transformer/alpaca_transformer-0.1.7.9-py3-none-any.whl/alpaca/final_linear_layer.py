0f1c05126e12b3c2297cc86f0e148242fe0198c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 14:05:52 +0000

    Add package alpaca-transformer/0.1.7.9/alpaca_transformer-0.1.7.9-py3-none-any.whl

---
+++ packages/alpaca-transformer/alpaca_transformer-0.1.7.9-py3-none-any.whl/alpaca/final_linear_layer.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+
+class FinalLinear(nn.Module):
+    def __init__(self, d_model, vocab_size, device=None):
+        super().__init__()
+        self.device = device if device else ('cuda' if torch.cuda.is_available() else 'cpu')
+        self.linear = nn.Linear(d_model, vocab_size).to(self.device)
+        self.softmax = nn.Softmax(-1).to(self.device)
+
+    def forward(self, x):
+        x = x.to(self.device)
+        x = self.linear(x)
+        #x = self.softmax(x)
+        return x
