542fd9e04df728f991f2af301ce06efbf288cf37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 15:07:53 +0000

    Add package nonebot-plugin-emojilike-automonkey/0.0.7/nonebot_plugin_emojilike_automonkey-0.0.7-py3-none-any.whl

---
+++ packages/nonebot-plugin-emojilike-automonkey/nonebot_plugin_emojilike_automonkey-0.0.7-py3-none-any.whl/nonebot_plugin_emojilike_automonkey/config.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, root_validator
+
+class Config(BaseModel):
+    automonkey_users: str
+    #automonkey_command_priority: int = 10
+    automonkey_plugin_enabled: bool = True
+
+    @root_validator("automonkey_command_priority")
+    @classmethod
+    def check_priority(cls, v: int) -> int:
+        if v >= 1:
+            return v
+        raise ValueError("automonkey command priority must greater than 1")
