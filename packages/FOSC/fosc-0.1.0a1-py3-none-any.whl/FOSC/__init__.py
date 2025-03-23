2b2732fbc9d2e6098fd1c442484e54aa98cdfce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 04:06:04 +0000

    Add package FOSC/0.1.0a1/fosc-0.1.0a1-py3-none-any.whl

---
+++ packages/FOSC/fosc-0.1.0a1-py3-none-any.whl/FOSC/__init__.py
@@ -0,0 +1,7 @@
+from .cluster import Cluster
+from .dendrogram import DendrogramStructure
+from .fosc import FOSC
+from .plot import Plot
+from .utilClasses import TreeSet
+
+__all__ = ['Cluster', 'FOSC', 'Plot', 'DendrogramStructure', 'TreeSet']
