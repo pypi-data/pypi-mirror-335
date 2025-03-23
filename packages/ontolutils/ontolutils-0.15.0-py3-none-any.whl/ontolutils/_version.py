596dbffff7b5405ed27af5597a21cfd13db4cf84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 04:06:16 +0000

    Add package ontolutils/0.15.0/ontolutils-0.15.0-py3-none-any.whl

---
+++ packages/ontolutils/ontolutils-0.15.0-py3-none-any.whl/ontolutils/_version.py
@@ -0,0 +1,7 @@
+"""Version library for ontolutils package. Determines the version of the package."""
+try:
+    from importlib.metadata import version as _version
+except ImportError as e:
+    raise ImportError('Most likely you have python<3.8 installed. At least 3.8 is required.') from e
+
+__version__ = _version('ontolutils')
