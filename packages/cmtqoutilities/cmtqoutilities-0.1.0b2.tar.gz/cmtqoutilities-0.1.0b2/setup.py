9fba3f6346eae9985feb2f451f103ef26293667f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 16:06:18 +0000

    Add package cmtqoutilities/0.1.0b2/cmtqoutilities-0.1.0b2.tar.gz

---
+++ packages/cmtqoutilities/cmtqoutilities-0.1.0b2.tar.gz/cmtqoutilities-0.1.0b2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cmtqoutilities",    # Unique PyPI package name
+    version="0.1.0b2",
+    description="API for all cmtqo devices",    
+    author="Sebastian Huber",
+    author_email="huberse@phys.ethz.ch",
+    url="https://gitlab.phys.ethz.ch/code/experiment/mm-runexperiment",
+    packages=find_packages(include=["cmtqoutilities", "cmtqoutilities.utilities", "cmtqoutilities.utilities.*"]),
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy>=1.18.0",
+        "requests>=2.25.0",
+        "scipy>=1.5.0",
+        "pyserial>=3.5",
+        "zaber.serial>=0.9"
+    ]
+)
