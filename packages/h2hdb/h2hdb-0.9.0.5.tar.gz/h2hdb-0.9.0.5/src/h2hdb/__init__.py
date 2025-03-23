467326faa7266752b7c91e03bdb0ebd27a99588a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 15:06:47 +0000

    Add package h2hdb/0.9.0.5/h2hdb-0.9.0.5.tar.gz

---
+++ packages/h2hdb/h2hdb-0.9.0.5.tar.gz/h2hdb-0.9.0.5/src/h2hdb/__init__.py
@@ -0,0 +1,24 @@
+__all__ = [
+    "__version__",
+    "__version_info__",
+    "H2HDB",
+    "DatabaseConfig",
+    "LoggerConfig",
+    "H2HConfig",
+    "H2HDBConfig",
+    "load_config",
+    "HentaiDBLogger",
+    "setup_logger",
+]
+__author__ = "Kuan-Lun Wang"
+
+
+from .h2h_db import H2HDB
+from .config_loader import (
+    DatabaseConfig,
+    LoggerConfig,
+    H2HConfig,
+    H2HDBConfig,
+    load_config,
+)
+from .logger import HentaiDBLogger, setup_logger
