cae611d3326c95ee648f825903fa50124a56e095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 04:06:13 +0000

    Add package PyTemplateCode/0.1/pytemplatecode-0.1.tar.gz

---
+++ packages/PyTemplateCode/pytemplatecode-0.1.tar.gz/pytemplatecode-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PyTemplateCode",
+    version="0.01",
+    packages=find_packages(),
+    install_requires=[],
+    author="KusokMedi",
+    author_email="matvejs.stepanovs116@gmail.com",
+    description="Простая библиотека с полезными функциями..",
+    long_description_content_type="text/markdown",
+    url="https://github.com/KusokMedi/pytemplatecode",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
