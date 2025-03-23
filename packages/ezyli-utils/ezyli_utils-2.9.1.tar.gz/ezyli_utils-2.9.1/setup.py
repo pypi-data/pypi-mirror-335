24d84762219c408f0f3ab6abba7f4931c0f71a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 03:18:57 +0000

    Add package ezyli-utils/2.9.1/ezyli_utils-2.9.1.tar.gz

---
+++ packages/ezyli-utils/ezyli_utils-2.9.1.tar.gz/ezyli_utils-2.9.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ezyli_utils',
+    version='2.9.1',
+    packages=find_packages(),
+    install_requires=[
+        'pika==1.3.2',
+        'jsonschema==4.20.0'
+    ],
+)
