978180be6bd315949d28f202912016ae6d3e5e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 14:05:48 +0000

    Add package imagegamepy/0.0.2/imagegamepy-0.0.2.tar.gz

---
+++ packages/imagegamepy/imagegamepy-0.0.2.tar.gz/imagegamepy-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A package to create image games and light novels'
+LONG_DESCRIPTION = 'A package to create image games and light novels easily'
+
+# Setting up
+setup(
+    name="imagegamepy",
+    version=VERSION,
+    author="WinnyCode (Etchike Hugues Winny Lyonnais)",
+    author_email="<winnyblackstart@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['Pillow', 'pygame', 'numpy'],
+    keywords=['python', 'video game', 'Novel', 'game', 'image', 'image game'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
