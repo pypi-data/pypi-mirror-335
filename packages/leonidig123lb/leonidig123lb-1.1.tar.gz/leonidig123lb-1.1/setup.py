be35086b2b9e3ed77abfb04b9d21e7fa63e8f077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 05:06:20 +0000

    Add package leonidig123lb/1.1/leonidig123lb-1.1.tar.gz

---
+++ packages/leonidig123lb/leonidig123lb-1.1.tar.gz/leonidig123lb-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='leonidig123lb',
+    version='1.1',
+    author='leonidig',
+    author_email='leonid200955@gmail.com',
+    description='Test Library',
+    packages=find_packages(),
+    python_requires='>=3.12',
+)
