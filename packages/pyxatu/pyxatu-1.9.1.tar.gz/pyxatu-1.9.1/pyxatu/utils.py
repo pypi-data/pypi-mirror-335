46f32b0f8f3c6518e0ac5a6c14c6b0569656c149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 16:06:17 +0000

    Add package pyxatu/1.9.1/pyxatu-1.9.1.tar.gz

---
+++ packages/pyxatu/pyxatu-1.9.1.tar.gz/pyxatu-1.9.1/pyxatu/utils.py
@@ -0,0 +1,52 @@
+import time
+import logging
+from functools import wraps
+from typing import Any, Callable, TypeVar
+
+TABLES = {
+    "beacon_api_eth_v1_events_block": "default.beacon_api_eth_v1_events_block",
+    "canonical_beacon_proposer_duty": "default.canonical_beacon_proposer_duty",
+    "beacon_api_eth_v1_events_chain_reorg": "default.beacon_api_eth_v1_events_chain_reorg",
+    "canonical_beacon_block": "default.canonical_beacon_block",
+    "canonical_beacon_elaborated_attestation": "default.canonical_beacon_elaborated_attestation",
+    "beacon_api_eth_v1_events_attestation": "default.beacon_api_eth_v1_events_attestation",
+    "beacon_api_eth_v1_events_blob_sidecar": "default.beacon_api_eth_v1_events_blob_sidecar",
+    "canonical_beacon_blob_sidecar": "default.canonical_beacon_blob_sidecar",
+    "beacon_api_eth_v1_beacon_committee": "default.beacon_api_eth_v1_beacon_committee",
+    "canonical_beacon_block_withdrawal": "default.canonical_beacon_block_withdrawal",    
+    "beacon_api_eth_v2_beacon_block": "default.beacon_api_eth_v2_beacon_block",
+    "canonical_beacon_block_execution_transaction": "default.canonical_beacon_block_execution_transaction",
+    "mempool_transaction": "default.mempool_transaction",
+    "canonical_execution_transaction": "default.canonical_execution_transaction"
+}
+
+GENESIS_TIME_ETH_POS = 1606824023
+SECONDS_PER_SLOT = 12
+
+CONSTANTS = {
+    "TABLES": TABLES,
+    "GENESIS_TIME_ETH_POS": GENESIS_TIME_ETH_POS,
+    "SECONDS_PER_SLOT": SECONDS_PER_SLOT
+}
+
+F = TypeVar('F', bound=Callable[..., Any])
+
+def retry_on_failure(max_retries: int = 1, initial_wait: float = 1.0, backoff_factor: float = 2.0) -> Callable[[F], F]:
+    """Decorator to retry a function if an exception occurs."""
+    def decorator(func: F) -> F:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            attempt = 0
+            wait_time = initial_wait
+            while attempt < max_retries:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    logging.warning(f"Attempt {attempt + 1}/{max_retries} failed: {e}. Retrying in {wait_time} seconds...")
+                    time.sleep(wait_time)
+                    wait_time *= backoff_factor
+                    attempt += 1
+            logging.error(f"Max retries reached. Failed to complete operation.")
+            return None
+        return wrapper  # type: ignore
+    return decorator
---
+++ packages/pyxatu/pyxatu-1.9.1.tar.gz/pyxatu-1.9.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyxatu',
+    version='1.9.1',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'pyxatu': ['config.json'],
+    },
+    install_requires=[
+        'requests',
+        'pandas',
+        'tqdm',
+        'bs4',
+        'termcolor',
+        'fastparquet',
+        'click',
+        'tabulate'
+    ],
+    entry_points={
+        'console_scripts': [
+            'xatu=pyxatu.cli:cli',
+        ],
+    },
+    author='Toni Wahrstätter',
+    author_email='toni@ethereum.org',
+    description='A Python interface for the Xatu API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nerolation/pyxatu',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
