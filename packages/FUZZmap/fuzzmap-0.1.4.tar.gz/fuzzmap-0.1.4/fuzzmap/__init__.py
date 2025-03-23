470fd1a10d464d9f6947f789b0474fefb052e7b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 04:06:08 +0000

    Add package FUZZmap/0.1.4/fuzzmap-0.1.4.tar.gz

---
+++ packages/FUZZmap/fuzzmap-0.1.4.tar.gz/fuzzmap-0.1.4/fuzzmap/__init__.py
@@ -0,0 +1,14 @@
+from .core.controller.controller import Controller
+from .core.handler.param_recon import ParamReconHandler
+from .core.handler.common_payload import CommonPayloadHandler
+from .core.handler.advanced_payload import AdvancedPayloadHandler
+
+__version__ = "0.1.4"
+__author__ = "Offensive Tooling"
+
+__all__ = [
+    "Controller",
+    "ParamReconHandler",
+    "CommonPayloadHandler",
+    "AdvancedPayload"
+] 
