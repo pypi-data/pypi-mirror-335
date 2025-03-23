84cbb7209d8754ca918beb46629fb9ac9ddfa37c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 03:18:56 +0000

    Add package anysqldiag/0.0.2/anysqldiag-0.0.2-py3-none-any.whl

---
+++ packages/anysqldiag/anysqldiag-0.0.2-py3-none-any.whl/anysqldiag/anysqldiag.py
@@ -0,0 +1,30 @@
+import json
+import typing as t
+
+import sqlglot
+
+
+def anysqldiag(
+    sql: str,
+    error_level: t.Optional[str] = None,
+    error_message_context: int = 100,
+    max_errors: int = 3,
+    dialect: str = None,
+    json_indent: int = 2,
+):
+    error_level = (
+        getattr(sqlglot.ErrorLevel, error_level.upper()) if error_level else None
+    )
+    diagnostics = []
+    try:
+        sqlglot.parse(
+            sql,
+            error_level=error_level,
+            error_message_context=error_message_context,
+            max_errors=max_errors,
+            dialect=dialect,
+        )
+    except sqlglot.errors.ParseError as e:
+        diagnostics = e.errors
+    json_str = json.dumps(diagnostics, indent=json_indent)
+    return json_str
