f74ae60ed6bc3fa63dd0d1b5c3022960266549e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 15:06:50 +0000

    Add package dmconvert/0.0.1/dmconvert-0.0.1-py3-none-any.whl

---
+++ packages/dmconvert/dmconvert-0.0.1-py3-none-any.whl/dmconvert/cli.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2025 DanmakuConvert
+
+import argparse
+import sys
+import os
+import logging
+from dmconvert.convert import convert_xml_to_ass
+
+
+def cli():
+    parser = argparse.ArgumentParser(
+        description="The Python toolkit package and cli designed for convert danmaku to ass."
+    )
+    parser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version="dmconvert 0.0.1",
+        help="Print version information",
+    )
+    parser.add_argument(
+        "-f",
+        "--fontsize",
+        default=38,
+        type=int,
+        help="The font size of the danmaku, default is 38",
+    )
+    parser.add_argument(
+        "-x",
+        "--resolutionx",
+        default=1920,
+        type=int,
+        help="The resolution x of the danmaku, default is 1920",
+    )
+    parser.add_argument(
+        "-y",
+        "--resolutiony",
+        default=1080,
+        type=int,
+        help="The resolution y of the danmaku, default is 1080",
+    )
+    parser.add_argument(
+        "-i", "--xml", required=True, type=str, help="The input xml file"
+    )
+    parser.add_argument("-o", "--ass", default="", type=str, help="The output ass file")
+
+    args = parser.parse_args()
+
+    if os.path.splitext(args.xml)[1] == ".xml":
+        xml_file = args.xml
+        ass_file = args.ass if args.ass else os.path.splitext(xml_file)[0] + ".ass"
+        convert_xml_to_ass(
+            args.fontsize,
+            args.resolutionx,
+            args.resolutiony,
+            xml_file,
+            ass_file,
+        )
+    else:
+        print("Please assign the correct input xml file.")
+
+
+if __name__ == "__main__":
+    cli()
