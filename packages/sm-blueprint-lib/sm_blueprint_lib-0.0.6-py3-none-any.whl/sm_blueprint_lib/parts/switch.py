106bcecc779cabdcd98415ef82b344d4b8321000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 05:06:09 +0000

    Add package sm-blueprint-lib/0.0.6/sm_blueprint_lib-0.0.6-py3-none-any.whl

---
+++ packages/sm-blueprint-lib/sm_blueprint_lib-0.0.6-py3-none-any.whl/sm_blueprint_lib/parts/switch.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass, field
+
+from ..bases.parts.baseinteractablepart import BaseInteractablePart
+from ..constants import SHAPEID
+
+
+@dataclass
+class Switch(BaseInteractablePart):
+    raise NotImplemented
