73af9a560db4c31c33bfde72fd8e59a663b1c824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 05:06:40 +0000

    Add package schubmult/2.0.1/schubmult-2.0.1-py3-none-any.whl

---
+++ packages/schubmult/schubmult-2.0.1-py3-none-any.whl/schubmult/schubmult_double/__init__.py
@@ -0,0 +1,18 @@
+from ._funcs import (
+    compute_positive_rep,
+    schubmult,
+    single_variable,
+    mult_poly,
+    posify,
+    div_diff
+)
+
+
+__all__ = [
+    "compute_positive_rep",
+    "schubmult",
+    "single_variable",
+    "mult_poly",
+    "posify",
+    "div_diff"
+]
