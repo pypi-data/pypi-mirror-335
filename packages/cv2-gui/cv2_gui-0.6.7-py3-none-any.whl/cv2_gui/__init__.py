97db16e89aab9e367962b6d113a7a9b9ea23ce88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 15:07:44 +0000

    Add package cv2-gui/0.6.7/cv2_gui-0.6.7-py3-none-any.whl

---
+++ packages/cv2-gui/cv2_gui-0.6.7-py3-none-any.whl/cv2_gui/__init__.py
@@ -0,0 +1,14 @@
+# Import required modules and classes
+from .cv2_gui import create_button_manager, create_toggle_button, create_cycle_button, create_eyedropper,create_slider, create_dpad
+
+# Expose CreateButtonManager as the primary public interface
+
+
+# Automatically import OpenCV and NumPy for library-wide use
+import cv2
+import numpy as np
+import time
+import math
+from typing import Callable, Optional
+
+
