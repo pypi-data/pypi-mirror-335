6e5a0d0f2ecf34bf94b7b9fafac7c0c500916b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 03:18:51 +0000

    Add package mapping-kit/0.1.0a4/mapping_kit-0.1.0a4-py3-none-any.whl

---
+++ packages/mapping-kit/mapping_kit-0.1.0a4-py3-none-any.whl/mapping_kit/__init__.py
@@ -0,0 +1,15 @@
+"""
+Implements useful new maps that make development that slightly easier.
+"""
+
+from .version import __version__
+from .attribute_dict import AttributeDict
+from .group_dict import GroupDict
+from .super_dict import SuperDict
+
+__all__ = [
+    "__version__",
+    "AttributeDict",
+    "GroupDict",
+    "SuperDict",
+]
