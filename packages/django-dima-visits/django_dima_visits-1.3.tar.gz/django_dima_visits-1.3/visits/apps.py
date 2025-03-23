42c5c75b2e9cf6caeff492e7912e36088fee609a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 05:06:29 +0000

    Add package django-dima-visits/1.3/django_dima_visits-1.3.tar.gz

---
+++ packages/django-dima-visits/django_dima_visits-1.3.tar.gz/django_dima_visits-1.3/visits/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class VisitsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'visits'
+    verbose_name = _("Django Dima Visits")  
+    
