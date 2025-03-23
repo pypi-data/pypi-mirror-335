503fc12237fc7da84dfae5fbbaa20b499dd9c3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 04:06:09 +0000

    Add package vital-ai-chat/0.1.20/vital_ai_chat-0.1.20-py3-none-any.whl

---
+++ packages/vital-ai-chat/vital_ai_chat-0.1.20-py3-none-any.whl/ai_chat_domain/model/HaleyChatInteraction.pyi
@@ -0,0 +1,23 @@
+
+import datetime
+from vital_ai_vitalsigns.model.VITAL_Node import VITAL_Node
+
+
+class HaleyChatInteraction(VITAL_Node):
+        agentInstallURI: str
+        agentVariantURI: str
+        chatUpdateTimestamp: datetime
+        haleyChatInteractionCategoryURI: str
+        haleyChatInteractionModelProviderURI: str
+        haleyChatInteractionModelTypeURI: str
+        haleyChatInteractionScopeChannelURI: str
+        haleyChatInteractionScopeLoginURI: str
+        haleyChatInteractionScopeURI: str
+        haleyChatInteractionStatusURI: str
+        haleyChatInteractionTypeURI: str
+        haleyChatQuotaCreditUsed: int
+        haleyChatQuotaStatusURI: str
+        initiatingMessageURI: str
+        currentAssistant: bool
+        titleManuallySet: bool
+
