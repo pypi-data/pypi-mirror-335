b887b4e88cedf1b6f15f3540bcd2c9295d4f9670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 04:06:07 +0000

    Add package twilio-manager-mcp/0.1.5/twilio_manager_mcp-0.1.5-py3-none-any.whl

---
+++ packages/twilio-manager-mcp/twilio_manager_mcp-0.1.5-py3-none-any.whl/client.py
@@ -0,0 +1,64 @@
+import asyncio
+import os
+
+from mcp import ClientSession
+from mcp.client.stdio import StdioServerParameters, stdio_client
+
+
+class MCPClient:
+    def __init__(self, command: str, args: list[str], env: dict[str, str]):
+        self.server_params = StdioServerParameters(
+            command="uv",
+            args=["run", "mcp", "run", "./twilio_manager_mcp.py"],
+            env=env,
+        )
+        self.session = None
+
+    async def initialize(self):
+        streams_context = stdio_client(self.server_params)
+        streams = await streams_context.__aenter__()
+        self._session_context = ClientSession(*streams)
+        self.session = await self._session_context.__aenter__()
+        await self.session.initialize()
+        print("Session with mcp server initialized")
+
+    async def __aenter__(self):
+        await self.initialize()
+        if self.session is None:
+            raise ValueError("Session not initialized")
+        return self.session
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if self.session:
+            await self._session_context.__aexit__(exc_type, exc_val, exc_tb)
+            self.session = None
+
+
+async def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--command", type=str, default="uv")
+    parser.add_argument(
+        "--args", nargs="+", default=["run", "mcp", "run", "./twilio_manager_mcp.py"]
+    )
+    args = parser.parse_args()
+    env = {
+        "TWILIO_ACCOUNT_SID": os.getenv("TWILIO_ACCOUNT_SID"),
+        "TWILIO_AUTH_TOKEN": os.getenv("TWILIO_AUTH_TOKEN"),
+    }
+    if env["TWILIO_ACCOUNT_SID"] is None or env["TWILIO_AUTH_TOKEN"] is None:
+        raise ValueError("TWILIO_ACCOUNT_SID and TWILIO_AUTH_TOKEN must be set")
+
+    async with MCPClient(args.command, args.args, env=env) as session:  # type: ignore
+        print(await session.send_ping())
+        tools = (await session.list_tools()).tools
+        print([tool.name for tool in tools])
+
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    asyncio.run(main())
