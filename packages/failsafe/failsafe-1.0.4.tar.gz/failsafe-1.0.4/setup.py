01b837dfed103d896927bc49d81911e971ab5c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 04:06:20 +0000

    Add package failsafe/1.0.4/failsafe-1.0.4.tar.gz

---
+++ packages/failsafe/failsafe-1.0.4.tar.gz/failsafe-1.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="failsafe",
+    version="1.0.4",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    description="A lightweight, simpler, more pythonic testing framework",
+    author="TheOmniOnic",
+    license="MIT",
+    packages=find_packages(where="."), 
+    include_package_data=True, 
+    url="https://github.com/TheOmniOnic/failsafe",
+    classifiers=[  
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
