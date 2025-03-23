1d083be4d228231155ec9dd1bfee43b961f2716c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 03:18:56 +0000

    Add package IRSTD/0.0.0.0/irstd-0.0.0.0.tar.gz

---
+++ packages/IRSTD/irstd-0.0.0.0.tar.gz/irstd-0.0.0.0/IRTSD/callbacks/__init__.py
@@ -0,0 +1,4 @@
+# from IRSTD.callbacks.trainer import get_IRSTDtrain_config, IRSTDTrainer
+from IRTSD.callbacks.trainer import get_IRSTDtrain_config, IRSTDTrainer
+
+__all__ = ['get_IRSTDtrain_config','IRSTDTrainer']
---
+++ packages/IRSTD/irstd-0.0.0.0.tar.gz/irstd-0.0.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="IRSTD",
+    version="0.0.0.0",
+    python_requires='>=3.7',
+    author="Aohua Li",
+    author_email="liah24@mails.jlu.edu.cn",
+    description="It is a DeepLearning package to foster developed by Aohua Li",
+    url="https://github.com/PepperCS/IRSTD",
+    packages=setuptools.find_packages(),
+    zip_safe=True,
+    # install_requires=requirements,
+)
