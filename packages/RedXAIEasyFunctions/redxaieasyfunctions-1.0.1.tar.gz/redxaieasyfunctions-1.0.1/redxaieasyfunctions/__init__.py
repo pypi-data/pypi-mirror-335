946941f067903cf5ac29dc56f40922fe46be3591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 15:06:50 +0000

    Add package RedXAIEasyFunctions/1.0.1/redxaieasyfunctions-1.0.1.tar.gz

---
+++ packages/RedXAIEasyFunctions/redxaieasyfunctions-1.0.1.tar.gz/redxaieasyfunctions-1.0.1/redxaieasyfunctions/__init__.py
@@ -0,0 +1,21 @@
+# easyfunctions/__init__.py
+
+from .core import (
+    LocalizePath,
+    LocalSearch,
+    LoadJson,
+    JSEasySearch,
+    JSEasyChange,
+    JSEasyAdd,
+    JSEasyRemove,
+    JSEasySave,
+    JSEasyJsonCreate,
+    EasyFileCreate,
+    EasyFolderCreate,
+    EasyZip,
+    EasyProjectTree,
+    EasyRandom,
+    EasyRandomPlus,
+    EasyJsTable,
+    EasyJSKey
+)
