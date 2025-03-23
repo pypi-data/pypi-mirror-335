d37f70a319ad53045145f7df8c2780de64274220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 15:07:35 +0000

    Add package extract-list/0.2.3/extract_list-0.2.3.tar.gz

---
+++ packages/extract-list/extract_list-0.2.3.tar.gz/extract_list-0.2.3/setup.py
@@ -0,0 +1,25 @@
+#! /usr/local/bin/python3
+"""Setup file specifying build of apo_tools.whl with APO tools library."""
+
+from setuptools import setup
+
+setup(
+  name='extract-list',
+  version='0.2.3',
+  description='Extract a list from JSON or XML, save to excel, csv, etc.',
+  author='Tom Björkholm',
+  author_email='klausuler_linnet0q@icloud.com',
+  python_requires='>=3.13.2',
+  packages=['extract_list'],
+  package_dir={'extract_list': 'src/extract_list'},
+  package_data={'extract_list': ['src/py.typed']},
+  install_requires=[
+    'excel-list-transform >= 0.7.6',
+    'xmltodict >= 0.14.2',
+    'types-xmltodict >= 0.14.0.20241009',
+    'pip >= 25.0.1',
+    'setuptools >= 77.0.3',
+    'build >= 1.2.2.post1',
+    'wheel>=0.45.1'
+  ]
+)
