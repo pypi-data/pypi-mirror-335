8f6e9cbfbca32463efc135c3ea36a3e30a220345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 15:07:21 +0000

    Add package pylogr/0.1.0/pylogr-0.1.0.tar.gz

---
+++ packages/pylogr/pylogr-0.1.0.tar.gz/pylogr-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pylogr",
+    version="0.1.0",
+    author="Ditzel",
+    description="A beautiful and colorful logging utility for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ralle/pylogr",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.6",
+) 
