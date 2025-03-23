735807a9431252fe4c7e7319e020c4d0308b0750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 05:06:28 +0000

    Add package readpdfutils/0.2.3/readpdfutils-0.2.3.tar.gz

---
+++ packages/readpdfutils/readpdfutils-0.2.3.tar.gz/readpdfutils-0.2.3/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup, Extension
+import numpy 
+import sys
+import os
+from os import getenv
+
+PLATFORM = sys.platform
+
+if getenv('LIBLINK'):
+    PLATFORM = 'android'
+
+
+library_dirs = [] if not os.environ.get('ARCH') else ['lib/' + os.environ.get('ARCH')]
+
+extension = Extension('readpdfutils', ['utils.c', 'pdf.c'], libraries=["pdfium"], library_dirs=library_dirs, include_dirs=["include", numpy.get_include()])
+setup(name="utils",
+      version="0.2.3",
+      ext_modules = [
+        extension
+    ]
+)
