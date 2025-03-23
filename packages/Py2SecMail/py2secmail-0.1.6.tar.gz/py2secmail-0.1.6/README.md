cd221baeec186ab3fb9211c05713031742e0aa0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 04:06:34 +0000

    Add package Py2SecMail/0.1.6/py2secmail-0.1.6.tar.gz

---
+++ packages/Py2SecMail/py2secmail-0.1.6.tar.gz/py2secmail-0.1.6/README.md
@@ -0,0 +1,10 @@
+# Py2SecMail
+
+`Py2SecMail` est une bibliothèque Python permettant d'interagir facilement avec l'API de 2SecMail, un service permettant de créer et gérer des emails temporaires. Ce projet est idéal pour les tests ou pour protéger votre adresse email personnelle en utilisant des adresses jetables.
+
+## Installation
+
+Vous pouvez installer `Py2SecMail` via `pip` :
+
+```bash
+pip install Py2SecMail
---
+++ packages/Py2SecMail/py2secmail-0.1.6.tar.gz/py2secmail-0.1.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Py2SecMail',  # Nom du package
+    version='0.1.6',  # Version de votre package
+    description='Librairie pour interagir avec l\'API 2SecMail pour des emails temporaires',
+    long_description=open('README.md').read(),  # Lire le fichier README.md pour une description plus concise
+    long_description_content_type='text/markdown',
+    author='ItsukiPth',  # Remplacez par votre nom
+    author_email='itsukiche@gmail.com',  # Remplacez par votre email
+    url='https://github.com/alvorks/Py2SecMail',  # Remplacez par l'URL de votre projet
+    packages=find_packages(),  # Détecte automatiquement les packages dans votre projet
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[  # Liste des dépendances nécessaires
+        'requests',  # Pour effectuer les requêtes HTTP
+    ],
+    python_requires='>=3.6',  # Version de Python requise
+)
