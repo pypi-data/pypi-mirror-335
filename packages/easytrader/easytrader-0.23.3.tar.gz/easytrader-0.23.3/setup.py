fd646756c46af875f6e821612c37a28e042aaf10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 04:06:05 +0000

    Add package easytrader/0.23.3/easytrader-0.23.3.tar.gz

---
+++ packages/easytrader/easytrader-0.23.3.tar.gz/easytrader-0.23.3/setup.py
@@ -0,0 +1,41 @@
+# coding:utf8
+from setuptools import setup
+
+
+setup(
+    name="easytrader",
+    version="0.23.3",
+    description="A utility for China Stock Trade",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="shidenggui",
+    author_email="longlyshidenggui@gmail.com",
+    license="BSD",
+    url="https://github.com/shidenggui/easytrader",
+    keywords="China stock trade",
+    install_requires=[
+        "requests",
+        "six",
+        "easyutils",
+        "flask",
+        "pywinauto==0.6.6",
+        "pillow",
+        "pandas",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: BSD License",
+    ],
+    packages=["easytrader", "easytrader.config", "easytrader.utils"],
+    package_data={
+        "": ["*.jar", "*.json"],
+        "config": ["config/*.json"],
+        "thirdlibrary": ["thirdlibrary/*.jar"],
+    },
+)
