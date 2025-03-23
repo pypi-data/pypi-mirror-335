426fd623b54f90961a894979e8fa941670ed97ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-23 03:18:52 +0000

    Add package sevml/0.3.5/sevml-0.3.5.tar.gz

---
+++ packages/sevml/sevml-0.3.5.tar.gz/sevml-0.3.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    descrition = f.read()
+
+setup(
+    name="sevml",
+    version="0.3.5",
+    description="Machine learning tools for biological analysis",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy<2",
+        "matplotlib",
+        "shap",
+        "xgboost",
+        "scikit-learn"
+    ],
+    entry_points={
+        "console_scripts": [
+            "sevml = sevml.main:main"
+        ]
+    },
+    long_description=descrition,
+    long_description_content_type="text/markdown",
+)
