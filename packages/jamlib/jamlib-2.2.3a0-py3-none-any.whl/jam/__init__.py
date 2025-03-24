623c4d0932b44a5ae296f8e323280271997740d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:19:38 +0000

    Add package jamlib/2.2.3a0/jamlib-2.2.3a0-py3-none-any.whl

---
+++ packages/jamlib/jamlib-2.2.3a0-py3-none-any.whl/jam/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+JAM - Universal Authorization Library
+
+Source code: https://github.com/lyaguxafrog/jam
+Documentation: https://jam.makridenko.ru
+"""
+
+from .__core_staffs__ import Config, Jam
+
+
+__version__ = "2.2.1-alpha"
+__all__ = ["Config", "Jam"]
