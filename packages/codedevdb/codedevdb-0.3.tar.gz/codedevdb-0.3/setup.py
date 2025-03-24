736904283ba83c4577369d7a67cd9bc438d1cdbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:20:03 +0000

    Add package codedevdb/0.3/codedevdb-0.3.tar.gz

---
+++ packages/codedevdb/codedevdb-0.3.tar.gz/codedevdb-0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="codedevdb",
+    version="0.3",
+    author="CodeDev Company",
+    author_email="codedev.eg@hotmail.com",
+    description="A library dedicated to easy and seamless handling of sqlite3 / db files",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://t.me/midoghanam",
+    project_urls={
+        'Channel': 'https://t.me/mido_ghanam'
+    },
+    packages=find_packages(),
+    install_requires=["sqlite3"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
