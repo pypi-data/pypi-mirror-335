232f3777ff0c7b579d8d45e0d70ef45fd5b12377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:20:26 +0000

    Add package async-dag/0.1.2/async_dag-0.1.2.tar.gz

---
+++ packages/async-dag/async_dag-0.1.2.tar.gz/async_dag-0.1.2/tests/test_task_manager.py
@@ -0,0 +1,210 @@
+from collections.abc import Awaitable, Callable
+from dataclasses import dataclass
+
+import pytest
+
+from async_dag.task_manager import TaskManager, build_dag
+
+
+@dataclass
+class Input:
+    starting_number: int
+
+
+async def imm() -> int:
+    return 1
+
+
+async def from_args(args: Input) -> int:
+    return args.starting_number
+
+
+async def inc(n: int) -> int:
+    return n + 1
+
+
+async def to_str(n: int) -> str:
+    return str(n)
+
+
+async def to_int(n: str, _: Input) -> int:
+    return int(n)
+
+
+async def inc_str(n: str) -> int:
+    return int(n) + 1
+
+
+async def inc_max(a: int, b: int) -> int:
+    return max(a, b) + 1
+
+
+async def test_task_manager_sanity() -> None:
+    tm = TaskManager[Input]()
+    starting_node = tm.add_node(from_args)
+
+    inc_1 = tm.add_node(inc, starting_node)
+
+    str_node = tm.add_node(to_str, starting_node)
+    str_to_int_node = tm.add_node(to_int, str_node)
+    int_node = tm.add_node(inc_str, str_node)
+    inc_2 = tm.add_node(inc, int_node)
+
+    end_1 = tm.add_node(inc_max, inc_2, inc_1)
+
+    end_2 = tm.add_node(inc_max, inc_1, starting_node)
+
+    end_3 = tm.add_node(inc_max, str_to_int_node, starting_node)
+
+    tm.sort()
+
+    result_1 = await tm.invoke(Input(0))
+
+    result_2 = await tm.invoke(Input(999))
+
+    assert end_1.extract_result(result_1) == 3
+    assert end_2.extract_result(result_1) == 2
+    assert end_3.extract_result(result_1) == 1
+
+    assert end_1.extract_result(result_2) == 1002
+    assert end_2.extract_result(result_2) == 1001
+    assert end_3.extract_result(result_2) == 1000
+
+
+async def test_build_dag_sanity() -> None:
+    with build_dag(Input) as tm:
+        starting_node = tm.add_node(from_args)
+
+        inc_1 = tm.add_node(inc, starting_node)
+
+        str_node = tm.add_node(to_str, starting_node)
+        str_to_int_node = tm.add_node(to_int, str_node)
+        int_node = tm.add_node(inc_str, str_node)
+        inc_2 = tm.add_node(inc, int_node)
+
+        end_1 = tm.add_node(inc_max, inc_2, inc_1)
+
+        end_2 = tm.add_node(inc_max, inc_1, starting_node)
+
+        end_3 = tm.add_node(inc_max, str_to_int_node, starting_node)
+
+    result_1 = await tm.invoke(Input(0))
+
+    result_2 = await tm.invoke(Input(999))
+
+    assert end_1.extract_result(result_1) == 3
+    assert end_2.extract_result(result_1) == 2
+    assert end_3.extract_result(result_1) == 1
+
+    assert end_1.extract_result(result_2) == 1002
+    assert end_2.extract_result(result_2) == 1001
+    assert end_3.extract_result(result_2) == 1000
+
+
+async def test_multiple_invocation() -> None:
+    with build_dag(Input) as tm:
+        from_args_node = tm.add_node(from_args)
+
+    expected_1 = 0
+    result_1 = await tm.invoke(Input(expected_1))
+
+    expected_2 = 999
+    result_2 = await tm.invoke(Input(expected_2))
+
+    assert from_args_node.extract_result(result_1) == expected_1
+    assert from_args_node.extract_result(result_2) == expected_2
+
+
+async def test_dag_with_single_node() -> None:
+    with build_dag() as tm:
+        starting_node = tm.add_node(imm)
+    result = await tm.invoke(None)
+
+    assert starting_node.extract_result(result) == await imm()
+
+
+async def test_dag_with_zero_nodes() -> None:
+    with build_dag() as tm:
+        pass
+    result = await tm.invoke(None)
+    assert result is not None
+
+
+async def test_sorting_twice_should_error() -> None:
+    with pytest.raises(ValueError), build_dag() as tm:
+        tm.sort()
+
+    with pytest.raises(ValueError):
+        tm = TaskManager[None]()
+        tm.sort()
+        tm.sort()
+
+
+async def test_adding_nodes_after_sort_should_error() -> None:
+    with pytest.raises(ValueError):
+        with build_dag() as tm:
+            pass
+        tm.add_node(imm)
+
+    with pytest.raises(ValueError):
+        tm = TaskManager[None]()
+        tm.sort()
+        tm.add_node(imm)
+
+
+async def test_invoke_before_sort_should_error() -> None:
+    with pytest.raises(ValueError), build_dag() as tm:
+        await tm.invoke(None)
+
+    with pytest.raises(ValueError):
+        tm = TaskManager[None]()
+        await tm.invoke(None)
+
+
+async def test_calling_order_of_dag() -> None:
+    def define_step(expected_state: int) -> Callable[[int], Awaitable[int]]:
+        async def inner(current_state: int) -> int:
+            assert expected_state == current_state
+            return current_state + 1
+
+        return inner
+
+    async def assert_equal(a: int, b: int) -> None:
+        assert a == b
+
+    with build_dag(int) as tm:
+        starting_node = tm.add_node(define_step(0))
+
+        node_1_1 = tm.add_node(define_step(1), starting_node)
+        node_1_2 = tm.add_node(define_step(1), starting_node)
+
+        node_2_1 = tm.add_node(define_step(2), node_1_1)
+        node_2_2 = tm.add_node(define_step(2), node_1_2)
+        tm.add_node(assert_equal, node_2_1, node_2_2)
+
+    await tm.invoke(0)
+
+
+async def test_add_node_with_mixed_managers_errors() -> None:
+    with pytest.raises(ValueError), build_dag() as tm_1, build_dag() as tm_2:
+        starting_node = tm_1.add_node(imm)
+        tm_2.add_node(inc, starting_node)
+
+
+async def test_sort_with_non_dag_graph_errors() -> None:
+    with pytest.raises(ValueError), build_dag() as tm:
+        node_1 = tm.add_node(imm)
+        node_2 = tm.add_node(imm)
+
+        node_1._dependencies_ids = [node_2._id]
+        node_2._dependencies_ids = [node_1._id]
+
+
+async def test_immediate_node_should_return_its_value() -> None:
+    expected = "Hello World"
+    with build_dag() as tm:
+        imm_node = tm.add_immediate_node(expected)
+
+    result = await tm.invoke(None)
+
+    assert imm_node.extract_result(result) == expected
