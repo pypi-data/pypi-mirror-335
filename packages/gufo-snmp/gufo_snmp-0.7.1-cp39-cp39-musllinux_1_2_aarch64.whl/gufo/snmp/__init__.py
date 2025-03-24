62600f39032cb799b855714321c64ad417cd33e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-24 03:20:07 +0000

    Add package gufo-snmp/0.7.1/gufo_snmp-0.7.1-cp39-cp39-musllinux_1_2_aarch64.whl

---
+++ packages/gufo-snmp/gufo_snmp-0.7.1-cp39-cp39-musllinux_1_2_aarch64.whl/gufo/snmp/__init__.py
@@ -0,0 +1,43 @@
+# ---------------------------------------------------------------------
+# Gufo SNMP: Python SNMP Library
+# ---------------------------------------------------------------------
+# Copyright (C) 2023-25, Gufo Labs
+# See LICENSE.md for details
+# ---------------------------------------------------------------------
+
+"""Gufo SNMP: The accelerated Python SNMP client library.
+
+Attributes:
+    __version__: Current version
+"""
+
+# Gufo Labs modules
+from ._fast import (
+    NoSuchInstance,
+    SnmpAuthError,
+    SnmpDecodeError,
+    SnmpEncodeError,
+    SnmpError,
+)
+from .async_client import SnmpSession
+from .typing import ValueType
+from .user import Aes128Key, DesKey, Md5Key, Sha1Key, User
+from .version import SnmpVersion
+
+__version__: str = "0.7.1"
+__all__ = [
+    "__version__",
+    "Aes128Key",
+    "DesKey",
+    "Md5Key",
+    "Sha1Key",
+    "SnmpAuthError",
+    "SnmpSession",
+    "SnmpVersion",
+    "SnmpError",
+    "SnmpEncodeError",
+    "SnmpDecodeError",
+    "NoSuchInstance",
+    "User",
+    "ValueType",
+]
